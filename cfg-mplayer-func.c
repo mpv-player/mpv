7f68f9ce7d970f0aee01f9d04ba5cae821c76bb0
arpi_esp <arpi_esp@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-03-19 03:53:55 +0000

    no more used
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@156 b3059339-0415-0410-9bf9-f77b7e298cf2

--- cfg-mplayer-func.c
+++
@@ -1,9 +0,0 @@
-#include "cfgparser.h"
-#include "version.h"
-#include "help_mp.h"
-
-int cfg_func_help(struct config *conf)
-{
-	printf("%s", help_text);
-	exit(1);
-}
--- cfg-mplayer-func.h
+++
@@ -1,1 +0,0 @@
-int cfg_func_help(struct config *conf);
