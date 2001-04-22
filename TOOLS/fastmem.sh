cf33c27bb1f20726cdc8c2c351212af44f5a49b9
arpi_esp <arpi_esp@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-04-22 03:40:16 +0000

    run fastmem tests
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@572 b3059339-0415-0410-9bf9-f77b7e298cf2

---
+++ TOOLS/fastmem.sh
@@ -0,0 +1,19 @@
+
+sync
+sleep 2
+./fastmem-k6
+sleep 2
+./fastmem-k7
+sleep 2
+./fastmem-mmx
+sleep 2
+./fastmem-sse
+sleep 2
+./fastmem2-k6
+sleep 2
+./fastmem2-k7
+sleep 2
+./fastmem2-mmx
+sleep 2
+./fastmem2-sse
+sleep 2
