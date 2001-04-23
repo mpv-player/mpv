ab397f89f3f76a2eebd363c1d5af341acb65e164
szabii <szabii@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-04-23 23:04:02 +0000

    warnings killed
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@603 b3059339-0415-0410-9bf9-f77b7e298cf2

--- aviprint.c
+++ aviprint.c
@@ -17,10 +17,10 @@
 
 void print_avih(MainAVIHeader *h){
   printf("======= AVI Header =======\n");
-  printf("us/frame: %d  (fps=%5.3f)\n",h->dwMicroSecPerFrame,1000000.0f/(float)h->dwMicroSecPerFrame);
-  printf("max bytes/sec: %d\n",h->dwMaxBytesPerSec);
-  printf("padding: %d\n",h->dwPaddingGranularity);
-  printf("flags: (%d)%s%s%s%s%s%s\n",h->dwFlags,
+  printf("us/frame: %ld  (fps=%5.3f)\n",h->dwMicroSecPerFrame,1000000.0f/(float)h->dwMicroSecPerFrame);
+  printf("max bytes/sec: %ld\n",h->dwMaxBytesPerSec);
+  printf("padding: %ld\n",h->dwPaddingGranularity);
+  printf("flags: (%ld)%s%s%s%s%s%s\n",h->dwFlags,
     (h->dwFlags&AVIF_HASINDEX)?" HAS_INDEX":"",
     (h->dwFlags&AVIF_MUSTUSEINDEX)?" MUST_USE_INDEX":"",
     (h->dwFlags&AVIF_ISINTERLEAVED)?" IS_INTERLEAVED":"",
@@ -28,23 +28,23 @@ void print_avih(MainAVIHeader *h){
     (h->dwFlags&AVIF_WASCAPTUREFILE)?" WAS_CAPTUREFILE":"",
     (h->dwFlags&AVIF_COPYRIGHTED)?" COPYRIGHTED":""
   );
-  printf("frames  total: %d   initial: %d\n",h->dwTotalFrames,h->dwInitialFrames);
-  printf("streams: %d\n",h->dwStreams);
-  printf("Suggested BufferSize: %d\n",h->dwSuggestedBufferSize);
-  printf("Size:  %d x %d\n",h->dwWidth,h->dwHeight);
+  printf("frames  total: %ld   initial: %ld\n",h->dwTotalFrames,h->dwInitialFrames);
+  printf("streams: %ld\n",h->dwStreams);
+  printf("Suggested BufferSize: %ld\n",h->dwSuggestedBufferSize);
+  printf("Size:  %ld x %ld\n",h->dwWidth,h->dwHeight);
 }
 
 void print_strh(AVIStreamHeader *h){
   printf("======= STREAM Header =======\n");
-  printf("Type: %.4s   FCC: %.4s (%X)\n",&h->fccType,&h->fccHandler,h->fccHandler);
-  printf("Flags: %d\n",h->dwFlags);
+  printf("Type: %.4s   FCC: %.4s (%X)\n",(char *)&h->fccType,(char *)&h->fccHandler,(unsigned int)h->fccHandler);
+  printf("Flags: %ld\n",h->dwFlags);
   printf("Priority: %d   Language: %d\n",h->wPriority,h->wLanguage);
-  printf("InitialFrames: %d\n",h->dwInitialFrames);
-  printf("Rate: %d/%d = %5.3f\n",h->dwRate,h->dwScale,(float)h->dwRate/(float)h->dwScale);
-  printf("Start: %d   Len: %d\n",h->dwStart,h->dwLength);
-  printf("Suggested BufferSize: %d\n",h->dwSuggestedBufferSize);
-  printf("Quality %d\n",h->dwQuality);
-  printf("Sample size: %d\n",h->dwSampleSize);
+  printf("InitialFrames: %ld\n",h->dwInitialFrames);
+  printf("Rate: %ld/%ld = %5.3f\n",h->dwRate,h->dwScale,(float)h->dwRate/(float)h->dwScale);
+  printf("Start: %ld   Len: %ld\n",h->dwStart,h->dwLength);
+  printf("Suggested BufferSize: %ld\n",h->dwSuggestedBufferSize);
+  printf("Quality %ld\n",h->dwQuality);
+  printf("Sample size: %ld\n",h->dwSampleSize);
 }
 
 void print_wave_header(WAVEFORMATEX *h){
@@ -52,8 +52,8 @@ void print_wave_header(WAVEFORMATEX *h){
   printf("======= WAVE Format =======\n");
   printf("Format Tag: %d (0x%X)\n",h->wFormatTag,h->wFormatTag);
   printf("Channels: %d\n",h->nChannels);
-  printf("Samplerate: %d\n",h->nSamplesPerSec);
-  printf("avg byte/sec: %d\n",h->nAvgBytesPerSec);
+  printf("Samplerate: %ld\n",h->nSamplesPerSec);
+  printf("avg byte/sec: %ld\n",h->nAvgBytesPerSec);
   printf("Block align: %d\n",h->nBlockAlign);
   printf("bits/sample: %d\n",h->wBitsPerSample);
   printf("cbSize: %d\n",h->cbSize);
@@ -62,13 +62,13 @@ void print_wave_header(WAVEFORMATEX *h){
 
 void print_video_header(BITMAPINFOHEADER *h){
   printf("======= VIDEO Format ======\n");
-	printf("  biSize %d\n", h->biSize);
-	printf("  biWidth %d\n", h->biWidth);
-	printf("  biHeight %d\n", h->biHeight);
+	printf("  biSize %ld\n", h->biSize);
+	printf("  biWidth %ld\n", h->biWidth);
+	printf("  biHeight %ld\n", h->biHeight);
 	printf("  biPlanes %d\n", h->biPlanes);
 	printf("  biBitCount %d\n", h->biBitCount);
-	printf("  biCompression %d='%.4s'\n", h->biCompression, &h->biCompression);
-	printf("  biSizeImage %d\n", h->biSizeImage);
+	printf("  biCompression %ld='%.4s'\n", h->biCompression, (char *)&h->biCompression);
+	printf("  biSizeImage %ld\n", h->biSizeImage);
   printf("===========================\n");
 }
 
@@ -76,10 +76,10 @@ void print_video_header(BITMAPINFOHEADER *h){
 void print_index(AVIINDEXENTRY *idx,int idx_size){
   int i;
   for(i=0;i<idx_size;i++){
-    printf("%5d:  %.4s  %4X  %08X  %d\n",i,
-      &idx[i].ckid,
-      idx[i].dwFlags,
-      idx[i].dwChunkOffset,
+    printf("%5d:  %.4s  %4X  %08X  %ld\n",i,
+      (char *)&idx[i].ckid,
+      (unsigned int)idx[i].dwFlags,
+      (unsigned int)idx[i].dwChunkOffset,
 //      idx[i].dwChunkOffset+demuxer->movi_start,
       idx[i].dwChunkLength
     );
