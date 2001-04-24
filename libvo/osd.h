9126c8286e950b45eaccccd7937184644ca2496f
pontscho <pontscho@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-04-24 11:35:07 +0000

    init.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@616 b3059339-0415-0410-9bf9-f77b7e298cf2

---
+++ libvo/osd.h
@@ -0,0 +1,15 @@
+
+#ifndef __MPLAYER_OSD_H
+#define __MPLAYER_OSD_H
+
+// Generic alpha renderers for all YUV modes and RGB depths.
+// These are "reference implementations", should be optimized later (MMX, etc)
+
+extern void vo_draw_alpha_yv12(int w,int h, unsigned char* src, unsigned char *srca, int srcstride, unsigned char* dstbase,int dststride);
+extern void vo_draw_alpha_yuy2(int w,int h, unsigned char* src, unsigned char *srca, int srcstride, unsigned char* dstbase,int dststride);
+extern void vo_draw_alpha_rgb24(int w,int h, unsigned char* src, unsigned char *srca, int srcstride, unsigned char* dstbase,int dststride);
+extern void vo_draw_alpha_rgb32(int w,int h, unsigned char* src, unsigned char *srca, int srcstride, unsigned char* dstbase,int dststride);
+extern void vo_draw_alpha_rgb15(int w,int h, unsigned char* src, unsigned char *srca, int srcstride, unsigned char* dstbase,int dststride);
+extern void vo_draw_alpha_rgb16(int w,int h, unsigned char* src, unsigned char *srca, int srcstride, unsigned char* dstbase,int dststride);
+
+#endif
