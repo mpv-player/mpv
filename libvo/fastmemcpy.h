91df40213443c8f04d231309a552566dce8b79c6
pontscho <pontscho@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-05-14 18:18:14 +0000

    USE_FASTMEMCPY bug fixed.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@801 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/fastmemcpy.h
+++ libvo/fastmemcpy.h
@@ -1,11 +1,15 @@
 #ifndef __MPLAYER_MEMCPY
 #define __MPLAYER_MEMCPY 1
 
+#ifdef USE_FASTMEMCPY
+
 #if defined( HAVE_MMX2 ) || defined( HAVE_3DNOW ) || defined( HAVE_MMX )
 #include <stddef.h>
 
 extern void * fast_memcpy(void * to, const void * from, size_t len);
 #define memcpy(a,b,c) fast_memcpy(a,b,c)
+
+#endif
 
 #endif
 
