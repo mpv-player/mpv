9334252452e3758b795cf0b5049446ca409f2006
al3x <al3x@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-06-13 11:18:20 +0000

    audio_format_out_name update
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@1122 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libao2/audio_out.c
+++ libao2/audio_out.c
@@ -64,11 +64,12 @@ char *audio_out_format_name(int format)
 {
     switch (format)
     {
-/*
 	case AFMT_MU_LAW:
+	    return("Mu-Law");
 	case AFMT_A_LAW:
+	    return("A-Law");
 	case AFMT_IMA_ADPCM:
-*/
+	    return("Ima-ADPCM");
 	case AFMT_S8:
 	    return("Signed 8-bit");
 	case AFMT_U8:
@@ -83,8 +84,10 @@ char *audio_out_format_name(int format)
 	    return("Unsigned 16-bit (Big-Endian)");
 	case AFMT_MPEG:
 	    return("MPEG (2) audio");
-	// the following two formats are not available with old linux kernel
-	// headers (e.g. in 2.2.16)
+/*
+  the following two formats are not available with old linux kernel
+  headers (e.g. in 2.2.16)
+*/
 #ifdef AFMT_S32_LE
 	case AFMT_S32_LE:
 	    return("Signed 32-bit (Little-Endian)");
