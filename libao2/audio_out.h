fa1d83cbb5f6160971e2a242b19da1463e1da27b
al3x <al3x@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-06-14 15:25:00 +0000

    audio_out_format_name prototype added, ao_control_vol_t for aocontrol_get/set_volume added
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@1128 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libao2/audio_out.h
+++ libao2/audio_out.h
@@ -1,4 +1,3 @@
-
 typedef struct ao_info_s
 {
         /* driver name ("Matrox Millennium G200/G400" */
@@ -11,47 +10,22 @@ typedef struct ao_info_s
         const char *comment;
 } ao_info_t;
 
-typedef struct ao_functions_s {
-
+typedef struct ao_functions_s
+{
 	ao_info_t *info;
-
-        /*
-         */
         int (*control)(int cmd,int arg);
-
-        /*
-         */
         int (*init)(int rate,int channels,int format,int flags);
-
-        /*
-         */
         void (*uninit)();
-
-        /*
-         */
         void (*reset)();
-
-        /*
-         */
         int (*get_space)();
-
-        /*
-         */
         int (*play)(void* data,int len,int flags);
-
-        /*
-         */
         int (*get_delay)();
-
-        /*
-         */
         void (*pause)();
-
-        /*
-         */
         void (*resume)();
-
 } ao_functions_t;
+
+// prototypes
+extern char *audio_out_format_name(int format);
 
 // NULL terminated array of all drivers
 extern ao_functions_t* audio_out_drivers[];
@@ -72,6 +46,11 @@ extern int ao_buffersize;
 
 #define AOCONTROL_SET_DEVICE 1
 #define AOCONTROL_GET_DEVICE 2
-#define AOCONTROL_QUERY_FORMAT 3
+#define AOCONTROL_QUERY_FORMAT 3 /* test for availabilty of a format */
 #define AOCONTROL_GET_VOLUME 4
 #define AOCONTROL_SET_VOLUME 5
+
+typedef struct ao_control_vol_s {
+	float left;
+	float right;
+} ao_control_vol_t;
