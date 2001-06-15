0fa160eedf03b2a9a5a180f98a2da660272c5a64
al3x <al3x@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-06-15 12:42:00 +0000

    cosmetic changes and sse/sse included
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@1132 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/fastmemcpy.h
+++ libvo/fastmemcpy.h
@@ -1,16 +1,16 @@
 #ifndef __MPLAYER_MEMCPY
 #define __MPLAYER_MEMCPY 1
 
-#ifdef USE_FASTMEMCPY
+#include "../config.h"
 
-#if defined( HAVE_MMX2 ) || defined( HAVE_3DNOW ) || defined( HAVE_MMX )
+#ifdef USE_FASTMEMCPY
+#if defined(HAVE_MMX) || defined(HAVE_MMX2) || defined(HAVE_3DNOW) \
+/*    || defined(HAVE_SSE) || defined(HAVE_SSE2) */
 #include <stddef.h>
 
 extern void * fast_memcpy(void * to, const void * from, size_t len);
 #define memcpy(a,b,c) fast_memcpy(a,b,c)
 
-#endif
-
-#endif
-
+#endif /* HAVE_MMX/MMX2/3DNOW/SSE/SSE2 */
+#endif /* USE_FASTMEMCPY */
 #endif
