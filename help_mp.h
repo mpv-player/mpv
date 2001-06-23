517707c56b50489770921ed5d4d685df8363d956
gabucino <gabucino@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-06-23 18:06:10 +0000

    *** empty log message ***
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@1207 b3059339-0415-0410-9bf9-f77b7e298cf2

--- help_mp.h
+++ help_mp.h
@@ -11,31 +11,16 @@ static char help_text[]=
 "    -ao <driver>    select audio output driver (see '-ao help' for driver list)\n"
 "    -vcd <track>    play video cd track from device instead of plain file\n"
 //"    -bg             play in background (X11 only!)\n"
-"    -sb <bytepos>   seek to byte position\n"
-//"    -ss <timepos>   seek to second position (with timestamp)\n"
+"    -ss <timepos>   seek to second position (with timestamp)\n"
 "    -nosound        don't play sound\n"
-"    -abs <bytes>    audio buffer size (in bytes, default: measuring)\n"
-"    -delay <secs>   audio delay in seconds (may be +/- float value)\n"
-#ifdef AVI_SYNC_BPS
-"    -nobps          do not use avg. byte/sec value for A-V sync (AVI)\n"
-#else
-"    -bps            use avg. byte/sec value for A-V sync (AVI)\n"
-#endif
-#ifdef ALSA_TIMER
-"    -noalsa         disable timing code\n"
-#else
-"    -alsa           enable timing code (works better with ALSA)\n"
-#endif
 #ifdef USE_FAKE_MONO
 "    -stereo         select MPEG1 stereo output (0:stereo 1:left 2:right)\n"
 #endif
-"    -aid <id>       select audio channel [MPG: 0-31  AVI: 1-99]\n"
-"    -vid <id>       select video channel [MPG: 0-15  AVI:  -- ]\n"
-"    -fps <value>    force frame rate (if value is wrong in the header)\n"
-"    -mc <s/5f>      maximum sync correction per 5 frames (in seconds)\n"
-//"    -afm <1-5>      force audio format  1:MPEG 2:PCM 3:AC3 4:Win32 5:aLaw\n"
 "    -fs -vm -zoom   fullscreen playing options (fullsc,vidmode chg,softscale)\n"
+"    -sub <file>     specify subtitle file to use\n"
 "    -x <x> -y <y>   scale image to <x> * <y> resolution [if scalable!]\n"
+"    -aid -vid       change audio/video stream to play\n"
+"    -fps -srate     change video/audio rate\n"
 "\n"
 "  Keys:\n"
 "    <-  or  ->      seek backward/forward  10 seconds\n"
