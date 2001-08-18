9e42c3cf4acf0208e7a782c5985515ea9993def0
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-08-18 21:12:20 +0000

    open.c/demuxer.c messages translated
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@1586 b3059339-0415-0410-9bf9-f77b7e298cf2

---
+++ help_mp-hu.h
@@ -0,0 +1,118 @@
+#ifdef HELP_MP_DEFINE_STATIC
+static char* banner_text=
+"\n\n"
+"MPlayer " VERSION "(C) 2000-2001 Arpad Gereoffy (see DOCS/AUTHORS)\n"
+"\n";
+
+static char help_text[]=
+"Usage:   mplayer [options] [path/]name\n"
+"\n"
+"Options:\n"
+" -vo <drv[:dev]> select video output driver & device (see '-vo help' for list)\n"
+" -ao <drv[:dev]> select audio output driver & device (see '-ao help' for list)\n"
+" -vcd <trackno>  play VCD (video cd) track from device instead of plain file\n"
+#ifdef HAVE_LIBCSS
+" -dvdauth <dev>  specify DVD device for authentication (for encrypted discs)\n"
+#endif
+" -ss <timepos>   seek to given (seconds or hh:mm:ss) position\n"
+" -nosound        don't play sound\n"
+#ifdef USE_FAKE_MONO
+" -stereo         select MPEG1 stereo output (0:stereo 1:left 2:right)\n"
+#endif
+" -fs -vm -zoom   fullscreen playing options (fullscr,vidmode chg,softw.scale)\n"
+" -x <x> -y <y>   scale image to <x> * <y> resolution [if -vo driver supports!]\n"
+" -sub <file>     specify subtitle file to use (see also -subfps, -subdelay)\n"
+" -vid x -aid y   options to select video (x) and audio (y) stream to play\n"
+" -fps x -srate y options to change video (x fps) and audio (y Hz) rate\n"
+" -pp <quality>   enable postprocessing filter (0-4 for DivX, 0-63 for mpegs)\n"
+" -bps            use alternative A-V sync method for AVI files (may help!)\n"
+" -framedrop      enable frame-dropping (for slow machines)\n"
+"\n"
+"Keys:\n"
+" <-  or  ->      seek backward/forward 10 seconds\n"
+" up or down      seek backward/forward  1 minute\n"
+" p or SPACE      pause movie (press any key to continue)\n"
+" q or ESC        stop playing and quit program\n"
+" + or -          adjust audio delay by +/- 0.1 second\n"
+" o               cycle OSD mode:  none / seekbar / seekbar+timer\n"
+" * or /          increase or decrease volume (press 'm' to select master/pcm)\n"
+" z or x          adjust subtitle delay by +/- 0.1 second\n"
+"\n"
+" * * * SEE MANPAGE FOR DETAILS, FURTHER OPTIONS AND KEYS ! * * *\n"
+"\n";
+#endif
+
+// mplayer.c: 
+
+#define MSGTR_Exiting "\nExiting... (%s)\n"
+#define MSGTR_Exit_frames "Requested number of frames played"
+#define MSGTR_Exit_quit "Quit"
+#define MSGTR_Exit_eof "End of file"
+#define MSGTR_IntBySignal "\nMPlayer interrupted by signal %d in module: %s \n"
+#define MSGTR_NoHomeDir "Can't find HOME dir\n"
+#define MSGTR_GetpathProblem "get_path(\"config\") problem\n"
+#define MSGTR_CreatingCfgFile "Creating config file: %s\n"
+#define MSGTR_InvalidVOdriver "Invalid video output driver name: %s\nUse '-vo help' to get a list of available video drivers.\n"
+#define MSGTR_InvalidAOdriver "Invalid audio output driver name: %s\nUse '-ao help' to get a list of available audio drivers.\n"
+#define MSGTR_CopyCodecsConf "(copy/link DOCS/codecs.conf to ~/.mplayer/codecs.conf)\n"
+#define MSGTR_CantLoadFont "Can't load font: %s\n"
+#define MSGTR_CantLoadSub "Can't load subtitles: %s\n"
+#define MSGTR_ErrorDVDkey "Error processing DVD KEY.\n"
+#define MSGTR_CmdlineDVDkey "DVD command line requested key is stored for descrambling.\n"
+#define MSGTR_DVDauthOk "DVD auth sequence seems to be OK.\n"
+#define MSGTR_DumpSelectedSteramMissing "dump: FATAL: selected stream missing!\n"
+#define MSGTR_CantOpenDumpfile "Can't open dump file!!!\n"
+#define MSGTR_CoreDumped "core dumped :)\n"
+#define MSGTR_FPSnotspecified "FPS not specified (or invalid) in the header! Use the -fps option!\n"
+#define MSGTR_NoVideoStream "Sorry, no video stream... it's unplayable yet\n"
+#define MSGTR_TryForceAudioFmt "Trying to force audio codec driver family %d ...\n"
+#define MSGTR_CantFindAfmtFallback "Can't find audio codec for forced driver family, fallback to other drivers.\n"
+#define MSGTR_CantFindAudioCodec "Can't find codec for audio format 0x%X !\n"
+#define MSGTR_TryUpgradeCodecsConfOrRTFM "*** Try to upgrade %s from DOCS/codecs.conf\n*** If it's still not OK, then read DOCS/CODECS!\n"
+#define MSGTR_CouldntInitAudioCodec "Couldn't initialize audio codec! -> nosound\n"
+#define MSGTR_TryForceVideoFmt "Trying to force video codec driver family %d ...\n"
+#define MSGTR_CantFindVfmtFallback "Can't find video codec for forced driver family, fallback to other drivers.\n"
+#define MSGTR_CantFindVideoCodec "Can't find codec for video format 0x%X !\n"
+#define MSGTR_VOincompCodec "Sorry, selected video_out device is incompatible with this codec.\n"
+#define MSGTR_CouldntInitVideoCodec "FATAL: Couldn't initialize video codec :(\n"
+#define MSGTR_EncodeFileExists "File already exists: %s (don't overwrite your favourite AVI!)\n"
+#define MSGTR_CantCreateEncodeFile "Cannot create file for encoding\n"
+#define MSGTR_CannotInitVO "FATAL: Cannot initialize video driver!\n"
+#define MSGTR_CannotInitAO "couldn't open/init audio device -> NOSOUND\n"
+#define MSGTR_StartPlaying "Start playing...\n"
+#define MSGTR_SystemTooSlow "\n************************************************************************"\
+			    "\n** Your system is too SLOW to play this! try with -framedrop or RTFM! **"\
+			    "\n************************************************************************\n"
+//#define MSGTR_
+
+// open.c: 
+#define MSGTR_CdDevNotfound "A CD-ROM meghajtó (%s) nem található!\n"
+#define MSGTR_ErrTrackSelect "Hiba a VCD sáv kiválasztásakor!"
+#define MSGTR_ReadSTDIN "Olvasás a szabványos bemenetrõl (stdin)...\n"
+#define MSGTR_UnableOpenURL "Nem megnyitható az URL: %s\n"
+#define MSGTR_ConnToServer "Csatlakozom a szerverhez: %s\n"
+#define MSGTR_FileNotFound "A fájl nem található: '%s'\n"
+
+// demuxer.c:
+#define MSGTR_AudioStreamRedefined "Vigyázat! Többszörösen definált Audio folyam! (Hibás fájl?)\n"
+#define MSGTR_VideoStreamRedefined "Vigyázat! Többszörösen definált Video folyam! (Hibás fájl?)\n"
+#define MSGTR_TooManyAudioInBuffer "\nDEMUXER: Túl sok (%d db, %d bájt) audio csomag a pufferben!\n"
+#define MSGTR_TooManyVideoInBuffer "\nDEMUXER: Túl sok (%d db, %d bájt) video csomag a pufferben!\n"
+#define MSGTR_MaybeNI "(talán ez egy nem összefésült fájl vagy a CODEC nem mûködik jól)\n"
+#define MSGTR_DetectedAVIfile "Ez egy AVI formátumú fájl!\n"
+#define MSGTR_DetectedASFfile "Ez egy ASF formátumú fájl!\n"
+#define MSGTR_DetectedMPEGPESfile "Ez egy MPEG-PES formátumú fájl!\n"
+#define MSGTR_DetectedMPEGPSfile "Ez egy MPEG-PS formátumú fájl!\n"
+#define MSGTR_DetectedMPEGESfile "Ez egy MPEG-ES formátumú fájl!\n"
+#define MSGTR_DetectedQTMOVfile "Ez egy QuickTime/MOV formátumú fájl! (ez még nem támogatott)\n"
+#define MSGTR_MissingMpegVideo "Nincs MPEG video folyam!? Lépj kapcsolatba a készítõkkel, lehet hogy hiba!\n"
+#define MSGTR_InvalidMPEGES "Hibás MPEG-ES folyam??? Lépj kapcsolatba a készítõkkel, lehet hogy hiba!\n"
+#define MSGTR_FormatNotRecognized "========= Sajnos ez a fájlformátum ismeretlen vagy nem támogatott ===========\n"\
+				  "= Ha ez egy AVI, ASF vagy MPEG fájl, lépj kapcsolatba a készítõkkel (hiba)! =\n"
+#define MSGTR_MissingASFvideo "ASF: Nincs kép folyam!\n"
+#define MSGTR_MissingASFaudio "ASF: Nincs hang folyam...  -> hang nélkül\n"
+#define MSGTR_MissingMPEGaudio "MPEG: Nincs hang folyam...  -> hang nélkül\n"
+
+//#define MSGTR_
+
+
