5625f52f464acd56c00239f9a6d5de42be7767cc
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-08-30 12:47:13 +0000

    stop/pause/seek gui
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@1773 b3059339-0415-0410-9bf9-f77b7e298cf2

--- mplayer.c
+++ mplayer.c
@@ -1498,7 +1498,7 @@ if(auto_quality>0){
 #ifdef HAVE_NEW_GUI
              if(use_gui){
 		wsHandleEvents();mplTimerHandler(0); // handle GUI timer events
-		if(mplShMem->Playing!=2) break; // end of pause
+		if(mplShMem->Playing!=2 || (rel_seek_secs || abs_seek_pos)) break; // end of pause or seek
              }
 #endif
              if(use_stdin) usec_sleep(1000); // do not eat the CPU
@@ -1769,14 +1769,8 @@ if(rel_seek_secs || abs_seek_pos){
 	}
 	mplShMem->TimeSec=d_video->pts; 
 	printf("mplShMem->Playing=%d  \n",mplShMem->Playing);
-	switch(mplShMem->Playing){
-	case 0: // stop
-//	  osd_function=OSD_PAUSE;break;
-	case 1: // play
-	  break;
-	case 2: // pause
-	  osd_function=OSD_PAUSE;break;
-	}
+	if(mplShMem->Playing==0) break; // STOP
+	if(mplShMem->Playing==2) osd_function=OSD_PAUSE;
       }
 #endif
 
