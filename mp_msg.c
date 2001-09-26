8f7170e9dd29215ec75400347bca23c6c19dbb73
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-09-26 18:40:57 +0000

    cleanup
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@1972 b3059339-0415-0410-9bf9-f77b7e298cf2

--- mp_msg.c
+++ mp_msg.c
@@ -26,44 +26,38 @@ void mp_msg_init(int verbose){
 
 void mp_msg_c( int x, const char *format, ... ){
     va_list va;
-    char tmp[8*80];
     if((x&255)>mp_msg_levels[x>>8]) return; // do not display
     va_start(va, format);
-    vsnprintf( tmp,128,format,va );
-/*    
-    if((x&255)<=MSGL_ERR){
-//	fprintf(stderr,"%%%%%% ");
-	vfprintf(stderr,format, va);
-    } else {
-//	printf("%%%%%% ");
-	vprintf(format, va);
-    }
-*/    
-    va_end(va);
-    switch( x&255 )
-     {
+#ifdef HAVE_NEW_GUI
+    if(use_gui){
+      char tmp[8*80];
+      vsnprintf( tmp,8*80,format,va ); tmp[8*80-1]=0;
+      switch( x&255 ) {
        case MSGL_FATAL: 
               fprintf( stderr,"%s",tmp );
-              #ifdef HAVE_NEW_GUI
-               if ( use_gui ) gtkMessageBox( GTK_MB_FATAL|GTK_MB_SIMPLE,tmp );
-              #endif
+              gtkMessageBox( GTK_MB_FATAL|GTK_MB_SIMPLE,tmp );
        	   break;
        case MSGL_ERR:
               fprintf( stderr,"%s",tmp );
-              #ifdef HAVE_NEW_GUI 
-               if ( use_gui ) gtkMessageBox( GTK_MB_ERROR|GTK_MB_SIMPLE,tmp );
-              #endif
+              gtkMessageBox( GTK_MB_ERROR|GTK_MB_SIMPLE,tmp );
        	   break;
        case MSGL_WARN:
               printf( "%s",tmp );
-              #ifdef HAVE_NEW_GUI 
-               if ( use_gui ) gtkMessageBox( GTK_MB_WARNING|GTK_MB_SIMPLE,tmp );
-              #endif
+              gtkMessageBox( GTK_MB_WARNING|GTK_MB_SIMPLE,tmp );
        	   break;
        default:
               printf( "%s",tmp );
-       	   break;
-     }
+      }
+    } else
+#endif
+    if((x&255)<=MSGL_ERR){
+//    fprintf(stderr,"%%%%%% ");
+      vfprintf(stderr,format, va);
+    } else {
+//	printf("%%%%%% ");
+      vprintf(format, va);
+    }
+    va_end(va);
 }
 
 #else
