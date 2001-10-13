377dda939f4583ab7ba06475eef047ff8d3401ba
atmos4 <atmos4@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-10-13 13:47:44 +0000

    don't expect win32 bin yet, only wanna have this already in cvs, btw. it has to be copied to main/inttypes.h
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@2184 b3059339-0415-0410-9bf9-f77b7e298cf2

---
+++ TOOLS/cygwin_inttypes.h
@@ -0,0 +1,8 @@
+#ifndef _CYGWIN_INTTYPES_H
+#define _CYGWIN_INTTYPES_H
+#include <sys/types.h>
+typedef u_int8_t uint8_t;
+typedef u_int16_t uint16_t;
+typedef u_int32_t uint32_t;
+typedef u_int64_t uint64_t;
+#endif
