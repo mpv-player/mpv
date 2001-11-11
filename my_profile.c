6fde34dde88c6d840718600485a696eaa97a1d4b
nick <nick@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-11-11 14:41:19 +0000

    Precised profile stuff
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@2833 b3059339-0415-0410-9bf9-f77b7e298cf2

---
+++ my_profile.c
@@ -0,0 +1,13 @@
+/*
+ *  my_profile.c
+ *
+ *	Copyright (C) Nick Kurshev <nickols_k@mail.ru> - Oct 2001
+ *
+ *  You can redistribute this file under terms and conditions
+ *  of GNU General Public licence v2.
+ *  This file is partly based on vbetest.c from lrmi distributive.
+ */
+ 
+#include "my_profile.h"
+
+unsigned long long int my_profile_start,my_profile_end,my_profile_total=0ULL;
