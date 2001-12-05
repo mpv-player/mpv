a41998df59ba46731011ea13d4bcf85dc909bf0a
mswitch <mswitch@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-12-05 07:11:36 +0000

    Minor typo fix (which I thought I commited yesterday)
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@3334 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vo_dxr3.c
+++ libvo/vo_dxr3.c
@@ -21,7 +21,7 @@
 #include "config.h"
 #include "video_out.h"
 #include "video_out_internal.h"
-#include <libmp1e.h>
+#include "../libmp1e/libmp1e.h"
 
 #include "../postproc/rgb2rgb.h"
 #ifdef HAVE_MMX
