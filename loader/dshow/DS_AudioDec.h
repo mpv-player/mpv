cc4dea654d70255406df7c7482242737420adc1a
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-12-10 21:57:53 +0000

    obsoleted files
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@3447 b3059339-0415-0410-9bf9-f77b7e298cf2

--- loader/dshow/DS_AudioDec.h
+++ loader/dshow/DS_AudioDec.h
@@ -10,14 +10,15 @@
 extern "C" {
 #endif /* defined(__cplusplus) */
 
-int DS_AudioDecoder_Open(char* dllname, GUID* guid, WAVEFORMATEX* wf);
+void* DS_AudioDecoder_Open(char* dllname, GUID* guid, WAVEFORMATEX* wf);
 
-void DS_AudioDecoder_Close();
+void DS_AudioDecoder_Close(void* _handle);
 
-int DS_AudioDecoder_GetSrcSize(int dest_size);
+int DS_AudioDecoder_GetSrcSize(void* _handle, int dest_size);
 
-int DS_AudioDecoder_Convert(unsigned char* in_data, unsigned in_size,
-	     unsigned char* out_data, unsigned out_size,
+int DS_AudioDecoder_Convert(void* _handle,
+	    unsigned char* in_data, unsigned in_size,
+	    unsigned char* out_data, unsigned out_size,
 	    unsigned* size_read, unsigned* size_written);
 
 #ifdef __cplusplus
--- loader/dshow/DS_VideoDec.h
+++ loader/dshow/DS_VideoDec.h
@@ -10,21 +10,19 @@
 extern "C" {
 #endif /* defined(__cplusplus) */
 
-int DS_VideoDecoder_Open(char* dllname, GUID* guid, BITMAPINFOHEADER* format, int flip,char** d_ptr);
+void* DS_VideoDecoder_Open(char* dllname, GUID* guid, BITMAPINFOHEADER* format, int flip, int maxauto);
 
-void DS_VideoDecoder_Start();
+void DS_VideoDecoder_StartInternal(void* _handle);
 
-void DS_VideoDecoder_Stop();
+void DS_VideoDecoder_Stop(void* _handle);
 
-void DS_VideoDecoder_Restart();
+void DS_VideoDecoder_Destroy(void* _handle);
 
-void DS_VideoDecoder_Close();
+int DS_VideoDecoder_DecodeInternal(void* _handle, char* src, int size, int is_keyframe, char* dest);
 
-int DS_VideoDecoder_DecodeFrame(char* src, int size, int is_keyframe, int render);
+int DS_VideoDecoder_SetDestFmt(void* _handle, int bits, int csp);
 
-int DS_VideoDecoder_SetDestFmt(int bits, int csp);
-
-int DS_SetValue_DivX(char* name, int value);
+int DS_VideoDecoder_SetValue(void* _handle, char* name, int value);
 int DS_SetAttr_DivX(char* attribute, int value);
 
 #ifdef __cplusplus
