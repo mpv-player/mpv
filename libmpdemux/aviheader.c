8b2f0c064d878672cdd423311ad75d3a0d3f4ab2
atmos4 <atmos4@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-12-17 16:51:34 +0000

    Divide by Zero fix.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@3562 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/aviheader.c
+++ libmpdemux/aviheader.c
@@ -223,8 +223,7 @@ if(index_mode>=2 || (priv->idx_size==0 && index_mode==1)){
 	    case 2: if(c==0x1B6) idx->dwFlags=0;break; // divx 4
 	}
       }
-    //mp_msg(MSGT_HEADER,MSGL_STATUS,"Generating Index: %02d%%\r", (int)demuxer->filepos/((int)demuxer->movi_end/100));
-    mp_msg(MSGT_HEADER,MSGL_STATUS,"Generating Index...\r");
+    mp_msg(MSGT_HEADER,MSGL_STATUS,"Generating Index: %02d%%\r", (int)demuxer->filepos/((int)demuxer->movi_end/100+1));
     fflush(stdout);
     mp_dbg(MSGT_HEADER,MSGL_DBG2,"%08X %08X %.4s %08X %X\n",(int)demuxer->filepos,id,(char *) &id,(int)c,(unsigned int) idx->dwFlags);
 #if 0
