8e368a8571409b7ddb8db2d633003f6da8fd4f27
pl <pl@b3059339-0415-0410-9bf9-f77b7e298cf2>
2001-12-25 16:30:25 +0000

    warning fix
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@3732 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpeg2/idct.c
+++ libmpeg2/idct.c
@@ -105,7 +105,7 @@ void idct_init (void)
  * c[1..7] = 128*sqrt (2)
  */
 
-static void inline idct_row (int16_t * block)
+static inline void idct_row (int16_t * block)
 {
     int x0, x1, x2, x3, x4, x5, x6, x7, x8;
 
@@ -174,7 +174,7 @@ static void inline idct_row (int16_t * block)
  * c[1..7] = (1/1024)*sqrt (2)
  */
 
-static void inline idct_col (int16_t *block)
+static inline void idct_col (int16_t *block)
 {
     int x0, x1, x2, x3, x4, x5, x6, x7, x8;
 
