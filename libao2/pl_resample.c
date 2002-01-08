10aa8c8539722e4956bc0cce64e2677a192924ec
anders <anders@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-01-08 11:37:54 +0000

    Correction of lisensing comment
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@4050 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libao2/pl_resample.c
+++ libao2/pl_resample.c
@@ -1,20 +1,7 @@
 /*=============================================================================
 //	
-//  This file is part of mplayer.
-//
-//  mplayer is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
-//
-//  mplayer is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with mplayer; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//  This software has been released under the terms of the GNU Public
+//  license. See http://www.gnu.org/copyleft/gpl.html for details.
 //
 //  Copyright 2001 Anders Johansson ajh@atri.curtin.edu.au
 //
