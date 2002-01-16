491a353915efecabfd0359ebac5bc0969e1883df
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-01-16 01:19:22 +0000

    bpp != depth
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@4187 b3059339-0415-0410-9bf9-f77b7e298cf2

--- mp_image.h
+++ mp_image.h
@@ -13,9 +13,14 @@
 // set if it's swapped plane/byteorder
 #define MP_IMGFLAG_SWAPPED 0x40
 
+#define MP_IMGTYPE_EXPORT 0
+#define MP_IMGTYPE_STATIC 1
+#define MP_IMGTYPE_TEMP 2
+
 typedef struct mp_image_s {
     unsigned short flags;
-    unsigned short bpp;
+    unsigned char type;
+    unsigned char bpp;  // bits/pixel. NOT depth! for RGB it will be n*8
     unsigned int imgfmt;
     int width,height;  // stored dimensions
     int x,y,w,h;  // visible dimensions
@@ -30,11 +35,11 @@ static inline void mp_image_setfmt(mp_image_t* mpi,unsigned int out_fmt){
     mpi->flags&=~(MP_IMGFLAG_PLANAR|MP_IMGFLAG_YUV|MP_IMGFLAG_SWAPPED);
     mpi->out_fmt=out_fmt;
     if( (out_fmt&IMGFMT_RGB_MASK) == IMGFMT_RGB ){
-	mpi->bpp=out_fmt&255;
+	mpi->bpp=((out_fmt&255)+7)&(~7);
 	return;
     }
     if( (out_fmt&IMGFMT_BGR_MASK) == IMGFMT_BGR ){
-	mpi->bpp=out_fmt&255;
+	mpi->bpp=((out_fmt&255)+7)&(~7);
 	mpi->flags|=MP_IMGFLAG_SWAPPED;
 	return;
     }
