2fbdb9bdda72f29fb94a7bfdd66ff93093f6632b
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-03-20 03:03:29 +0000

    InitializeQTML(0) added
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@5212 b3059339-0415-0410-9bf9-f77b7e298cf2

--- loader/qtx/list.c
+++ loader/qtx/list.c
@@ -19,20 +19,37 @@ void* GetProcAddress(void* handle,char* func);
 #define __cdecl   __attribute__((__cdecl__))
 #define APIENTRY 
 
+typedef long OSErr;
+
 int main(int argc, char *argv[]){
     void *handler;
     ComponentDescription desc;
     Component (*FindNextComponent)(Component prev,ComponentDescription* desc);
     long (*CountComponents)(ComponentDescription* desc);
+    OSErr (*InitializeQTML)(long flags);
+    OSErr (*EnterMovies)(void);
+    OSErr ret;
 
     Setup_LDT_Keeper();
     handler = LoadLibraryA("/usr/lib/win32/qtmlClient.dll");
-    FindNextComponent = GetProcAddress(handler, "FindNextComponent");
-    CountComponents = GetProcAddress(handler, "CountComponents");
-    printf("handler: %p, funcs: %p, %p\n", handler, FindNextComponent,CountComponents);
+    printf("***************************\n");
+    InitializeQTML = 0x1000c870; //GetProcAddress(handler, "InitializeQTML");
+    EnterMovies = 0x10003ac0; //GetProcAddress(handler, "EnterMovies");
+    FindNextComponent = 0x1000d5f0; //GetProcAddress(handler, "FindNextComponent");
+    CountComponents = 0x1000d5d0; //GetProcAddress(handler, "CountComponents");
+//     = GetProcAddress(handler, "");
+    printf("handler: %p, funcs: %p %p %p, %p\n", handler, InitializeQTML, EnterMovies, FindNextComponent,CountComponents);
+
+    ret=InitializeQTML(0);
+    printf("InitializeQTML->%d\n",ret);
+    ret=EnterMovies();
+    printf("EnterMovies->%d\n",ret);
 
     memset(&desc,0,sizeof(desc));
-    desc.componentType=0;
+    desc.componentType= (((unsigned char)'S')<<24)|
+			(((unsigned char)'V')<<16)|
+			(((unsigned char)'Q')<<8)|
+			(((unsigned char)'5'));
     desc.componentSubType=0;
     desc.componentManufacturer=0;
     desc.componentFlags=0;
