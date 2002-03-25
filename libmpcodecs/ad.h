94d3170bd0561939a0ec322860be1f28d711e271
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-03-25 19:53:41 +0000

    changed to be more-or-less compatible with Nick's stuff
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@5340 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/ad.h
+++ libmpcodecs/ad.h
@@ -6,21 +6,21 @@ typedef mp_codec_info_t ad_info_t;
 typedef struct ad_functions_s
 {
 	ad_info_t *info;
+        int (*preinit)(sh_audio_t *sh);
         int (*init)(sh_audio_t *sh);
-        int (*start)(sh_audio_t *sh);
         void (*uninit)(sh_audio_t *sh);
         int (*control)(sh_audio_t *sh,int cmd,void* arg, ...);
-        int (*decode)(sh_audio_t *sh,unsigned char* buffer,int minlen,int maxlen,int flags);
+        int (*decode_audio)(sh_audio_t *sh,unsigned char* buffer,int minlen,int maxlen);
 } ad_functions_t;
 
 // NULL terminated array of all drivers
 extern ad_functions_t* mpcodecs_ad_drivers[];
 
 // fallback if ADCTRL_RESYNC not implemented: sh_audio->a_in_buffer_len=0;
-#define ADCTRL_RESYNC 1       /* resync, called after seeking! */
+#define ADCTRL_RESYNC_STREAM 1       /* resync, called after seeking! */
 
 // fallback if ADCTRL_SKIP not implemented: ds_fill_buffer(sh_audio->ds);
-#define ADCTRL_SKIP 2         /* skip block/frame, called while seeking! */
+#define ADCTRL_SKIP_FRAME 2         /* skip block/frame, called while seeking! */
 
 // fallback if ADCTRL_QUERY_FORMAT not implemented: sh_audio->sample_format
 #define ADCTRL_QUERY_FORMAT 3 /* test for availabilty of a format */
