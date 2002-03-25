ccd1ad61ab27924ecf97fc28ab0c65d868650e59
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-03-25 02:39:36 +0000

    common stuff moved to mpc_info.h
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@5324 b3059339-0415-0410-9bf9-f77b7e298cf2

---
+++ libmpcodecs/mpc_info.h
@@ -0,0 +1,27 @@
+
+#ifndef MP_CODEC_INFO_T
+#define MP_CODEC_INFO_T
+typedef struct mp_codec_info_s
+{
+        /* codec long name ("Autodesk FLI/FLC Animation decoder" */
+        const char *name;
+        /* short name (same as driver name in codecs.conf) ("dshow") */
+        const char *short_name;
+	/* codec family: -vfm id */
+	const int id;
+        /* interface author/maintainer */
+        const char *maintainer;
+        /* codec author ("Aaron Holtzman <aholtzma@ess.engr.uvic.ca>") */
+        const char *author;
+        /* any additional comments */
+        const char *comment;
+} mp_codec_info_t;
+
+#define CONTROL_OK 1
+#define CONTROL_TRUE 1
+#define CONTROL_FALSE 0
+#define CONTROL_UNKNOWN -1
+#define CONTROL_ERROR -2
+#define CONTROL_NA -3
+
+#endif
--- libmpcodecs/vd.h
+++ libmpcodecs/vd.h
@@ -1,18 +1,6 @@
-typedef struct vd_info_s
-{
-        /* codec long name ("Autodesk FLI/FLC Animation decoder" */
-        const char *name;
-        /* short name (same as driver name in codecs.conf) ("dshow") */
-        const char *short_name;
-	/* codec family: -vfm id */
-	const int id;
-        /* interface author/maintainer */
-        const char *maintainer;
-        /* codec author ("Aaron Holtzman <aholtzma@ess.engr.uvic.ca>") */
-        const char *author;
-        /* any additional comments */
-        const char *comment;
-} vd_info_t;
+
+#include "mpc_info.h"
+typedef mp_codec_info_t vd_info_t;
 
 /* interface of video decoder drivers */
 typedef struct vd_functions_s
@@ -26,13 +14,6 @@ typedef struct vd_functions_s
 
 // NULL terminated array of all drivers
 extern vd_functions_t* mpcodecs_vd_drivers[];
-
-#define CONTROL_OK 1
-#define CONTROL_TRUE 1
-#define CONTROL_FALSE 0
-#define CONTROL_UNKNOWN -1
-#define CONTROL_ERROR -2
-#define CONTROL_NA -3
 
 #define VDCTRL_QUERY_FORMAT 3 /* test for availabilty of a format */
 #define VDCTRL_QUERY_MAX_PP_LEVEL 4 /* test for postprocessing support (max level) */
