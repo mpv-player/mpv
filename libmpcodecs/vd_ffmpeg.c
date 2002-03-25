a827a9ceb5c2a63f071436638aab802a33465a19
atmos4 <atmos4@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-03-25 09:52:15 +0000

    Fix aspect info for square pixel.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@5332 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/vd_ffmpeg.c
+++ libmpcodecs/vd_ffmpeg.c
@@ -132,8 +132,6 @@ static mp_image_t* decode(sh_video_t *sh,void* data,int len,int flags){
 		sh->aspect = 16.0/9.0;
 		break;
 	    case FF_ASPECT_SQUARE:
-		sh->aspect = 1.0/1.0;
-		break;
 	    default:
 		sh->aspect = 0.0;
 		break;
