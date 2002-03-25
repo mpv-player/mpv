8784c88322b8e702c15cb7e9bbcea68e1d27773e
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-03-25 02:39:58 +0000

    proposal for audio decoders
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@5325 b3059339-0415-0410-9bf9-f77b7e298cf2

---
+++ libmpcodecs/ad.h
@@ -0,0 +1,30 @@
+
+#include "mpc_info.h"
+typedef mp_codec_info_t ad_info_t;
+
+/* interface of video decoder drivers */
+typedef struct ad_functions_s
+{
+	ad_info_t *info;
+        int (*init)(sh_audio_t *sh);
+        int (*start)(sh_audio_t *sh);
+        void (*uninit)(sh_audio_t *sh);
+        int (*control)(sh_audio_t *sh,int cmd,void* arg, ...);
+        int (*decode)(sh_audio_t *sh,unsigned char* buffer,int minlen,int maxlen,int flags);
+} ad_functions_t;
+
+// NULL terminated array of all drivers
+extern ad_functions_t* mpcodecs_ad_drivers[];
+
+// fallback if ADCTRL_RESYNC not implemented: sh_audio->a_in_buffer_len=0;
+#define ADCTRL_RESYNC 1       /* resync, called after seeking! */
+
+// fallback if ADCTRL_SKIP not implemented: ds_fill_buffer(sh_audio->ds);
+#define ADCTRL_SKIP 2         /* skip block/frame, called while seeking! */
+
+// fallback if ADCTRL_QUERY_FORMAT not implemented: sh_audio->sample_format
+#define ADCTRL_QUERY_FORMAT 3 /* test for availabilty of a format */
+
+// fallback: use hw mixer in libao
+#define ADCTRL_SET_VOLUME 4 /* set volume (used for mp3lib and liba52) */
+
