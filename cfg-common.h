7498cf71456f9b64005250c9c780797d169ba23d
atmos4 <atmos4@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-03-29 02:52:45 +0000

    Allow cache to be disabled at commandline.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@5388 b3059339-0415-0410-9bf9-f77b7e298cf2

--- cfg-common.h
+++ cfg-common.h
@@ -3,6 +3,7 @@
 
 #ifdef USE_STREAM_CACHE
 	{"cache", &stream_cache_size, CONF_TYPE_INT, CONF_RANGE, 4, 65536, NULL},
+	{"nocache", &stream_cache_size, CONF_TYPE_FLAG, 0, 1, 0, NULL},
 #else
 	{"cache", "MPlayer was compiled WITHOUT cache2 support\n", CONF_TYPE_PRINT, CONF_NOCFG, 0, 0, NULL},
 #endif
