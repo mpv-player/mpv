dc8013694c04a4ca63f9f3fbe491454399645d3e
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-03-31 10:11:49 +0000

    copy palette to 4*256 bytes area to avoid sig11 when colors<256 but index>colors (broken files)
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@5420 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/vd_rle.c
+++ libmpcodecs/vd_rle.c
@@ -24,13 +24,17 @@ static int control(sh_video_t *sh,int cmd,void* arg,...){
 
 // init driver
 static int init(sh_video_t *sh){
+    unsigned int* pal;
+    unsigned int* dpal;
+    int cols;
     if(!mpcodecs_config_vo(sh,sh->disp_w,sh->disp_h,IMGFMT_BGR24)) return 0;
+    sh->context=dpal=malloc(4*256); // for the palette
+    memset(dpal,0,4*256);
+    pal=(unsigned int*)(((char*)sh->bih)+40);
+    cols=(sh->bih->biSize-40)/4;
+    if(cols>256) cols=256;
     if( (((sh->codec->outfmt[sh->outfmtidx]&255)+7)/8)==2 ){
-     unsigned int* pal=(unsigned int*)(((char*)sh->bih)+40);
-     int cols=(sh->bih->biSize-40)/4;
-     //int cols=1<<(sh_video->bih->biBitCount);
      int i;
-     if(cols>256) cols=256;
      mp_msg(MSGT_DECVIDEO,MSGL_V,"RLE: converting palette for %d colors.\n",cols);
      for(i=0;i<cols;i++){
         unsigned int c=pal[i];
@@ -38,16 +42,18 @@ static int init(sh_video_t *sh){
 	unsigned int g=(c>>8)&255;
 	unsigned int r=(c>>16)&255;
 	if((sh->codec->outfmt[sh->outfmtidx]&255)==15)
-	  pal[i]=((r>>3)<<10)|((g>>3)<<5)|((b>>3));
+	  dpal[i]=((r>>3)<<10)|((g>>3)<<5)|((b>>3));
 	else
-	  pal[i]=((r>>3)<<11)|((g>>2)<<5)|((b>>3));
+	  dpal[i]=((r>>3)<<11)|((g>>2)<<5)|((b>>3));
      }
-    }
+    } else
+     memcpy(dpal,pal,4*cols);
     return 1;
 }
 
 // uninit driver
 static void uninit(sh_video_t *sh){
+    free(sh->context); sh->context=NULL;
 }
 
 //mp_image_t* mpcodecs_get_image(sh_video_t *sh, int mp_imgtype, int mp_imgflag, int w, int h);
@@ -65,8 +71,8 @@ static mp_image_t* decode(sh_video_t *sh,void* data,int len,int flags){
     if(!mpi) return NULL;
 
     AVI_Decode_RLE8(mpi->planes[0],data,len,
-       (int*)(((char*)sh->bih)+40),
-      sh->disp_w,sh->disp_h,((mpi->imgfmt&255)+7)/8);
+       (int*)sh->context,
+      sh->disp_w,sh->disp_h,mpi->bpp/8);
     
     return mpi;
 }
