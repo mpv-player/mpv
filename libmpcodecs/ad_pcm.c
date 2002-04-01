8b9eea7eb0994ff6971cf3439054c87394df22e3
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-04-01 15:25:50 +0000

    twos audio fixed
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@5454 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/ad_pcm.c
+++ libmpcodecs/ad_pcm.c
@@ -29,7 +29,8 @@ static int init(sh_audio_t *sh_audio)
     case 0x7:  sh_audio->sample_format=AFMT_MU_LAW;break;
     case 0x11: sh_audio->sample_format=AFMT_IMA_ADPCM;break;
     case 0x50: sh_audio->sample_format=AFMT_MPEG;break;
-    case 0x736F7774: sh_audio->sample_format=AFMT_S16_LE;sh_audio->codec->driver=AFM_DVDPCM;break;
+    //  format 0x736f7774  ; "twos" (MOV files)
+    case 0x736F7774: sh_audio->sample_format=AFMT_S16_LE;/*sh_audio->codec->driver=AFM_DVDPCM;*/ break;
 /*    case 0x2000: sh_audio->sample_format=AFMT_AC3; */
     default: sh_audio->sample_format=(sh_audio->samplesize==2)?AFMT_S16_LE:AFMT_U8;
   }
@@ -62,5 +63,15 @@ static int control(sh_audio_t *sh,int cmd,void* arg, ...)
 
 static int decode_audio(sh_audio_t *sh_audio,unsigned char *buf,int minlen,int maxlen)
 {
+  if(sh_audio->format==0x736F7774){ // "twos" is swapped byteorder
+    int j,len;
+    len=demux_read_data(sh_audio->ds,buf,(minlen+1)&(~1));
+    for(j=0;j<len;j+=2){
+      char x=buf[j];
+      buf[j]=buf[j+1];
+      buf[j+1]=x;
+    }
+    return len;
+  }
   return demux_read_data(sh_audio->ds,buf,minlen);
 }
