f16ffbbb3f1947ffca1874bb746dec188b213cc9
mswitch <mswitch@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-04-03 10:05:35 +0000

    Subpic encoder added (used on DXR3 and similar hardware that has a subpic channel)
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@5468 b3059339-0415-0410-9bf9-f77b7e298cf2

---
+++ libvo/spuenc.h
@@ -0,0 +1,40 @@
+/*
+ * subpic_encode.c - encodes a pixmap with RLE
+ *
+ * Copyright (C) 2000   Alejandro J. Cura <alecu@protocultura.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <stdlib.h>
+#define DATASIZE 53220
+
+
+typedef struct {
+	int x, y;
+	u_int rgb[4];
+	u_char* pixels;
+} pixbuf;
+
+typedef struct {
+	u_char data[DATASIZE];
+	int count;	/* the count of bytes written */
+	int oddstart;
+	int nibblewaiting;
+} encodedata;
+
+void pixbuf_encode_rle(int x, int y, int w, int h, char *inbuf, int stride, encodedata *ed);
+void pixbuf_delete(pixbuf* pb);
