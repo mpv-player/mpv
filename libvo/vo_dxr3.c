af5d80aedc1386d922d25afec6d079f6b8004d0f
michael <michael@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-04-09 23:55:47 +0000

    trying to fix davids "libavcodec bug" ...
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@5543 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vo_dxr3.c
+++ libvo/vo_dxr3.c
@@ -408,7 +408,7 @@ static uint32_t config(uint32_t width, uint32_t height, uint32_t d_width, uint32
 			}
 			avc_context->frame_rate = (int) (vo_fps * FRAME_RATE_BASE);
 			avc_context->bit_rate = 6e6;
-			avc_context->flags = CODEC_FLAG_HQ | CODEC_FLAG_QSCALE;
+			avc_context->flags = CODEC_FLAG_QSCALE;
 			avc_context->quality = 2;
 			avc_context->pix_fmt = PIX_FMT_YUV420P;
 			if (avcodec_open(avc_context, avc_codec) < 0) {
