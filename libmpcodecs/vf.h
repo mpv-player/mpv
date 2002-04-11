804c9adfe322c9bc75a5790d8245bdc6fd7129e7
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-04-11 15:41:40 +0000

    VFCAP flags added
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@5563 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/vf.h
+++ libmpcodecs/vf.h
@@ -47,6 +47,30 @@ typedef struct vf_instance_s {
 #define VFCTRL_SET_PP_LEVEL 5 /* set postprocessing level */
 #define VFCTRL_SET_EQUALIZER 6 /* set color options (brightness,contrast etc) */
 
+// VFCAP_* values: they are flags, returned by query_format():
+
+// set, if the given colorspace is supported (with or without conversion)
+#define VFCAP_CSP_SUPPORTED 0x1
+// set, if the given colorspace is supported _without_ conversion
+#define VFCAP_CSP_SUPPORTED_BY_HW 0x2
+// set if the driver/filter can draw OSD
+#define VFCAP_OSD 0x4
+// set if the driver/filter can handle compressed SPU stream
+#define VFCAP_SPU 0x8
+// scaling up/down by hardware, or software:
+#define VFCAP_HWSCALE_UP 0x10
+#define VFCAP_HWSCALE_DOWN 0x20
+#define VFCAP_SWSCALE 0x40
+// driver/filter can do vertical flip (upside-down)
+#define VFCAP_FLIP 0x80
+
+// driver/hardware handles timing (blocking)
+#define VFCAP_TIMER 0x100
+// driver _always_ flip image upside-down (for ve_vfw)
+#define VFCAP_FLIPPED 0x200
+// driver accept stride: (put_image/draw_frame)
+#define VFCAP_ACCEPT_STRIDE 0x400
+
 // functions:
 mp_image_t* vf_get_image(vf_instance_t* vf, unsigned int outfmt, int mp_imgtype, int mp_imgflag, int w, int h);
 
