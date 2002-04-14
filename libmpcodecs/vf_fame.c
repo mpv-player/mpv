d414a3f2f7e6060a03c3f458c8658012f2295d21
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-04-14 02:07:44 +0000

    fame doesn't accept stride
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@5616 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/vf_fame.c
+++ libmpcodecs/vf_fame.c
@@ -81,7 +81,7 @@ static int query_format(struct vf_instance_s* vf, unsigned int fmt){
     case IMGFMT_YV12:
     case IMGFMT_I420:
     case IMGFMT_IYUV:
-	return (vf_next_query_format(vf,fmt) & (~VFCAP_CSP_SUPPORTED_BY_HW));
+	return (vf_next_query_format(vf,fmt) & (~(VFCAP_CSP_SUPPORTED_BY_HW|VFCAP_ACCEPT_STRIDE)));
     }
     return 0;
 }
