0b657de32c1aa7c1b9fd598224f97a48a247ebc0
pontscho <pontscho@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-04-16 11:40:19 +0000

    upsz, 10l :)
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@5650 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vo_jpeg.c
+++ libvo/vo_jpeg.c
@@ -3,7 +3,7 @@
 /* 
  * vo_jpeg.c, JPEG Renderer for Mplayer
  *
- * Copyright 2001 by Pontscho (pontscho@makacs.poliod.hu)
+ * Copyright 2002 by Pontscho (pontscho@makacs.poliod.hu)
  *
  */
 
@@ -46,7 +46,7 @@ int jpeg_progressive_mode = 0;
 int jpeg_optimize = 100;
 int jpeg_smooth = 0;
 int jpeg_quality = 75;
-char * jpeg_outdir;
+char * jpeg_outdir = '.';
 
 #define bpp 24
 
