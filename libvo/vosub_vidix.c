27a59e4c70a5303cea105dd885219c3ace959f0d
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-04-24 20:00:13 +0000

    libdir changed
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@5815 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vosub_vidix.c
+++ libvo/vosub_vidix.c
@@ -694,7 +694,7 @@ int vidix_preinit(const char *drvname,void *server)
 	  printf("vosub_vidix: You have wrong version of VIDIX library\n");
 	  return -1;
 	}
-	vidix_handler = vdlOpen(LIBDIR"/vidix/",
+	vidix_handler = vdlOpen(LIBDIR"/mplayer/vidix/",
 				drvname ? drvname[0] == ':' ? &drvname[1] : drvname[0] ? drvname : NULL : NULL,
 				TYPE_OUTPUT,
 				verbose);
