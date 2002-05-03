74c7dd3d84878a12eb12ca2eacc42ea347bc1330
atmos4 <atmos4@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-05-03 19:17:19 +0000

    Fix for darwins date
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@5950 b3059339-0415-0410-9bf9-f77b7e298cf2

--- version.sh
+++ version.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 last_cvs_update=`date -r CVS/Entries +%y%m%d-%H:%M 2>/dev/null`
-if [ $? -ne 0 ]; then
-	# probably no gnu date installed(?), use current date
-	last_cvs_update=`date +%y%m%d-%H:%M`
+if test $? -ne 0 ; then
+        # probably no gnu date installed(?), use current date
+        last_cvs_update=`date +%y%m%d-%H:%M`
+elif test `uname -s` = 'Darwin' ; then
+        # darwin's date has different meaning for -r
+        last_cvs_update=`date +%y%m%d-%H:%M`
 fi
 
 echo "#define VERSION \"CVS-${last_cvs_update}-$1 \"" >version.h
