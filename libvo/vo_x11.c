f8e68b858f36e580cf78374a5bc369431abb1f29
atmos4 <atmos4@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-07-10 04:58:51 +0000

    Markus Rechberger <revenger at shell.dnload.com>: XMapWindow (vo_x11.c didn't map the Window after using -wid 0x....)
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@6694 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vo_x11.c
+++ libvo/vo_x11.c
@@ -334,6 +334,7 @@ static uint32_t config( uint32_t width,uint32_t height,uint32_t d_width,uint32_t
         XUnmapWindow( mDisplay,vo_window );
         XChangeWindowAttributes( mDisplay,vo_window,xswamask,&xswa );
 	XSelectInput( mDisplay,vo_window,StructureNotifyMask | KeyPressMask | PropertyChangeMask | PointerMotionMask | ButtonPressMask | ButtonReleaseMask | ExposureMask );
+	XMapWindow( mDisplay,vo_window );
        } else XSelectInput( mDisplay,vo_window,ExposureMask );
     }
     else
