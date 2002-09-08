ccffaee52bc3eefae58fffc770bd2a5ec93f069b
alex <alex@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-09-08 16:05:59 +0000

    fixed get_format/set_format
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@7320 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/tvi_dummy.c
+++ libmpdemux/tvi_dummy.c
@@ -58,11 +58,13 @@ static int control(priv_t *priv, int cmd, void *arg)
 	case TVI_CONTROL_IS_VIDEO:
 	    return(TVI_CONTROL_TRUE);
 	case TVI_CONTROL_VID_GET_FORMAT:
-	    (int)*(void **)arg = IMGFMT_YV12;
+//	    (int)*(void **)arg = IMGFMT_YV12;
+	    *(int *)arg = IMGFMT_YV12;
 	    return(TVI_CONTROL_TRUE);
 	case TVI_CONTROL_VID_SET_FORMAT:
 	{
-	    int req_fmt = (int)*(void **)arg;
+//	    int req_fmt = (int)*(void **)arg;
+	    int req_fmt = *(int *)arg;
 	    if (req_fmt != IMGFMT_YV12)
 		return(TVI_CONTROL_FALSE);
 	    return(TVI_CONTROL_TRUE);
