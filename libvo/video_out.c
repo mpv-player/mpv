c5ed6cf6000bc92d01a2475aef7eb29d893442b6
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-09-09 20:03:21 +0000

    cosmetics - copyright msg removed, this file has nothing with mpeg2dec or original Aaron code, except its filename :)
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@7339 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/video_out.c
+++ libvo/video_out.c
@@ -1,23 +1,6 @@
-/*
- * video_out.c,
- *
- * Copyright (C) Aaron Holtzman - June 2000
- *
- *  mpeg2dec is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2, or (at your option)
- *  any later version.
- *
- *  mpeg2dec is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with GNU Make; see the file COPYING.  If not, write to
- *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- */
+
+/* this file contains libvo's common functions, variables used by 
+   many/all drivers. */
 
 #include <stdio.h>
 #include <stdlib.h>
