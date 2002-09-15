bb8a55c4f8b1c468b033b6838242966a0f8666f6
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-09-15 21:23:59 +0000

    standard GSM support for .mov files
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@7411 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/ad_msgsm.c
+++ libmpcodecs/ad_msgsm.c
@@ -7,7 +7,7 @@
 
 static ad_info_t info = 
 {
-	"native MSGSM audio decoder",
+	"native GSM/MSGSM audio decoder",
 	"msgsm",
 	"A'rpi",
 	"XAnim",
@@ -28,7 +28,12 @@ static int init(sh_audio_t *sh_audio)
   // decodes 65 byte -> 320 short
   // 1 sec: sh_audio->channels*sh_audio->samplerate  samples
   // 1 frame: 320 samples
-  sh_audio->i_bps=65*(sh_audio->channels*sh_audio->samplerate)/320;  // 1:10
+  if(sh_audio->format==0x31 || sh_audio->format==0x32){
+    sh_audio->ds->ss_mul=65; sh_audio->ds->ss_div=320;
+  } else {
+    sh_audio->ds->ss_mul=33; sh_audio->ds->ss_div=160;
+  }
+  sh_audio->i_bps=sh_audio->ds->ss_mul*(sh_audio->channels*sh_audio->samplerate)/sh_audio->ds->ss_div;  // 1:10
   return 1;
 }
 
@@ -49,8 +54,15 @@ static int control(sh_audio_t *sh,int cmd,void* arg, ...)
 
 static int decode_audio(sh_audio_t *sh_audio,unsigned char *buf,int minlen,int maxlen)
 {
+  if(sh_audio->format==0x31 || sh_audio->format==0x32){
     unsigned char ibuf[65]; // 65 bytes / frame
     if(demux_read_data(sh_audio->ds,ibuf,65)!=65) return -1; // EOF
     XA_MSGSM_Decoder(ibuf,(unsigned short *) buf); // decodes 65 byte -> 320 short
     return 2*320;
+  } else {
+    unsigned char ibuf[33]; // 33 bytes / frame
+    if(demux_read_data(sh_audio->ds,ibuf,33)!=33) return -1; // EOF
+    XA_GSM_Decoder(ibuf,(unsigned short *) buf); // decodes 33 byte -> 160 short
+    return 2*160;
+  }
 }
