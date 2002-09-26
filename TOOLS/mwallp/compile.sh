944d42db69937b4196cfd365e013f62af366bc4d
gabucino <gabucino@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-09-26 20:37:54 +0000

    fixing compilation on LFS plus a note, it can't be compiled with --enable-gui (undef references)
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@7512 b3059339-0415-0410-9bf9-f77b7e298cf2

--- TOOLS/mwallp/compile.sh
+++ TOOLS/mwallp/compile.sh
@@ -1,5 +1,6 @@
-
+echo NOTE: do not compile with --enable-gui
+echo
 gcc -g mwallp.c jpeg.c ../../libvo/aclib.o ../../libmpcodecs/img_format.o \
     ../../cpudetect.o ../../mp_msg.o ../../postproc/libpostproc.a \
     -I../../libmpcodecs -I../.. -I../../postproc \
-    -L/usr/X11/lib -lX11 -ljpeg -o mwallp
+    -L/usr/X11/lib -lX11 -lm -ljpeg -o mwallp
