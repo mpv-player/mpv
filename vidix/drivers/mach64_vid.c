8c28841ed170adade5b4cab1069b98e16ab0e7dd
michael <michael@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-10-22 11:52:21 +0000

    RGB16 big endian fix patch by (Colin Leroy <colin at colino dot net>)
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@7837 b3059339-0415-0410-9bf9-f77b7e298cf2

--- vidix/drivers/mach64_vid.c
+++ vidix/drivers/mach64_vid.c
@@ -1002,6 +1002,7 @@ int vixSetGrKeys(const vidix_grkey_t *grkey)
 			  ((mach64_grkey.ckey.blue &0xF8)>>3)
 			| ((mach64_grkey.ckey.green&0xFC)<<3)
 			| ((mach64_grkey.ckey.red  &0xF8)<<8);
+		besr.graphics_key_clr=le2me_32(besr.graphics_key_clr);
 		break;
 	case 24:
 		besr.graphics_key_msk=0xFFFFFF;
