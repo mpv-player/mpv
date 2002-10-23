f40b7e91b83395ad27184af90280e378f4e7840a
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-10-23 23:15:25 +0000

    fixed unaligned access (bus error on sparc)
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@7889 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/ad_msadpcm.c
+++ libmpcodecs/ad_msadpcm.c
@@ -44,8 +44,9 @@ static int ms_adapt_coeff2[] =
 
 #define MS_ADPCM_PREAMBLE_SIZE 6
 
-#define LE_16(x) (le2me_16(*(unsigned short *)(x)))
-#define LE_32(x) (le2me_32(*(unsigned int *)(x)))
+#define LE_16(x) (le2me_16((x)[1]+(256*(x)[0])))
+//#define LE_16(x) (le2me_16(*(unsigned short *)(x)))
+//#define LE_32(x) (le2me_32(*(unsigned int *)(x)))
 
 // useful macros
 // clamp a number between 0 and 88
