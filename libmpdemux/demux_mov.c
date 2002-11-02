9adea82e2ad6ccd1b51655eca4d7da289b2e0f70
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-11-02 22:39:02 +0000

    fix possible sig8 - found by Alex
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@8062 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/demux_mov.c
+++ libmpdemux/demux_mov.c
@@ -865,7 +865,7 @@ static void lschunks(demuxer_t* demuxer,int level,off_t endpos,mov_track_t* trak
 		sh->wf->wBitsPerSample=(trak->stdata[18]<<8)+trak->stdata[19];
 		// sh->wf->nSamplesPerSec=trak->timescale;
 		sh->wf->nSamplesPerSec=(trak->stdata[24]<<8)+trak->stdata[25];
-		if(trak->stdata_len >= 44 && trak->stdata[9]>=1){
+		if(trak->stdata_len >= 44 && trak->stdata[9]>=1 && char2int(trak->stdata,28)>0){
 		//Audio header: samp/pack=4096 bytes/pack=743 bytes/frame=1486 bytes/samp=2
 		  sh->wf->nAvgBytesPerSec=(sh->wf->nChannels*sh->wf->nSamplesPerSec*
 		      char2int(trak->stdata,32)+char2int(trak->stdata,28)/2)
