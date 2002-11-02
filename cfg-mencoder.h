7794467ba5618306a061aefb5c8af8b1b15cb73f
rfelker <rfelker@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-11-02 03:51:05 +0000

    added obsolete message for -pass
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@8034 b3059339-0415-0410-9bf9-f77b7e298cf2

--- cfg-mencoder.h
+++ cfg-mencoder.h
@@ -150,6 +150,7 @@ static config_t mencoder_opts[]={
 	// override FOURCC in output file
 	{"ffourcc", &force_fourcc, CONF_TYPE_STRING, 0, 4, 4, NULL},
 
+	{"pass", "The -pass option is obsolete. Use -lavcopts vpass=n or -divx4opts pass=n!\nRTFM!\n", CONF_TYPE_PRINT, CONF_NOCFG, 0, 0, NULL},
 	{"passlogfile", &passtmpfile, CONF_TYPE_STRING, 0, 0, 0, NULL},
 	
 	{"vobsubout", &vobsub_out, CONF_TYPE_STRING, 0, 0, 0, NULL},
