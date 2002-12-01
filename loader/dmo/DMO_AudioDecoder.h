ba86dd3f1f8a5f4f36fdb280e813e21b090dde2e
arpi <arpi@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-12-01 16:38:56 +0000

    DMO Audio support (ugly, needs cleanup)
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@8326 b3059339-0415-0410-9bf9-f77b7e298cf2

---
+++ loader/dmo/DMO_AudioDecoder.h
@@ -0,0 +1,17 @@
+#ifndef AVIFILE_DMO_AUDIODECODER_H
+#define AVIFILE_DMO_AUDIODECODER_H
+
+typedef struct _DMO_AudioDecoder DMO_AudioDecoder;
+
+//DMO_AudioDecoder * DMO_AudioDecoder_Create(const CodecInfo * info, const WAVEFORMATEX* wf);
+DMO_AudioDecoder * DMO_AudioDecoder_Open(char* dllname, GUID* guid, WAVEFORMATEX* wf,int out_channels);
+
+void DMO_AudioDecoder_Destroy(DMO_AudioDecoder *this);
+
+int DMO_AudioDecoder_Convert(DMO_AudioDecoder *this, const void* in_data, unsigned int in_size,
+			     void* out_data, unsigned int out_size,
+			     unsigned int* size_read, unsigned int* size_written);
+
+int DMO_AudioDecoder_GetSrcSize(DMO_AudioDecoder *this, int dest_size);
+
+#endif // AVIFILE_DMO_AUDIODECODER_H
