dc871c209f9343ec864040440e5cf02dd82da8ff
gabucino <gabucino@b3059339-0415-0410-9bf9-f77b7e298cf2>
2002-12-03 22:32:38 +0000

    license fixed
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@8339 b3059339-0415-0410-9bf9-f77b7e298cf2

--- TOOLS/mwallp/compile.sh
+++ TOOLS/mwallp/compile.sh
@@ -3,4 +3,4 @@ echo
 gcc -g mwallp.c jpeg.c ../../libvo/aclib.o ../../libmpcodecs/img_format.o \
     ../../cpudetect.o ../../mp_msg.o ../../postproc/libpostproc.a \
     -I../../libmpcodecs -I../.. -I../../postproc \
-    -L/usr/X11/lib -lX11 -lm -ljpeg -o mwallp
+    -L/usr/X11/lib -L/usr/X11R6/lib -lX11 -lm -ljpeg -o mwallp
