fd5f02e1ff7792ffd50e6abec69cec38afb413b1
rfelker <rfelker@b3059339-0415-0410-9bf9-f77b7e298cf2>
2003-01-03 18:01:49 +0000

    10l, geometry always returns 1. i think this is the correct fix.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@8744 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vo_xv.c
+++ libvo/vo_xv.c
@@ -281,10 +281,8 @@ static uint32_t config(uint32_t width, uint32_t height, uint32_t d_width, uint32
  
  vo_mouse_autohide=1;
 
- // Quick fix for handling the geometry option.
- if(!geometry(&vo_dx, &vo_dy, vo_screenwidth, vo_screenheight, d_width, d_height,0)) {
-    vo_dx=( vo_screenwidth - d_width ) / 2; vo_dy=( vo_screenheight - d_height ) / 2;
- }
+ vo_dx=( vo_screenwidth - d_width ) / 2; vo_dy=( vo_screenheight - d_height ) / 2;
+ geometry(&vo_dx, &vo_dy, vo_screenwidth, vo_screenheight, d_width, d_height,0))
  vo_dwidth=d_width; vo_dheight=d_height;
      
 #ifdef HAVE_XF86VM
