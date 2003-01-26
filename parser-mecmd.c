5048def3c4889070b25e2a4a867f0abecd031611
colin <colin@b3059339-0415-0410-9bf9-f77b7e298cf2>
2003-01-26 12:02:38 +0000

    fix a typo
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@9097 b3059339-0415-0410-9bf9-f77b7e298cf2

--- parser-mecmd.c
+++ parser-mecmd.c
@@ -101,7 +101,7 @@ m_config_parse_me_command_line(m_config_t *config, int argc, char **argv)
 	mp_opt = m_config_get_option(config,opt);
 	if(!mp_opt) {
 	  tmp = M_OPT_UNKNOW;
-	  mp_msg(MSGT_CFGPARSER, MSGL_ERR, "%s in not an MEncoder option\n",opt);
+	  mp_msg(MSGT_CFGPARSER, MSGL_ERR, "%s is not an MEncoder option\n",opt);
 	  goto err_out;
 	}
 	// Hack for the -vcd ... options
