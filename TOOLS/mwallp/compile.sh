4923b022eb47c0a587e9cb57fd4ab9742d3945e4
gabucino <gabucino@b3059339-0415-0410-9bf9-f77b7e298cf2>
2003-02-17 11:45:10 +0000

    mwallp compiles again (michael moved libpostproc.....)
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@9451 b3059339-0415-0410-9bf9-f77b7e298cf2

--- TOOLS/mwallp/compile.sh
+++ TOOLS/mwallp/compile.sh
@@ -1,6 +1,6 @@
 echo
 gcc -g mwallp.c jpeg.c ../../libvo/aclib.o ../../libmpcodecs/img_format.o \
-    ../../cpudetect.o ../../mp_msg-mencoder.o ../../postproc/libpostproc.a \
+    ../../cpudetect.o ../../mp_msg-mencoder.o ../../libavcodec/libavcodec.a \
     ../../postproc/libswscale.a \
     -I../../libmpcodecs -I../.. -I../../postproc \
     -L/usr/X11/lib -L/usr/X11R6/lib -lX11 -lm -ljpeg -o mwallp
