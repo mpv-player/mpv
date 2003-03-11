c9dd54daf92bb9a9aa71e832840cb1e1f38e6f41
rsf <rsf@b3059339-0415-0410-9bf9-f77b7e298cf2>
2003-03-11 19:03:31 +0000

    Made "force_fps" non-static, because code in "libmpdemux" refers to it.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@9565 b3059339-0415-0410-9bf9-f77b7e298cf2

--- mencoder.c
+++ mencoder.c
@@ -135,7 +135,7 @@ static float audio_preload=0.5;
 static float audio_delay=0.0;
 static int audio_density=2;
 
-static float force_fps=0;
+float force_fps=0;
 static float force_ofps=0; // set to 24 for inverse telecine
 static int skip_limit=-1;
 
