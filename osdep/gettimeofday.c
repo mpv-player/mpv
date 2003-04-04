a8dd582c4570ad649c237a9cdbf44ee12ce32ec7
faust3 <faust3@b3059339-0415-0410-9bf9-f77b7e298cf2>
2003-04-04 19:27:46 +0000

    gettimeofday() emulation using ftime()
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@9830 b3059339-0415-0410-9bf9-f77b7e298cf2

---
+++ osdep/gettimeofday.c
@@ -0,0 +1,11 @@
+
+#ifndef HAVE_GETTIMEOFDAY
+#include <sys/time.h>
+#include <sys/timeb.h>
+void gettimeofday(struct timeval* t,void* timezone)
+{       struct timeb timebuffer;
+        ftime( &timebuffer );
+        t->tv_sec=timebuffer.time;
+        t->tv_usec=1000*timebuffer.millitm;
+}
+#endif
