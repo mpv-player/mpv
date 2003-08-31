8a7b7a430dac11b1295ccad96e97ae059e3de2ce
attila <attila@b3059339-0415-0410-9bf9-f77b7e298cf2>
2003-08-31 12:33:49 +0000

    geometry support for x11
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@10729 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vo_x11.c
+++ libvo/vo_x11.c
@@ -245,6 +245,7 @@ static uint32_t config( uint32_t width,uint32_t height,uint32_t d_width,uint32_t
  srcW= width;
  srcH= height;
  vo_dx=( vo_screenwidth - d_width ) / 2; vo_dy=( vo_screenheight - d_height ) / 2;
+ geometry(&vo_dx, &vo_dy, &d_width, &d_height, vo_screenwidth, vo_screenheight);
  vo_dwidth=d_width; vo_dheight=d_height;
  
  if( flags&0x03 ) fullscreen = 1;
