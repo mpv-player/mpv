a9faa876948ae08f263ca6eb01cc511735c2677d
faust3 <faust3@b3059339-0415-0410-9bf9-f77b7e298cf2>
2003-09-21 15:44:23 +0000

    fix fullscreen switching in vo gl and vo gl2
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@10920 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vo_gl.c
+++ libvo/vo_gl.c
@@ -82,6 +82,8 @@ config(uint32_t width, uint32_t height, uint32_t d_width, uint32_t d_height, uin
 
 	image_height = height;
 	image_width = width;
+    vo_dwidth = d_width;
+    vo_dheight = d_height;
 
 	int_pause = 0;
 
