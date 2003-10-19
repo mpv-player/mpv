c6b7917d714172710568119edc03a909e087f54c
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2003-10-19 21:16:22 +0000

    wrong path
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@11185 b3059339-0415-0410-9bf9-f77b7e298cf2

--- help/help_mp-zh_TW.h
+++ help/help_mp-zh_TW.h
@@ -78,7 +78,7 @@ static char help_text[]=
 #define MSGTR_TryForceAudioFmtStr "正嘗試強行指定音效解碼驅動程式組群 %s...\n"
 #define MSGTR_CantFindAfmtFallback "無法找到指定之驅動程式組群的音效解碼器，回復到其他驅動程式。\n"
 #define MSGTR_CantFindAudioCodec "無法為音效格式 0x%X 找到解碼器。\n"
-#define MSGTR_RTFMCodecs "參閱DOCS/en/codecs.html﹗\n"
+#define MSGTR_RTFMCodecs "參閱DOCS/zh/codecs.html﹗\n"
 #define MSGTR_CouldntInitAudioCodec "無法為音效解碼器初始化 -> 沒有聲音。\n"
 #define MSGTR_TryForceVideoFmtStr "正嘗試強行指定視訊解碼驅動程式組群 %s...\n"
 #define MSGTR_CantFindVideoCodec "無法為所選擇的 -vo 與視訊格式 0x%X 找到適合的解碼器。\n"
