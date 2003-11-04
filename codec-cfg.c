96dfb6e86021b3f734d0c132000a92c322f9ba89
rik <rik@b3059339-0415-0410-9bf9-f77b7e298cf2>
2003-11-04 20:02:09 +0000

    updates for new image formats for zoran mjpeg passthrough
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@11389 b3059339-0415-0410-9bf9-f77b7e298cf2

--- codec-cfg.c
+++ codec-cfg.c
@@ -160,6 +160,9 @@ static int add_to_format(char *s, char *alias,unsigned int *fourcc, unsigned int
 		{"BGR1",  IMGFMT_BGR|1},
 
 		{"MPES",  IMGFMT_MPEGPES},
+		{"ZRMJPEGNI", IMGFMT_ZRMJPEGNI},
+		{"ZRMJPEGIT", IMGFMT_ZRMJPEGIT},
+		{"ZRMJPEGIB", IMGFMT_ZRMJPEGIB},
 
 		{"IDCT_MPEG2",IMGFMT_XVMC_IDCT_MPEG2},
 		{"MOCO_MPEG2",IMGFMT_XVMC_MOCO_MPEG2},
@@ -289,6 +292,7 @@ static short get_driver(char *s,int audioflag)
 		"ijpg",
 		"zlib",
 		"mpegpes",
+		"zrmjpeg",
 		"realvid",
 		"xvid",
 		"libdv",
