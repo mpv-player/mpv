ee3708a55150af9e795abad3e535372d7b04f4e2
alex <alex@b3059339-0415-0410-9bf9-f77b7e298cf2>
2004-01-28 08:02:40 +0000

    use inttypes.h and std types, this fixes compilation problems on solaris 9 x86
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@11865 b3059339-0415-0410-9bf9-f77b7e298cf2

--- loader/wrapper.h
+++ loader/wrapper.h
@@ -1,13 +1,13 @@
 #ifndef _WRAPPER_H
 #define _WRAPPER_H
 
-#include <sys/types.h>
+#include <inttypes.h>
 
 typedef struct {
-  u_int32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+  uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
 } reg386_t;
 
-typedef int (*wrapper_func_t)(void *stack_base, int stack_size, reg386_t *reg,  u_int32_t *flags);
+typedef int (*wrapper_func_t)(void *stack_base, int stack_size, reg386_t *reg,  uint32_t *flags);
 
 extern wrapper_func_t report_entry, report_ret;
 
