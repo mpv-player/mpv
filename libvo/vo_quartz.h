8b55d438c1b53b737af5d485e6068fb96594a0f3
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2004-04-27 14:15:17 +0000

    Copyright notice added back.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@12316 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vo_quartz.h
+++ libvo/vo_quartz.h
@@ -1,6 +1,29 @@
 /* 
  * vo_quartz.h
  * Mac keyboard def taken from SDL
+ * See the CVS log for a list of changes.
+ */
+
+/*
+    SDL - Simple DirectMedia Layer
+    Copyright (C) 1997, 1998, 1999, 2000, 2001, 2002  Sam Lantinga
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Sam Lantinga
+    slouken@libsdl.org
  */
 
 #ifndef VO_QUARTZ_H
