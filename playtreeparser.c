30ccc5dd1ea350eb1c7f862f49d3d2559a538f0f
rtognimp <rtognimp@b3059339-0415-0410-9bf9-f77b7e298cf2>
2004-06-06 19:06:39 +0000

    Enhance detection of embedded smil playlist, add embedded ram playlist support Patch by adland
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@12539 b3059339-0415-0410-9bf9-f77b7e298cf2

--- playtreeparser.c
+++ playtreeparser.c
@@ -558,13 +558,18 @@ parse_textplain(play_tree_parser_t* p) {
     if(line[0] == '\0')
       continue;
 
-    //Special check for smil reference in file
+    //Special check for embedded smil or ram reference in file
     embedded = 0;
     if (strlen(line) > 5)
       for(c = line; c[0]; c++ )
-        if((c[0] == '.') && (tolower(c[1]) == 's') && (tolower(c[2])== 'm') && 
+        if ( ((c[0] == '.') && //start with . and next have smil with optional ? or &
+           (tolower(c[1]) == 's') && (tolower(c[2])== 'm') && 
            (tolower(c[3]) == 'i') && (tolower(c[4]) == 'l') &&
-           (!c[5] || c[5] == '?' || c[5] == '&')) {
+           (!c[5] || c[5] == '?' || c[5] == '&')) || // or
+          ((c[0] == '.') && // start with . and next have smi or ram with optional ? or &
+          ( ((tolower(c[1]) == 's') && (tolower(c[2])== 'm') && (tolower(c[3]) == 'i')) || 
+            ((tolower(c[1]) == 'r') && (tolower(c[2])== 'a') && (tolower(c[3]) == 'm')) )
+           && (!c[4] || c[4] == '?' || c[4] == '&')) ){
           entry=embedded_playlist_parse(line);
           embedded = 1;
           break;
