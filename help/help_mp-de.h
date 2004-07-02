6f6ab24d4466af2fa89fd8ea96efb22fad11463a
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2004-07-02 01:40:43 +0000

    better wording by Sebastian Krämer
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@12746 b3059339-0415-0410-9bf9-f77b7e298cf2

--- help/help_mp-de.h
+++ help/help_mp-de.h
@@ -114,7 +114,7 @@ static char help_text[]=
 
 #define MSGTR_NoGui "MPlayer wurde OHNE GUI-Unterstützung kompiliert.\n"
 #define MSGTR_GuiNeedsX "MPlayer GUI erfordert X11.\n"
-#define MSGTR_Playing "Spiele %s\n"
+#define MSGTR_Playing "Wiedergabe %s\n"
 #define MSGTR_NoSound "Audio: kein Ton!\n"
 #define MSGTR_FPSforced "FPS von %5.3f erzwungen (ftime: %5.3f)\n"
 #define MSGTR_CompiledWithRuntimeDetection "MPlayer mit CPU-Erkennung zur Laufzeit kompiliert - WARNUNG, das ist nicht optimal!\nKompiliere MPlayer mit --disable-runtime-cpudetection für beste Performance.\n"
