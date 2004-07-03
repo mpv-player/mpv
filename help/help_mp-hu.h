561612e85db670a05a3af52765282660428b6ba6
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2004-07-03 02:19:14 +0000

    Point at new XML documentation translation.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@12755 b3059339-0415-0410-9bf9-f77b7e298cf2

--- help/help_mp-hu.h
+++ help/help_mp-hu.h
@@ -103,8 +103,8 @@ static char help_text[]=
 "  - Próbáld ki a -cache 8192 opciót.\n"\
 "- Talán egy non-interleaved AVI filet próbálsz -cache opcióval lejátszani?\n"\
 "  - Használd a -nocache opciót.\n"\
-"Tuninghoz tippeket a DOCS/hu/video.html és DOCS/hu/sound.html file-ok tartalmaznak.\n"\
-"Ha ezek után sem tetszik az eredmény, olvasd el a DOCS/hu/bugreports.html file-t.\n\n"
+"Tuninghoz tippeket a DOCS/HTML/hu/video.html és DOCS/HTML/hu/sound.html file-ok tartalmaznak.\n"\
+"Ha ezek után sem tetszik az eredmény, olvasd el a DOCS/HTML/hu/bugreports.html file-t.\n\n"
 
 #define MSGTR_NoGui "Az MPlayer grafikus felület NÉLKÜL lett fordítva!\n"
 #define MSGTR_GuiNeedsX "Az MPlayer grafikus felületének X11-re van szüksége!\n"
@@ -137,7 +137,7 @@ static char help_text[]=
 #define MSGTR_Exit_SIGILL_RTCpuSel \
 "- Az MPlayer egy 'illegális utasítást' hajtott végre.\n"\
 "  Lehet hogy a futásidejû CPU detektáló kód bugja...\n"\
-"  Olvasd el a DOCS/hu/bugreports.html file-t.\n"
+"  Olvasd el a DOCS/HTML/hu/bugreports.html file-t.\n"
 #define MSGTR_Exit_SIGILL \
 "- Az MPlayer egy 'illegális utasítást' hajtott végre.\n"\
 "  Ez akkor történik amikor más CPU-n futtatod az MPlayer-t mint amire a\n"\
@@ -145,12 +145,12 @@ static char help_text[]=
 #define MSGTR_Exit_SIGSEGV_SIGFPE \
 "- Az MPlayer röpke félrelépése miatt hiba lépett fel a CPU/FPU/RAM-ban.\n"\
 "  Fordítsd újra az MPlayer-t az --enable-debug opcióval, és készíts egy\n"\
-"  'gdb' backtrace-t. A részletek a DOCS/hu/bugreports.html file-ban találhatóak.\n"
+"  'gdb' backtrace-t. A részletek a DOCS/HTML/hu/bugreports.html file-ban találhatóak.\n"
 #define MSGTR_Exit_SIGCRASH \
 "- Az MPlayer elcrashelt. Ennek nem lenne szabad megtörténnie.\n"\
 "  Az ok lehet egy hiba az MPlayer kódjában _vagy_ a Te meghajtóidban, _vagy_\n"\
 "  a gcc-ben. Ha úgy véled hogy ez egy MPlayer hiba, úgy olvasd el a\n"\
-"  DOCS/hu/bugreports.html file-t. Amig ezt figyelmesen meg nem teszed, nem tudunk\n"\
+"  DOCS/HTML/hu/bugreports.html file-t. Amig ezt figyelmesen meg nem teszed, nem tudunk\n"\
 "  segíteni.\n"
 
 
