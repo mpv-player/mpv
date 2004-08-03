9bb8541622921fcbb1e9081e302a15de63cdd7bf
faust3 <faust3@b3059339-0415-0410-9bf9-f77b7e298cf2>
2004-08-03 18:54:26 +0000

    avoid outoptimization of static variables patch by ismail dönmez <ismail.donmez at gmail.com>
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@12946 b3059339-0415-0410-9bf9-f77b7e298cf2

--- mp3lib/decode_i586.c
+++ mp3lib/decode_i586.c
@@ -27,9 +27,9 @@
 #include "../mangle.h"
 #define real float /* ugly - but only way */
 
-static long buffs[1088]={0};
+static long attribute_used buffs[1088]={0};
 static long attribute_used bo=1;
-static long saved_ebp=0;
+static long attribute_used saved_ebp=0;
 
 int synth_1to1_pent(real *bandPtr, int channel, short *samples)
 {
