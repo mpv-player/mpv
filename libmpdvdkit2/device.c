77e529d9f7499b54a9ce236fe633e29e35a12b76
joey <joey@b3059339-0415-0410-9bf9-f77b7e298cf2>
2004-08-12 22:37:08 +0000

    mpdvdkit now accepts X:\ as a device name, as well as X:
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@13012 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdvdkit2/device.c
+++ libmpdvdkit2/device.c
@@ -134,8 +134,11 @@ int _dvdcss_open ( dvdcss_t dvdcss )
     _dvdcss_debug( dvdcss, psz_debug );
 
 #if defined( WIN32 )
-    /* If device is not "X:", we are actually opening a file. */
-    dvdcss->b_file = !psz_device[0] || psz_device[1] != ':' || psz_device[2];
+    dvdcss->b_file = 1;
+    /* If device is "X:" or "X:\", we are not actually opening a file. */
+    if (psz_device[0] && psz_device[1] == ':' &&
+       (!psz_device[2] || (psz_device[2] == '\\' && !psz_device[3])))
+        dvdcss->b_file = 0;
 
     /* Initialize readv temporary buffer */
     dvdcss->p_readv_buffer   = NULL;
