0cd7b2f4df37d2e3bca688a5868c98b93853f8db
alex <alex@b3059339-0415-0410-9bf9-f77b7e298cf2>
2004-08-21 19:31:00 +0000

    support snow
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@13075 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/ve_lavc.c
+++ libmpcodecs/ve_lavc.c
@@ -971,6 +971,8 @@ static int vf_open(vf_instance_t *vf, char* args){
 	mux_v->bih->biCompression = mmioFOURCC('A', 'S', 'V', '2');
     else if (!strcasecmp(lavc_param_vcodec, "ffv1"))
 	mux_v->bih->biCompression = mmioFOURCC('F', 'F', 'V', '1');
+    else if (!strcasecmp(lavc_param_vcodec, "snow"))
+	mux_v->bih->biCompression = mmioFOURCC('S', 'N', 'O', 'W');
     else
 	mux_v->bih->biCompression = mmioFOURCC(lavc_param_vcodec[0],
 		lavc_param_vcodec[1], lavc_param_vcodec[2], lavc_param_vcodec[3]); /* FIXME!!! */
