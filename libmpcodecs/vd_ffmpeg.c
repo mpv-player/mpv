05d729ee2bc00b8660a27f36bd82081b0daa84d4
michael <michael@b3059339-0415-0410-9bf9-f77b7e298cf2>
2004-09-07 01:20:19 +0000

    disable direct rendering for h264 until now we did try with dr, and disabled it if more then 1 reference frame was used, but that rarely resulted in dr+h264 and its not guranteed to work as stride may end up different, and remocing CODEC_CAP_DR1 from h264 in ffmpeg isnt correct either as the h264 codec does support it
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@13276 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/vd_ffmpeg.c
+++ libmpcodecs/vd_ffmpeg.c
@@ -188,7 +188,7 @@ static int init(sh_video_t *sh){
     if(vd_use_slices && (lavc_codec->capabilities&CODEC_CAP_DRAW_HORIZ_BAND) && !do_vis_debug)
 	ctx->do_slices=1;
  
-    if(lavc_codec->capabilities&CODEC_CAP_DR1 && !do_vis_debug)
+    if(lavc_codec->capabilities&CODEC_CAP_DR1 && !do_vis_debug && lavc_codec->id != CODEC_ID_H264)
 	ctx->do_dr1=1;
     ctx->b_age= ctx->ip_age[0]= ctx->ip_age[1]= 256*256*256*64;
     ctx->ip_count= ctx->b_count= 0;
