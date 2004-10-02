8352d2c24438d6f5738dbbe73c515df622675ac9
faust3 <faust3@b3059339-0415-0410-9bf9-f77b7e298cf2>
2004-10-02 12:17:28 +0000

    support newer gcc
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@13529 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmenu/vf_menu.c
+++ libmenu/vf_menu.c
@@ -30,7 +30,7 @@ static struct vf_priv_s* st_priv = NULL;
 static mp_image_t* pause_mpi = NULL;
 static int go2pause = 0;
 /// if nonzero display menu at startup
-int menu_startup = 0;
+int attribute_used menu_startup = 0;
 
 struct vf_priv_s {
   menu_t* root;
