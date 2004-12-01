1d97ef0ec341576dd6c6a98fdbb750baa7cf0f1c
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2004-12-01 23:24:25 +0000

    Add "Available video filters:" line to -vf help.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@14086 b3059339-0415-0410-9bf9-f77b7e298cf2

--- m_option.c
+++ m_option.c
@@ -1439,6 +1439,7 @@ static int parse_obj_settings_list(m_option_t* opt,char *name,
 
   if(!strcmp(param,"help")) {
     m_obj_list_t* ol = opt->priv;
+    mp_msg(MSGT_VFILTER,MSGL_INFO,"Available video filters:\n");
     for(n = 0 ; ol->list[n] ; n++)
       mp_msg(MSGT_VFILTER,MSGL_INFO,"  %-15s: %s\n",
 	     M_ST_MB(char*,ol->list[n],ol->name_off),
