4bfc3589a4241e88979b32f48968412bd426c771
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2004-12-17 13:27:34 +0000

    Allow Y8 and Y800 as OUT format in codecs.conf
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@14171 b3059339-0415-0410-9bf9-f77b7e298cf2

--- codec-cfg.c
+++ codec-cfg.c
@@ -143,6 +143,8 @@ static int add_to_format(char *s, char *alias,unsigned int *fourcc, unsigned int
 		{"444P",  IMGFMT_444P},
 		{"422P",  IMGFMT_422P},
 		{"411P",  IMGFMT_411P},
+		{"Y800",  IMGFMT_Y800},
+		{"Y8",    IMGFMT_Y8},
 
 		{"YUY2",  IMGFMT_YUY2},
 		{"UYVY",  IMGFMT_UYVY},
