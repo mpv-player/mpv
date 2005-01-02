8b803d1fd928676a656dcad04c93fcfb0c3f9dd7
faust3 <faust3@b3059339-0415-0410-9bf9-f77b7e298cf2>
2005-01-02 15:47:48 +0000

    Mingw compile fix, hm doesn't the inttypes solution always work?
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@14300 b3059339-0415-0410-9bf9-f77b7e298cf2

--- tremor/os_types.h
+++ tremor/os_types.h
@@ -41,12 +41,12 @@
    typedef unsigned __int32 ogg_uint32_t;
    typedef __int16 ogg_int16_t;
 #  else
-   /* Cygwin */
-   #include <_G_config.h>
-   typedef _G_int64_t ogg_int64_t;
-   typedef _G_int32_t ogg_int32_t;
-   typedef _G_uint32_t ogg_uint32_t;
-   typedef _G_int16_t ogg_int16_t;
+   /* Cygwin/MinGW */
+   #include <inttypes.h>
+   typedef int64_t ogg_int64_t;
+   typedef int32_t ogg_int32_t;
+   typedef uint32_t ogg_uint32_t;
+   typedef int16_t ogg_int16_t;
 #  endif
 
 #elif defined(__MACOS__)
