c2a4be43c7523fefe3bf77748d6ea55d5b08baef
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2005-01-14 00:14:45 +0000

    icc support by Darek Ostolski <ostolski at kwantum dot gda dot pl>
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@14493 b3059339-0415-0410-9bf9-f77b7e298cf2

--- loader/wine/basetsd.h
+++ loader/wine/basetsd.h
@@ -28,6 +28,8 @@ extern "C" {
 
 /* Type model indepent typedefs */
 
+#ifndef __INTEL_COMPILER
+
 typedef char          __int8;
 typedef unsigned char __uint8;
 
@@ -39,6 +41,15 @@ typedef unsigned int __uint32;
 
 typedef long long          __int64;
 typedef unsigned long long __uint64;
+
+#else
+
+typedef unsigned __int8  __uint8;
+typedef unsigned __int16 __uint16;
+typedef unsigned __int32 __uint32;
+typedef unsigned __int64 __uint64;
+
+#endif /* __INTEL_COMPILER */
 
 #if defined(_WIN64)
 
