1270185b0ffe4e2870708f63cd31caf220897ae2
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2005-02-19 02:32:12 +0000

    Mark locally modified files as such to comply more closely with GPL 2a.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@14733 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpeg2/idct_alpha.c
+++ libmpeg2/idct_alpha.c
@@ -20,6 +20,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Modified for use with MPlayer, see libmpeg-0.4.0.diff for the exact changes.
+ * detailed CVS changelog at http://www.mplayerhq.hu/cgi-bin/cvsweb.cgi/main/
+ * $Id$
  */
 
 #include "config.h"
