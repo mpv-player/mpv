1361d538173330b3a9e211dbf886edd4b697d873
rfelker <rfelker@b3059339-0415-0410-9bf9-f77b7e298cf2>
2005-02-24 16:52:18 +0000

    stride must be signed! otherwise negative stride is broken on 64bit systems
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@14793 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/mp_image.h
+++ libmpcodecs/mp_image.h
@@ -79,7 +79,7 @@ typedef struct mp_image_s {
     int width,height;  // stored dimensions
     int x,y,w,h;  // visible dimensions
     unsigned char* planes[MP_MAX_PLANES];
-    unsigned int stride[MP_MAX_PLANES];
+    int stride[MP_MAX_PLANES];
     char * qscale;
     int qstride;
     int pict_type; // 0->unknown, 1->I, 2->P, 3->B
