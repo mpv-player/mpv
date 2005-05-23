e92e250a3ca92dbaf7b576c1c3b34992e031c14a
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2005-05-23 21:24:47 +0000

    Use the public sys/kbio.h header instead of messing with the MI headers, which doesn't exist anymore on DragonFly.  Approved by the FreeBSD ports maintainer. patch by Joerg Sonnenberger <joerg - at - britannica - bec - de>
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@15556 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libdha/sysdep/pci_freebsd.c
+++ libdha/sysdep/pci_freebsd.c
@@ -5,10 +5,7 @@
 */
 #include <errno.h>
 #include <sys/file.h>
-/* machine/console.h seems to be outdated by recent FreeBSD *
- * however pcvt_ioctl.h seems to exist for very long time   */
-/* #include <machine/console.h>*/
-#include <machine/pcvt_ioctl.h>
+#include <sys/kbio.h>
 #ifndef GCCUSESGAS
 #define GCCUSESGAS
 #endif
