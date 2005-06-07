869decabf8bf6d4c2cf6ac1af932392b7626fe62
faust3 <faust3@b3059339-0415-0410-9bf9-f77b7e298cf2>
2005-06-07 13:54:17 +0000

    mingw and maybe other system define the __int* types to char, short..., so the typedefs become typedef char char; etc.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@15692 b3059339-0415-0410-9bf9-f77b7e298cf2

--- loader/wine/basetsd.h
+++ loader/wine/basetsd.h
@@ -36,17 +36,33 @@ extern "C" {
 
 #ifndef __INTEL_COMPILER
 
+#ifndef __int8
 typedef char          __int8;
+#endif
+#ifndef __uint8
 typedef unsigned char __uint8;
+#endif
 
+#ifndef __int16
 typedef short          __int16;
+#endif
+#ifndef __uint16
 typedef unsigned short __uint16;
+#endif
 
+#ifndef __int32
 typedef int          __int32;
+#endif
+#ifndef __uint32
 typedef unsigned int __uint32;
+#endif
 
+#ifndef __int64
 typedef long long          __int64;
+#endif
+#ifndef __uint64
 typedef unsigned long long __uint64;
+#endif
 
 #else
 
