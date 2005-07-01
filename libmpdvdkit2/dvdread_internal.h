6dec8bd58df22f92314d23cf0653beed661232d9
aurel <aurel@b3059339-0415-0410-9bf9-f77b7e298cf2>
2005-07-01 11:26:38 +0000

    drop annoying debug messages from libdvdread
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@15881 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdvdkit2/dvdread_internal.h
+++ libmpdvdkit2/dvdread_internal.h
@@ -2,11 +2,7 @@
 #define DVDREAD_INTERNAL_H
 
 
-#define CHECK_VALUE(arg) \
- if(!(arg)) { \
-   fprintf(stderr, "\n*** libdvdread: CHECK_VALUE failed in %s:%i ***" \
-                   "\n*** for %s ***\n\n", \
-                   __FILE__, __LINE__, # arg ); \
- }
+#define CHECK_VALUE(arg)
+
 
 #endif /* DVDREAD_INTERNAL_H */
