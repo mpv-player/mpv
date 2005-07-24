e23ff6c88cc69764d2feb4ea8b6d4968efb1237e
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2005-07-24 12:22:30 +0000

    Some ICY servers (e.g. http://broadcast.spnet.net:8000/darikhigh) do not set the protocol correctly, so look for icy-metaint in the response instead.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@16079 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/http.c
+++ libmpdemux/http.c
@@ -841,7 +841,7 @@ static int http_streaming_start(stream_t *stream, int* file_format) {
 
 static int fixup_open(stream_t *stream,int seekable) {
 	HTTP_header_t *http_hdr = stream->streaming_ctrl->data;
-	int is_icy = http_hdr && strcasecmp(http_hdr->protocol, "ICY") == 0;
+	int is_icy = http_hdr && http_get_field(http_hdr, "Icy-MetaInt");
 	char *content_type = http_get_field( http_hdr, "Content-Type" );
 	int is_ultravox = strcasecmp(stream->streaming_ctrl->url->protocol, "unsv") == 0;
 
