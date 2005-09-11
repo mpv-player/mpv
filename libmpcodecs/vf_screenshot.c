9227bb4a99aacfcd18a872d61e22f07e239958ad
henry <henry@b3059339-0415-0410-9bf9-f77b7e298cf2>
2005-09-11 15:08:48 +0000

    use slices if DR isn't available
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@16457 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/vf_screenshot.c
+++ libmpcodecs/vf_screenshot.c
@@ -181,6 +181,12 @@ static void get_image(struct vf_instance_s* vf, mp_image_t *mpi){
     vf->dmpi= vf_get_image(vf->next, mpi->imgfmt, 
 			   mpi->type, mpi->flags | MP_IMGFLAG_READABLE, mpi->width, mpi->height);
 
+    if((vf->dmpi->flags & MP_IMGFLAG_DRAW_CALLBACK) &&
+       !(vf->dmpi->flags & MP_IMGFLAG_DIRECT)){
+	// use slices if DR isn't possible
+	return;
+    }
+
     mpi->planes[0]=vf->dmpi->planes[0];
     mpi->stride[0]=vf->dmpi->stride[0];
     if(mpi->flags&MP_IMGFLAG_PLANAR){
