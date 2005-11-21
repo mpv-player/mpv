7440b82eb787d2b29d36a3e4752b0e0d5074d469
ptt <ptt@b3059339-0415-0410-9bf9-f77b7e298cf2>
2005-11-21 23:51:27 +0000

    resolves problem in module stop_xscreensaver, crashing mp after sleep and awake or enabling/disabling xssaver by hand
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@17025 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/x11_common.c
+++ libvo/x11_common.c
@@ -1572,6 +1572,8 @@ void xscreensaver_heartbeat(void)
     unsigned int time = GetTimerMS();
     XEvent ev;
 
+    if (mDisplay)
+        xs_windowid = find_xscreensaver_window(mDisplay);
     if (mDisplay && xs_windowid &&
         ((time - time_last) > 30000 || (time - time_last) < 0))
     {
