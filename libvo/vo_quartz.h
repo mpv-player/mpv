988281c471a3a5f3e69073a0449ce8875c54304c
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-01-13 00:23:32 +0000

    Update licensing information: The FSF changed postal address.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@17368 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vo_quartz.h
+++ libvo/vo_quartz.h
@@ -20,7 +20,7 @@
 
     You should have received a copy of the GNU Library General Public
     License along with this library; if not, write to the Free
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
     Sam Lantinga
     slouken@libsdl.org
