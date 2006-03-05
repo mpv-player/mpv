4eab536f60e9e825ff86ec677eba954db214296d
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-03-05 18:28:20 +0000

    Comment fix: mp_msg_levels[] is inited to -2, not 2.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@17743 b3059339-0415-0410-9bf9-f77b7e298cf2

--- mp_msg.c
+++ mp_msg.c
@@ -20,7 +20,7 @@ extern int use_gui;
 /* maximum message length of mp_msg */
 #define MSGSIZE_MAX 3072
 
-int mp_msg_levels[MSGT_MAX]; // verbose level of this module. inited to 2
+int mp_msg_levels[MSGT_MAX]; // verbose level of this module. inited to -2
 int mp_msg_level_all = MSGL_STATUS;
 int verbose = 0;
 
