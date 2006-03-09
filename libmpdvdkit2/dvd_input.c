d7d3f533178bc644879ef9a28a111b0ef49fd4e4
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-03-09 17:39:34 +0000

    fix another sizeof(dvd_input_t) bug (must be struct dvd_input_s instead)
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@17787 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdvdkit2/dvd_input.c
+++ libmpdvdkit2/dvd_input.c
@@ -158,7 +158,7 @@ static dvd_input_t file_open(const char *target)
   dvd_input_t dev;
   
   /* Allocate the library structure */
-  dev = (dvd_input_t) malloc(sizeof(dvd_input_t));
+  dev = (dvd_input_t) malloc(sizeof(struct dvd_input_s));
   if(dev == NULL) {
     fprintf(stderr, "libdvdread: Could not allocate memory.\n");
     return NULL;
