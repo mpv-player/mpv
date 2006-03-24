b7e160dbe9a0a60ee2e6124e71f0a8653d1c7432
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-03-24 01:32:04 +0000

    Add DBG5 verbosity level to prepare for upcoming mp_msg_test changes.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@17931 b3059339-0415-0410-9bf9-f77b7e298cf2

--- mp_msg.h
+++ mp_msg.h
@@ -20,6 +20,7 @@ extern int identify;
 #define MSGL_DBG2 7   // v=2
 #define MSGL_DBG3 8   // v=3
 #define MSGL_DBG4 9   // v=4
+#define MSGL_DBG5 10  // v=5
 
 #define MSGL_FIXME 1  // for conversions from printf where the appropriate MSGL is not known; set equal to ERR for obtrusiveness
 #define MSGT_FIXME 0  // for conversions from printf where the appropriate MSGT is not known; set equal to GLOBAL for obtrusiveness
