27ea82157718ebafa1f68a3b517a2e1d18d2f370
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-04-12 11:19:49 +0000

    missing ifoClose and return 1. Patch by kiriuja (mplayer-patches at en-directo net)
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@18079 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/stream_dvd.c
+++ libmpdemux/stream_dvd.c
@@ -465,7 +465,10 @@ static int mp_describe_titleset(dvd_reader_t *dvd, tt_srpt_t *tt_srpt, int vts_n
         return 0;
 
     if(!vts_file->vtsi_mat || !vts_file->vts_pgcit)
+    {
+        ifoClose(vts_file);
         return 0;
+    }
 
     for(title_no = 0; title_no < tt_srpt->nr_of_srpts; title_no++)
     {
@@ -475,6 +478,7 @@ static int mp_describe_titleset(dvd_reader_t *dvd, tt_srpt_t *tt_srpt, int vts_n
         mp_msg(MSGT_GLOBAL, MSGL_INFO, "ID_DVD_TITLE_%d_LENGTH=%d.%03d\n", title_no + 1, msec / 1000, msec % 1000);
     }
     ifoClose(vts_file);
+    return 1;
 }
 
 static int control(stream_t *stream,int cmd,void* arg) 
