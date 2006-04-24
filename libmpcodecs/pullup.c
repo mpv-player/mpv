549e1b3d4f811de9edd335fdc9ce0062a494dc7e
nplourde <nplourde@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-04-24 23:05:43 +0000

    Get rid of unneeded align
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@18264 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/pullup.c
+++ libmpcodecs/pullup.c
@@ -5,7 +5,6 @@
 #include <string.h>
 #include "pullup.h"
 #include "config.h"
-#include "asmalign.h"
 
 
 
@@ -19,7 +18,6 @@ static int diff_y_mmx(unsigned char *a, unsigned char *b, int s)
 		"pxor %%mm4, %%mm4 \n\t"
 		"pxor %%mm7, %%mm7 \n\t"
 		
-		ASMALIGN16
 		"1: \n\t"
 		
 		"movq (%%esi), %%mm0 \n\t"
@@ -68,7 +66,6 @@ static int licomb_y_mmx(unsigned char *a, unsigned char *b, int s)
 		"pxor %%mm7, %%mm7 \n\t"
 		"subl %%eax, %%edi \n\t"
 		
-		ASMALIGN16
 		"2: \n\t"
 
 		"movq (%%esi), %%mm0 \n\t"
@@ -157,7 +154,6 @@ static int var_y_mmx(unsigned char *a, unsigned char *b, int s)
 		"pxor %%mm4, %%mm4 \n\t"
 		"pxor %%mm7, %%mm7 \n\t"
 		
-		ASMALIGN16
 		"1: \n\t"
 		
 		"movq (%%esi), %%mm0 \n\t"
