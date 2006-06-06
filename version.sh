f854d9407e384112188244cb89de686c566b6b25
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-06-06 12:00:07 +0000

    Drop all system-specific version generation stuff.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@18595 b3059339-0415-0410-9bf9-f77b7e298cf2

--- version.sh
+++ version.sh
@@ -1,29 +1,6 @@
 #!/bin/sh
 
-OS=`uname -s`
-case "$OS" in
-  CYGWIN*|Linux|MINGW*)
     last_cvs_update=r`grep committed-rev .svn/entries | head -n 1 | cut -d '"' -f 2 2>/dev/null`
-    ;;
-  Darwin|*BSD*)
-    # BSD 'date -r' does not print modification time
-    # LC_ALL=C sets month/day order and English language in the date string
-    # The if in the awk call works around wrong day/month order.
-    last_cvs_update=`LC_ALL=C ls -lT .svn | \
-      awk '{ \
-        day=$7; \
-        month=index(" JanFebMarAprMayJunJulAugSepOctNovDec", $6); \
-        if(month==0) { \
-          day=$6; \
-          month=index(" JanFebMarAprMayJunJulAugSepOctNovDec",$7); } \
-        printf("%s%.02d%.02d-%s", \
-          substr($9, 3, 2), (month+1)/3, day, substr($8, 0, 5)); \
-      }'`
-    ;;
-  *)
-    last_cvs_update=`date +%y%m%d-%H:%M`
-    ;;
-esac
 
 extra=""
 if test "$1" ; then
