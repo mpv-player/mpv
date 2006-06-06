e63c3f261ae7aae460d75597fa5ec6497f6b6c31
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-06-06 12:01:34 +0000

    cosmetics
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@18596 b3059339-0415-0410-9bf9-f77b7e298cf2

--- version.sh
+++ version.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-    last_cvs_update=r`grep committed-rev .svn/entries | head -n 1 | cut -d '"' -f 2 2>/dev/null`
+revision=r`grep committed-rev .svn/entries | head -n 1 | cut -d '"' -f 2 2>/dev/null`
 
 extra=""
 if test "$1" ; then
   extra="-$1"
 fi
-echo "#define VERSION \"dev-SVN-${last_cvs_update}${extra}\"" >version.h
+
+echo "#define VERSION \"dev-SVN-${revision}${extra}\"" > version.h
