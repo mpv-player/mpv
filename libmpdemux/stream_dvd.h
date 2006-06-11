689316c4d6dca6ad3398da3e6f8e8a881116a3c3
nicodvb <nicodvb@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-06-11 21:26:35 +0000

    moved stream_dvd's interface to stream_dvd.h
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@18684 b3059339-0415-0410-9bf9-f77b7e298cf2

---
+++ libmpdemux/stream_dvd.h
@@ -0,0 +1,48 @@
+
+#ifdef USE_DVDREAD
+
+#ifdef USE_MPDVDKIT
+#include "libmpdvdkit2/dvd_reader.h"
+#include "libmpdvdkit2/ifo_types.h"
+#include "libmpdvdkit2/ifo_read.h"
+#include "libmpdvdkit2/nav_read.h"
+#else
+#include <dvdread/dvd_reader.h>
+#include <dvdread/ifo_types.h>
+#include <dvdread/ifo_read.h>
+#include <dvdread/nav_read.h>
+#endif
+
+typedef struct {
+  dvd_reader_t *dvd;
+  dvd_file_t *title;
+  ifo_handle_t *vmg_file;
+  tt_srpt_t *tt_srpt;
+  ifo_handle_t *vts_file;
+  vts_ptt_srpt_t *vts_ptt_srpt;
+  pgc_t *cur_pgc;
+//
+  int cur_title;
+  int cur_cell;
+  int last_cell;
+  int cur_pack;
+  int cell_last_pack;
+// Navi:
+  int packs_left;
+  dsi_t dsi_pack;
+  int angle_seek;
+// audio datas
+  int nr_of_channels;
+  stream_language_t audio_streams[32];
+// subtitles
+  int nr_of_subtitles;
+  stream_language_t subtitles[32];
+} dvd_priv_t;
+
+int dvd_number_of_subs(stream_t *stream);
+int dvd_lang_from_sid(stream_t *stream, int id);
+int dvd_aid_from_lang(stream_t *stream, unsigned char* lang);
+int dvd_sid_from_lang(stream_t *stream, unsigned char* lang);
+int dvd_chapter_from_cell(dvd_priv_t *dvd,int title,int cell);
+
+#endif
