92f2863d63b75e30f13473a5d76cdb19cd5fc982
ivo <ivo@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-07-02 20:31:02 +0000

    fix linking on some systems
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@18889 b3059339-0415-0410-9bf9-f77b7e298cf2

--- TOOLS/GL-test/compile.sh
+++ TOOLS/GL-test/compile.sh
@@ -1,2 +1,2 @@
 
-gcc -g -O4 gltest.c ../../osdep/timer-lx.o -o gltest -L/usr/X11/lib -lglut -lGL -lGLU -lX11 -lXext -lXmu -lXi -lm
+gcc -g -O4 gltest.c ../../osdep/timer-lx.o -o gltest -L/usr/X11/lib -L/usr/X11R6/lib -lglut -lGL -lGLU -lX11 -lXext -lXmu -lXi -lm
