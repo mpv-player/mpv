2915a2921d59535ea1c35d19626bc1bf8557bfdf
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-07-30 22:20:50 +0000

    Clarify licensing situation.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@19270 b3059339-0415-0410-9bf9-f77b7e298cf2

--- mp3lib/layer1.c
+++ mp3lib/layer1.c
@@ -1,9 +1,3 @@
-/*
- * Modified for use with MPlayer, for details see the changelog at
- * http://svn.mplayerhq.hu/mplayer/trunk/
- * $Id$
- */
-
 /* 
  * Mpeg Layer-1 audio decoder 
  * --------------------------
@@ -12,6 +6,16 @@
  *
  * may have a few bugs after last optimization ... 
  *
+ */
+
+/*
+ * Modified for use with MPlayer, for details see the changelog at
+ * http://svn.mplayerhq.hu/mplayer/trunk/
+ * $Id$
+ *
+ * The above-mentioned README file has the following to say about licensing:
+ *
+ *   COPYING: you may use this source under LGPL terms!
  */
 
 //#include "mpg123.h"
