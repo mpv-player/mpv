2fafb67e962b99a159ef0834de41c0aa7edb3451
ben <ben@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-08-04 19:38:59 +0000

    removed some useless includes
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@19326 b3059339-0415-0410-9bf9-f77b7e298cf2

--- stream/rtp.h
+++ stream/rtp.h
@@ -8,13 +8,6 @@
 #ifndef _RTP_H
 #define _RTP_H
 
-#include "config.h"
-#ifndef HAVE_WINSOCK2
-#include <sys/socket.h>
-#else
-#include <winsock2.h>
-#endif
-
 int read_rtp_from_server(int fd, char *buffer, int length);
 
 #endif
