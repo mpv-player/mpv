822fa55de5977a1a75e29b2e1690e9b7e85e315e
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-08-14 14:24:17 +0000

    Do not call VFCTRL_FLUSH_FRAMES when the filter chain was never configured, since that will cause crashes (try with e.g. -ss 99999999999999).
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@19393 b3059339-0415-0410-9bf9-f77b7e298cf2

--- mencoder.c
+++ mencoder.c
@@ -1543,6 +1543,9 @@ if (!interrupted && filelist[++curfile].name != 0) {
 
 /* Emit the remaining frames in the video system */
 /*TODO emit frmaes delayed by decoder lag*/
+if (!((vf_instance_t *)sh_video->vfilter)->fmt.have_configured) {
+	mp_msg(MSGT_MENCODER, MSGL_WARN, "\nFilters have not been configured! Empty file?\n");
+} else
 if(sh_video && sh_video->vfilter){
 	mp_msg(MSGT_MENCODER, MSGL_INFO, "\nFlushing video frames\n");
 	((vf_instance_t *)sh_video->vfilter)->control(sh_video->vfilter,
