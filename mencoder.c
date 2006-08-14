1f258a4a460e9df46ded1295f81c980a06a97c99
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-08-14 16:17:26 +0000

    re-indent properly after last patch.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@19396 b3059339-0415-0410-9bf9-f77b7e298cf2

--- mencoder.c
+++ mencoder.c
@@ -1546,9 +1546,9 @@ if (!interrupted && filelist[++curfile].name != 0) {
 if(sh_video && sh_video->vfilter){
 	mp_msg(MSGT_MENCODER, MSGL_INFO, "\nFlushing video frames\n");
 	if (!((vf_instance_t *)sh_video->vfilter)->fmt.have_configured)
-	mp_msg(MSGT_MENCODER, MSGL_WARN, "Filters have not been configured! Empty file?\n");
+		mp_msg(MSGT_MENCODER, MSGL_WARN, "Filters have not been configured! Empty file?\n");
 	else
-	((vf_instance_t *)sh_video->vfilter)->control(sh_video->vfilter,
+		((vf_instance_t *)sh_video->vfilter)->control(sh_video->vfilter,
     	                                              VFCTRL_FLUSH_FRAMES, 0);
 }
 
