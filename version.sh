9b51e80117c29281bd4be2afa257f1eb1c10aaaa
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-09-05 23:19:22 +0000

    FFmpeg sync: Fall back on grep if svn client is not installed.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@19702 b3059339-0415-0410-9bf9-f77b7e298cf2

--- version.sh
+++ version.sh
@@ -3,6 +3,8 @@
 test "$1" && extra="-$1"
 
 svn_revision=`LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
+test $svn_revision || svn_revision=`cd "$1" && grep revision .svn/entries | \
+                                    cut -d '"' -f2 2> /dev/null`
 test $svn_revision || svn_revision=UNKNOWN
 
 NEW_REVISION="#define VERSION \"dev-SVN-r${svn_revision}${extra}\""
