8d8bca056c7c75f3ce1b0ff0f2d2590865266a93
nicodvb <nicodvb@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-09-15 22:43:48 +0000

    at start, when not playing a specific titleset, try to call the Title menu (as is common practice among players)
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@19848 b3059339-0415-0410-9bf9-f77b7e298cf2

--- stream/stream_dvdnav.c
+++ stream/stream_dvdnav.c
@@ -401,8 +401,10 @@ static int open_s(stream_t *stream,int mode, void* opts, int* file_format) {
   }
   } else if(p->track == -1)
       dvdnav_menu_call(dvdnav_priv->dvdnav, DVD_MENU_Root);
-    else
+    else {
     mp_msg(MSGT_OPEN,MSGL_INFO,"dvdnav_stream, you didn't specify a track number (as in dvdnav://1), playing whole disc\n");
+    dvdnav_menu_call(dvdnav_priv->dvdnav, DVD_MENU_Title);
+  }
 
   stream->sector_size = 2048;
   stream->flags = STREAM_READ | STREAM_SEEK;
