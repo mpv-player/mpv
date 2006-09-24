dc5712ce3a7a7e6d2c05155fca5e74e8f5a4f283
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-09-24 15:53:23 +0000

    Remove copy and paste error from FFmpeg sync, MPlayer does not support out-of-tree build directories.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@19969 b3059339-0415-0410-9bf9-f77b7e298cf2

--- version.sh
+++ version.sh
@@ -3,7 +3,7 @@
 test "$1" && extra="-$1"
 
 svn_revision=`LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
-test $svn_revision || svn_revision=`cd "$1" && grep revision .svn/entries | \
+test $svn_revision || svn_revision=`grep revision .svn/entries | \
                                     cut -d '"' -f2 2> /dev/null`
 test $svn_revision || svn_revision=UNKNOWN
 
