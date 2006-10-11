ee88e6f26fcacea021c5f0f78258ba1f6f43d7ef
ods15 <ods15@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-10-11 20:53:33 +0000

    gcc 2.95 fix
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@20164 b3059339-0415-0410-9bf9-f77b7e298cf2

--- stream/stream_dvd.c
+++ stream/stream_dvd.c
@@ -142,10 +142,10 @@ int dvd_chapter_from_cell(dvd_priv_t* dvd,int title,int cell)
 
 int dvd_lang_from_aid(stream_t *stream, int id) {
   dvd_priv_t *d;
+  int i;
   if (!stream) return 0;
   d = stream->priv;
   if (!d) return 0;
-  int i;
   for(i=0;i<d->nr_of_channels;i++) {
     if(d->audio_streams[i].id==id)
       return d->audio_streams[i].language;
