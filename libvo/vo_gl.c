3a5074f460f0e91a6144ef2b492dac72106fede3
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-10-24 18:51:38 +0000

    Fix single-black-pixel-in-upper-left-corner bug (how do people notice a single black pixel?? Amazing...).
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@20436 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vo_gl.c
+++ libvo/vo_gl.c
@@ -585,7 +585,7 @@ static void create_osd_texture(int x0, int y0, int w, int h,
   char *tmp = malloc(stride * h);
   // convert alpha from weird MPlayer scale.
   // in-place is not possible since it is reused for future OSDs
-  for (i = h * stride - 1; i > 0; i--)
+  for (i = h * stride - 1; i >= 0; i--)
     tmp[i] = srca[i] - 1;
   glUploadTex(gl_target, GL_ALPHA, GL_UNSIGNED_BYTE, tmp, stride,
               0, 0, w, h, 0);
