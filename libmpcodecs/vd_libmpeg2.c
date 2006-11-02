6083bf727003920485bd9971406b6595d39f7604
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-11-02 16:20:25 +0000

    Set mpi stride, mostly fixes http://samples.mplayerhq.hu/MPEG2/res_change_ffmpeg_aspect.ts with e.g. -vo x11 and -vo gl (-vo xv was not affected).
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@20607 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/vd_libmpeg2.c
+++ libmpcodecs/vd_libmpeg2.c
@@ -211,6 +211,9 @@ static mp_image_t* decode(sh_video_t *sh,void* data,int len,int flags){
 
 	    if(!mpi_new) return 0; // VO ERROR!!!!!!!!
 	    mpeg2_set_buf(mpeg2dec, mpi_new->planes, mpi_new);
+	    mpi_new->stride[0] = info->sequence->width;
+	    mpi_new->stride[1] = info->sequence->chroma_width;
+	    mpi_new->stride[2] = info->sequence->chroma_width;
 	    if (info->current_picture->flags&PIC_FLAG_TOP_FIELD_FIRST)
 		mpi_new->fields |= MP_IMGFIELD_TOP_FIRST;
 	    else mpi_new->fields &= ~MP_IMGFIELD_TOP_FIRST;
