2ea83c999e25f0467cf7e6c65cb0136d229716e1
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-11-03 10:09:39 +0000

    Remove HAVE_MPLAYER check, we unconditionally compile with HAVE_MPLAYER set. Also remove the corresponding else case, it was unreachable.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@20625 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdvdkit2/config.h
+++ libmpdvdkit2/config.h
@@ -2,40 +2,7 @@
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 
-#ifdef HAVE_MPLAYER
-
 #include "../config.h"
-
-#else
-
-/* Define if <extras/BSDI_dvdioctl/dvd.h> defines DVD_STRUCT. */
-#undef DVD_STRUCT_IN_BSDI_DVDIOCTL_DVD_H
-
-/* Define if <dvd.h> defines DVD_STRUCT. */
-#undef DVD_STRUCT_IN_DVD_H
-
-/* Define if <linux/cdrom.h> defines DVD_STRUCT. */
-#define DVD_STRUCT_IN_LINUX_CDROM_H 1
-
-/* Define if <sys/cdio.h> defines dvd_struct. */
-#undef DVD_STRUCT_IN_SYS_CDIO_H
-
-/* Define if <sys/dvdio.h> defines dvd_struct. */
-#undef DVD_STRUCT_IN_SYS_DVDIO_H
-
-/* Define if FreeBSD-like dvd_struct is defined. */
-#undef HAVE_BSD_DVD_STRUCT
-
-/* Define if Linux-like dvd_struct is defined. */
-#define HAVE_LINUX_DVD_STRUCT 1
-
-/* Define if <sys/scsi.h> defines sctl_io. */
-#undef HPUX_SCTL_IO
-
-/* Have userspace SCSI headers. */
-#undef SOLARIS_USCSI
-
-#endif /* HAVE_MPLAYER */
 
 /* Version number of package */
 #define VERSION "1.2.9"
