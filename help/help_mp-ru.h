057630b2a5c3680b0c8a312a34bf6e7f51440e7a
voroshil <voroshil@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-11-07 18:20:07 +0000

    sync tag update, no real changes needed
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@20752 b3059339-0415-0410-9bf9-f77b7e298cf2

--- help/help_mp-ru.h
+++ help/help_mp-ru.h
@@ -2,7 +2,7 @@
 // Dmitry Baryshkov <mitya@school.ioffe.ru>
 // Reworked by: Andrew Savchenko aka Bircoph <Bircoph[at]list[dot]ru>
 
-// Synced with help_mp-en.h: r20712
+// Synced with help_mp-en.h: r20739
 
 // ========================= MPlayer help ===========================
 
