09be641ba5458ab0003ff705db7d56cccd978ae9
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-11-18 12:30:48 +0000

    reinit_video_chain might set sh_video = NULL, so check against that.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@21014 b3059339-0415-0410-9bf9-f77b7e298cf2

--- mplayer.c
+++ mplayer.c
@@ -4263,7 +4263,7 @@ if(!sh_video) goto main; // audio-only
 
 if(!reinit_video_chain()) {
   if(!video_out) goto goto_next_file;
-  if(!sh_video->inited){
+  if(!sh_video || !sh_video->inited){
     if(!sh_audio) goto goto_next_file;
     goto main; // exit_player(MSGTR_Exit_error);
   }
