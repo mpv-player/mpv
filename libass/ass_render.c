bec3287bd430e12fedf5f8a841da9af1a6fc5d2a
eugeni <eugeni@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-11-20 17:13:19 +0000

    Cosmetics: remove unneeded curly brackets.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@21114 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libass/ass_render.c
+++ libass/ass_render.c
@@ -2082,19 +2082,18 @@ static int fit_segment(segment_t* s, segment_t* fixed, int* cnt, int dir)
 	int i;
 	int shift = 0;
 
-	if (dir == 1) { // move down
+	if (dir == 1) // move down
 		for (i = 0; i < *cnt; ++i) {
 			if (s->b + shift <= fixed[i].a || s->a + shift >= fixed[i].b)
 				continue;
 			shift = fixed[i].b - s->a;
 		}
-	} else { // dir == -1, move up
+	else // dir == -1, move up
 		for (i = *cnt-1; i >= 0; --i) {
 			if (s->b + shift <= fixed[i].a || s->a + shift >= fixed[i].b)
 				continue;
 			shift = fixed[i].a - s->b;
 		}
-	}
 
 	fixed[*cnt].a = s->a + shift;
 	fixed[*cnt].b = s->b + shift;
