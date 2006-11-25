badde7586229af3cfbce3a1430d7cd11540a9f38
ben <ben@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-11-25 17:58:14 +0000

    missing include for uint16_t
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@21236 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/sub.h
+++ libvo/sub.h
@@ -125,6 +125,7 @@ extern int vo_osd_changed_flag;
 unsigned utf8_get_char(char **str);
 
 #ifdef USE_DVDNAV
+#include <inttypes.h>
 void osd_set_nav_box (uint16_t sx, uint16_t sy, uint16_t ex, uint16_t ey);
 #endif
 
