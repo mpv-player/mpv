a0897b21c11b2debdbbaee0c90b9051670ad5d6d
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2006-12-09 18:57:48 +0000

    Forgotten closesocket on error, patch by Nicolas Baradakis  <nbk sitadelle com>
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@21552 b3059339-0415-0410-9bf9-f77b7e298cf2

--- stream/asf_mmst_streaming.c
+++ stream/asf_mmst_streaming.c
@@ -626,7 +626,10 @@ int asf_mmst_streaming_start(stream_t *stream)
 
   asf_header_len = get_header (s, asf_header, stream->streaming_ctrl);
 //  mp_msg(MSGT_NETWORK,MSGL_INFO,"---------------------------------- asf_header %d\n",asf_header);
-  if (asf_header_len==0) return -1; //error reading header
+  if (asf_header_len==0) { //error reading header
+    closesocket(s);
+    return -1;
+  }
   packet_length = interp_header (asf_header, asf_header_len);
 
 
