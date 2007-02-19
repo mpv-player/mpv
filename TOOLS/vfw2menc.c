531b2e2975e2355b69ce13e0bf989f9e84882057
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-02-19 00:20:48 +0000

    Fix license header.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@22271 b3059339-0415-0410-9bf9-f77b7e298cf2

--- TOOLS/vfw2menc.c
+++ TOOLS/vfw2menc.c
@@ -4,17 +4,17 @@
  * Copyright (c) 2006 Gianluigi Tiesi <sherpya@netfarm.it>
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
+ * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Library General Public License for more details.
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Library General Public
- * License along with this software; if not, write to the
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
