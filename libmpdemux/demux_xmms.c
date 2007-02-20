40b89d03c852a1d69de58623f60bb9ce652ee39f
ivo <ivo@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-02-20 16:54:49 +0000

    proper GPL heading
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@22289 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/demux_xmms.c
+++ libmpdemux/demux_xmms.c
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (C) 2002-2004 Balatoni Denes and A'rpi
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ */
+
 // This is not reentrant because of global static variables, but most of
 // the plugins are not reentrant either perhaps 
 #include "config.h"
