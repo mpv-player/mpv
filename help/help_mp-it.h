6219ac67cbd5f6abde5f868684685775d71b70fb
ptt <ptt@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-03-20 01:32:02 +0000

    tab typo fixed
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@22762 b3059339-0415-0410-9bf9-f77b7e298cf2

--- help/help_mp-it.h
+++ help/help_mp-it.h
@@ -1007,7 +1007,7 @@ static char help_text[]=
 
 #define MSGTR_VOincompCodec "Il dispositivo di uscita video_out scelto è incompatibile con questo codec.\n"\
                 "Prova aggiungendo il filtro scale alla sequenza dei filtri,\n"\
-		"per esempio -vf spp,scale invece di -vf spp.\n"
+                "per esempio -vf spp,scale invece di -vf spp.\n"
 #define MSGTR_VO_GenericError "E' accaduto questo errore"
 #define MSGTR_VO_UnableToAccess "Impossibile accedere a"
 #define MSGTR_VO_ExistsButNoDirectory "già esiste, ma non è una directory."
