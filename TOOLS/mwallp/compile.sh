e40f658344802f2198a1d7ad1f71ab2aab45b207
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-03-31 14:17:31 +0000

    Remove unused/nonworking wallpaper setting utility.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@22859 b3059339-0415-0410-9bf9-f77b7e298cf2

--- TOOLS/mwallp/compile.sh
+++
@@ -1,6 +0,0 @@
-echo
-gcc -g mwallp.c jpeg.c ../../libvo/aclib.o ../../libmpcodecs/img_format.o \
-    ../../cpudetect.o ../../mp_msg-mencoder.o ../../libavcodec/libavcodec.a \
-    ../../libswscale/libswscale.a ../../osdep/libosdep.a \
-    -I../../libmpcodecs -I../.. -I../../libswscale \
-    -L/usr/X11/lib -L/usr/X11R6/lib -lX11 -lm -ljpeg -o mwallp
