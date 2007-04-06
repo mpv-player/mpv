fe56fcafbab5b079cfc4720ecad51cd897f2f1fc
ben <ben@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-04-06 15:30:39 +0000

    missing headers in vidix drivers registry files
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@22920 b3059339-0415-0410-9bf9-f77b7e298cf2

--- vidix/vidixlib.h
+++ vidix/vidixlib.h
@@ -6,7 +6,7 @@
  * In the future it may be expanded up to capturing and audio things.
  * Main goal of this this interface imlpementation is providing DGA
  * everywhere where it's possible (unlike X11 and other).
- * Copyright 2002 Nick Kurshev
+ * Copyright 2002 Nick Kurshev, 2007 Benjamin Zores
  * Licence: GPL
  * This interface is based on v4l2, fbvid.h, mga_vid.h projects
  * and personally my ideas.
