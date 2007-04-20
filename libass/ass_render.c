3cbdb850468293c5d15302f598d48bdbfd33546a
eugeni <eugeni@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-04-20 22:51:19 +0000

    Cosmetics: reindentation.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@23030 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libass/ass_render.c
+++ libass/ass_render.c
@@ -408,13 +408,13 @@ static ass_image_t* render_text(text_info_t* text_info, int dst_x, int dst_y)
 			if (text_info->glyphs[i].hash_key.frx == 0 &&
 			    text_info->glyphs[i].hash_key.fry == 0 &&
 			    text_info->glyphs[i].hash_key.frz == 0) {
-			hash_val.bbox_scaled = text_info->glyphs[i].bbox;
-			hash_val.bm_o = text_info->glyphs[i].bm_o;
-			hash_val.bm = text_info->glyphs[i].bm;
-			hash_val.bm_s = text_info->glyphs[i].bm_s;
-			hash_val.advance.x = text_info->glyphs[i].advance.x;
-			hash_val.advance.y = text_info->glyphs[i].advance.y;
-			cache_add_glyph(&(text_info->glyphs[i].hash_key), &hash_val);
+				hash_val.bbox_scaled = text_info->glyphs[i].bbox;
+				hash_val.bm_o = text_info->glyphs[i].bm_o;
+				hash_val.bm = text_info->glyphs[i].bm;
+				hash_val.bm_s = text_info->glyphs[i].bm_s;
+				hash_val.advance.x = text_info->glyphs[i].advance.x;
+				hash_val.advance.y = text_info->glyphs[i].advance.y;
+				cache_add_glyph(&(text_info->glyphs[i].hash_key), &hash_val);
 			}
 
 		}
