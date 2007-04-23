5e91f60b1ea99100489f5b3eb42132675e1bbb53
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-04-23 08:01:10 +0000

    Use proper GPL header.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@23097 b3059339-0415-0410-9bf9-f77b7e298cf2

--- gui/win32/gui.c
+++ gui/win32/gui.c
@@ -1,23 +1,25 @@
 /*
-  MPlayer GUI for Win32
-  Copyright (c) 2003 Sascha Sommer <saschasommer@freenet.de>
-  Copyright (c) 2006 Erik Augustson <erik_27can@yahoo.com>
-  Copyright (c) 2006 Gianluigi Tiesi <sherpya@netfarm.it>
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02111-1307 USA
-*/
+ * MPlayer GUI for Win32
+ * Copyright (C) 2003 Sascha Sommer <saschasommer@freenet.de>
+ * Copyright (C) 2006 Erik Augustson <erik_27can@yahoo.com>
+ * Copyright (C) 2006 Gianluigi Tiesi <sherpya@netfarm.it>
+ *
+ * This file is part of MPlayer.
+ *
+ * MPlayer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MPlayer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MPlayer; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
