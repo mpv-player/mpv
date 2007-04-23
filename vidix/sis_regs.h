c9cfb8eed3f5fa7365b603b152176cfe31cc3f53
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-04-23 07:03:58 +0000

    Use standard GPL header.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@23092 b3059339-0415-0410-9bf9-f77b7e298cf2

--- vidix/sis_regs.h
+++ vidix/sis_regs.h
@@ -1,24 +1,25 @@
-/**
-    SiS register definitions and access macros.
-    From SiS X11 driver.
-
-    Copyright 2001-2003 by Thomas Winischhofer, Vienna, Austria.
-  
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-
-**/
+/*
+ *  SiS register definitions and access macros
+ *  From SiS X11 driver
+ *
+ *  Copyright (C) 2001-2003 by Thomas Winischhofer, Vienna, Austria
+ *
+ * This file is part of MPlayer.
+ *
+ * MPlayer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MPlayer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MPlayer; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 #ifndef VIDIX_SIS_REGS_H
 #define VIDIX_SIS_REGS_H
