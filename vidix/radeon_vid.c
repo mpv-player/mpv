c9f91073d0cd3b5cd80aa3a884118d7a935759c7
ben <ben@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-04-30 11:41:47 +0000

    spelling fix
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@23181 b3059339-0415-0410-9bf9-f77b7e298cf2

--- vidix/radeon_vid.c
+++ vidix/radeon_vid.c
@@ -1380,13 +1380,13 @@ static int radeon_init(void)
       (def_cap.device_id == DEVICE_ATI_RADEON_MOBILITY_M6 ||
        def_cap.device_id == DEVICE_ATI_RADEON_MOBILITY_M62))
   {
-      printf(RADEON_MSG" Workarounding buggy Radeon Mobility M6 (0 vs. 8MB ram)\n");
+      printf(RADEON_MSG" Working around buggy Radeon Mobility M6 (0 vs. 8MB ram)\n");
       radeon_ram_size = 8192*1024;
   }
   else if (radeon_ram_size == 0 &&
            (def_cap.device_id == DEVICE_ATI_RS482_RADEON_XPRESS))
   {
-      printf(RADEON_MSG" Workarounding buggy RS482 Radeon Xpress 200 (0 vs. >32MB ram)\n");
+      printf(RADEON_MSG" Working around buggy RS482 Radeon Xpress 200 (0 vs. >32MB ram)\n");
       /* Minimal selectable shared memory from bios 32M */
       radeon_ram_size = 32768*1024;
   } 
