83ebbc9fcde4a54f32ad22f7baa780beb0ae55ab
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-05-28 17:13:52 +0000

    Add LGPL license header. OKed by Arpi and Michael, the authors of all non-trivial parts.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@23403 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/fastmemcpy.h
+++ libvo/fastmemcpy.h
@@ -1,3 +1,21 @@
+/*
+ * This file is part of MPlayer.
+ *
+ * MPlayer is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * MPlayer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with MPlayer; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
 #ifndef __MPLAYER_MEMCPY
 #define __MPLAYER_MEMCPY 1
 
