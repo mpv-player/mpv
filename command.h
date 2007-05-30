a931f0083edbfc12e98333ea0fddaf61a6aeda39
albeu <albeu@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-05-30 07:05:34 +0000

    Make command.h usable without the MPContext typedef.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@23415 b3059339-0415-0410-9bf9-f77b7e298cf2

--- command.h
+++ command.h
@@ -1,3 +1,3 @@
-int run_command(MPContext *mpctx, mp_cmd_t *cmd);
-char *property_expand_string(MPContext *mpctx, char *str);
+int run_command(struct MPContext *mpctx, mp_cmd_t *cmd);
+char *property_expand_string(struct MPContext *mpctx, char *str);
 void property_print_help(void);
