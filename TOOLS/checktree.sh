ce138ecc7f709bd9c80ce78de732e0ced1e5341b
ivo <ivo@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-07-31 08:17:35 +0000

    Update copyright info and license
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@23960 b3059339-0415-0410-9bf9-f77b7e298cf2

--- TOOLS/checktree.sh
+++ TOOLS/checktree.sh
@@ -4,8 +4,21 @@
 
 # Check source-tree for anomalies
 #
-# (C)opyright 2005 by Ivo van Poorten
-# Licensed under GNU General Public License version 2
+# Copyright (C) 2005-2007 by Ivo van Poorten
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 #
 # Thanks to Melchior Franz of the FlightGear project for the original idea
 # of a source-tree checker and Torinthiel for the feedback along the way.
