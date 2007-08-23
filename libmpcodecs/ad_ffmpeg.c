b7ce6217d6f2647365719a0e510000ab8ca97fdb
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-08-23 20:29:57 +0000

    Use new request_channels
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@24128 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/ad_ffmpeg.c
+++ libmpcodecs/ad_ffmpeg.c
@@ -66,6 +66,7 @@ static int init(sh_audio_t *sh_audio)
 	lavc_context->block_align = sh_audio->wf->nBlockAlign;
 	lavc_context->bits_per_sample = sh_audio->wf->wBitsPerSample;
     }
+    lavc_context->request_channels = audio_output_channels;
     lavc_context->codec_tag = sh_audio->format; //FOURCC
     lavc_context->codec_id = lavc_codec->id; // not sure if required, imho not --A'rpi
 
