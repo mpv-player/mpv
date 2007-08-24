437895cc46d8246b197f5ff40117b20a30e3f09b
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-08-24 09:23:06 +0000

    Remove fallback round implementation that is protected by a preprocessor directive that configure never sets or checks for.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@24134 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/gtf.c
+++ libvo/gtf.c
@@ -25,13 +25,6 @@
 
 static GTF_constants GTF_given_constants = { 3.0,550.0,1,8,1.8,8,40,20,128,600 };
 
-#ifndef HAVE_ROUND
-static double round(double v) 
-{ 
-        return floor(v + 0.5); 
-} 
-#endif
-	
 static void GetRoundedConstants(GTF_constants *c)
     {
     c->Vsync_need = round(GTF_given_constants.Vsync_need);
