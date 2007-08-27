686cfb9f056bd2beded49f441c97458003fab9a4
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-08-27 11:04:10 +0000

    warning fixes: realcodecs/drv2.c: In function 'build_crc': realcodecs/drv2.c:316: warning: unused variable 'b' realcodecs/drv2.c: In function 'RV20toYUV420Transform': realcodecs/drv2.c:348: warning: unused variable 'crc2' realcodecs/drv2.c:348: warning: unused variable 'crc1' realcodecs/drv2.c:348: warning: unused variable 'len'
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@24237 b3059339-0415-0410-9bf9-f77b7e298cf2

--- TOOLS/realcodecs/drv2.c
+++ TOOLS/realcodecs/drv2.c
@@ -175,7 +175,7 @@ static void hexdump(void *pos, int len) {
 
 ulong RV20toYUV420CustomMessage(ulong* p1,ulong p2) {
 	ulong result;
-	ulong *pp1=p1;
+//	ulong *pp1=p1;
 	ulong temp[16];
 	fprintf(stderr, "#R# => RV20toYUV420CustomMessage(%p,%p) [%d,%d,%d] \n", p1, p2, p1[0],p1[1],p1[2]);
 
@@ -313,7 +313,8 @@ ulong RV20toYUV420Init(ulong p1,ulong p2) {
 }
 
 unsigned long build_crc(unsigned char *pch, unsigned long len) {
-	unsigned long crc=0, a, b;
+	unsigned long crc=0, a;
+//	unsigned long b;
 	// it's not the real crc function, but so what...
 	while (len--) {
 		a=*(pch++);
@@ -345,7 +346,8 @@ ulong RV20toYUV420Transform(ulong p1,ulong p2,ulong p3,ulong p4,ulong p5) {
 	ulong temp[128];
 	int i;
 
-	unsigned long len,crc_src, crc0, crc1, crc2;
+	unsigned long crc_src, crc0;
+//	unsigned long len, crc1, crc2;
 	unsigned char *pch=(char *)p1;
 	fprintf(stderr, "#R# RV20toYUV420Transform(in=%p,out=%p,tin=%p,tout=%p,yuv=%p)\n",p1,p2,p3,p4,p5);
 	// input data, length=*p3
