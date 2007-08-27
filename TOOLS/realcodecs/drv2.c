70261e715f8ac25a894aac06724ad033bd5923d7
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-08-27 10:16:10 +0000

    Fix "incompatible implicit declaration of built-in function 'exit'" warnings.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@24228 b3059339-0415-0410-9bf9-f77b7e298cf2

--- TOOLS/realcodecs/drv2.c
+++ TOOLS/realcodecs/drv2.c
@@ -11,6 +11,7 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <dlfcn.h>
 #include <sys/time.h>
 
--- TOOLS/realcodecs/sipr.c
+++ TOOLS/realcodecs/sipr.c
@@ -28,6 +28,7 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <dlfcn.h>
 #include <sys/time.h>
 
