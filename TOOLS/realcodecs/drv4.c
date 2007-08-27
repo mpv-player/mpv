fa364a03dc2d24058e9ab17acee44c9d6a7ada65
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-08-27 10:32:46 +0000

    Fix a ton of warnings: warning: incompatible implicit declaration of built-in function 'memset' warning: incompatible implicit declaration of built-in function 'memcpy'
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@24230 b3059339-0415-0410-9bf9-f77b7e298cf2

--- TOOLS/realcodecs/drv4.c
+++ TOOLS/realcodecs/drv4.c
@@ -12,6 +12,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <dlfcn.h>
 #include <sys/time.h>
 
--- TOOLS/realcodecs/sipr.c
+++ TOOLS/realcodecs/sipr.c
@@ -29,6 +29,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <dlfcn.h>
 #include <sys/time.h>
 
