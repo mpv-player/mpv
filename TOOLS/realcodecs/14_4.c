2c013c187581b3d616b65f5af63476519a0a3ff6
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-08-27 10:55:27 +0000

    warning fixes: realcodecs/14_4.c: In function 'RAInitDecoder': realcodecs/14_4.c:238: warning: unused variable 'temp2' realcodecs/14_4.c:237: warning: unused variable 'temp' realcodecs/14_4.c: In function 'RASetFlavor': realcodecs/14_4.c:273: warning: unused variable 'property' realcodecs/14_4.c:272: warning: unused variable 'flavor' realcodecs/14_4.c:272: warning: unused variable 'numflavors' realcodecs/14_4.c:271: warning: unused variable 'result1' realcodecs/14_4.c:271: warning: unused variable 'numprop'
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@24233 b3059339-0415-0410-9bf9-f77b7e298cf2

--- TOOLS/realcodecs/14_4.c
+++ TOOLS/realcodecs/14_4.c
@@ -234,8 +234,8 @@ ulong RAGetNumberOfFlavors(void) {
 
 ulong RAInitDecoder(ulong p1,ulong p2) {
 	ulong result;
-	int temp[256];
-	unsigned char temp2[256];
+//	int temp[256];
+//	unsigned char temp2[256];
 	fprintf(stderr, "RAInitDecoder(ulong p1=0x%0x(%d), ", p1, p1);
 	fprintf(stderr, "ulong p2=0x%0x(%d))\n", p2, p2);
 	hexdump((void*)p2, 4*7);
@@ -268,9 +268,9 @@ ulong RAOpenCodec2(ulong p1) {
 }
 
 ulong RASetFlavor(ulong p1) {
-	ulong result, numprop=0, result1=0;
-	ulong numflavors, flavor;
-	unsigned short property;
+	ulong result;
+//	ulong numflavors, flavor, numprop=0, result1=0;
+//	unsigned short property;
 	fprintf(stderr, "RASetFlavor(ulong p1=0x%0x(%d))\n", p1, p1);
 	hexdump((void*)p1, 44);
 //	hexdump((void*)p1, 44);
