abebb1855d88513ad3abe58dfd28efee238d82bb
ivo <ivo@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-09-06 16:28:31 +0000

    add ; at the end of the sed commands. this fixes operation under cygwin.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@24352 b3059339-0415-0410-9bf9-f77b7e298cf2

--- version.sh
+++ version.sh
@@ -4,7 +4,7 @@ test "$1" && extra="-$1"
 
 svn_revision=`LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
 test $svn_revision || svn_revision=`grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
-test $svn_revision || svn_revision=`sed -n -e '/^dir$/{n;p;q}' .svn/entries 2>/dev/null`
+test $svn_revision || svn_revision=`sed -n -e '/^dir$/{n;p;q;}' .svn/entries 2>/dev/null`
 test $svn_revision || svn_revision=UNKNOWN
 
 NEW_REVISION="#define VERSION \"dev-SVN-r${svn_revision}${extra}\""
