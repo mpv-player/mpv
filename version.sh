fdb70353c29d1c77ef50138620235b024586dcea
ivo <ivo@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-09-06 16:25:44 +0000

    remove cut&paste from ffmpeg mistake. cd "$1" does not make any sense here, as $1 is (g)cc's version number
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@24351 b3059339-0415-0410-9bf9-f77b7e298cf2

--- version.sh
+++ version.sh
@@ -3,8 +3,8 @@
 test "$1" && extra="-$1"
 
 svn_revision=`LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
-test $svn_revision || svn_revision=`cd "$1" && grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
-test $svn_revision || svn_revision=`cd "$1" && sed -n -e '/^dir$/{n;p;q}' .svn/entries 2>/dev/null`
+test $svn_revision || svn_revision=`grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
+test $svn_revision || svn_revision=`sed -n -e '/^dir$/{n;p;q}' .svn/entries 2>/dev/null`
 test $svn_revision || svn_revision=UNKNOWN
 
 NEW_REVISION="#define VERSION \"dev-SVN-r${svn_revision}${extra}\""
