c85832df25c2f936df343771f4157d2d37e36fd1
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-09-10 13:19:46 +0000

    warning fix: dshow/mediatype.c:46: warning: implicit declaration of function 'printf' dshow/mediatype.c:46: warning: incompatible implicit declaration of built-in function 'printf'
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@24411 b3059339-0415-0410-9bf9-f77b7e298cf2

--- loader/dshow/mediatype.c
+++ loader/dshow/mediatype.c
@@ -20,6 +20,7 @@
  *     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  *
  */
+#include <stdio.h>
 #include "mediatype.h"
 #include "mp_msg.h"
 #include "wine/winerror.h"
