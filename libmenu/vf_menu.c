40a4f3c7c6765dbcb13a77c8a6976f688f9a4bf3
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-11-03 16:22:45 +0000

    Fix link to email explaining the paused vf_menu behaviour change
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@24957 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmenu/vf_menu.c
+++ libmenu/vf_menu.c
@@ -56,7 +56,12 @@ static int cmd_filter(mp_cmd_t* cmd, int paused, struct vf_priv_s * priv) {
 
   switch(cmd->id) {
   case MP_CMD_PAUSE :
-#if 0 // http://lists.mplayerhq.hu/pipermail/mplayer-dev-eng/2003-March/017286.html
+#if 0
+    /* disabled because of
+     * http://lists.mplayerhq.hu/pipermail/mplayer-dev-eng/2003-March/017281.html
+     * Subject: -menu & pause bug fix?
+     * Date: Sun Mar 23 11:00:49 CET 2003
+     */
     if(!paused && !go2pause) { // Initial pause cmd -> wait the next put_image
       go2pause = 1;
       return 1;
