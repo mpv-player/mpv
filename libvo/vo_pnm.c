1d22c3bf0339f81fb248219b0247714058222254
ivo <ivo@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-11-29 14:33:18 +0000

    Proper license header. Change license from strict version 2 to version 2 or later.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@25201 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vo_pnm.c
+++ libvo/vo_pnm.c
@@ -3,9 +3,21 @@
 /* 
  * vo_pnm.c, PPM/PGM/PGMYUV Video Output Driver for MPlayer
  *
- * 
  * Written by Ivo van Poorten. (C) Copyright 2004, 2005.
- * Licensed under GNU General Public License version 2.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *
  */
 
