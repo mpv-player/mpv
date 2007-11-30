32b9034e23cdbfe37ebe16b2e04dffb2fadaccfd
michael <michael@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-11-30 23:46:32 +0000

    simplify
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@25221 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/demux_rawvideo.c
+++ libmpdemux/demux_rawvideo.c
@@ -77,7 +77,7 @@ static demuxer_t* demux_rawvideo_open(demuxer_t* demuxer) {
   case IMGFMT_NV12:
   case IMGFMT_HM12:
   case IMGFMT_YV12: imgsize=width*height+2*(width>>1)*(height>>1);break;
-  case IMGFMT_YUY2: imgsize=width*height*2;break;
+  case IMGFMT_YUY2:
   case IMGFMT_UYVY: imgsize=width*height*2;break;
   case IMGFMT_Y800:
   case IMGFMT_Y8: imgsize=width*height;break;
