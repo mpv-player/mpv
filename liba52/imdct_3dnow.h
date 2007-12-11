222ea2c463a96932d17f69ae6f701a908b93c957
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-12-11 14:53:14 +0000

    Relicense as GPL v2 or later like the rest of liba52. Permission given by Nick Kurshev.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@25351 b3059339-0415-0410-9bf9-f77b7e298cf2

--- liba52/imdct_3dnow.h
+++ liba52/imdct_3dnow.h
@@ -1,8 +1,24 @@
 /*
-   3DNOW and 3DNOWEX optimized IMDCT
-   Licence: GPL v2
-   Copyrights: Nick Kurshev
-*/
+ * 3DNOW and 3DNOWEX optimized IMDCT
+ * Copyright (C) 2002 Nick Kurshev
+ *
+ * This file is part of a52dec, a free ATSC A-52 stream decoder.
+ * See http://liba52.sourceforge.net/ for updates.
+ *
+ * a52dec is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * a52dec is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #undef FFT_4_3DNOW
 #undef FFT_8_3DNOW
