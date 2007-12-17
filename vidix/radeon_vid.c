b411a839c2be3459ac236f06fd614644af32991f
ben <ben@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-12-17 19:35:30 +0000

    r25345 patched the wrong line. Here's the right fix (100l to me, i deserved them) Patch by Guillaume Lecerf.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@25443 b3059339-0415-0410-9bf9-f77b7e298cf2

--- vidix/radeon_vid.c
+++ vidix/radeon_vid.c
@@ -1138,7 +1138,6 @@ static void radeon_vid_make_default(void)
   besr.graphics_key_msk=0;
   besr.graphics_key_clr=0;
   besr.ckey_cntl = VIDEO_KEY_FN_TRUE|GRAPHIC_KEY_FN_TRUE|CMP_MIX_AND;
-  besr.merge_cntl |= 0x00000100;  /* DISP_RGB_OFFSET_EN */
 }
 
 static int find_chip(unsigned chip_id)
@@ -3400,6 +3399,7 @@ static int set_gr_key( void )
 	besr.graphics_key_msk=0;
 	besr.graphics_key_clr=0;
 	besr.ckey_cntl = VIDEO_KEY_FN_TRUE|GRAPHIC_KEY_FN_TRUE|CMP_MIX_AND;
+	besr.merge_cntl |= 0x00000100;  /* DISP_RGB_OFFSET_EN */
     }
     radeon_fifo_wait(3);
     OUTREG(OV0_GRAPHICS_KEY_MSK, besr.graphics_key_msk);
