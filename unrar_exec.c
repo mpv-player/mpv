92f52e811b6b556eeab3c7faeb20d61cacb47afb
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-12-17 00:11:33 +0000

    Use correct #include for waitpid, fixes the warning: unrar_exec.c:127: warning: implicit declaration of function 'waitpid'
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@25427 b3059339-0415-0410-9bf9-f77b7e298cf2

--- unrar_exec.c
+++ unrar_exec.c
@@ -21,7 +21,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
