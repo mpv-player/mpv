fdfc6ffdf71f340a9bf030a237433ab79f7d12f4
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-12-22 17:10:09 +0000

    cosmetics: Remove trailing whitespace.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@25502 b3059339-0415-0410-9bf9-f77b7e298cf2

--- liba52/compare.c
+++ liba52/compare.c
@@ -10,25 +10,25 @@ int main(int argc, char **argv)
 {
 	FILE *f0, *f1;
 	int dif=0;
-	
-	if(argc!=3) 
+
+	if(argc!=3)
 	{
 		printf("compare <file1> <file2>\n");
 		exit(2);
 	}
-	
+
 	f0= fopen(argv[1], "rb");
 	f1= fopen(argv[2], "rb");
-	
+
 	for(;;)
 	{
 		short c0;
 		short c1;
 		int d;
-		
+
 		int e0= fread(&c0, 2, 1, f0);
 		int e1= fread(&c1, 2, 1, f1);
-		
+
 		d=c0-c1;
 		if(e0==0 && e1==0) break;
 		if(e0==0 || e1==0)
@@ -36,20 +36,20 @@ int main(int argc, char **argv)
 			printf("FATAL error, files have different size!\n");
 			exit(1);
 		}
-		
+
 		if(d<0) d=-d; // ABS
 		if(d>1)
 		{
 			printf("FATAL error, too large a difference found (%d)!\n", d);
 			exit(1);
 		}
-		
+
 		if(d) dif++;
 	}
-	
+
 	fclose(f0);
 	fclose(f1);
-	
+
 	printf("%d (+/-1)differences found\n", dif);
 	exit(0);
 }
