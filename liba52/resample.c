ff08ff09aceb3f402a08a479d8c3347ae57bf2e3
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-12-22 19:11:54 +0000

    Add proper copyright/license headers.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@25511 b3059339-0415-0410-9bf9-f77b7e298cf2

--- liba52/resample.c
+++ liba52/resample.c
@@ -1,3 +1,26 @@
+/*
+ * resample.c
+ * Copyright (C) 2001 Árpád Gereöffy
+ *
+ * This file is part of a52dec, a free ATSC A-52 stream decoder.
+ * See http://liba52.sourceforge.net/ for updates.
+ *
+ * File added for use with MPlayer and not part of original a52dec.
+ *
+ * a52dec is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * a52dec is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 // a52_resample_init should find the requested converter (from type flags ->
 // given number of channels) and set up some function pointers...
--- liba52/resample_altivec.c
+++ liba52/resample_altivec.c
@@ -1,4 +1,26 @@
-// AltiVec support Copyright (c) 2004 Romain Dolbeau <romain@dolbeau.org>
+/*
+ * resample.c
+ * Copyright (C) 2004 Romain Dolbeau <romain@dolbeau.org>
+ *
+ * This file is part of a52dec, a free ATSC A-52 stream decoder.
+ * See http://liba52.sourceforge.net/ for updates.
+ *
+ * File added for use with MPlayer and not part of original a52dec.
+ *
+ * a52dec is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * a52dec is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #ifdef HAVE_ALTIVEC_H
 #include <altivec.h>
