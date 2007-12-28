921cef29ce5dd9bb757e7b6b778cddd6618e690e
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2007-12-28 23:12:20 +0000

    Fix warnings when compiling test application.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@25538 b3059339-0415-0410-9bf9-f77b7e298cf2

--- osdep/timer-linux.c
+++ osdep/timer-linux.c
@@ -65,7 +65,8 @@ void InitTimer(void){
 
 
 #if 0
-void main(void){
+#include <stdio.h>
+int main(void){
   float t=0;
   InitTimer();
   while(1){ t+=GetRelativeTime();printf("time= %10.6f\r",t);fflush(stdout); }
