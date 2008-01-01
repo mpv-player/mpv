4a36933f7b1babbdd7324b0ce06cecc4254e8e69
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-01-01 19:52:23 +0000

    Fix license header to read Lesser General Public License 2.1, a Lesser General Public License 2 never existed.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@25570 b3059339-0415-0410-9bf9-f77b7e298cf2

--- TOOLS/subedit.pl
+++ TOOLS/subedit.pl
@@ -12,7 +12,7 @@
 # This script is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
+# version 2.1 of the License, or (at your option) any later version.
 # Retain original credits when modifying.
 #
 # This script is distributed in the hope that it will be useful,
