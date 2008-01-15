6fed08aee33418948dcb5c3c4cdfae1ddf13df11
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-01-15 17:54:48 +0000

    Document vo gl lscale=3
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@25756 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vo_gl.c
+++ libvo/vo_gl.c
@@ -855,6 +855,7 @@ static int preinit(const char *arg)
               "    0: use standard bilinear scaling for luma.\n"
               "    1: use improved bicubic scaling for luma.\n"
               "    2: use cubic in X, linear in Y direction scaling for luma.\n"
+              "    3: as 1 but without using a lookup texture.\n"
               "  cscale=<n>\n"
               "    as lscale but for chroma (2x slower with little visible effect).\n"
               "  customprog=<filename>\n"
