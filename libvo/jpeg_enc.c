e4f2123310b7d8d327f7bbd7618b9bc46116b639
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-01-18 01:57:40 +0000

    Remove broken test program that likely never worked.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@25781 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/jpeg_enc.c
+++ libvo/jpeg_enc.c
@@ -488,29 +488,3 @@ void jpeg_enc_uninit(jpeg_enc_t *j) {
 	av_free(j->s);
 	av_free(j);
 }
-
-#if 0
-
-#define		W	32	
-#define		H	32
-
-int quant_store[MBR+1][MBC+1];
-unsigned char buf[W*H*3/2];
-char code[256*1024];
-
-
-main(void) {
-	int i, size;
-	FILE *fp;
-
-	memset(buf, 0, W*H);
-	memset(buf+W*H, 255, W*H/4);
-	memset(buf+5*W*H/4, 0, W*H/4);
-	mjpeg_encoder_init(W, H, 1, W, 1, W/2, 1, W/2, 1, 1, 0);
-
-	size = mjpeg_encode_frame(buf, buf+W*H, buf+5*W*H/4, code);
-	fp = fopen("test.jpg", "w");
-	fwrite(code, 1, size, fp);
-	fclose(fp);
-}
-#endif
