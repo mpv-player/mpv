fd35d1bfe3bbae307ae46d576be2913a470b2098
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-01-19 14:38:24 +0000

    Avoid a division by 0 if i_bps is 0.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@25799 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/demux_audio.c
+++ libmpdemux/demux_audio.c
@@ -691,7 +691,7 @@ static void demux_close_audio(demuxer_t* demuxer) {
 
 static int demux_audio_control(demuxer_t *demuxer,int cmd, void *arg){
     sh_audio_t *sh_audio=demuxer->audio->sh;
-    int audio_length = demuxer->movi_end / sh_audio->i_bps;
+    int audio_length = sh_audio->i_bps ? demuxer->movi_end / sh_audio->i_bps : 0;
     da_priv_t* priv = demuxer->priv;
 	    
     switch(cmd) {
