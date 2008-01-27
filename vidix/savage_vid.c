1af6d4f5909d4fd5c5e0fd92386b5bd0f3def047
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-01-27 17:40:38 +0000

    Change #warning to FIXME comments.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@25888 b3059339-0415-0410-9bf9-f77b7e298cf2

--- vidix/savage_vid.c
+++ vidix/savage_vid.c
@@ -544,7 +544,7 @@ static void SavageInitStreamsOld(void)
 	    case 16: format = 5 << 24; break;
 	    case 24: format = 7 << 24; break;
 	}
-#warning enable this again
+	/* FIXME: enable this again */
 	OUTREG(PSTREAM_FBSIZE_REG, 
 		info->screen_y * info->screen_x * (info->bpp >> 3));
     
@@ -572,7 +572,7 @@ static void SavageInitStreamsOld(void)
     OUTREG( SSTREAM_VSCALE_REG, 0 );
     OUTREG( SSTREAM_LINES_REG, 0 );
     OUTREG( SSTREAM_VINITIAL_REG, 0 );
-#warning is this needed?
+    /* FIXME: Is this needed? */
     OUTREG( SSTREAM_WINDOW_START_REG, OS_XY(0xfffe, 0xfffe) );
     OUTREG( SSTREAM_WINDOW_SIZE_REG, OS_WH(10,2) );
 
