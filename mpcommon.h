c767c782c05ed17a09ce596e2813a0659c2dd1b8
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-02-20 19:03:16 +0000

    Fix compilation with ASS disabled
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@26040 b3059339-0415-0410-9bf9-f77b7e298cf2

--- mpcommon.h
+++ mpcommon.h
@@ -2,7 +2,7 @@
 #define MPCOMMON_H
 
 extern double sub_last_pts;
-extern ass_track_t *ass_track;
+extern struct ass_track_s *ass_track;
 extern subtitle *vo_sub_last;
 void update_subtitles(sh_video_t *sh_video, demux_stream_t *d_dvdsub, int reset);
 void update_teletext(sh_video_t *sh_video, demuxer_t *demuxer, int reset);
