9c30916c7a74ad1d3e05ff18648948d138633962
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-02-25 09:45:55 +0000

    10l: Revert nonsense commit.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@26100 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/vf_pp.c
+++ libmpcodecs/vf_pp.c
@@ -20,7 +20,12 @@
 #ifdef USE_LIBPOSTPROC_SO
 #include <postproc/postprocess.h>
 #elif defined(USE_LIBPOSTPROC)
+#define EMU_OLD
 #include "libpostproc/postprocess.h"
+
+#ifdef EMU_OLD
+#include "libpostproc/postprocess_internal.h"
+#endif
 #endif
 
 struct vf_priv_s {
