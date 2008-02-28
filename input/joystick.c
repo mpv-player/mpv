a21190b9d3ba6bdf72097077963db9bd40bc6dad
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-02-28 18:32:17 +0000

    joystick.c is only ever compiled on Linux, remove pointless #ifdef around the whole file and dummy functions.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@26112 b3059339-0415-0410-9bf9-f77b7e298cf2

--- input/joystick.c
+++ input/joystick.c
@@ -24,8 +24,6 @@
 #define JS_DEV "/dev/input/js0"
 #endif
 
-#ifdef TARGET_LINUX
-
 #include <linux/joystick.h>
 
 int axis[256];
@@ -145,18 +143,3 @@ int mp_input_joystick_read(int fd) {
 
   return MP_INPUT_NOTHING;
 }
-
-#else /* TARGET_LINUX */
-
-// dummy function
-
-int mp_input_joystick_init(char* dev) {
-  return -1;
-}
-
-int mp_input_joystick_read(int fd) {
-  
-  return MP_INPUT_NOTHING;
-}
-
-#endif /* TARGET_LINUX */
