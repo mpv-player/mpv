622540e56f6d0519e41c9ed72c5cdcba2163a336
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-03-04 21:14:05 +0000

    Remove pointless extern keyword from function declaration.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@26161 b3059339-0415-0410-9bf9-f77b7e298cf2

--- parser-mpcmd.h
+++ parser-mpcmd.h
@@ -1,6 +1,6 @@
 #ifndef MPLAYER_PARSER_MPCMD_H
 #define MPLAYER_PARSER_MPCMD_H
 
-extern play_tree_t* m_config_parse_mp_command_line(m_config_t *config, int argc, char **argv);
+play_tree_t* m_config_parse_mp_command_line(m_config_t *config, int argc, char **argv);
 
 #endif /* MPLAYER_PARSER_MPCMD_H */
