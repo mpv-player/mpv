6b8b2a51e5fb84713599d53587faa7e7d9090540
ulion <ulion@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-03-05 02:46:50 +0000

    Fix ffvorbis decoder's output channel order with channel reordering function.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@26164 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/ad_ffmpeg.c
+++ libmpcodecs/ad_ffmpeg.c
@@ -175,6 +175,8 @@ static int decode_audio(sh_audio_t *sh_audio,unsigned char *buf,int minlen,int m
               src_ch_layout = AF_CHANNEL_LAYOUT_AAC_DEFAULT;
             else if (!strcasecmp(codec, "liba52"))
               src_ch_layout = AF_CHANNEL_LAYOUT_LAVC_LIBA52_DEFAULT;
+            else if (!strcasecmp(codec, "vorbis"))
+              src_ch_layout = AF_CHANNEL_LAYOUT_LAVC_VORBIS_DEFAULT;
             else
               src_ch_layout = AF_CHANNEL_LAYOUT_MPLAYER_DEFAULT;
             reorder_channel_nch(buf, src_ch_layout,
