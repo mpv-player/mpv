54c48f18a1c448ffd136846fea9dafecc0ac8ae1
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-03-29 11:35:29 +0000

    Fix possible integer overflow in malloc by using calloc instead. Should fix CVE-2008-0073 as far as MPlayer is affected by this problem.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@26299 b3059339-0415-0410-9bf9-f77b7e298cf2

--- stream/realrtsp/sdpplin.c
+++ stream/realrtsp/sdpplin.c
@@ -330,7 +330,8 @@ sdpplin_t *sdpplin_parse(char *data) {
     
     if(filter(data,"a=StreamCount:integer;",&buf)) {
       desc->stream_count=(unsigned int)atoi(buf);
-      desc->stream=malloc(sizeof(sdpplin_stream_t*)*desc->stream_count);
+      desc->stream=calloc(desc->stream_count, sizeof(sdpplin_stream_t*));
+      if (!desc->stream) desc->stream_count = 0;
       handled=1;
       data=nl(data);
     }
