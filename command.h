2b2792565e5a0c6686c9238631e621969fec2d2c
uau <uau@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-03-31 04:10:54 +0000

    command.h: Remove unnecessary includes
    Remove #include of "mp_core.h" and "input/input.h". Their only use was
    that functions declared in command.h took pointers to structs defined
    in those headers. Declare the structs directly as incomplete types
    instead.
    
    
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@26305 b3059339-0415-0410-9bf9-f77b7e298cf2

--- command.h
+++ command.h
@@ -1,10 +1,10 @@
 #ifndef MPLAYER_COMMAND_H
 #define MPLAYER_COMMAND_H
 
-#include "mp_core.h"
-#include "input/input.h"
+struct MPContext;
+struct mp_cmd;
 
-int run_command(struct MPContext *mpctx, mp_cmd_t *cmd);
+int run_command(struct MPContext *mpctx, struct mp_cmd *cmd);
 char *property_expand_string(struct MPContext *mpctx, char *str);
 void property_print_help(void);
 
