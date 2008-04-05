e826cc2d72f7fb4ab1e1ba4687d247d63aeaef12
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-04-05 09:19:55 +0000

    Set demuxer->audio->id to avoid breakage due to r26301
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@26327 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/demux_nsv.c
+++ libmpdemux/demux_nsv.c
@@ -181,6 +181,7 @@ static demuxer_t* demux_open_nsv ( demuxer_t* demuxer )
         // PCM fourcc needs extra parsing for every audio chunk, yet to implement
         if((demuxer->audio->id != -2) && strncmp(hdr+8,"NONE", 4)){//&&strncmp(hdr+8,"VLB ", 4)){
             sh_audio = new_sh_audio ( demuxer, 0 );
+            demuxer->audio->id = 0;
             demuxer->audio->sh = sh_audio;
             sh_audio->format=mmioFOURCC(hdr[8],hdr[9],hdr[10],hdr[11]);
             sh_audio->ds = demuxer->audio;
