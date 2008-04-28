f800a42e45fe47fa9c5ad51094adebb5ea662a0d
Uoti Urpala <uau@symbol.nonexistent.invalid>
2008-04-28 12:12:56 +0300

    osdep: Remove cruft
    Delete some commented out or otherwise unused lines.

--- osdep/timer.h
+++ osdep/timer.h
@@ -6,13 +6,7 @@ extern const char timer_name[];
 void InitTimer(void);
 unsigned int GetTimer(void);
 unsigned int GetTimerMS(void);
-//int uGetTimer();
 
 int usec_sleep(int usec_delay);
-
-/* timer's callback handling */
-typedef void timer_callback( void );
-extern unsigned set_timer_callback(unsigned ms,timer_callback func);
-extern void restore_timer(void);
 
 #endif /* MPLAYER_TIMER_H */
