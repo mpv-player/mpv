6f0085c08733ac012a63319bfe847e79d7fa7c09
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-05-03 11:19:27 +0000

    Remove modification notes from unmodified files.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@26651 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpeg2/idct.c
+++ libmpeg2/idct.c
@@ -19,10 +19,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Modified for use with MPlayer, see libmpeg-0.4.1.diff for the exact changes.
- * detailed changelog at http://svn.mplayerhq.hu/mplayer/trunk/
- * $Id$
  */
 
 #include "config.h"
