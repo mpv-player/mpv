e9de5ea8dfd94ce8da8247dfd52e552ce69df268
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-05-07 07:49:35 +0000

    Relicense file as GPL v2+; bero granted permission on IRC.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@26679 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/ve_x264.c
+++ libmpcodecs/ve_x264.c
@@ -7,21 +7,21 @@
  *
  * Written by Bernhard Rosenkraenzer <bero@arklinux.org>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation, or if, and only if,
- * version 2 is ruled invalid in a court of law, any later version
- * of the GNU General Public License published by the Free Software
- * Foundation.
+ * This file is part of MPlayer.
  *
- * This program is distributed in the hope that it will be useful,
+ * MPlayer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MPlayer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * You should have received a copy of the GNU General Public License along
+ * with MPlayer; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  *****************************************************************************/
 
