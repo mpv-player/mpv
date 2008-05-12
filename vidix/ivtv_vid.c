233ac8d3192f3fc3f7e76d5393cc6f479adb7da9
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-05-12 17:39:36 +0000

    Use standard license headers.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@26751 b3059339-0415-0410-9bf9-f77b7e298cf2

--- vidix/ivtv_vid.c
+++ vidix/ivtv_vid.c
@@ -1,24 +1,25 @@
-/**
-    VIDIX driver for Hauppauge PVR 350.
-
-    Copyright 2007 Lutz Koschorreck.
-
-    Based on genfb_vid.c and ivtv_xv.c
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-**/
+/*
+ * VIDIX driver for Hauppauge PVR 350
+ *
+ * Copyright 2007 Lutz Koschorreck
+ * Based on genfb_vid.c and ivtv_xv.c
+ *
+ * This file is part of MPlayer.
+ *
+ * MPlayer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MPlayer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with MPlayer; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #include <errno.h>
 #include <stdio.h>
--- vidix/sis_bridge.c
+++ vidix/sis_bridge.c
@@ -18,9 +18,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with MPlayer; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * You should have received a copy of the GNU General Public License along
+ * with MPlayer; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #include <stdio.h>
