47ef5936172737584a9a37f9f7b0b70b2f1ddccb
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-05-13 10:16:20 +0000

    Use standard license headers.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@26759 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/ve_xvid4.c
+++ libmpcodecs/ve_xvid4.c
@@ -1,25 +1,25 @@
-/*****************************************************************************
+/*
+ * - XviD 1.x decoder module for mplayer/mencoder -
  *
- *  - XviD 1.x export module for mplayer/mencoder -
+ * Copyright(C) 2003      Marco Belli <elcabesa@inwind.it>
+ *              2003-2004 Edouard Gomez <ed.gomez@free.fr>
  *
- *  Copyright(C) 2003      Marco Belli <elcabesa@inwind.it>
- *               2003-2004 Edouard Gomez <ed.gomez@free.fr>
+ * This file is part of MPlayer.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * MPlayer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * MPlayer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- *
- ****************************************************************************/
+ * You should have received a copy of the GNU General Public License along
+ * with MPlayer; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 /*****************************************************************************
  * Includes
--- libmpcodecs/vf_kerndeint.c
+++ libmpcodecs/vf_kerndeint.c
@@ -1,20 +1,23 @@
 /*
-    Original AVISynth Filter Copyright (C) 2003 Donald A. Graft
-    Adapted to MPlayer by Tobias Diedrich
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-*/
+ * Original AVISynth Filter Copyright (C) 2003 Donald A. Graft
+ *  Adapted to MPlayer by Tobias Diedrich
+ *
+ * This file is part of MPlayer.
+ *
+ * MPlayer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MPlayer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with MPlayer; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
