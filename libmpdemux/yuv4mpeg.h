0c6e4c6dbdae7c854e7bef99fa88c88f96a3030b
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-05-14 18:11:45 +0000

    10l: Revert license header cleanup on imported files.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@26776 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/yuv4mpeg.h
+++ libmpdemux/yuv4mpeg.h
@@ -1,27 +1,27 @@
 /*
- * functions for reading and writing "new" YUV4MPEG2 streams
- * Stream format is described at the end of this file.
+ *  yuv4mpeg.h:  Functions for reading and writing "new" YUV4MPEG2 streams.
  *
- * Copyright (C) 2001 Matthew J. Marjanovic <maddog@mir.com>
+ *               Stream format is described at the end of this file.
  *
- * This file is ripped from the lavtools package (mjpeg.sourceforge.net)
- * Ported to mplayer by Rik Snel <rsnel@cube.dyndns.org>
  *
- * This file is part of MPlayer.
+ *  Copyright (C) 2001 Matthew J. Marjanovic <maddog@mir.com>
  *
- * MPlayer is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ *  This file is ripped from the lavtools package (mjpeg.sourceforge.net)
+ *  Ported to mplayer by Rik Snel <rsnel@cube.dyndns.org>
  *
- * MPlayer is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
  *
- * You should have received a copy of the GNU General Public License along
- * with MPlayer; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 #ifndef MPLAYER_YUV4MPEG_H
