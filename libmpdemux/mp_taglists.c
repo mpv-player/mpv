3ea9517367737804bdce10cea1206f56eeb0b868
compn <compn@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-05-16 23:31:05 +0000

    add ffmpeg ea maxis xa adpcm audio decoder
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@26792 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/mp_taglists.c
+++ libmpdemux/mp_taglists.c
@@ -24,6 +24,7 @@
 static const AVCodecTag mp_wav_tags[] = {
     { CODEC_ID_ADPCM_4XM,         MKTAG('4', 'X', 'M', 'A')},
     { CODEC_ID_ADPCM_EA,          MKTAG('A', 'D', 'E', 'A')},
+    { CODEC_ID_ADPCM_EA_MAXIS_XA, MKTAG('A', 'D', 'X', 'A')},
     { CODEC_ID_ADPCM_IMA_WS,      MKTAG('A', 'I', 'W', 'S')},
     { CODEC_ID_ADPCM_THP,         MKTAG('T', 'H', 'P', 'A')},
     { CODEC_ID_AMR_NB,            MKTAG('n', 'b',   0,   0)},
