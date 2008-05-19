bbfb15653962dce67c7233a1fe4d6ddb4ee2fa6c
eugeni <eugeni@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-05-19 18:37:27 +0000

    In case 2 styles have the same name, prefer the latest one.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@26839 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libass/ass.c
+++ libass/ass.c
@@ -163,7 +163,7 @@ static void rskip_spaces(char** str, char* limit) {
 static int lookup_style(ass_track_t* track, char* name) {
 	int i;
 	if (*name == '*') ++name; // FIXME: what does '*' really mean ?
-	for (i=0; i<track->n_styles; ++i) {
+	for (i = track->n_styles - 1; i >= 0; --i) {
 		// FIXME: mb strcasecmp ?
 		if (strcmp(track->styles[i].Name, name) == 0)
 			return i;
