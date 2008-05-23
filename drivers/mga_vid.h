42918f96337176a4ebbadcbcec59026bae406b36
attila <attila@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-05-23 19:46:41 +0000

    revert changes 26035 and 26061
    mga_vid is _NOT_ part of MPlayer, although it has been
    historicaly developed in the same repo. 
    
    
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@26861 b3059339-0415-0410-9bf9-f77b7e298cf2

--- drivers/mga_vid.h
+++ drivers/mga_vid.h
@@ -1,28 +1,19 @@
 /*
- * Matrox MGA G200/G400 YUV Video Interface module Version 0.1.0
- * BES == Back End Scaler
+ *
+ * mga_vid.h
  *
  * Copyright (C) 1999 Aaron Holtzman
- *
- * This file is part of MPlayer.
- *
- * MPlayer is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * MPlayer is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with MPlayer; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+ * Matrox MGA G200/G400 YUV Video Interface module Version 0.1.0
+ * 
+ * BES == Back End Scaler
+ * 
+ * This software has been released under the terms of the GNU Public
+ * license. See http://www.gnu.org/copyleft/gpl.html for details.
  */
 
-#ifndef MPLAYER_MGA_VID_H
-#define MPLAYER_MGA_VID_H
+#ifndef MGA_VID_H
+#define MGA_VID_H
 
 typedef struct mga_vid_config_s
 {
@@ -65,4 +56,4 @@ uint32_t num_frames;
 
 #define MGA_VID_VERSION 0x0201
 
-#endif /* MPLAYER_MGA_VID_H */
+#endif /* MGA_VID_H */
