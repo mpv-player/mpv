6a55bbb4a21f4a0ddf8afce5806dc01466b35cb7
ben <ben@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-06-20 19:51:43 +0000

    Only "pop" subtree params if they had previously been "pushed", and afterwards reset the "pushed" value to 0 again. Similarly only set the PLAY_TREE_RND_PLAYED flag if the entry had been pushed before.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@27106 b3059339-0415-0410-9bf9-f77b7e298cf2

--- playtree.c
+++ playtree.c
@@ -710,8 +710,9 @@ play_tree_iter_up_step(play_tree_iter_t* iter, int d,int with_nodes) {
   iter->tree = iter->tree->parent;
 
   // Pop subtree params
-  if(iter->config) {
+  if(iter->config && iter->entry_pushed > 0) {
     m_config_pop(iter->config);
+    iter->entry_pushed = 0;
     if(iter->mode == PLAY_TREE_ITER_RND)
       iter->tree->flags |= PLAY_TREE_RND_PLAYED;
   }
