88a044276569d7a9da7b5539725bb8f4dbc7ba74
eugeni <eugeni@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-06-23 14:46:55 +0000

    Check if the font set returned from FcFontSort in not NULL.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@27128 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libass/ass_fontconfig.c
+++ libass/ass_fontconfig.c
@@ -129,6 +129,8 @@ static char* _select_font(fc_instance_t* priv, const char* family, unsigned bold
 		goto error;
 
 	fset = FcFontSort(priv->config, pat, FcTrue, NULL, &result);
+	if (!fset)
+		goto error;
 
 	for (curf = 0; curf < fset->nfont; ++curf) {
 		FcPattern* curp = fset->fonts[curf];
