a078ef894caac98ecbe680c6d9fc571c605ddd7c
zuxy <zuxy@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-07-17 10:07:21 +0000

    Force gcc to emit function body under -gnu99
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@27306 b3059339-0415-0410-9bf9-f77b7e298cf2

--- gui/win32/gui.c
+++ gui/win32/gui.c
@@ -1069,7 +1069,7 @@ static LRESULT CALLBACK EventProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM
     return DefWindowProc(hWnd, message, wParam, lParam);
 }
 
-inline void startplay(gui_t *gui)
+__attribute__((gnu_inline)) inline void startplay(gui_t *gui)
 {
     handlemsg(gui->mainwindow, evDropFile);
 }
