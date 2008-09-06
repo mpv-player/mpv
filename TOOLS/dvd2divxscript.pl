5e5c1feb69bd6b6869d866e09d1c6099af070a99
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-09-06 18:39:12 +0000

    license header cosmetics
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@27536 b3059339-0415-0410-9bf9-f77b7e298cf2

--- TOOLS/dvd2divxscript.pl
+++ TOOLS/dvd2divxscript.pl
@@ -12,14 +12,14 @@
 # thankx to all the mplayer developers for this really *great* piece of software
 #
 #
-#  This script is free software; you can redistribute it and/or
+# This script is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2 of the License, or (at your option) any later version.
 #
 # This script is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public
