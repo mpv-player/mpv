e00c1a9e6de39a6b4d5ab683c519bb32602324b4
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-10-13 14:47:11 +0000

    Remove redundantly declared definitions FILE_ANY_ACCESS and CTL_CODE, fixes: vidix/sysdep/libdha_win32.c:34:1: warning: "FILE_ANY_ACCESS" redefined vidix/sysdep/libdha_win32.c:35:1: warning: "CTL_CODE" redefined
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@27755 b3059339-0415-0410-9bf9-f77b7e298cf2

--- vidix/sysdep/libdha_win32.c
+++ vidix/sysdep/libdha_win32.c
@@ -31,9 +31,6 @@ typedef struct MapDevRequest
 /*#include "winioctl.h"*/
 #define FILE_DEVICE_UNKNOWN             0x00000022
 #define METHOD_NEITHER                  3
-#define FILE_ANY_ACCESS                 0
-#define CTL_CODE( DeviceType, Function, Method, Access ) ( \
-    ((DeviceType)<<16) | ((Access)<<14) | ((Function)<<2) | (Method) )
 
     
 int IsWinNT(){
