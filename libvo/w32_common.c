c4386a8333c19e28005d7a47aa2a34c60e139912
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2008-12-03 10:38:50 +0000

    Do not override the vo_dwidth/vo_dheight values in vo_w32_config in -wid mode because we ignore the requested size in that case.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@28072 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/w32_common.c
+++ libvo/w32_common.c
@@ -380,10 +380,13 @@ int vo_w32_config(uint32_t width, uint32_t height, uint32_t flags) {
     o_dwidth = width;
     o_dheight = height;
 
+    if (WinID < 0) {
+    // the desired size is ignored in wid mode, it always matches the window size.
     prev_width = vo_dwidth = width;
     prev_height = vo_dheight = height;
     prev_x = vo_dx;
     prev_y = vo_dy;
+    }
 
     vo_fs = flags & VOFLAG_FULLSCREEN;
     vo_vm = flags & VOFLAG_MODESWITCHING;
