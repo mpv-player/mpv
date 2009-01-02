21f5949d4a662370e72a9fd8e5838cd4022c9bf8
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-01-02 11:46:34 +0000

    Relicense to GPLv2 or later with the author's permission.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@28230 b3059339-0415-0410-9bf9-f77b7e298cf2

--- TOOLS/realcodecs/28_8.c
+++ TOOLS/realcodecs/28_8.c
@@ -1,9 +1,23 @@
 /*
-   GPL v2 blah blah
-   
-   This is a small dll that works as a wrapper for the actual real28_8.so.6.0
-   dll from real player 8.0. 
-*/
+ * This is a small DLL that works as a wrapper for the actual real28_8.so.6.0
+ * DLL from RealPlayer 8.0.
+ *
+ * This file is part of MPlayer.
+ *
+ * MPlayer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MPlayer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with MPlayer; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 /*
    Assuming that RACloseCodec is the last call.
