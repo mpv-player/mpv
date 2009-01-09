f506bc4646f63ca203466bd2de5e3ff8721cf2a6
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-01-09 09:52:20 +0000

    Replace reference to LICENSE file with GPL notice from said file.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@28282 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/genres.h
+++ libmpdemux/genres.h
@@ -1,6 +1,20 @@
 /***********************************
  * Copyright (C) 2001  Jason Carter
- * See the file "LICENSE" for details
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
  ***********************************
  * FILE: GENRES.H
  * NAME: ID3EDIT - ID3 tag v 1.1 editor
