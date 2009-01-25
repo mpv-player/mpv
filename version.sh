fc8435ee6d830070c72913224f10ffa10c0d4323
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-01-25 23:31:45 +0000

    Drop dev- prefix from printed version number, just SVN-rXXXXX is enough.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@28367 b3059339-0415-0410-9bf9-f77b7e298cf2

--- version.sh
+++ version.sh
@@ -7,9 +7,9 @@ test $svn_revision || svn_revision=`grep revision .svn/entries 2>/dev/null | cut
 test $svn_revision || svn_revision=`sed -n -e '/^dir$/{n;p;q;}' .svn/entries 2>/dev/null`
 test $svn_revision || svn_revision=UNKNOWN
 
-NEW_REVISION="#define VERSION \"dev-SVN-r${svn_revision}${extra}\""
+NEW_REVISION="#define VERSION \"SVN-r${svn_revision}${extra}\""
 OLD_REVISION=`cat version.h 2> /dev/null`
-TITLE="#define MP_TITLE \"%s dev-SVN-r${svn_revision}${extra} (C) 2000-2009 MPlayer Team\\\n\""
+TITLE="#define MP_TITLE \"%s SVN-r${svn_revision}${extra} (C) 2000-2009 MPlayer Team\\\n\""
 
 # Update version.h only on revision changes to avoid spurious rebuilds
 if test "$NEW_REVISION" != "$OLD_REVISION"; then
