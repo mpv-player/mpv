be5a9adfbf460276864ffb41e6edeee70bc305de
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-01-29 00:18:32 +0000

    Use single quotes to avoid escaping double quotes in a string.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@28385 b3059339-0415-0410-9bf9-f77b7e298cf2

--- version.sh
+++ version.sh
@@ -9,7 +9,7 @@ test $svn_revision || svn_revision=UNKNOWN
 
 NEW_REVISION="#define VERSION \"SVN-r${svn_revision}${extra}\""
 OLD_REVISION=`cat version.h 2> /dev/null`
-TITLE="#define MP_TITLE \"%s \"VERSION\" (C) 2000-2009 MPlayer Team\n\""
+TITLE='#define MP_TITLE "%s "VERSION" (C) 2000-2009 MPlayer Team\n"'
 
 # Update version.h only on revision changes to avoid spurious rebuilds
 if test "$NEW_REVISION" != "$OLD_REVISION"; then
