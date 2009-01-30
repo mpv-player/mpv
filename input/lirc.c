57ff94b5cdf869298672728b3c5ed8932202ce98
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-01-30 15:41:45 +0000

    Move setting of O_NONBLOCK before lirc_readconfig, this avoids a memleak due to not freeing the lirc config on error.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@28390 b3059339-0415-0410-9bf9-f77b7e298cf2

--- input/lirc.c
+++ input/lirc.c
@@ -46,17 +46,17 @@ mp_input_lirc_init(void) {
     return -1;
   }
 
-  if(lirc_readconfig( lirc_configfile,&lirc_config,NULL )!=0 ){
-    mp_msg(MSGT_LIRC,MSGL_ERR,MSGTR_LIRCcfgerr,
-		    lirc_configfile == NULL ? "~/.lircrc" : lirc_configfile);
-    lirc_deinit();
-    return -1;
-  }
-
   mode = fcntl(lirc_sock, F_GETFL);
   if (mode < 0 || fcntl(lirc_sock, F_SETFL, mode | O_NONBLOCK) < 0) {
     mp_msg(MSGT_LIRC, MSGL_ERR, "setting non-blocking mode failed: %s\n",
 	strerror(errno));
+    lirc_deinit();
+    return -1;
+  }
+
+  if(lirc_readconfig( lirc_configfile,&lirc_config,NULL )!=0 ){
+    mp_msg(MSGT_LIRC,MSGL_ERR,MSGTR_LIRCcfgerr,
+		    lirc_configfile == NULL ? "~/.lircrc" : lirc_configfile);
     lirc_deinit();
     return -1;
   }
