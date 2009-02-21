db74aa4315402c814eca56b38516e135d1057562
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-02-21 11:44:10 +0000

    100l, place vdpau below xv, it should not normally be preferred for auto-selection (yet).
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@28688 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/video_out.c
+++ libvo/video_out.c
@@ -170,11 +170,11 @@ const vo_functions_t* const video_out_drivers[] =
 #ifdef CONFIG_3DFX
         &video_out_3dfx,
 #endif
-#if CONFIG_VDPAU
-        &video_out_vdpau,
-#endif
 #ifdef CONFIG_XV
         &video_out_xv,
+#endif
+#if CONFIG_VDPAU
+        &video_out_vdpau,
 #endif
 #ifdef CONFIG_X11
         &video_out_x11,
