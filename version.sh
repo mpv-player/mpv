9e8a06bc4e1731a4b3cef61b2818cef130b27f74
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-02-24 22:57:55 +0000

    cosmetics: Use $() instead of ``, the former can be nested more easily.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@28726 b3059339-0415-0410-9bf9-f77b7e298cf2

--- version.sh
+++ version.sh
@@ -2,13 +2,13 @@
 
 test "$1" && extra="-$1"
 
-svn_revision=`LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
-test $svn_revision || svn_revision=`grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
-test $svn_revision || svn_revision=`sed -n -e '/^dir$/{n;p;q;}' .svn/entries 2>/dev/null`
+svn_revision=$(LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2)
+test $svn_revision || svn_revision=$(grep revision .svn/entries 2>/dev/null | cut -d '"' -f2)
+test $svn_revision || svn_revision=$(sed -n -e '/^dir$/{n;p;q;}' .svn/entries 2>/dev/null)
 test $svn_revision || svn_revision=UNKNOWN
 
 NEW_REVISION="#define VERSION \"SVN-r${svn_revision}${extra}\""
-OLD_REVISION=`cat version.h 2> /dev/null`
+OLD_REVISION=$(cat version.h 2> /dev/null)
 TITLE='#define MP_TITLE "%s "VERSION" (C) 2000-2009 MPlayer Team\n"'
 
 # Update version.h only on revision changes to avoid spurious rebuilds
