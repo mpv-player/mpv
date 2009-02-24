d1e6b2360c90db4293f3ff40a92c8e4dfa331679
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-02-24 23:15:26 +0000

    Read revision string from the file snapshot_version if available. This will be used by daily snapshots without Subversion metadata.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@28728 b3059339-0415-0410-9bf9-f77b7e298cf2

--- version.sh
+++ version.sh
@@ -2,7 +2,10 @@
 
 test "$1" && extra="-$1"
 
-svn_revision=$(LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2)
+# Extract revision number from file used by daily tarball snapshots
+# or from the places different Subversion versions have it.
+svn_revision=$(cat snapshot_version 2> /dev/null)
+test $svn_revision || svn_revision=$(LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2)
 test $svn_revision || svn_revision=$(grep revision .svn/entries 2>/dev/null | cut -d '"' -f2)
 test $svn_revision || svn_revision=$(sed -n -e '/^dir$/{n;p;q;}' .svn/entries 2>/dev/null)
 test $svn_revision || svn_revision=UNKNOWN
