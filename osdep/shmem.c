86a2eadf4c5a610177fd290f588713d9714d11b2
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-03-01 13:13:25 +0000

    Add standard license headers to files.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@28779 b3059339-0415-0410-9bf9-f77b7e298cf2

--- osdep/shmem.c
+++ osdep/shmem.c
@@ -1,9 +1,24 @@
 /*
- *   shmem.c - Shared memory allocation
- *   
- *   based on mpg123's xfermem.c by
- *   Oliver Fromme  <oliver.fromme@heim3.tu-clausthal.de>
- *   Sun Apr  6 02:26:26 MET DST 1997
+ * shared memory allocation
+ *
+ * based on mpg123's xfermem.c by
+ * Oliver Fromme <oliver.fromme@heim3.tu-clausthal.de>
+ *
+ * This file is part of MPlayer.
+ *
+ * MPlayer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MPlayer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with MPlayer; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #include "config.h"
