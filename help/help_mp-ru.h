cddd44a99629b493e9d2edff01a8e7d4c728cc84
bircoph <bircoph@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-03-15 12:53:40 +0000

    Update sync tag. All *.h files were actually changed in r28860.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@28960 b3059339-0415-0410-9bf9-f77b7e298cf2

--- help/help_mp-ru.h
+++ help/help_mp-ru.h
@@ -2,7 +2,7 @@
 // Dmitry Baryshkov <mitya@school.ioffe.ru>
 // Reworked by: Andrew Savchenko aka Bircoph <Bircoph[at]list[dot]ru>
 
-// Synced with help_mp-en.h: r28122
+// Synced with help_mp-en.h: r28860
 
 
 // ========================= MPlayer help ===========================
