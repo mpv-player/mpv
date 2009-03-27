98c7abd005003196f8042f471286d8da99296459
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-03-27 20:33:13 +0000

    Directly include libavutil/x86_cpu.h in cpudetect.h instead of duplicating it incompletely.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@29076 b3059339-0415-0410-9bf9-f77b7e298cf2

--- cpudetect.h
+++ cpudetect.h
@@ -8,33 +8,7 @@
 #define CPUTYPE_I586	5
 #define CPUTYPE_I686    6
 
-#if ARCH_X86_64
-#  define REGa    rax
-#  define REGb    rbx
-#  define REGBP   rbp
-#  define REGSP   rsp
-#  define REG_a  "rax"
-#  define REG_b  "rbx"
-#  define REG_c  "rcx"
-#  define REG_d  "rdx"
-#  define REG_S  "rsi"
-#  define REG_D  "rdi"
-#  define REG_SP "rsp"
-#  define REG_BP "rbp"
-#else
-#  define REGa    eax
-#  define REGb    ebx
-#  define REGBP   ebp
-#  define REGSP   esp
-#  define REG_a  "eax"
-#  define REG_b  "ebx"
-#  define REG_c  "ecx"
-#  define REG_d  "edx"
-#  define REG_S  "esi"
-#  define REG_D  "edi"
-#  define REG_SP "esp"
-#  define REG_BP "ebp"
-#endif
+#include "libavutil/x86_cpu.h"
 
 typedef struct cpucaps_s {
 	int cpuType;
