df1af7047583d6b963301495490b12aad178f1e5
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-03-28 15:53:32 +0000

    Simplify extracting title number from ifo name
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@29082 b3059339-0415-0410-9bf9-f77b7e298cf2

--- stream/stream_dvd.c
+++ stream/stream_dvd.c
@@ -1108,12 +1108,8 @@ static int ifo_stream_open (stream_t *stream, int mode, void *opts, int *file_fo
 
     spriv=calloc(1, sizeof(struct stream_priv_s));
     spriv->device = strdup(dirname(stream->url));
-    if(!strncasecmp(filename,"vts_",4))
-    {
-        if(sscanf(filename+3, "_%02d_", &spriv->title)!=1)
+        if(sscanf(filename, "vts_%02d_", &spriv->title)!=1)
             spriv->title=1;
-    }else
-        spriv->title=1;
 
     free(filename);
     free(stream->url);
