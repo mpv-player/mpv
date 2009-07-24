de3d544c4362f04e46f1e85283a3b7d7401229e3
cladisch <cladisch@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-07-24 10:35:22 +0000

    Use the correct OSS format, where possible, for mplayer's packed 24-bit format (AFMT_x24_xE would be 32-bit aligned).
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@29438 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libao2/ao_oss.c
+++ libao2/ao_oss.c
@@ -70,17 +70,8 @@ static int format2oss(int format)
     case AF_FORMAT_U16_BE: return AFMT_U16_BE;
     case AF_FORMAT_S16_LE: return AFMT_S16_LE;
     case AF_FORMAT_S16_BE: return AFMT_S16_BE;
-#ifdef AFMT_U24_LE
-    case AF_FORMAT_U24_LE: return AFMT_U24_LE;
-#endif
-#ifdef AFMT_U24_BE
-    case AF_FORMAT_U24_BE: return AFMT_U24_BE;
-#endif
-#ifdef AFMT_S24_LE
-    case AF_FORMAT_S24_LE: return AFMT_S24_LE;
-#endif
-#ifdef AFMT_S24_BE
-    case AF_FORMAT_S24_BE: return AFMT_S24_BE;
+#ifdef AFMT_S24_PACKED
+    case AF_FORMAT_S24_LE: return AFMT_S24_PACKED;
 #endif
 #ifdef AFMT_U32_LE
     case AF_FORMAT_U32_LE: return AFMT_U32_LE;
@@ -122,17 +113,8 @@ static int oss2format(int format)
     case AFMT_U16_BE: return AF_FORMAT_U16_BE;
     case AFMT_S16_LE: return AF_FORMAT_S16_LE;
     case AFMT_S16_BE: return AF_FORMAT_S16_BE;
-#ifdef AFMT_U24_LE
-    case AFMT_U24_LE: return AF_FORMAT_U24_LE;
-#endif
-#ifdef AFMT_U24_BE
-    case AFMT_U24_BE: return AF_FORMAT_U24_BE;
-#endif
-#ifdef AFMT_S24_LE
-    case AFMT_S24_LE: return AF_FORMAT_S24_LE;
-#endif
-#ifdef AFMT_S24_BE
-    case AFMT_S24_BE: return AF_FORMAT_S24_BE;
+#ifdef AFMT_S24_PACKED
+    case AFMT_S24_PACKED: return AF_FORMAT_S24_LE;
 #endif
 #ifdef AFMT_U32_LE
     case AFMT_U32_LE: return AF_FORMAT_U32_LE;
