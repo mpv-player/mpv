2eab60ecf3e6ace1bbe700c18ce6c73f2928ac28
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-09-18 16:51:19 +0000

    Get rid of several (probably) pointless ifdefs
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@29688 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vo_gl.c
+++ libvo/vo_gl.c
@@ -435,10 +435,8 @@ static void uninitGl(void) {
     DeleteBuffers(2, gl_buffer_uv);
   gl_buffer_uv[0] = gl_buffer_uv[1] = 0; gl_buffersize_uv = 0;
   gl_bufferptr_uv[0] = gl_bufferptr_uv[1] = 0;
-#ifndef GL_WIN32
   if (mesa_bufferptr)
     FreeMemoryMESA(mDisplay, mScreen, mesa_bufferptr);
-#endif
   mesa_bufferptr = NULL;
   err_shown = 0;
 }
@@ -545,9 +543,7 @@ config(uint32_t width, uint32_t height, uint32_t d_width, uint32_t d_height, uin
   if (use_gui) {
     // GUI creates and manages window for us
     guiGetEvent(guiSetShVideo, 0);
-#ifndef GL_WIN32
     goto glconfig;
-#endif
   }
 #endif
 #ifdef GL_WIN32
@@ -777,7 +773,6 @@ static uint32_t get_image(mp_image_t *mpi) {
   mpi->stride[0] = mpi->width * mpi->bpp / 8;
   needed_size = mpi->stride[0] * mpi->height;
   if (mesa_buffer) {
-#ifndef GL_WIN32
     if (mesa_bufferptr && needed_size > mesa_buffersize) {
       FreeMemoryMESA(mDisplay, mScreen, mesa_bufferptr);
       mesa_bufferptr = NULL;
@@ -785,7 +780,6 @@ static uint32_t get_image(mp_image_t *mpi) {
     if (!mesa_bufferptr)
       mesa_bufferptr = AllocateMemoryMESA(mDisplay, mScreen, needed_size, 0, 0, 0);
     mesa_buffersize = needed_size;
-#endif
     mpi->planes[0] = mesa_bufferptr;
   } else {
     if (!gl_buffer)
