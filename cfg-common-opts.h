76cc7886d8cd484daed0e444fd150db01f0f7c0a
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-09-28 11:31:25 +0000

    Allow -1 and -2 values for -aid, -vid and -sid (meaning "auto" and "disabled" respectively).
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@29729 b3059339-0415-0410-9bf9-f77b7e298cf2

--- cfg-common-opts.h
+++ cfg-common-opts.h
@@ -128,10 +128,10 @@
 	{"loadidx", &index_file_load, CONF_TYPE_STRING, 0, 0, 0, NULL},
 
 	// select audio/video/subtitle stream
-	{"aid", &audio_id, CONF_TYPE_INT, CONF_RANGE, 0, 8190, NULL},
+	{"aid", &audio_id, CONF_TYPE_INT, CONF_RANGE, -2, 8190, NULL},
 	{"ausid", &audio_substream_id, CONF_TYPE_INT, 0, 0, 0, NULL},
-	{"vid", &video_id, CONF_TYPE_INT, CONF_RANGE, 0, 8190, NULL},
-	{"sid", &dvdsub_id, CONF_TYPE_INT, CONF_RANGE, 0, 8190, NULL},
+	{"vid", &video_id, CONF_TYPE_INT, CONF_RANGE, -2, 8190, NULL},
+	{"sid", &dvdsub_id, CONF_TYPE_INT, CONF_RANGE, -2, 8190, NULL},
 	{"novideo", &video_id, CONF_TYPE_FLAG, 0, -1, -2, NULL},
 
 	{ "hr-mp3-seek", &hr_mp3_seek, CONF_TYPE_FLAG, 0, 0, 1, NULL },
