87a4e870b706f45f348d28e3d2c7749072344392
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-11-09 14:12:14 +0000

    The code for the non-networking case is the same whether networking support was enabled or no, so avoid duplicating it.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@29861 b3059339-0415-0410-9bf9-f77b7e298cf2

--- stream/stream.c
+++ stream/stream.c
@@ -248,12 +248,9 @@ int stream_fill_buffer(stream_t *s){
 #ifdef CONFIG_NETWORK
     if( s->streaming_ctrl!=NULL && s->streaming_ctrl->streaming_read ) {
 	    len=s->streaming_ctrl->streaming_read(s->fd,s->buffer,STREAM_BUFFER_SIZE, s->streaming_ctrl);
-    } else {
-      len=read(s->fd,s->buffer,STREAM_BUFFER_SIZE);
-    }
-#else
-    len=read(s->fd,s->buffer,STREAM_BUFFER_SIZE);
+    } else
 #endif
+      len=read(s->fd,s->buffer,STREAM_BUFFER_SIZE);
     break;
   case STREAMTYPE_DS:
     len = demux_read_data((demux_stream_t*)s->priv,s->buffer,STREAM_BUFFER_SIZE);
