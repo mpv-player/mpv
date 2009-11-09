0d782226c1428214d7c73263897459a21d5636f4
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-11-09 14:08:35 +0000

    Factor out triplicated break statement.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@29860 b3059339-0415-0410-9bf9-f77b7e298cf2

--- stream/stream.c
+++ stream/stream.c
@@ -247,13 +247,14 @@ int stream_fill_buffer(stream_t *s){
   case STREAMTYPE_STREAM:
 #ifdef CONFIG_NETWORK
     if( s->streaming_ctrl!=NULL && s->streaming_ctrl->streaming_read ) {
-	    len=s->streaming_ctrl->streaming_read(s->fd,s->buffer,STREAM_BUFFER_SIZE, s->streaming_ctrl);break;
+	    len=s->streaming_ctrl->streaming_read(s->fd,s->buffer,STREAM_BUFFER_SIZE, s->streaming_ctrl);
     } else {
-      len=read(s->fd,s->buffer,STREAM_BUFFER_SIZE);break;
+      len=read(s->fd,s->buffer,STREAM_BUFFER_SIZE);
     }
 #else
-    len=read(s->fd,s->buffer,STREAM_BUFFER_SIZE);break;
+    len=read(s->fd,s->buffer,STREAM_BUFFER_SIZE);
 #endif
+    break;
   case STREAMTYPE_DS:
     len = demux_read_data((demux_stream_t*)s->priv,s->buffer,STREAM_BUFFER_SIZE);
     break;
