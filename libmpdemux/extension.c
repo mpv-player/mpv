c0aeb1c1c7d8e1ce75fdfe819d4ef0956c4058f2
compn <compn@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-12-24 18:20:03 +0000

    force cdg to lavf demuxer in extensions.c, misdetected as mpeg otherwise
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30109 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/extension.c
+++ libmpdemux/extension.c
@@ -81,6 +81,7 @@ static struct {
         { "eac3",DEMUXER_TYPE_LAVF },
         { "mac", DEMUXER_TYPE_LAVF },
         { "str", DEMUXER_TYPE_LAVF },
+        { "cdg", DEMUXER_TYPE_LAVF },
 
 // At least the following are hacks against broken autodetection
 // that should not be there
