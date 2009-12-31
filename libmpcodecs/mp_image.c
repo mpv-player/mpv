a994808e314ae5d3124e600bf233388c2feea21e
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2009-12-31 23:09:35 +0000

    Deduplicate the mp_image planes allocation code. The code in vf.c and mp_image.c is almost the same, though the one in vf.c is more up-to-date/has more bug fixes and thus is used as the basis (which is why the diff is so big).
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30154 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/mp_image.c
+++ libmpcodecs/mp_image.c
@@ -14,38 +14,48 @@
 
 #include "libvo/fastmemcpy.h"
 
+void mp_image_alloc_planes(mp_image_t *mpi) {
+  // IF09 - allocate space for 4. plane delta info - unused
+  if (mpi->imgfmt == IMGFMT_IF09) {
+    mpi->planes[0]=memalign(64, mpi->bpp*mpi->width*(mpi->height+2)/8+
+                            mpi->chroma_width*mpi->chroma_height);
+    /* export delta table */
+    mpi->planes[3]=mpi->planes[0]+(mpi->width*mpi->height)+2*(mpi->chroma_width*mpi->chroma_height);
+  } else
+    mpi->planes[0]=memalign(64, mpi->bpp*mpi->width*(mpi->height+2)/8);
+  if (mpi->flags&MP_IMGFLAG_PLANAR) {
+    int bpp = IMGFMT_IS_YUVP16(mpi->imgfmt)? 2 : 1;
+    // YV12/I420/YVU9/IF09. feel free to add other planar formats here...
+    mpi->stride[0]=bpp*mpi->width;
+    if(mpi->num_planes > 2){
+      mpi->stride[1]=mpi->stride[2]=bpp*mpi->chroma_width;
+      if(mpi->flags&MP_IMGFLAG_SWAPPED){
+        // I420/IYUV  (Y,U,V)
+        mpi->planes[1]=mpi->planes[0]+mpi->stride[0]*mpi->height;
+        mpi->planes[2]=mpi->planes[1]+mpi->stride[1]*mpi->chroma_height;
+      } else {
+        // YV12,YVU9,IF09  (Y,V,U)
+        mpi->planes[2]=mpi->planes[0]+mpi->stride[0]*mpi->height;
+        mpi->planes[1]=mpi->planes[2]+mpi->stride[1]*mpi->chroma_height;
+      }
+    } else {
+      // NV12/NV21
+      mpi->stride[1]=mpi->chroma_width;
+      mpi->planes[1]=mpi->planes[0]+mpi->stride[0]*mpi->height;
+    }
+  } else {
+    mpi->stride[0]=mpi->width*mpi->bpp/8;
+    if (mpi->flags & MP_IMGFLAG_RGB_PALETTE)
+      mpi->planes[1] = memalign(64, 1024);
+  }
+  mpi->flags|=MP_IMGFLAG_ALLOCATED;
+}
+
 mp_image_t* alloc_mpi(int w, int h, unsigned long int fmt) {
   mp_image_t* mpi = new_mp_image(w,h);
 
   mp_image_setfmt(mpi,fmt);
-  // IF09 - allocate space for 4. plane delta info - unused
-  if (mpi->imgfmt == IMGFMT_IF09)
-    {
-      mpi->planes[0]=memalign(64, mpi->bpp*mpi->width*(mpi->height+2)/8+
-			      mpi->chroma_width*mpi->chroma_height);
-      /* delta table, just for fun ;) */
-      mpi->planes[3]=mpi->planes[0]+2*(mpi->chroma_width*mpi->chroma_height);
-    }
-  else
-    mpi->planes[0]=memalign(64, mpi->bpp*mpi->width*(mpi->height+2)/8);
-  if(mpi->flags&MP_IMGFLAG_PLANAR){
-    int bpp = IMGFMT_IS_YUVP16(fmt)? 2 : 1;
-    // YV12/I420/YVU9/IF09. feel free to add other planar formats here...
-    if(!mpi->stride[0]) mpi->stride[0]=bpp*mpi->width;
-    if(!mpi->stride[1]) mpi->stride[1]=mpi->stride[2]=bpp*mpi->chroma_width;
-    if(mpi->flags&MP_IMGFLAG_SWAPPED){
-      // I420/IYUV  (Y,U,V)
-      mpi->planes[1]=mpi->planes[0]+mpi->stride[0]*mpi->height;
-      mpi->planes[2]=mpi->planes[1]+mpi->stride[1]*mpi->chroma_height;
-    } else {
-      // YV12,YVU9,IF09  (Y,V,U)
-      mpi->planes[2]=mpi->planes[0]+mpi->stride[0]*mpi->height;
-      mpi->planes[1]=mpi->planes[2]+mpi->stride[1]*mpi->chroma_height;
-    }
-  } else {
-    if(!mpi->stride[0]) mpi->stride[0]=mpi->width*mpi->bpp/8;
-  }
-  mpi->flags|=MP_IMGFLAG_ALLOCATED;
+  mp_image_alloc_planes(mpi);
 
   return mpi;
 }
--- libmpcodecs/vf.c
+++ libmpcodecs/vf.c
@@ -372,47 +372,9 @@ mp_image_t* vf_get_image(vf_instance_t* vf, unsigned int outfmt, int mp_imgtype,
 	      }
 	  }
 
-	  // IF09 - allocate space for 4. plane delta info - unused
-	  if (mpi->imgfmt == IMGFMT_IF09)
-	  {
-	     mpi->planes[0]=memalign(64, mpi->bpp*mpi->width*(mpi->height+2)/8+
-	    				mpi->chroma_width*mpi->chroma_height);
-	     /* export delta table */
-	     mpi->planes[3]=mpi->planes[0]+(mpi->width*mpi->height)+2*(mpi->chroma_width*mpi->chroma_height);
-	  }
-	  else
-	     mpi->planes[0]=memalign(64, mpi->bpp*mpi->width*(mpi->height+2)/8);
-	  if(mpi->flags&MP_IMGFLAG_PLANAR){
-	      int bpp = IMGFMT_IS_YUVP16(mpi->imgfmt)? 2 : 1;
-	      // YV12/I420/YVU9/IF09. feel free to add other planar formats here...
-	      //if(!mpi->stride[0])
-	      mpi->stride[0]=bpp*mpi->width;
-	      //if(!mpi->stride[1])
-	      if(mpi->num_planes > 2){
-	      mpi->stride[1]=mpi->stride[2]=bpp*mpi->chroma_width;
-	      if(mpi->flags&MP_IMGFLAG_SWAPPED){
-	          // I420/IYUV  (Y,U,V)
-	          mpi->planes[1]=mpi->planes[0]+mpi->stride[0]*mpi->height;
-	          mpi->planes[2]=mpi->planes[1]+mpi->stride[1]*mpi->chroma_height;
-	      } else {
-	          // YV12,YVU9,IF09  (Y,V,U)
-	          mpi->planes[2]=mpi->planes[0]+mpi->stride[0]*mpi->height;
-	          mpi->planes[1]=mpi->planes[2]+mpi->stride[1]*mpi->chroma_height;
-	      }
-	      } else {
-	          // NV12/NV21
-	          mpi->stride[1]=mpi->chroma_width;
-	          mpi->planes[1]=mpi->planes[0]+mpi->stride[0]*mpi->height;
-	      }
-	  } else {
-	      //if(!mpi->stride[0])
-	      mpi->stride[0]=mpi->width*mpi->bpp/8;
-	      if (mpi->flags & MP_IMGFLAG_RGB_PALETTE)
-	          mpi->planes[1] = memalign(64, 1024);
-	  }
+	  mp_image_alloc_planes(mpi);
 //	  printf("clearing img!\n");
 	  vf_mpi_clear(mpi,0,0,mpi->width,mpi->height);
-	  mpi->flags|=MP_IMGFLAG_ALLOCATED;
         }
     }
     if(mpi->flags&MP_IMGFLAG_DRAW_CALLBACK)
