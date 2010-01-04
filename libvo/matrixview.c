55c7b19ae1ba5fa39f4fb003e36fa6a730f46ae7
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-01-04 13:05:05 +0000

    Refer to MatrixView instead of MPlayer in code taken from MatrixView.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30202 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/matrixview.c
+++ libvo/matrixview.c
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along
- * with MPlayer; if not, write to the Free Software Foundation, Inc.,
+ * with MatrixView; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
