a91c19964de606c8002531cbb87aa6de3d89508f
cehoyos <cehoyos@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-01-09 11:33:06 +0000

    Fix MP1 with demuxer lavf in MPEG (PS) files.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30250 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/mp_taglists.c
+++ libmpdemux/mp_taglists.c
@@ -35,6 +35,7 @@ static const AVCodecTag mp_wav_tags[] = {
     { CODEC_ID_EAC3,              MKTAG('E', 'A', 'C', '3')},
     { CODEC_ID_INTERPLAY_DPCM,    MKTAG('I', 'N', 'P', 'A')},
     { CODEC_ID_MLP,               MKTAG('M', 'L', 'P', ' ')},
+    { CODEC_ID_MP1,               0x50},
     { CODEC_ID_MUSEPACK7,         MKTAG('M', 'P', 'C', ' ')},
     { CODEC_ID_MUSEPACK8,         MKTAG('M', 'P', 'C', '8')},
     { CODEC_ID_NELLYMOSER,        MKTAG('N', 'E', 'L', 'L')},
