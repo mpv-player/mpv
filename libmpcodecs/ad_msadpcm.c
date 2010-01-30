23db422c3d40a5202d61b5ca154cf5286d9ac0a5
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-01-30 16:57:40 +0000

    Add missing license headers to all files in the libmpcodecs directory.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30463 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/ad_msadpcm.c
+++ libmpcodecs/ad_msadpcm.c
@@ -1,11 +1,28 @@
 /*
-    MS ADPCM Decoder for MPlayer
-      by Mike Melanson
-
-    This file is responsible for decoding Microsoft ADPCM data.
-    Details about the data format can be found here:
-      http://www.pcisys.net/~melanson/codecs/
-*/
+ * MS ADPCM decoder
+ *
+ * This file is responsible for decoding Microsoft ADPCM data.
+ * Details about the data format can be found here:
+ *   http://www.pcisys.net/~melanson/codecs/
+ *
+ * Copyright (c) 2002 Mike Melanson
+ *
+ * This file is part of MPlayer.
+ *
+ * MPlayer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MPlayer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with MPlayer; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
