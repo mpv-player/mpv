ce3ffd42929006daf5e94256fbb468c707859f2e
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-01-30 13:35:03 +0000

    Add standard license header to C tools missing them.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30458 b3059339-0415-0410-9bf9-f77b7e298cf2

--- TOOLS/avisubdump.c
+++ TOOLS/avisubdump.c
@@ -2,9 +2,22 @@
  * avisubdump
  *
  * avi vobsub subtitle stream dumper (c) 2004 Tobias Diedrich
- * Licensed under GNU GPLv2 or (at your option) any later version.
  *
  * The subtitles are dumped to stdout.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #include <unistd.h>
