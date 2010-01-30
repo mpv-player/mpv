1e4bf6639eb23c43c14618ace924f8ff6cae05ac
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-01-30 13:22:24 +0000

    Add missing license header and multiple inclusion guards.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30456 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/osx_common.h
+++ libvo/osx_common.h
@@ -1,3 +1,26 @@
+/*
+ * This file is part of MPlayer.
+ *
+ * MPlayer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MPlayer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with MPlayer; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef MPLAYER_OSX_COMMON_H
+#define MPLAYER_OSX_COMMON_H
+
 int convert_key(unsigned key, unsigned charcode);
 void change_movie_aspect(float new_aspect);
 void config_movie_aspect(float config_aspect);
+
+#endif /* MPLAYER_OSX_COMMON_H */
