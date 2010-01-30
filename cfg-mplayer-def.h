4a05e71cc7e64f44aed3306dec113c80f1f31265
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-01-30 23:20:10 +0000

    Add missing multiple inclusion guards.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30470 b3059339-0415-0410-9bf9-f77b7e298cf2

--- cfg-mplayer-def.h
+++ cfg-mplayer-def.h
@@ -1,6 +1,10 @@
+#ifndef MPLAYER_CFG_MPLAYER_DEF_H
+#define MPLAYER_CFG_MPLAYER_DEF_H
+
 static char* default_config=
 "# Write your default config options here!\n"
 "\n"
 //"nosound=nem"
 "\n";
 
+#endif /* MPLAYER_CFG_MPLAYER_DEF_H */
