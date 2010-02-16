20b2f493bb061cbb51b32ecc4f75bc29c3f7f15f
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-02-16 21:54:50 +0000

    Support FFmpeg's ALS decoder.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30604 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/mp_taglists.c
+++ libmpdemux/mp_taglists.c
@@ -36,6 +36,7 @@ static const AVCodecTag mp_wav_tags[] = {
     { CODEC_ID_INTERPLAY_DPCM,    MKTAG('I', 'N', 'P', 'A')},
     { CODEC_ID_MLP,               MKTAG('M', 'L', 'P', ' ')},
     { CODEC_ID_MP1,               0x50},
+    { CODEC_ID_MP4ALS,            MKTAG('A', 'L', 'S', ' ')},
     { CODEC_ID_MUSEPACK7,         MKTAG('M', 'P', 'C', ' ')},
     { CODEC_ID_MUSEPACK8,         MKTAG('M', 'P', 'C', '8')},
     { CODEC_ID_NELLYMOSER,        MKTAG('N', 'E', 'L', 'L')},
