965f604636d3a3ee161dbdc818d6b323893bf2a1
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-02-17 23:10:56 +0000

    Add read_avi_header() to aviheader.h; avoids forward declarations.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30623 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/demux_avi.c
+++ libmpdemux/demux_avi.c
@@ -434,8 +434,6 @@ int index_mode=-1;  // -1=untouched  0=don't use index  1=use (generate) index
 char *index_file_save = NULL, *index_file_load = NULL;
 int force_ni=0;     // force non-interleaved AVI parsing
 
-void read_avi_header(demuxer_t *demuxer,int index_mode);
-
 static demuxer_t* demux_open_avi(demuxer_t* demuxer){
     demux_stream_t *d_audio=demuxer->audio;
     demux_stream_t *d_video=demuxer->video;
