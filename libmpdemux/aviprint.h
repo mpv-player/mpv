22e310b9aef77477e022874beb388514911ead1e
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-02-21 23:36:36 +0000

    Declare public function print_vprp() in aviprint.h.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30694 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/aviprint.h
+++ libmpdemux/aviprint.h
@@ -27,6 +27,7 @@ void print_avih(MainAVIHeader *h, int verbose_level);
 void print_strh(AVIStreamHeader *h, int verbose_level);
 void print_wave_header(WAVEFORMATEX *h, int verbose_level);
 void print_video_header(BITMAPINFOHEADER *h, int verbose_level);
+void print_vprp(VideoPropHeader *vprp, int verbose_level);
 void print_index(AVIINDEXENTRY *idx, int idx_size, int verbose_level);
 void print_avistdindex_chunk(avistdindex_chunk *h, int verbose_level);
 void print_avisuperindex_chunk(avisuperindex_chunk *h, int verbose_level);
