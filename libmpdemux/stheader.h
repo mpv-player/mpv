6ed62435a74490185a5acb2e2e99b383c21df2e2
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-02-21 20:02:50 +0000

    Add names to anonymous structs sh_common, sh_audio, sh_video, sh_sub.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30685 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/stheader.h
+++ libmpdemux/stheader.h
@@ -43,11 +43,11 @@
   char* lang; /* track language */ \
   int default_track; \
 
-typedef struct {
+typedef struct sh_common {
   SH_COMMON
 } sh_common_t;
 
-typedef struct {
+typedef struct sh_audio {
   SH_COMMON
   int aid;
   // output format:
@@ -86,7 +86,7 @@ typedef struct {
   int pts_bytes; // bytes output by decoder after last known pts
 } sh_audio_t;
 
-typedef struct {
+typedef struct sh_video {
   SH_COMMON
   int vid;
   float timer;		  // absolute time in video stream, since last start/seek
@@ -119,7 +119,7 @@ typedef struct {
   void* ImageDesc; // for quicktime codecs
 } sh_video_t;
 
-typedef struct {
+typedef struct sh_sub {
   SH_COMMON
   int sid;
   char type;                    // t = text, v = VobSub, a = SSA/ASS
