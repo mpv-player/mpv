88f86bcc4d1863fdbbe0843229e1e97db61f0850
kostya <kostya@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-02-21 13:38:55 +0000

    FFmpeg supports Bink, let MPlayer play it as well
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30679 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/mp_taglists.c
+++ libmpdemux/mp_taglists.c
@@ -30,6 +30,8 @@ static const AVCodecTag mp_wav_tags[] = {
     { CODEC_ID_ADPCM_THP,         MKTAG('T', 'H', 'P', 'A')},
     { CODEC_ID_ADPCM_XA,          MKTAG('P', 'S', 'X', 'A')},
     { CODEC_ID_AMR_NB,            MKTAG('n', 'b',   0,   0)},
+    { CODEC_ID_BINKAUDIO_DCT,     MKTAG('B', 'A', 'U', '1')},
+    { CODEC_ID_BINKAUDIO_RDFT,    MKTAG('B', 'A', 'U', '2')},
     { CODEC_ID_COOK,              MKTAG('c', 'o', 'o', 'k')},
     { CODEC_ID_DSICINAUDIO,       MKTAG('D', 'C', 'I', 'A')},
     { CODEC_ID_EAC3,              MKTAG('E', 'A', 'C', '3')},
