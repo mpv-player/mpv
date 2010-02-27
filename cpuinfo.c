26cab7638fa5aab4f52fabce143ee1e9158c05bc
zuxy <zuxy@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-02-27 12:10:34 +0000

    Replace =A with =a and =d; make frequency calculation correct under x86-64
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30756 b3059339-0415-0410-9bf9-f77b7e298cf2

--- cpuinfo.c
+++ cpuinfo.c
@@ -77,10 +77,10 @@ cpuid(int func) {
 static int64_t
 rdtsc(void)
 {
-  uint64_t i;
+  uint64_t hi, lo;
 #define RDTSC   ".byte 0x0f, 0x31; "
-  __asm__ volatile (RDTSC : "=A"(i) : );
-  return i;
+  __asm__ volatile (RDTSC : "=a"(lo), "=d"(hi) : );
+  return hi << 32 | lo;
 }
 
 static const char*
