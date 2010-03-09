12d78a9e5c7fe369588046954433d18437fd5083
cehoyos <cehoyos@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-03-09 14:33:11 +0000

    Support SEGA CRI adx codec with demuxer lavf.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30875 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/mp_taglists.c
+++ libmpdemux/mp_taglists.c
@@ -23,6 +23,7 @@
 #include "libavformat/riff.h"
 
 static const AVCodecTag mp_wav_tags[] = {
+    { CODEC_ID_ADPCM_ADX,         MKTAG('S', 'a', 'd', 'x')},
     { CODEC_ID_ADPCM_4XM,         MKTAG('4', 'X', 'M', 'A')},
     { CODEC_ID_ADPCM_EA,          MKTAG('A', 'D', 'E', 'A')},
     { CODEC_ID_ADPCM_EA_MAXIS_XA, MKTAG('A', 'D', 'X', 'A')},
