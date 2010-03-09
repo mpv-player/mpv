97ef28ad9aa2f34aea0779062976ad4294c0af51
cehoyos <cehoyos@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-03-09 17:15:07 +0000

    Fix alpabetical order.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30876 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/mp_taglists.c
+++ libmpdemux/mp_taglists.c
@@ -23,8 +23,8 @@
 #include "libavformat/riff.h"
 
 static const AVCodecTag mp_wav_tags[] = {
-    { CODEC_ID_ADPCM_ADX,         MKTAG('S', 'a', 'd', 'x')},
     { CODEC_ID_ADPCM_4XM,         MKTAG('4', 'X', 'M', 'A')},
+    { CODEC_ID_ADPCM_ADX,         MKTAG('S', 'a', 'd', 'x')},
     { CODEC_ID_ADPCM_EA,          MKTAG('A', 'D', 'E', 'A')},
     { CODEC_ID_ADPCM_EA_MAXIS_XA, MKTAG('A', 'D', 'X', 'A')},
     { CODEC_ID_ADPCM_IMA_WS,      MKTAG('A', 'I', 'W', 'S')},
