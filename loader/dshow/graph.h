4abf80e24c825a0f5a0d9f26009f19c5e039ba6b
sesse <sesse@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-03-09 13:19:25 +0000

    Fix a syntax error in graph.h.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30874 b3059339-0415-0410-9bf9-f77b7e298cf2

--- loader/dshow/graph.h
+++ loader/dshow/graph.h
@@ -19,7 +19,7 @@
  * You should have received a copy of the GNU General Public License along
  * with MPlayer; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- /
+ */
 
 #include "interfaces.h"
 #include "cmediasample.h"
