f8a9b4e17ae1e3ed0008ca6af515139ae8449c68
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-04-04 15:57:17 +0000

    Revert marking m_option struct member as const. It is used for non-const things, so the original change was wrong.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30996 b3059339-0415-0410-9bf9-f77b7e298cf2

--- m_option.h
+++ m_option.h
@@ -280,7 +280,7 @@ struct m_option {
    *  use the priv field but this was inherited from older versions of the
    *  config code.
    */
-  const void *p;
+  void *p;
 
   /// Option type.
   const m_option_type_t* type;
