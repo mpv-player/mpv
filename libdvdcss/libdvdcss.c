da988249d76776d44c3220e188507c6335e9b5b5
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-05-11 10:51:46 +0000

    libdvdcss: cosmetics: Fix FSF address and program name in license headers. This merges upstream revisions 219 and 221.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@31155 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libdvdcss/libdvdcss.c
+++ libdvdcss/libdvdcss.c
@@ -7,19 +7,19 @@
  * Copyright (C) 1998-2008 VideoLAN
  * $Id$
  *
- * This program is free software; you can redistribute it and/or modify
+ * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA.
+ * You should have received a copy of the GNU General Public License along
+ * with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 /**
