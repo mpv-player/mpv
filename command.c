mpv-player/mpv

commit 467013ecdc98ad80d50f425af1e41efc63a37f1f

Return the actually playing audio/video id for get_property, not the one selected e.g. via commandline (which resulted in -1 being returned most of the time).
git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@30124 b3059339-0415-0410-9bf9-f77b7e298cf2

command.c
@@ -795,21 +795,22 @@ static int mp_property_balance(m_option_t * prop, int action, void *arg,
 static int mp_property_audio(m_option_t * prop, int action, void *arg,
 			     MPContext * mpctx)
 {
-    int current_id = -1, tmp;
+    int current_id, tmp;
     if (!mpctx->demuxer || !mpctx->demuxer->audio)
         return M_PROPERTY_UNAVAILABLE;
+    current_id = mpctx->demuxer->audio->id;
 
     switch (action) {
     case M_PROPERTY_GET:
 	if (!arg)
 	    return M_PROPERTY_ERROR;
-	*(int *) arg = audio_id;
+	*(int *) arg = current_id;
 	return M_PROPERTY_OK;
     case M_PROPERTY_PRINT:
 	if (!arg)
 	    return M_PROPERTY_ERROR;
 
-	if (audio_id < 0)
+	if (current_id < 0)
 	    *(char **) arg = strdup(MSGTR_Disabled);
 	else {
 	    char lang[40] = MSGTR_Unknown;
@@ -818,7 +819,7 @@ static int mp_property_audio(m_option_t * prop, int action, void *arg,
                 av_strlcpy(lang, sh->lang, 40);
 #ifdef CONFIG_DVDREAD
 	    else if (mpctx->stream->type == STREAMTYPE_DVD) {
-		int code = dvd_lang_from_aid(mpctx->stream, audio_id);
+		int code = dvd_lang_from_aid(mpctx->stream, current_id);
 		if (code) {
 		    lang[0] = code >> 8;
 		    lang[1] = code;
@@ -829,10 +830,10 @@ static int mp_property_audio(m_option_t * prop, int action, void *arg,
 
 #ifdef CONFIG_DVDNAV
 	    else if (mpctx->stream->type == STREAMTYPE_DVDNAV)
-		mp_dvdnav_lang_from_aid(mpctx->stream, audio_id, lang);
+		mp_dvdnav_lang_from_aid(mpctx->stream, current_id, lang);
 #endif
 	    *(char **) arg = malloc(64);
-	    snprintf(*(char **) arg, 64, "(%d) %s", audio_id, lang);
+	    snprintf(*(char **) arg, 64, "(%d) %s", current_id, lang);
 	}
 	return M_PROPERTY_OK;
 
@@ -842,7 +843,6 @@ static int mp_property_audio(m_option_t * prop, int action, void *arg,
 	    tmp = *((int *) arg);
 	else
 	    tmp = -1;
-	current_id = mpctx->demuxer->audio->id;
 	audio_id = demuxer_switch_audio(mpctx->demuxer, tmp);
 	if (audio_id == -2
 	    || (audio_id > -1
@@ -869,26 +869,27 @@ static int mp_property_audio(m_option_t * prop, int action, void *arg,
 static int mp_property_video(m_option_t * prop, int action, void *arg,
 			     MPContext * mpctx)
 {
-    int current_id = -1, tmp;
+    int current_id, tmp;
     if (!mpctx->demuxer || !mpctx->demuxer->video)
         return M_PROPERTY_UNAVAILABLE;
+    current_id = mpctx->demuxer->video->id;
 
     switch (action) {
     case M_PROPERTY_GET:
 	if (!arg)
 	    return M_PROPERTY_ERROR;
-	*(int *) arg = video_id;
+	*(int *) arg = current_id;
 	return M_PROPERTY_OK;
     case M_PROPERTY_PRINT:
 	if (!arg)
 	    return M_PROPERTY_ERROR;
 
-	if (video_id < 0)
+	if (current_id < 0)
 	    *(char **) arg = strdup(MSGTR_Disabled);
 	else {
 	    char lang[40] = MSGTR_Unknown;
 	    *(char **) arg = malloc(64);
-	    snprintf(*(char **) arg, 64, "(%d) %s", video_id, lang);
+	    snprintf(*(char **) arg, 64, "(%d) %s", current_id, lang);
 	}
 	return M_PROPERTY_OK;
 
@@ -898,7 +899,6 @@ static int mp_property_video(m_option_t * prop, int action, void *arg,
 	    tmp = *((int *) arg);
 	else
 	    tmp = -1;
-	current_id = mpctx->demuxer->video->id;
 	video_id = demuxer_switch_video(mpctx->demuxer, tmp);
 	if (video_id == -2
 	    || (video_id > -1 && mpctx->demuxer->video->id != current_id

commit 90bedd0b872b6eea02351aafb62e30ffec5c2152

Make audio stream index handling saner in stream switching
The handling of audio stream numbering was handled in the stream
selection property was a total mess. The most important issue was
confusion between values used as index for demuxer->audio_streams[]
array (consistently stored in demuxer->audio->id) and values stored
in sh_audio->aid and used as "-aid N" option values. Now demuxer audio
switch control functions and demuxer_switch_audio() are supposed to
return the new value for the "-aid" option (internal MPEG demuxers
still don't; the demuxer requirement could perhaps be dropped as it
can be easily calculated afterwards). That is also the value
returned for the "switch_audio" property. The main changes are:

 - Make command.c mp_property_audio() consistently use and return the
   "-aid" values. Before it used that as input but the array index as
   output, with extra mess related to demuxer_switch_audio() return
   value. Don't modify the audio_id option field any more.

 - Make demuxer_switch_audio() always return "-aid" values (like it
   takes as input). There are two changes for this: picking this
   return value in case the demuxer doesn't support switching, and
   overriding demuxer return value (for internal MPEG demuxers).

 - Make demux_lavf return "-aid" values from DEMUXER_CTRL_SWITCH_AUDIO
   code. This isn't actually necessary because of the override part
   above.

Here's some history of the relevant behavior that I looked up:
* For most demuxers array index and "-aid" values are the same. At
  least demux_mkv, (some of?) the internal MPEG demuxers and demux_ogg
  have differed for a long time. demux_ogg doesn't matter because it
  doesn't support stream switching.
* Old code seemed to assume that demuxer_switch_audio() return value was
  array index, but this wasn't true at least for demux_mkv.
* In svn r19951 reimar mostly removed use of the return value.
* In r20162 ptt added mp_property_audio(). This set the global
  audio_id variable (-aid option value) to the return value of
  demuxer_switch_audio() and treated the global as the persistent
  value of the property, apparently assuming that it would be set to
  the "-aid" value, not array index. This was false for internal
  MPEG.
* In r30124 reimar changed the property code so that even though it
  still modified the option value it didn't use that as the value of
  the property any more; instead it incorrectly used the array index.
  This meant that for demux_mkv the return value didn't match -aid any
  more (though input still did, so setting the property and querying
  it didn't match as they used different value systems).
* In r31129 aurel made demux_lavf changes that resulted in its -aid
  and array index values no longer matching either. He didn't change
  the return value from audio switch when changing -aid, so it now
  matched array index only. The latter part didn't cause additional
  problems from r20162 though because either choice would have been
  broken anyway after r30124

command.c
@@ -851,11 +851,10 @@ static int mp_property_balance(m_option_t *prop, int action, void *arg,
 static int mp_property_audio(m_option_t *prop, int action, void *arg,
                              MPContext *mpctx)
 {
-    struct MPOpts *opts = &mpctx->opts;
     int current_id, tmp;
     if (!mpctx->demuxer || !mpctx->demuxer->audio)
         return M_PROPERTY_UNAVAILABLE;
-    current_id = mpctx->demuxer->audio->id;
+    current_id = mpctx->sh_audio ? mpctx->sh_audio->aid : -2;
 
     switch (action) {
     case M_PROPERTY_GET:
@@ -901,21 +900,17 @@ static int mp_property_audio(m_option_t *prop, int action, void *arg,
             tmp = *((int *) arg);
         else
             tmp = -1;
-        opts->audio_id = demuxer_switch_audio(mpctx->demuxer, tmp);
-        if (opts->audio_id == -2
-            || (opts->audio_id > -1
-                && mpctx->demuxer->audio->id != current_id && current_id != -2))
+        int new_id = demuxer_switch_audio(mpctx->demuxer, tmp);
+        if (new_id != current_id)
             uninit_player(mpctx, INITIALIZED_AO | INITIALIZED_ACODEC);
-        if (opts->audio_id > -1 && mpctx->demuxer->audio->id != current_id) {
+        if (new_id != current_id && new_id >= 0) {
             sh_audio_t *sh2;
             sh2 = mpctx->demuxer->a_streams[mpctx->demuxer->audio->id];
-            if (sh2) {
-                sh2->ds = mpctx->demuxer->audio;
-                mpctx->sh_audio = sh2;
-                reinit_audio_chain(mpctx);
-            }
+            sh2->ds = mpctx->demuxer->audio;
+            mpctx->sh_audio = sh2;
+            reinit_audio_chain(mpctx);
         }
-        mp_msg(MSGT_IDENTIFY, MSGL_INFO, "ID_AUDIO_TRACK=%d\n", opts->audio_id);
+        mp_msg(MSGT_IDENTIFY, MSGL_INFO, "ID_AUDIO_TRACK=%d\n", new_id);
         return M_PROPERTY_OK;
     default:
         return M_PROPERTY_NOT_IMPLEMENTED;

libmpdemux/demux_lavf.c
@@ -737,14 +737,15 @@ static int demux_lavf_control(demuxer_t *demuxer, int cmd, void *arg)
 	            newid = pstreams[i];
 	        }
 	    }
-	    if(i == curridx)
-	        return DEMUXER_CTRL_NOTIMPL;
-	    else
-	    {
+	    if (i == curridx) {
+                *(int *) arg = curridx;
+                return DEMUXER_CTRL_OK;
+            } else {
 	        ds_free_packs(ds);
 	        if(ds->id >= 0)
 	            priv->avfc->streams[ds->id]->discard = AVDISCARD_ALL;
-	        *((int*)arg) = ds->id = newid;
+                ds->id = newid;
+                *(int *) arg = i < 0 ? -2 : i;
 	        if(newid >= 0)
 	            priv->avfc->streams[newid]->discard = AVDISCARD_NONE;
 	        return DEMUXER_CTRL_OK;

libmpdemux/demuxer.c
@@ -1407,10 +1407,15 @@ int demuxer_get_percent_pos(demuxer_t *demuxer)
 int demuxer_switch_audio(demuxer_t *demuxer, int index)
 {
     int res = demux_control(demuxer, DEMUXER_CTRL_SWITCH_AUDIO, &index);
-    if (res == DEMUXER_CTRL_NOTIMPL)
-        index = demuxer->audio->id;
-    if (demuxer->audio->id >= 0)
-        demuxer->audio->sh = demuxer->a_streams[demuxer->audio->id];
+    if (res == DEMUXER_CTRL_NOTIMPL) {
+        struct sh_audio *sh_audio = demuxer->audio->sh;
+        return sh_audio ? sh_audio->aid : -2;
+    }
+    if (demuxer->audio->id >= 0) {
+        struct sh_audio *sh_audio = demuxer->a_streams[demuxer->audio->id];
+        demuxer->audio->sh = sh_audio;
+        index = sh_audio->aid; // internal MPEG demuxers don't set it right
+    }
     else
         demuxer->audio->sh = NULL;
     return index;