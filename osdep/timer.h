f5f6bbaa090d51a3db670abb135d16e91250b1a3
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-05-28 08:39:02 +0000

    Remove commented-out declaration of nonexisting function uGetTimer().
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@31248 b3059339-0415-0410-9bf9-f77b7e298cf2

--- osdep/timer.h
+++ osdep/timer.h
@@ -24,7 +24,6 @@ extern const char *timer_name;
 void InitTimer(void);
 unsigned int GetTimer(void);
 unsigned int GetTimerMS(void);
-//int uGetTimer(void);
 float GetRelativeTime(void);
 
 int usec_sleep(int usec_delay);
