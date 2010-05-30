49c904b517a6bbb789ceaea80cb40323dadc1d29
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-05-30 13:51:24 +0000

    Treat a NULL renderer string as indication for software renderer. Usually it means OpenGL will not work at all.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@31284 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vo_gl.c
+++ libvo/vo_gl.c
@@ -470,7 +470,7 @@ static void uninitGl(void) {
 static int isSoftwareGl(void)
 {
   const char *renderer = mpglGetString(GL_RENDERER);
-  return strcmp(renderer, "Software Rasterizer") == 0;
+  return !renderer || strcmp(renderer, "Software Rasterizer") == 0;
 }
 
 static void autodetectGlExtensions(void) {
