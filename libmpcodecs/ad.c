2ea247fddfb90540f6ed2bdf400fb4256662272d
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-09-07 20:26:37 +0000

    cosmetics: Remove commented-out mpcodecs_ad_null leftovers
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@32058 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/ad.c
+++ libmpcodecs/ad.c
@@ -31,7 +31,6 @@
 
 /* Missed vorbis, mad, dshow */
 
-//extern ad_functions_t mpcodecs_ad_null;
 extern const ad_functions_t mpcodecs_ad_mpg123;
 extern const ad_functions_t mpcodecs_ad_mp3lib;
 extern const ad_functions_t mpcodecs_ad_ffmpeg;
@@ -61,7 +60,6 @@ extern const ad_functions_t mpcodecs_ad_libdca;
 
 const ad_functions_t * const mpcodecs_ad_drivers[] =
 {
-//  &mpcodecs_ad_null,
 #ifdef CONFIG_MPG123
   &mpcodecs_ad_mpg123,
 #endif
