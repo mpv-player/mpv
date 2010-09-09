9b00269483584abdbc485cc1883201bb5e0ba495
diego <diego@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-09-09 16:46:08 +0000

    vf_fixpts: Mark vf_info_t declaration const
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@32118 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpcodecs/vf_fixpts.c
+++ libmpcodecs/vf_fixpts.c
@@ -126,7 +126,7 @@ static int open(vf_instance_t *vf, char *args)
     return 1;
 }
 
-vf_info_t vf_info_fixpts = {
+const vf_info_t vf_info_fixpts = {
     "Fix presentation timestamps",
     "fixpts",
     "Nicolas George",
