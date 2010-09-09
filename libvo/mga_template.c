fdce82091e6ff3d3bf05fdbbad381882e61c7234
ramiro <ramiro@b3059339-0415-0410-9bf9-f77b7e298cf2>
2010-09-09 14:50:51 +0000

    mga: disable g200 support
    g200 should use the swscale interface to convert to nv12. If anyone still uses
    such a card and is willing to test, just let me know.
    
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@32111 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/mga_template.c
+++ libvo/mga_template.c
@@ -19,7 +19,6 @@
 #include "fastmemcpy.h"
 #include "cpudetect.h"
 #include "libswscale/swscale.h"
-#include "libswscale/rgb2rgb.h"
 #include "libmpcodecs/vf_scale.h"
 #include "mp_msg.h"
 #include "old_vo_wrapper.h"
@@ -67,6 +66,7 @@ static void draw_osd(void)
 }
 
 
+#if 0 // Should use libswscale's interface to NV12
 static void
 draw_slice_g200(uint8_t *image[], int stride[], int width,int height,int x,int y)
 {
@@ -84,6 +84,7 @@ draw_slice_g200(uint8_t *image[], int stride[], int width,int height,int x,int y
 		width, height,
 		stride[1], stride[2], bespitch);
 }
+#endif
 
 static void
 draw_slice_g400(uint8_t *image[], int stride[], int w,int h,int x,int y)
@@ -126,9 +127,11 @@ draw_slice(uint8_t *src[], int stride[], int w,int h,int x,int y)
 	    w,h,x,y);
 #endif
 
+#if 0
 	if (mga_vid_config.card_type == MGA_G200)
             draw_slice_g200(src,stride,w,h,x,y);
 	else
+#endif
             draw_slice_g400(src,stride,w,h,x,y);
 	return 0;
 }
@@ -419,6 +422,10 @@ static int mga_init(int width,int height,unsigned int format){
 			return -1;
 		}
 	}
+	if (mga_vid_config.card_type == MGA_G200) {
+		mp_msg(MSGT_VO, MSGL_FATAL, "G200 cards support is currently broken. patches welcome.\n");
+		return -1;
+	}
 
 	mp_msg(MSGT_VO,MSGL_V,"[MGA] Using %d buffers.\n",mga_vid_config.num_frames);
 
@@ -453,7 +460,6 @@ static int preinit(const char *vo_subdevice)
 {
 	uint32_t ver;
   const char *devname=vo_subdevice?vo_subdevice:"/dev/mga_vid";
-	sws_rgb2rgb_init(get_sws_cpuflags());
 
 	f = open(devname,O_RDWR);
 	if(f == -1)
