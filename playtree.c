a71340bd40a11f0538dcc8130dfbfa5245dc3f08
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2011-05-04 20:27:38 +0000

    playtree: free parameter values when entry is freed
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@33364 b3059339-0415-0410-9bf9-f77b7e298cf2

--- playtree.c
+++ playtree.c
@@ -68,7 +68,14 @@ play_tree_free(play_tree_t* pt, int children) {
   for(iter = pt->child ; iter != NULL ; iter = iter->next)
     iter->parent = NULL;
 
-  //free(pt->params);
+  if (pt->params) {
+    int i;
+    for(i = 0 ; pt->params[i].name != NULL ; i++) {
+      free(pt->params[i].name);
+      free(pt->params[i].value);
+    }
+    free(pt->params);
+  }
   if(pt->files) {
     int i;
     for(i = 0 ; pt->files[i] != NULL ; i++)
