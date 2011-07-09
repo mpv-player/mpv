mpv-player/mpv

commit bc767c2a98ab0a0988313b0394561f0f791ac59a

vd_ffmpeg: use skip_frame instead of deprecated hurry_up field

libmpcodecs/vd_ffmpeg.c
@@ -749,7 +749,12 @@ static struct mp_image *decode(struct sh_video *sh, void *data, int len,
         }
     }
 
-    avctx->hurry_up=(flags&3)?((flags&2)?2:1):0;
+    if (flags & 2)
+        avctx->skip_frame = AVDISCARD_ALL;
+    else if (flags & 1)
+        avctx->skip_frame = AVDISCARD_NONREF;
+    else
+        avctx->skip_frame = 0;
 
     mp_msg(MSGT_DECVIDEO, MSGL_DBG2, "vd_ffmpeg data: %04x, %04x, %04x, %04x\n",
            ((int *)data)[0], ((int *)data)[1], ((int *)data)[2], ((int *)data)[3]);

commit 1a7ef3b39c38837b45f87ce8b673da5641289573

vd_ffmpeg: fix "-lavdopts skipframe" support
Support for -lavdopts skipframe had been broken since commit bc767c2a9
as framedrop logic now set the same field and thus overwrote the value
set from the option. Change that code to set it to the original value
instead of 0 when not dropping frames.

libmpcodecs/vd_ffmpeg.c
@@ -73,6 +73,7 @@ typedef struct {
     int b_count;
     AVRational last_sample_aspect_ratio;
     int lowres;
+    enum AVDiscard skip_frame;
 } vd_ffmpeg_ctx;
 
 #include "m_option.h"
@@ -283,6 +284,9 @@ static int init(sh_video_t *sh){
             return 0;
         }
     }
+
+    // Do this after the above avopt handling in case it changes values
+    ctx->skip_frame = avctx->skip_frame;
 
     mp_dbg(MSGT_DECVIDEO, MSGL_DBG2, "libavcodec.size: %d x %d\n", avctx->width, avctx->height);
     switch (sh->format) {
@@ -756,7 +760,7 @@ static struct mp_image *decode(struct sh_video *sh, void *data, int len,
     else if (flags & 1)
         avctx->skip_frame = AVDISCARD_NONREF;
     else
-        avctx->skip_frame = 0;
+        avctx->skip_frame = ctx->skip_frame;
 
     mp_msg(MSGT_DECVIDEO, MSGL_DBG3, "vd_ffmpeg data: %04x, %04x, %04x, %04x\n",
            ((int *)data)[0], ((int *)data)[1], ((int *)data)[2], ((int *)data)[3]);