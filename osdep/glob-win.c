1764bfacca7bd5031e4ba8aad0c53510d2651216
mplayer-svn <svn@mplayerhq.hu>
2011-10-23 11:51:44 +0000

    osdep:/glob-win.c: reformat
    Use uncrustify on glob-win.c to fix the indentation mess in it.
    
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@34239 b3059339-0415-0410-9bf9-f77b7e298cf2
    Author: reimar

--- osdep/glob-win.c
+++ osdep/glob-win.c
@@ -27,54 +27,45 @@
 #include "glob.h"
 
 int glob(const char *pattern, int flags,
-          int (*errfunc)(const char *epath, int eerrno), glob_t *pglob)
+         int (*errfunc)(const char *epath, int eerrno), glob_t *pglob)
 {
-	HANDLE searchhndl;
+    HANDLE searchhndl;
     WIN32_FIND_DATA found_file;
- 	if(errfunc)printf("glob():ERROR:Sorry errfunc not supported by this implementation\n");
-	if(flags)printf("glob():ERROR:Sorry no flags supported by this globimplementation\n");
-	//printf("PATTERN \"%s\"\n",pattern);
-	pglob->gl_pathc = 0;
-	searchhndl = FindFirstFile( pattern,&found_file);
-    if(searchhndl == INVALID_HANDLE_VALUE)
-	{
-		if(GetLastError() == ERROR_FILE_NOT_FOUND)
-		{
-			pglob->gl_pathc = 0;
-		    //printf("could not find a file matching your search criteria\n");
-	        return 1;
-		}
-		else
-		{
-			//printf("glob():ERROR:FindFirstFile: %i\n",GetLastError());
-			return 1;
-		}
-	 }
-    pglob->gl_pathv = malloc(sizeof(char*));
+    if (errfunc)
+        printf("glob():ERROR:Sorry errfunc not supported by this implementation\n");
+    if (flags)
+        printf("glob():ERROR:Sorry no flags supported by this globimplementation\n");
+    //printf("PATTERN \"%s\"\n",pattern);
+    pglob->gl_pathc = 0;
+    searchhndl      = FindFirstFile(pattern, &found_file);
+    if (searchhndl == INVALID_HANDLE_VALUE) {
+        if (GetLastError() == ERROR_FILE_NOT_FOUND) {
+            pglob->gl_pathc = 0;
+            //printf("could not find a file matching your search criteria\n");
+            return 1;
+        } else {
+            //printf("glob():ERROR:FindFirstFile: %i\n",GetLastError());
+            return 1;
+        }
+    }
+    pglob->gl_pathv    = malloc(sizeof(char *));
     pglob->gl_pathv[0] = strdup(found_file.cFileName);
     pglob->gl_pathc++;
-    while(1)
-    {
-		if(!FindNextFile(searchhndl,&found_file))
-		{
-			if(GetLastError()==ERROR_NO_MORE_FILES)
-			{
-				//printf("glob(): no more files found\n");
+    while (1) {
+        if (!FindNextFile(searchhndl, &found_file)) {
+            if (GetLastError() == ERROR_NO_MORE_FILES) {
+                //printf("glob(): no more files found\n");
                 break;
-			}
-			else
-			{
-				//printf("glob():ERROR:FindNextFile:%i\n",GetLastError());
-				return 1;
-			}
-		}
-		else
-		{
+            } else {
+                //printf("glob():ERROR:FindNextFile:%i\n",GetLastError());
+                return 1;
+            }
+        } else {
             //printf("glob: found file %s\n",found_file.cFileName);
             pglob->gl_pathc++;
-            pglob->gl_pathv = realloc(pglob->gl_pathv,pglob->gl_pathc * sizeof(char*));
-            pglob->gl_pathv[pglob->gl_pathc-1] = strdup(found_file.cFileName);
- 		}
+            pglob->gl_pathv = realloc(pglob->gl_pathv, pglob->gl_pathc * sizeof(char *));
+            pglob->gl_pathv[pglob->gl_pathc - 1] = strdup(found_file.cFileName);
+        }
     }
     FindClose(searchhndl);
     return 0;
@@ -82,25 +73,26 @@ int glob(const char *pattern, int flags,
 
 void globfree(glob_t *pglob)
 {
-	int i;
-	for(i=0; i <pglob->gl_pathc ;i++)
-	{
-		free(pglob->gl_pathv[i]);
-	}
-	free(pglob->gl_pathv);
+    int i;
+    for (i = 0; i < pglob->gl_pathc; i++)
+        free(pglob->gl_pathv[i]);
+    free(pglob->gl_pathv);
 }
 
 #if 0
-int main(void){
-   glob_t        gg;
-   printf("globtest\n");
-   glob( "*.jpeg",0,NULL,&gg );
-   {
+int main(void)
+{
+    glob_t gg;
+    printf("globtest\n");
+    glob("*.jpeg", 0, NULL, &gg);
+    {
         int i;
-        for(i=0;i<gg.gl_pathc;i++)printf("GLOBED:%i %s\n",i,gg.gl_pathv[i]);
+        for (i = 0; i < gg.gl_pathc; i++)
+            printf("GLOBED:%i %s\n", i, gg.gl_pathv[i]);
     }
-   globfree(&gg);
+    globfree(&gg);
 
-   return 0;
+    return 0;
 }
+
 #endif
