mpv-player/mpv

commit 5fe14d5bf11a22025914a1948770e0b720a8d46f

screenshot: improve error messages
Include filename in the error message, if the screenshot destination
file already exists.

Consistently quote filenames with 'quotes' in messages.

screenshot.c
@@ -400,13 +400,13 @@ static char *gen_fname(screenshot_ctx *ctx)
         if (!fexists(fname))
             return fname;
 
-        talloc_free(fname);
-
         if (sequence == prev_sequence) {
-            mp_msg(MSGT_CPLAYER, MSGL_ERR, "Can't save screenshot, file "
-                   "already exists!\n");
+            mp_msg(MSGT_CPLAYER, MSGL_ERR, "Can't save screenshot, file '%s' "
+                   "already exists!\n", fname);
             return NULL;
         }
+
+        talloc_free(fname);
     }
 }
 
@@ -447,8 +447,8 @@ void screenshot_save(struct MPContext *mpctx, struct mp_image *image)
     if (filename) {
         FILE *fp = fopen(filename, "wb");
         if (fp == NULL) {
-            mp_msg(MSGT_CPLAYER, MSGL_ERR, "\nError opening %s for writing!\n",
-                   filename);
+            mp_msg(MSGT_CPLAYER, MSGL_ERR,
+                   "\nError opening '%s' for writing!\n", filename);
         } else {
             mp_msg(MSGT_CPLAYER, MSGL_INFO, "*** screenshot '%s' ***\n",
                    filename);

commit 4488583dda2754a2e71ec991e9541bcc2c259da3

screenshot: fix minor memory leak
Introduced in commit 5fe14d5bf "screenshot: improve error messages".

screenshot.c
@@ -403,6 +403,7 @@ static char *gen_fname(screenshot_ctx *ctx)
         if (sequence == prev_sequence) {
             mp_msg(MSGT_CPLAYER, MSGL_ERR, "Can't save screenshot, file '%s' "
                    "already exists!\n", fname);
+            talloc_free(fname);
             return NULL;
         }
 