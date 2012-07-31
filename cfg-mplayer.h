mpv-player/mpv

commit 74e7a1e937c10d9f4d8ce9b0ba4edee52044a757

osd: use libass for OSD rendering
The OSD will now be rendered with libass. The old rendering code, which
used freetype/fontconfig and did text layout manually, is disabled. To
re-enable the old code, use the --disable-libass-osd configure switch.

Some switches do nothing with the new code enabled, such as -subalign,
-sub-bg-alpha, -sub-bg-color, and many more. (The reason is mostly that
the code for rendering unstyled subtitles with libass doesn't make any
attempts to support them. Some of them could be supported in theory.)

Teletext rendering is not implemented in the new OSD rendering code. I
don't have any teletext sources for testing, and since teletext is
being phased out world-wide, the need for this is questionable.

Note that rendering is extremely inefficient, mostly because the libass
output is blended with the extremely strange mplayer OSD format. This
could be improved at a later point.

Remove most OSD rendering from vo_aa.c, because that was extremely
hacky, can't be made work with osd_libass, and didn't work anyway in
my tests.

Internally, some cleanup is done. Subtitle and OSD related variable
declarations were literally all over the place. Move them to sub.h and
sub.c, which were hoarding most of these declarations already. Make the
player core in mplayer.c free of concerns like bitmap font loading.

The old OSD rendering code has been moved to osd_ft.c. The font_load.c
and font_load_ft.c are only needed and compiled if the old OSD
rendering code is configured.

cfg-mplayer.h
@@ -680,14 +680,12 @@ const m_option_t common_opts[] = {
     {"spualign", &spu_alignment, CONF_TYPE_INT, CONF_RANGE, -1, 2, NULL},
     {"spuaa", &spu_aamode, CONF_TYPE_INT, CONF_RANGE, 0, 31, NULL},
     {"spugauss", &spu_gaussvar, CONF_TYPE_FLOAT, CONF_RANGE, 0.0, 3.0, NULL},
-#ifdef CONFIG_FREETYPE
     {"subfont-encoding", &subtitle_font_encoding, CONF_TYPE_STRING, 0, 0, 0, NULL},
     {"subfont-text-scale", &text_font_scale_factor, CONF_TYPE_FLOAT, CONF_RANGE, 0, 100, NULL},
     {"subfont-osd-scale", &osd_font_scale_factor, CONF_TYPE_FLOAT, CONF_RANGE, 0, 100, NULL},
     {"subfont-blur", &subtitle_font_radius, CONF_TYPE_FLOAT, CONF_RANGE, 0, 8, NULL},
     {"subfont-outline", &subtitle_font_thickness, CONF_TYPE_FLOAT, CONF_RANGE, 0, 8, NULL},
     {"subfont-autoscale", &subtitle_autoscale, CONF_TYPE_INT, CONF_RANGE, 0, 3, NULL},
-#endif
     OPT_START_CONDITIONAL(CONFIG_ASS, "libass"),
     OPT_MAKE_FLAGS("ass", ass_enabled, 0),
     OPT_FLOATRANGE("ass-font-scale", ass_font_scale, 0, 0, 100),

command.c
@@ -43,7 +43,6 @@
 #include "mp_osd.h"
 #include "libvo/video_out.h"
 #include "libvo/csputils.h"
-#include "sub/font_load.h"
 #include "playtree.h"
 #include "libao2/audio_out.h"
 #include "mpcommon.h"
@@ -2062,7 +2061,6 @@ static int mp_property_sub_forced_only(m_option_t *prop, int action,
 
 }
 
-#ifdef CONFIG_FREETYPE
 /// Subtitle scale (RW)
 static int mp_property_sub_scale(m_option_t *prop, int action, void *arg,
                                  MPContext *mpctx)
@@ -2081,8 +2079,7 @@ static int mp_property_sub_scale(m_option_t *prop, int action, void *arg,
         }
 #endif
         text_font_scale_factor = *(float *) arg;
-        force_load_font = 1;
-        vo_osd_changed(OSDTYPE_SUBTITLE);
+        vo_osd_resized();
         return M_PROPERTY_OK;
     case M_PROPERTY_STEP_UP:
     case M_PROPERTY_STEP_DOWN:
@@ -2097,8 +2094,7 @@ static int mp_property_sub_scale(m_option_t *prop, int action, void *arg,
         text_font_scale_factor += (arg ? *(float *) arg : 0.1) *
                                   (action == M_PROPERTY_STEP_UP ? 1.0 : -1.0);
         M_PROPERTY_CLAMP(prop, text_font_scale_factor);
-        force_load_font = 1;
-        vo_osd_changed(OSDTYPE_SUBTITLE);
+        vo_osd_resized();
         return M_PROPERTY_OK;
     default:
 #ifdef CONFIG_ASS
@@ -2109,7 +2105,6 @@ static int mp_property_sub_scale(m_option_t *prop, int action, void *arg,
         return m_property_float_ro(prop, action, arg, text_font_scale_factor);
     }
 }
-#endif
 
 
 #ifdef CONFIG_TV
@@ -2378,10 +2373,8 @@ static const m_option_t mp_properties[] = {
       M_OPT_RANGE, 0, 1, NULL },
     { "sub_forced_only", mp_property_sub_forced_only, CONF_TYPE_FLAG,
       M_OPT_RANGE, 0, 1, NULL },
-#ifdef CONFIG_FREETYPE
     { "sub_scale", mp_property_sub_scale, CONF_TYPE_FLOAT,
       M_OPT_RANGE, 0, 100, NULL },
-#endif
 #ifdef CONFIG_ASS
     { "ass_use_margins", mp_property_ass_use_margins, CONF_TYPE_FLAG,
       M_OPT_RANGE, 0, 1, NULL },
@@ -2497,9 +2490,7 @@ static struct property_osd_display {
     { "sub_delay", 0, OSD_MSG_SUB_DELAY, _("Sub delay: %s") },
     { "sub_visibility", 0, -1, _("Subtitles: %s") },
     { "sub_forced_only", 0, -1, _("Forced sub only: %s") },
-#ifdef CONFIG_FREETYPE
     { "sub_scale", 0, -1, _("Sub Scale: %s")},
-#endif
     { "ass_vsfilter_aspect_compat", 0, -1,
       _("Subtitle VSFilter aspect compat: %s")},
 #ifdef CONFIG_TV
@@ -2624,9 +2615,7 @@ static struct {
     { "sub_delay", MP_CMD_SUB_DELAY, 0},
     { "sub_visibility", MP_CMD_SUB_VISIBILITY, 1},
     { "sub_forced_only", MP_CMD_SUB_FORCED_ONLY, 1},
-#ifdef CONFIG_FREETYPE
     { "sub_scale", MP_CMD_SUB_SCALE, 0},
-#endif
 #ifdef CONFIG_ASS
     { "ass_use_margins", MP_CMD_ASS_USE_MARGINS, 1},
 #endif

mplayer.c
@@ -80,12 +80,12 @@
 #include "sub/subreader.h"
 #include "sub/find_subfiles.h"
 #include "sub/dec_sub.h"
+#include "sub/sub_cc.h"
 
 #include "mp_osd.h"
 #include "libvo/video_out.h"
 #include "screenshot.h"
 
-#include "sub/font_load.h"
 #include "sub/sub.h"
 #include "sub/av_sub.h"
 #include "libmpcodecs/dec_teletext.h"
@@ -318,16 +318,6 @@ static int force_srate = 0;
 int frame_dropping = 0;      // option  0=no drop  1= drop vo  2= drop decode
 static int play_n_frames = -1;
 static int play_n_frames_mf = -1;
-
-// sub:
-char *font_name = NULL;
-char *sub_font_name = NULL;
-extern int font_fontconfig;
-float font_factor = 0.75;
-float sub_delay = 0;
-float sub_fps = 0;
-int subcc_enabled = 0;
-int suboverlap_enabled = 1;
 
 #include "sub/ass_mp.h"
 
@@ -708,14 +698,6 @@ void exit_player_with_rc(struct MPContext *mpctx, enum exit_reason how, int rc)
     current_module = "uninit_input";
     mp_input_uninit(mpctx->input);
 
-#ifdef CONFIG_FREETYPE
-    current_module = "uninit_font";
-    if (mpctx->osd && mpctx->osd->sub_font != vo_font)
-        free_font_desc(mpctx->osd->sub_font);
-    free_font_desc(vo_font);
-    vo_font = NULL;
-    done_freetype();
-#endif
     osd_free(mpctx->osd);
 
 #ifdef CONFIG_ASS
@@ -1652,7 +1634,6 @@ static void update_osd_msg(struct MPContext *mpctx)
     struct MPOpts *opts = &mpctx->opts;
     mp_osd_msg_t *msg;
     struct osd_state *osd = mpctx->osd;
-    char osd_text_timer[128];
 
     if (mpctx->add_osd_seek_info) {
         double percentage = get_percent_pos(mpctx);
@@ -1682,6 +1663,7 @@ static void update_osd_msg(struct MPContext *mpctx)
 
     if (mpctx->sh_video && opts->term_osd != 1) {
         // fallback on the timer
+        char osd_text_timer[128] = {0};
         if (opts->osd_level >= 2) {
             int len = get_time_length(mpctx);
             int percentage = -1;
@@ -1723,19 +1705,22 @@ static void update_osd_msg(struct MPContext *mpctx)
                 fractions_text[0] = 0;
             }
 
+            osd_get_function_sym(osd_text_timer, sizeof(osd_text_timer),
+                                 mpctx->osd_function);
+            size_t blen = strlen(osd_text_timer);
+
             if (opts->osd_level == 3)
-                snprintf(osd_text_timer, sizeof(osd_text_timer),
-                         "%c %02d:%02d:%02d%s / %02d:%02d:%02d%s",
-                         mpctx->osd_function, pts / 3600, (pts / 60) % 60, pts % 60,
-                         fractions_text, len / 3600, (len / 60) % 60, len % 60,
+                snprintf(osd_text_timer + blen, sizeof(osd_text_timer) - blen,
+                         " %02d:%02d:%02d%s / %02d:%02d:%02d%s",
+                         pts / 3600, (pts / 60) % 60, pts % 60, fractions_text,
+                         len / 3600, (len / 60) % 60, len % 60,
                          percentage_text);
             else
-                snprintf(osd_text_timer, sizeof(osd_text_timer),
-                         "%c %02d:%02d:%02d%s%s",
-                         mpctx->osd_function, pts / 3600, (pts / 60) % 60,
-                         pts % 60, fractions_text, percentage_text);
-        } else
-            osd_text_timer[0] = 0;
+                snprintf(osd_text_timer + blen, sizeof(osd_text_timer) - blen,
+                         " %02d:%02d:%02d%s%s",
+                         pts / 3600, (pts / 60) % 60, pts % 60, fractions_text,
+                         percentage_text);
+        }
 
         if (strcmp(osd->osd_text, osd_text_timer)) {
             osd_set_text(osd, osd_text_timer);
@@ -4090,44 +4075,11 @@ int main(int argc, char *argv[])
 
     //------ load global data first ------
 
-    mpctx->osd = osd_create();
-
-    // check font
-#ifdef CONFIG_FREETYPE
-    init_freetype();
-#endif
-#ifdef CONFIG_FONTCONFIG
-    if (font_fontconfig <= 0) {
-#endif
-#ifdef CONFIG_BITMAP_FONT
-    if (font_name) {
-        vo_font = read_font_desc(font_name, font_factor, verbose > 1);
-        if (!vo_font)
-            mp_tmsg(MSGT_CPLAYER, MSGL_ERR, "Cannot load bitmap font: %s\n",
-                    filename_recode(font_name));
-    } else {
-        // try default:
-        vo_font = read_font_desc(mem_ptr = get_path("font/font.desc"),
-                                 font_factor, verbose > 1);
-        free(mem_ptr); // release the buffer created by get_path()
-        if (!vo_font)
-            vo_font = read_font_desc(MPLAYER_DATADIR "/font/font.desc",
-                                     font_factor, verbose > 1);
-    }
-    if (sub_font_name)
-        mpctx->osd->sub_font = read_font_desc(sub_font_name, font_factor,
-                                              verbose > 1);
-    else
-        mpctx->osd->sub_font = vo_font;
-#endif
-#ifdef CONFIG_FONTCONFIG
-}
-#endif
-
 #ifdef CONFIG_ASS
     mpctx->ass_library = mp_ass_init(opts);
-    mpctx->osd->ass_library = mpctx->ass_library;
 #endif
+
+    mpctx->osd = osd_create(opts, mpctx->ass_library);
 
 #ifdef HAVE_RTC
     if (opts->rtc) {
@@ -4792,9 +4744,7 @@ goto_enable_cache:
     if (mpctx->sh_video) {
         if (mpctx->sh_video->output_flags & VFCAP_SPU && vo_spudec)
             spudec_set_hw_spu(vo_spudec, mpctx->video_out);
-#ifdef CONFIG_FREETYPE
-        force_load_font = 1;
-#endif
+        osd_font_invalidate();
     } else if (!mpctx->sh_audio)
         goto goto_next_file;
 

Makefile
@@ -213,6 +213,10 @@ SRCS_COMMON-$(WIN32DLL)              += libmpcodecs/ad_acm.c \
 SRCS_COMMON-$(XANIM_CODECS)          += libmpcodecs/vd_xanim.c
 SRCS_COMMON-$(XMMS_PLUGINS)          += libmpdemux/demux_xmms.c
 SRCS_COMMON-$(XVID4)                 += libmpcodecs/vd_xvid4.c
+
+SRCS_COMMON-$(OLD_OSD)               += sub/osd_ft.c
+SRCS_COMMON-$(LIBASS_OSD)            += sub/osd_libass.c
+
 SRCS_COMMON = asxparser.c \
               av_log.c \
               av_opts.c \

configure
@@ -361,6 +361,7 @@ Optional features:
   --disable-pthreads     disable Posix threads support [autodetect]
   --disable-w32threads   disable Win32 threads support [autodetect]
   --disable-libass       disable subtitle rendering with libass [autodetect]
+  --disable-libass-osd   disable OSD rendering with libass [autodetect]
   --enable-rpath         enable runtime linker path for extra libs [disabled]
   --disable-libpostproc  disable postprocess filter (vf_pp) [autodetect]
 
@@ -657,6 +658,7 @@ _vstream=auto
 _pthreads=auto
 _w32threads=auto
 _ass=auto
+_libass_osd=auto
 _rpath=no
 libpostproc=auto
 _asmalign_pot=auto
@@ -991,6 +993,8 @@ for ac_option do
   --disable-w32threads) _w32threads=no  ;;
   --enable-libass)      _ass=yes        ;;
   --disable-libass)     _ass=no         ;;
+  --enable-libass-osd)  _libass_osd=yes ;;
+  --disable-libass-osd) _libass_osd=no  ;;
   --enable-rpath)       _rpath=yes      ;;
   --disable-rpath)      _rpath=no       ;;
   --enable-libpostproc) libpostproc=yes ;;
@@ -4542,6 +4546,39 @@ else
     noinputmodules="cddb $noinputmodules"
 fi
 
+
+echocheck "SSA/ASS support"
+if test "$_ass" = auto ; then
+    if pkg_config_add libass ; then
+        _ass=yes
+        def_ass='#define CONFIG_ASS 1'
+    else
+        die "Unable to find development files for libass. Aborting. If you really mean to compile without libass support use --disable-libass."
+    fi
+else
+    def_ass='#undef CONFIG_ASS'
+fi
+echores "$_ass"
+
+
+echocheck "libass OSD support"
+_old_osd=yes
+if test "$_libass_osd" = auto ; then
+    _libass_osd=no
+    if test "$_ass" = yes ; then
+        _libass_osd=yes
+        _old_osd=no
+        _bitmap_font=no
+        # disable unneeded dependencies
+        _freetype=no
+        _fontconfig=no
+        # this should always be forced, because libass does bidi itself
+        _fribidi=no
+    fi
+fi
+echores "$_libass_osd"
+
+
 echocheck "bitmap font support"
 if test "$_bitmap_font" = yes ; then
   def_bitmap_font="#define CONFIG_BITMAP_FONT 1"
@@ -4591,20 +4628,6 @@ else
     def_fontconfig='#undef CONFIG_FONTCONFIG'
 fi
 echores "$_fontconfig"
-
-
-echocheck "SSA/ASS support"
-if test "$_ass" = auto ; then
-    if pkg_config_add libass ; then
-        _ass=yes
-        def_ass='#define CONFIG_ASS 1'
-    else
-        die "Unable to find development files for libass. Aborting. If you really mean to compile without libass support use --disable-libass."
-    fi
-else
-    def_ass='#undef CONFIG_ASS'
-fi
-echores "$_ass"
 
 
 echocheck "fribidi with charsets"
@@ -5724,6 +5747,8 @@ JPEG = $_jpeg
 LADSPA = $_ladspa
 LIBA52 = $_liba52
 LIBASS = $_ass
+LIBASS_OSD = $_libass_osd
+OLD_OSD = $_old_osd
 LIBBLURAY = $_bluray
 LIBBS2B = $_libbs2b
 LIBDCA = $_libdca

libmpcodecs/vf_ass.c
@@ -72,8 +72,6 @@ static const struct vf_priv_s {
     } *line_limits;
 } vf_priv_dflt;
 
-extern float sub_delay;
-
 static int config(struct vf_instance *vf,
                   int width, int height, int d_width, int d_height,
                   unsigned int flags, unsigned int outfmt)

libmpcodecs/vf_vo.c
@@ -33,8 +33,6 @@
 #include "sub/ass_mp.h"
 #include "sub/sub.h"
 
-extern float sub_delay;
-
 struct vf_priv_s {
     struct vo *vo;
 #ifdef CONFIG_ASS

libmpdemux/demux_ty.c
@@ -50,8 +50,7 @@
 #include "parse_es.h"
 #include "stheader.h"
 #include "sub/sub_cc.h"
-
-extern int sub_justify;
+#include "sub/sub.h"
 
 // 2/c0: audio data
 // 3/c0: audio packet header (PES header)

commit 423a75250f4fa66abf8be7b48586e2e5fc1212e9

mplayer: make display of playback status more uniform
The code to format the playback time was duplicated a few times. There
were also minor differences in how the time is formatted. Remove most
of these differences. This also fixes a bug in the output of the
osd_show_progression command, introduced in 74e7a1e937c10d9f4.

There was some logic to display the percent position in the OSD status
for a short while after seeking. Remove that logic and always display
the percent position.

Make --osd-fractions a flag option. This removes the ability to show
the number of frames played since the start of the current second
(i.e. the fraction of the time was turned into a frame number). This
features wasn't so great anyway, because modern video file formats
don't always have a (valid) FPS set, and could lead to inaccurate
display.

Still to sort out:

Unfortunately, the terminal status is still formatted differently from
the OSD, and even worse, it has a completely different time source.

Not sure if I like how the status line looks now (it's a bit "full"?).
Maybe it will be changed again later.

cfg-mplayer.h
@@ -744,7 +744,7 @@ const m_option_t mplayer_opts[]={
     {"use-filename-title", &use_filename_title, CONF_TYPE_FLAG, CONF_GLOBAL, 0, 1, NULL},
     OPT_INTRANGE("osdlevel", osd_level, 0, 0, 3),
     OPT_INTRANGE("osd-duration", osd_duration, 0, 0, 3600000),
-    OPT_INTRANGE("osd-fractions", osd_fractions, 0, 0, 2),
+    OPT_MAKE_FLAGS("osd-fractions", osd_fractions, 0),
 
     OPT_STRING("vobsub", vobsub_name, 0),
     {"vobsubid", &vobsub_id, CONF_TYPE_INT, CONF_RANGE, 0, 31, NULL},

command.c
@@ -3201,16 +3201,9 @@ void run_command(MPContext *mpctx, mp_cmd_t *cmd)
         mpctx->stop_play = PT_STOP;
         break;
 
-    case MP_CMD_OSD_SHOW_PROGRESSION: {
-        int len = get_time_length(mpctx);
-        int pts = get_current_time(mpctx);
-        set_osd_bar(mpctx, 0, "Position", 0, 100, get_percent_pos(mpctx));
-        set_osd_msg(OSD_MSG_TEXT, 1, osd_duration,
-                    "%c %02d:%02d:%02d / %02d:%02d:%02d",
-                    mpctx->osd_function, pts / 3600, (pts / 60) % 60, pts % 60,
-                    len / 3600, (len / 60) % 60, len % 60);
+    case MP_CMD_OSD_SHOW_PROGRESSION:
+        mp_show_osd_progression(mpctx);
         break;
-    }
 
 #ifdef CONFIG_RADIO
     case MP_CMD_RADIO_STEP_CHANNEL:

mp_core.h
@@ -92,8 +92,6 @@ typedef struct MPContext {
     struct subtitle *vo_sub_last;
 
     bool add_osd_seek_info;
-    // if nonzero, hide current OSD contents when GetTimerMS() reaches this
-    unsigned int osd_show_percentage_until;
     unsigned int osd_visible;
 
     int osd_function;

mp_osd.h
@@ -40,5 +40,6 @@ void set_osd_bar(struct MPContext *mpctx, int type,const char* name,double min,d
 void set_osd_msg(int id, int level, int time, const char* fmt, ...);
 void set_osd_tmsg(int id, int level, int time, const char* fmt, ...);
 void rm_osd_msg(int id);
+void mp_show_osd_progression(struct MPContext *mpctx);
 
 #endif /* MPLAYER_MP_OSD_H */

mplayer.c
@@ -1025,22 +1025,28 @@ static void saddf(char *buf, unsigned *pos, int len, const char *format, ...)
  * \param len maximum number of characters in buf, not including terminating 0
  * \param time time value to convert/append
  */
-static void sadd_hhmmssf(char *buf, unsigned *pos, int len, float time)
+static void sadd_hhmmssff(char *buf, unsigned *pos, int len, double time,
+                          bool fractions)
 {
-    int64_t tenths = 10 * time;
-    int f1 = tenths % 10;
-    int ss = (tenths /  10) % 60;
-    int mm = (tenths / 600) % 60;
-    int hh = tenths / 36000;
     if (time < 0) {
         saddf(buf, pos, len, "unknown");
         return;
     }
-    if (hh > 0)
-        saddf(buf, pos, len, "%2d:", hh);
-    if (hh > 0 || mm > 0)
-        saddf(buf, pos, len, "%02d:", mm);
-    saddf(buf, pos, len, "%02d.%1d", ss, f1);
+    int h, m, s = time;
+    h = s / 3600;
+    s -= h * 3600;
+    m = s / 60;
+    s -= m * 60;
+    saddf(buf, pos, len, "%02d:", h);
+    saddf(buf, pos, len, "%02d:", m);
+    saddf(buf, pos, len, "%02d", s);
+    if (fractions)
+        saddf(buf, pos, len, ".%02d", (int)((time - (int)time) * 100));
+}
+
+static void sadd_percentage(char *buf, unsigned *pos, int len, int percent) {
+    if (percent >= 0)
+        saddf(buf, pos, len, " (%d%%)", percent);
 }
 
 static void print_status(struct MPContext *mpctx, double a_pos, bool at_frame)
@@ -1100,41 +1106,43 @@ static void print_status(struct MPContext *mpctx, double a_pos, bool at_frame)
     if (cur != MP_NOPTS_VALUE) {
         saddf(line, &pos, width, "%6.1f ", cur);
         saddf(line, &pos, width, "(");
-        sadd_hhmmssf(line, &pos, width, cur);
-        saddf(line, &pos, width, ") ");
+        sadd_hhmmssff(line, &pos, width, cur, mpctx->opts.osd_fractions);
+        saddf(line, &pos, width, ")");
     } else
-        saddf(line, &pos, width, " ??? ");
+        saddf(line, &pos, width, " ???");
 
     double len = get_time_length(mpctx);
     if (len >= 0) {
-        saddf(line, &pos, width, "of %.1f (", len);
-        sadd_hhmmssf(line, &pos, width, len);
-        saddf(line, &pos, width, ") ");
+        saddf(line, &pos, width, " / %.1f (", len);
+        sadd_hhmmssff(line, &pos, width, len, mpctx->opts.osd_fractions);
+        saddf(line, &pos, width, ")");
     }
+
+    sadd_percentage(line, &pos, width, get_percent_pos(mpctx));
 
     // A-V sync
     if (mpctx->sh_audio && sh_video) {
         if (mpctx->last_av_difference != MP_NOPTS_VALUE)
-            saddf(line, &pos, width, "A-V:%7.3f ", mpctx->last_av_difference);
+            saddf(line, &pos, width, " A-V:%7.3f", mpctx->last_av_difference);
         else
-            saddf(line, &pos, width, "A-V: ??? ");
+            saddf(line, &pos, width, " A-V: ???");
         if (fabs(mpctx->total_avsync_change) > 0.01)
-            saddf(line, &pos, width, "ct:%7.3f ", mpctx->total_avsync_change);
+            saddf(line, &pos, width, " ct:%7.3f", mpctx->total_avsync_change);
     }
 
     // VO stats
     if (sh_video && drop_frame_cnt)
-        saddf(line, &pos, width, "Dropped: %d ", drop_frame_cnt);
+        saddf(line, &pos, width, " Dropped: %d", drop_frame_cnt);
 
 #ifdef CONFIG_STREAM_CACHE
     // cache stats
     if (stream_cache_size > 0)
-        saddf(line, &pos, width, "Cache: %d%% ", cache_fill_status(mpctx->stream));
+        saddf(line, &pos, width, " Cache: %d%%", cache_fill_status(mpctx->stream));
 #endif
 
     // other
     if (opts->playback_speed != 1)
-        saddf(line, &pos, width, "Speed: %4.2fx ", opts->playback_speed);
+        saddf(line, &pos, width, " Speed: %4.2fx", opts->playback_speed);
 
     // end
     if (erase_to_end_of_line) {
@@ -1321,8 +1329,6 @@ static mp_osd_msg_t *get_osd_msg(struct MPContext *mpctx)
             mpctx->osd_function = mpctx->paused ? OSD_PAUSE : OSD_PLAY;
         }
     }
-    if (mpctx->osd_show_percentage_until - now > 36000000)
-        mpctx->osd_show_percentage_until = 0;
 
     if (!last_update)
         last_update = now;
@@ -1412,6 +1418,28 @@ void set_osd_subtitle(struct MPContext *mpctx, subtitle *subs)
     }
 }
 
+// sym == mpctx->osd_function
+static void saddf_osd_function_sym(char *buffer, unsigned *pos, int len,
+                                   int sym)
+{
+    char temp[10];
+    osd_get_function_sym(temp, sizeof(temp), sym);
+    saddf(buffer, pos, len, "%s ", temp);
+}
+
+static void sadd_osd_status(char *buffer, unsigned *pos, int len,
+                            struct MPContext *mpctx, bool full)
+{
+    bool fractions = mpctx->opts.osd_fractions;
+    saddf_osd_function_sym(buffer, pos, len, mpctx->osd_function);
+    sadd_hhmmssff(buffer, pos, len, get_current_time(mpctx), fractions);
+    if (full) {
+        saddf(buffer, pos, len, " / ");
+        sadd_hhmmssff(buffer, pos, len, get_time_length(mpctx), fractions);
+        sadd_percentage(buffer, pos, len, get_percent_pos(mpctx));
+    }
+}
+
 /**
  * \brief Update the OSD message line.
  *
@@ -1428,10 +1456,7 @@ static void update_osd_msg(struct MPContext *mpctx)
     struct osd_state *osd = mpctx->osd;
 
     if (mpctx->add_osd_seek_info) {
-        double percentage = get_percent_pos(mpctx);
-        set_osd_bar(mpctx, 0, "Position", 0, 100, percentage);
-        if (mpctx->sh_video && opts->term_osd != 1)
-            mpctx->osd_show_percentage_until = (GetTimerMS() + 1000) | 1;
+        set_osd_bar(mpctx, 0, "Position", 0, 100, get_percent_pos(mpctx));
         mpctx->add_osd_seek_info = false;
     }
 
@@ -1455,67 +1480,15 @@ static void update_osd_msg(struct MPContext *mpctx)
 
     if (mpctx->sh_video && opts->term_osd != 1) {
         // fallback on the timer
-        char osd_text_timer[128] = {0};
-        if (opts->osd_level >= 2) {
-            int len = get_time_length(mpctx);
-            int percentage = -1;
-            char percentage_text[10];
-            char fractions_text[4];
-            double fpts = get_current_time(mpctx);
-            int pts = fpts;
+        char text[128] = "";
+        int len = sizeof(text);
+        unsigned pos = 0;
 
-            if (mpctx->osd_show_percentage_until)
-                percentage = get_percent_pos(mpctx);
+        if (opts->osd_level >= 2)
+            sadd_osd_status(text, &pos, len, mpctx, opts->osd_level == 3);
 
-            if (percentage >= 0)
-                snprintf(percentage_text, 9, " (%d%%)", percentage);
-            else
-                percentage_text[0] = 0;
-
-            if (opts->osd_fractions == 1) {
-                //print fractions as sub-second timestamp
-                snprintf(fractions_text, sizeof(fractions_text), ".%02d",
-                         (int)((fpts - pts) * 100));
-            } else if (opts->osd_fractions == 2) {
-                /* Print fractions by estimating the frame count within the
-                 * second.
-                 *
-                 * Rounding or cutting off numbers after the decimal point
-                 * causes problems because of float's precision and movies
-                 * whose first frame is not exactly at timestamp 0. Therefore,
-                 * we add 0.2 and cut off at the decimal point, which proved
-                 * to be good heuristic.
-                 */
-                double fps = mpctx->sh_video->fps;
-                if (fps <= 1 || fps > 99)
-                    strcpy(fractions_text, ".??");
-                else
-                    snprintf(fractions_text, sizeof(fractions_text), ".%02d",
-                             (int) ((fpts - pts) * fps + 0.2));
-            } else {
-                //do not print fractions
-                fractions_text[0] = 0;
-            }
-
-            osd_get_function_sym(osd_text_timer, sizeof(osd_text_timer),
-                                 mpctx->osd_function);
-            size_t blen = strlen(osd_text_timer);
-
-            if (opts->osd_level == 3)
-                snprintf(osd_text_timer + blen, sizeof(osd_text_timer) - blen,
-                         " %02d:%02d:%02d%s / %02d:%02d:%02d%s",
-                         pts / 3600, (pts / 60) % 60, pts % 60, fractions_text,
-                         len / 3600, (len / 60) % 60, len % 60,
-                         percentage_text);
-            else
-                snprintf(osd_text_timer + blen, sizeof(osd_text_timer) - blen,
-                         " %02d:%02d:%02d%s%s",
-                         pts / 3600, (pts / 60) % 60, pts % 60, fractions_text,
-                         percentage_text);
-        }
-
-        if (strcmp(osd->osd_text, osd_text_timer)) {
-            osd_set_text(osd, osd_text_timer);
+        if (strcmp(osd->osd_text, text)) {
+            osd_set_text(osd, text);
             vo_osd_changed(OSDTYPE_OSD);
         }
         return;
@@ -1528,6 +1501,17 @@ static void update_osd_msg(struct MPContext *mpctx)
     }
 }
 
+void mp_show_osd_progression(struct MPContext *mpctx)
+{
+    char text[128] = "";
+    int len = sizeof(text);
+    unsigned pos = 0;
+
+    sadd_osd_status(text, &pos, len, mpctx, true);
+    set_osd_msg(OSD_MSG_TEXT, 1, mpctx->opts.osd_duration, "%s", text);
+
+    set_osd_bar(mpctx, 0, "Position", 0, 100, get_percent_pos(mpctx));
+}
 
 void reinit_audio_chain(struct MPContext *mpctx)
 {