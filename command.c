mpv-player/mpv

commit 804bf91570a24b949a6c68530daaf9162df9a234

commands, dvd, dvdnav, bluray: cleanup sub/audio track language display
Code cleanup: Use a stream_control instead of global functions to
get the language associate with a audio or subtitle stream from
the streaming layer.

git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@34736 b3059339-0415-0410-9bf9-f77b7e298cf2

Support showing the stream language with br:// playback.

git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@34737 b3059339-0415-0410-9bf9-f77b7e298cf2

Fix DVDs showing the subtitle language as "unknown"
for a long time.

git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@34777 b3059339-0415-0410-9bf9-f77b7e298cf2

Author: reimar

Note: heavily modified by wm4 for this fork of mplayer.

command.c
@@ -941,36 +941,21 @@ static int mp_property_audio(m_option_t *prop, int action, void *arg,
         if (!arg)
             return M_PROPERTY_ERROR;
 
-        if (current_id < 0)
+        if (!sh || current_id < 0)
             *(char **) arg = talloc_strdup(NULL, mp_gtext("disabled"));
-        else if (sh && (sh->lang || sh->title)) {
-            char *lang = sh->lang ? sh->lang : mp_gtext("unknown");
+        else {
+            char *lang = demuxer_audio_lang(mpctx->demuxer, current_id);
+            if (!lang)
+                lang = talloc_strdup(NULL, mp_gtext("unknown"));
+
             if (sh->title)
                 *(char **)arg = talloc_asprintf(NULL, "(%d) %s (\"%s\")",
                                                 current_id, lang, sh->title);
             else
                 *(char **)arg = talloc_asprintf(NULL, "(%d) %s", current_id,
                                                 lang);
-        } else {
-            char lang[40];
-            strncpy(lang, mp_gtext("unknown"), sizeof(lang));
-            if (0) ;
-#ifdef CONFIG_DVDREAD
-            else if (mpctx->stream->type == STREAMTYPE_DVD) {
-                int code = dvd_lang_from_aid(mpctx->stream, current_id);
-                if (code) {
-                    lang[0] = code >> 8;
-                    lang[1] = code;
-                    lang[2] = 0;
-                }
-            }
-#endif
 
-#ifdef CONFIG_DVDNAV
-            else if (mpctx->stream->type == STREAMTYPE_DVDNAV)
-                mp_dvdnav_lang_from_aid(mpctx->stream, current_id, lang);
-#endif
-            *(char **)arg = talloc_asprintf(NULL, "(%d) %s", current_id, lang);
+            talloc_free(lang);
         }
         return M_PROPERTY_OK;
 
@@ -1572,34 +1557,6 @@ static int mp_property_sub(m_option_t *prop, int action, void *arg,
                                              strlen(tmp) < 20 ? tmp : tmp + strlen(tmp) - 19);
             return M_PROPERTY_OK;
         }
-#ifdef CONFIG_DVDNAV
-        if (mpctx->stream->type == STREAMTYPE_DVDNAV) {
-            if (vo_spudec && opts->sub_id >= 0) {
-                unsigned char lang[3];
-                if (mp_dvdnav_lang_from_sid(mpctx->stream, opts->sub_id,
-                                            lang)) {
-                    *(char **) arg = talloc_asprintf(NULL, "(%d) %s",
-                                                     opts->sub_id, lang);
-                    return M_PROPERTY_OK;
-                }
-            }
-        }
-#endif
-
-        if ((d_sub->demuxer->type == DEMUXER_TYPE_MATROSKA
-             || d_sub->demuxer->type == DEMUXER_TYPE_LAVF
-             || d_sub->demuxer->type == DEMUXER_TYPE_LAVF_PREFERRED)
-            && d_sub->sh && opts->sub_id >= 0) {
-            struct sh_sub *sh = d_sub->sh;
-            char *lang = sh->lang ? sh->lang : mp_gtext("unknown");
-            if (sh->title)
-                *(char **)arg = talloc_asprintf(NULL, "(%d) %s (\"%s\")",
-                                                opts->sub_id, lang, sh->title);
-            else
-                *(char **)arg = talloc_asprintf(NULL, "(%d) %s",
-                                                opts->sub_id, lang);
-            return M_PROPERTY_OK;
-        }
 
         if (vo_vobsub && vobsub_id >= 0) {
             const char *language = mp_gtext("unknown");
@@ -1608,22 +1565,13 @@ static int mp_property_sub(m_option_t *prop, int action, void *arg,
                                              vobsub_id, language ? language : mp_gtext("unknown"));
             return M_PROPERTY_OK;
         }
-#ifdef CONFIG_DVDREAD
-        if (vo_spudec && mpctx->stream->type == STREAMTYPE_DVD
-            && opts->sub_id >= 0) {
-            char lang[3];
-            int code = dvd_lang_from_sid(mpctx->stream, opts->sub_id);
-            lang[0] = code >> 8;
-            lang[1] = code;
-            lang[2] = 0;
-            *(char **) arg = talloc_asprintf(NULL, "(%d) %s",
-                                             opts->sub_id, lang);
-            return M_PROPERTY_OK;
-        }
-#endif
         if (opts->sub_id >= 0) {
+            char *lang = demuxer_sub_lang(mpctx->demuxer, opts->sub_id);
+            if (!lang)
+                lang = talloc_strdup(NULL, mp_gtext("unknown"));
             *(char **) arg = talloc_asprintf(NULL, "(%d) %s", opts->sub_id,
-                                             mp_gtext("unknown"));
+                                             lang);
+            talloc_free(lang);
             return M_PROPERTY_OK;
         }
         *(char **) arg = talloc_strdup(NULL, mp_gtext("disabled"));

libmpdemux/demuxer.h
@@ -415,4 +415,7 @@ int demuxer_angles_count(struct demuxer *demuxer);
 int demuxer_audio_track_by_lang_and_default(struct demuxer *d, char **langt);
 int demuxer_sub_track_by_lang_and_default(struct demuxer *d, char **langt);
 
+char *demuxer_audio_lang(demuxer_t *d, int id);
+char *demuxer_sub_lang(demuxer_t *d, int id);
+
 #endif /* MPLAYER_DEMUXER_H */

libmpdemux/demuxer.c
@@ -1452,6 +1452,42 @@ int demuxer_set_angle(demuxer_t *demuxer, int angle)
     return angle;
 }
 
+char *demuxer_audio_lang(demuxer_t *d, int id)
+{
+    struct stream_lang_req req;
+    sh_audio_t *sh;
+    if (id < 0 || id >= MAX_A_STREAMS)
+        return NULL;
+    sh = d->a_streams[id];
+    if (!sh)
+        return NULL;
+    if (sh->lang)
+        return talloc_strdup(NULL, sh->lang);
+    req.type = stream_ctrl_audio;
+    req.id = sh->aid;
+    if (stream_control(d->stream, STREAM_CTRL_GET_LANG, &req) == STREAM_OK)
+        return req.name;
+    return NULL;
+}
+
+char *demuxer_sub_lang(demuxer_t *d, int id)
+{
+    struct stream_lang_req req;
+    sh_sub_t *sh;
+    if (id < 0 || id >= MAX_S_STREAMS)
+        return NULL;
+    sh = d->s_streams[id];
+    if (sh && sh->lang)
+        return talloc_strdup(NULL, sh->lang);
+    req.type = stream_ctrl_sub;
+    // assume 1:1 mapping so we can show the language of
+    // DVD subs even when we have not yet created the stream.
+    req.id = sh ? sh->sid : id;
+    if (stream_control(d->stream, STREAM_CTRL_GET_LANG, &req) == STREAM_OK)
+        return req.name;
+    return NULL;
+}
+
 int demuxer_audio_track_by_lang_and_default(struct demuxer *d, char **langt)
 {
     int n = 0;

stream/cache2.c
@@ -88,6 +88,7 @@ typedef struct {
   volatile int control;
   volatile unsigned control_uint_arg;
   volatile double control_double_arg;
+  volatile struct stream_lang_req control_lang_arg;
   volatile int control_res;
   volatile double stream_time_length;
   volatile double stream_time_pos;
@@ -309,6 +310,9 @@ static int cache_execute_control(cache_vars_t *s) {
     case STREAM_CTRL_GET_ANGLE:
       s->control_res = s->stream->control(s->stream, s->control, &uint_res);
       s->control_uint_arg = uint_res;
+      break;
+    case STREAM_CTRL_GET_LANG:
+      s->control_res = s->stream->control(s->stream, s->control, (void *)&s->control_lang_arg);
       break;
     default:
       s->control_res = STREAM_UNSUPPORTED;
@@ -620,6 +624,8 @@ int cache_do_control(stream_t *stream, int cmd, void *arg) {
     case STREAM_CTRL_GET_CURRENT_TIME:
       *(double *)arg = s->stream_time_pos;
       return s->stream_time_pos != MP_NOPTS_VALUE ? STREAM_OK : STREAM_UNSUPPORTED;
+    case STREAM_CTRL_GET_LANG:
+      s->control_lang_arg = *(struct stream_lang_req *)arg;
     case STREAM_CTRL_GET_NUM_CHAPTERS:
     case STREAM_CTRL_GET_CURRENT_CHAPTER:
     case STREAM_CTRL_GET_ASPECT_RATIO:
@@ -664,6 +670,9 @@ int cache_do_control(stream_t *stream, int cmd, void *arg) {
     case STREAM_CTRL_GET_NUM_ANGLES:
     case STREAM_CTRL_GET_ANGLE:
       *(unsigned *)arg = s->control_uint_arg;
+      break;
+    case STREAM_CTRL_GET_LANG:
+      *(struct stream_lang_req *)arg = s->control_lang_arg;
       break;
   }
   return s->control_res;

stream/stream.h
@@ -99,7 +99,18 @@
 #define STREAM_CTRL_GET_ANGLE 10
 #define STREAM_CTRL_SET_ANGLE 11
 #define STREAM_CTRL_GET_NUM_TITLES 12
+#define STREAM_CTRL_GET_LANG 13
 
+enum stream_ctrl_type {
+	stream_ctrl_audio,
+	stream_ctrl_sub,
+};
+
+struct stream_lang_req {
+	enum stream_ctrl_type type;
+	int id;
+        char *name;
+};
 
 typedef enum {
 	streaming_stopped_e,

stream/stream_bluray.c
@@ -34,6 +34,7 @@
 #include "config.h"
 #include "libavutil/common.h"
 #include "libmpdemux/demuxer.h"
+#include "talloc.h"
 #include "mp_msg.h"
 #include "m_struct.h"
 #include "m_option.h"
@@ -189,6 +190,35 @@ static int bluray_stream_control(stream_t *s, int cmd, void *arg)
         bd_free_title_info(ti);
 
         return 1;
+    }
+
+    case STREAM_CTRL_GET_LANG: {
+        struct stream_lang_req *req = arg;
+        BLURAY_TITLE_INFO *ti = bd_get_title_info(b->bd, b->current_title, b->current_angle);
+        if (ti->clip_count) {
+            BLURAY_STREAM_INFO *si = NULL;
+            int count = 0;
+            switch (req->type) {
+            case stream_ctrl_audio:
+                count = ti->clips[0].audio_stream_count;
+                si = ti->clips[0].audio_streams;
+                break;
+            case stream_ctrl_sub:
+                count = ti->clips[0].pg_stream_count;
+                si = ti->clips[0].pg_streams;
+                break;
+            }
+            while (count-- > 0) {
+                if (si->pid == req->id) {
+                    req->name = talloc_strndup(NULL, si->lang, 4);
+                    bd_free_title_info(ti);
+                    return STREAM_OK;
+                }
+                si++;
+            }
+        }
+        bd_free_title_info(ti);
+        return STREAM_ERROR;
     }
 
     default:

stream/stream_dvd.c
@@ -24,6 +24,7 @@
 #include <string.h>
 
 #include "config.h"
+#include "talloc.h"
 #include "mp_msg.h"
 
 #include <libgen.h>
@@ -112,7 +113,7 @@ int dvd_chapter_from_cell(dvd_priv_t* dvd,int title,int cell)
   return chapter;
 }
 
-int dvd_lang_from_aid(stream_t *stream, int id) {
+static int dvd_lang_from_aid(stream_t *stream, int id) {
   dvd_priv_t *d;
   int i;
   if (!stream) return 0;
@@ -155,7 +156,7 @@ int dvd_number_of_subs(stream_t *stream) {
   return maxid + 1;
 }
 
-int dvd_lang_from_sid(stream_t *stream, int id) {
+static int dvd_lang_from_sid(stream_t *stream, int id) {
   int i;
   dvd_priv_t *d;
   if (!stream) return 0;
@@ -686,6 +687,23 @@ static int control(stream_t *stream,int cmd,void* arg)
             dvd_angle = ang - 1;
             d->angle_seek = 1;
             return 1;
+        }
+        case STREAM_CTRL_GET_LANG:
+        {
+            struct stream_lang_req *req = arg;
+            int lang = 0;
+            switch(req->type) {
+            case stream_ctrl_audio:
+                lang = dvd_lang_from_aid(stream, req->id);
+                break;
+            case stream_ctrl_sub:
+                lang = dvd_lang_from_sid(stream, req->id);
+                break;
+            }
+            if (!lang)
+                break;
+            req->name = talloc_strdup(NULL, (char[]) {lang >> 8, lang, 0});
+            return STREAM_OK;
         }
     }
     return STREAM_UNSUPPORTED;

stream/stream_dvd.h
@@ -57,8 +57,6 @@ typedef struct {
 } dvd_priv_t;
 
 int dvd_number_of_subs(stream_t *stream);
-int dvd_lang_from_aid(stream_t *stream, int id);
-int dvd_lang_from_sid(stream_t *stream, int id);
 int dvd_aid_from_lang(stream_t *stream, char **lang);
 int dvd_sid_from_lang(stream_t *stream, char **lang);
 int dvd_chapter_from_cell(dvd_priv_t *dvd,int title,int cell);

commit b4dd2194d5c2540583becf6c021d8eb0f52cddc0

command: fix subtitle selection display
Commit 9c02ae7e9510897 set the sh variable (see diff) to the struct of
type sh_sub instead the one of sh_stream. Unfortunately this didn't
crash, and merely made the OSD show "unknown" for the language.

Commit 804bf91570a24b9 accidentally removed the display of the track
title. Add it back.

command.c
@@ -1576,12 +1576,16 @@ static int mp_property_sub(m_option_t *prop, int action, void *arg,
             return M_PROPERTY_OK;
         }
         if (opts->sub_id >= 0 && mpctx->d_sub && mpctx->d_sub->sh) {
-            struct sh_stream *sh = mpctx->d_sub->sh;
+            struct sh_stream *sh = ((struct sh_sub *)mpctx->d_sub->sh)->gsh;
             char *lang = demuxer_stream_lang(mpctx->demuxer, sh);
             if (!lang)
                 lang = talloc_strdup(NULL, mp_gtext("unknown"));
-            *(char **) arg = talloc_asprintf(NULL, "(%d) %s", opts->sub_id,
-                                             lang);
+            if (sh->title)
+                *(char **)arg = talloc_asprintf(NULL, "(%d) %s (\"%s\")",
+                                                opts->sub_id, lang, sh->title);
+            else
+                *(char **) arg = talloc_asprintf(NULL, "(%d) %s", opts->sub_id,
+                                                 lang);
             talloc_free(lang);
             return M_PROPERTY_OK;
         }

libmpdemux/demuxer.h
@@ -136,7 +136,7 @@ typedef struct demux_stream {
 // ---- mov -----
     unsigned int ss_mul, ss_div;
 // ---- stream header ----
-    void *sh;
+    void *sh;              // points to sh_audio or sh_video
 } demux_stream_t;
 
 typedef struct demuxer_info {