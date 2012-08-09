mpv-player/mpv

commit ebaaa41f2a4f2d492141f8af86a1d0694778baae

Remove teletext support
Teletext requires special OSD support. Because I can't even test
teletext, I can't restore support for it. Since teletext can be
considered ancient and obscure, and since it doesn't make sense to keep
the remaining teletext code without being able to use it, I'm removing
it.

command.c
@@ -51,7 +51,6 @@
 #include "mixer.h"
 #include "libmpcodecs/dec_video.h"
 #include "libmpcodecs/dec_audio.h"
-#include "libmpcodecs/dec_teletext.h"
 #include "osdep/strsep.h"
 #include "sub/vobsub.h"
 #include "sub/spudec.h"
@@ -2110,91 +2109,6 @@ static int mp_property_tv_color(m_option_t *prop, int action, void *arg,
 
 #endif
 
-static int mp_property_teletext_common(m_option_t *prop, int action, void *arg,
-                                       MPContext *mpctx)
-{
-    int val, result;
-    int base_ioctl = prop->offset;
-    /*
-       for teletext's GET,SET,STEP ioctls this is not 0
-       SET is GET+1
-       STEP is GET+2
-     */
-    if (!mpctx->demuxer || !mpctx->demuxer->teletext)
-        return M_PROPERTY_UNAVAILABLE;
-    if (!base_ioctl)
-        return M_PROPERTY_ERROR;
-
-    switch (action) {
-    case M_PROPERTY_GET:
-        if (!arg)
-            return M_PROPERTY_ERROR;
-        result = teletext_control(mpctx->demuxer->teletext, base_ioctl, arg);
-        break;
-    case M_PROPERTY_SET:
-        if (!arg)
-            return M_PROPERTY_ERROR;
-        M_PROPERTY_CLAMP(prop, *(int *) arg);
-        result = teletext_control(mpctx->demuxer->teletext, base_ioctl + 1,
-                                  arg);
-        break;
-    case M_PROPERTY_STEP_UP:
-    case M_PROPERTY_STEP_DOWN:
-        result = teletext_control(mpctx->demuxer->teletext, base_ioctl, &val);
-        val += (arg ? *(int *) arg : 1) * (action == M_PROPERTY_STEP_DOWN ?
-                                           -1 : 1);
-        result = teletext_control(mpctx->demuxer->teletext, base_ioctl + 1,
-                                  &val);
-        break;
-    default:
-        return M_PROPERTY_NOT_IMPLEMENTED;
-    }
-
-    return result == VBI_CONTROL_TRUE ? M_PROPERTY_OK : M_PROPERTY_ERROR;
-}
-
-static int mp_property_teletext_mode(m_option_t *prop, int action, void *arg,
-                                     MPContext *mpctx)
-{
-    int result;
-    int val;
-
-    //with tvh==NULL will fail too
-    result = mp_property_teletext_common(prop, action, arg, mpctx);
-    if (result != M_PROPERTY_OK)
-        return result;
-
-    if (teletext_control(mpctx->demuxer->teletext,
-                         prop->offset, &val) == VBI_CONTROL_TRUE && val)
-        mp_input_set_section(mpctx->input, "teletext");
-    else
-        mp_input_set_section(mpctx->input, "tv");
-    return M_PROPERTY_OK;
-}
-
-static int mp_property_teletext_page(m_option_t *prop, int action, void *arg,
-                                     MPContext *mpctx)
-{
-    int result;
-    int val;
-    if (!mpctx->demuxer->teletext)
-        return M_PROPERTY_UNAVAILABLE;
-    switch (action) {
-    case M_PROPERTY_STEP_UP:
-    case M_PROPERTY_STEP_DOWN:
-        //This should be handled separately
-        val = (arg ? *(int *) arg : 1) * (action == M_PROPERTY_STEP_DOWN ?
-                                          -1 : 1);
-        result = teletext_control(mpctx->demuxer->teletext,
-                                  TV_VBI_CONTROL_STEP_PAGE, &val);
-        break;
-    default:
-        result = mp_property_teletext_common(prop, action, arg, mpctx);
-    }
-    return result;
-}
-
-
 /// All properties available in MPlayer.
 /** \ingroup Properties
  */
@@ -2357,17 +2271,6 @@ static const m_option_t mp_properties[] = {
     { "tv_hue", mp_property_tv_color, CONF_TYPE_INT,
       M_OPT_RANGE, -100, 100, .offset = TV_COLOR_HUE },
 #endif
-    { "teletext_page", mp_property_teletext_page, CONF_TYPE_INT,
-      M_OPT_RANGE, 100, 899, .offset = TV_VBI_CONTROL_GET_PAGE },
-    { "teletext_subpage", mp_property_teletext_common, CONF_TYPE_INT,
-      M_OPT_RANGE, 0, 64, .offset = TV_VBI_CONTROL_GET_SUBPAGE },
-    { "teletext_mode", mp_property_teletext_mode, CONF_TYPE_FLAG,
-      M_OPT_RANGE, 0, 1, .offset = TV_VBI_CONTROL_GET_MODE },
-    { "teletext_format", mp_property_teletext_common, CONF_TYPE_INT,
-      M_OPT_RANGE, 0, 3, .offset = TV_VBI_CONTROL_GET_FORMAT },
-    { "teletext_half_page", mp_property_teletext_common, CONF_TYPE_INT,
-      M_OPT_RANGE, 0, 2, .offset = TV_VBI_CONTROL_GET_HALF_PAGE },
-    { NULL, NULL, NULL, 0, 0, 0, NULL }
 };
 
 
@@ -3356,16 +3259,6 @@ void run_command(MPContext *mpctx, mp_cmd_t *cmd)
             tv_step_chanlist((tvi_handle_t *) (mpctx->demuxer->priv));
         break;
 #endif /* CONFIG_TV */
-    case MP_CMD_TV_TELETEXT_ADD_DEC:
-        if (mpctx->demuxer->teletext)
-            teletext_control(mpctx->demuxer->teletext, TV_VBI_CONTROL_ADD_DEC,
-                             &(cmd->args[0].v.s));
-        break;
-    case MP_CMD_TV_TELETEXT_GO_LINK:
-        if (mpctx->demuxer->teletext)
-            teletext_control(mpctx->demuxer->teletext, TV_VBI_CONTROL_GO_LINK,
-                             &(cmd->args[0].v.i));
-        break;
 
     case MP_CMD_SUB_LOAD:
         if (sh_video) {

Makefile
@@ -241,7 +241,6 @@ SRCS_COMMON = asxparser.c \
               libmpcodecs/ad_msadpcm.c \
               libmpcodecs/ad_pcm.c \
               libmpcodecs/dec_audio.c \
-              libmpcodecs/dec_teletext.c \
               libmpcodecs/dec_video.c \
               libmpcodecs/img_format.c \
               libmpcodecs/mp_image.c \

cfg-mplayer.h
@@ -141,10 +141,6 @@ const m_option_t tvopts_conf[]={
 #endif /* CONFIG_ALSA */
 #endif /* defined(CONFIG_TV_V4L2) */
     {"adevice", &stream_tv_defaults.adevice, CONF_TYPE_STRING, 0, 0, 0, NULL},
-    {"tdevice", &stream_tv_defaults.teletext.device, CONF_TYPE_STRING, 0, 0, 0, NULL},
-    {"tpage", &stream_tv_defaults.teletext.page, CONF_TYPE_INT, CONF_RANGE, 100, 899, NULL},
-    {"tformat", &stream_tv_defaults.teletext.format, CONF_TYPE_INT, CONF_RANGE, 0, 3, NULL},
-    {"tlang", &stream_tv_defaults.teletext.lang, CONF_TYPE_INT, CONF_RANGE, -1, 0x7f, NULL},
     {"audioid", &stream_tv_defaults.audio_id, CONF_TYPE_INT, CONF_RANGE, 0, 9, NULL},
 #ifdef CONFIG_TV_DSHOW
     {"hidden_video_renderer", &stream_tv_defaults.hidden_video_renderer, CONF_TYPE_FLAG, 0, 0, 1, NULL},

etc/input.conf
@@ -133,9 +133,6 @@ h tv_step_channel 1
 k tv_step_channel -1
 n tv_step_norm
 u tv_step_chanlist
-X step_property teletext_mode 1
-W step_property teletext_page 1
-Q step_property teletext_page -1
 
 #
 # DVDNAV

input/input.c
@@ -196,8 +196,6 @@ static const mp_cmd_t mp_cmds[] = {
   { MP_CMD_PLAYLIST_CLEAR, "playlist_clear", },
   { MP_CMD_RUN, "run", { ARG_STRING } },
   { MP_CMD_VF_CHANGE_RECTANGLE, "change_rectangle", { ARG_INT, ARG_INT } },
-  { MP_CMD_TV_TELETEXT_ADD_DEC, "teletext_add_dec", { ARG_STRING } },
-  { MP_CMD_TV_TELETEXT_GO_LINK, "teletext_go_link", { ARG_INT } },
 
 #ifdef CONFIG_DVDNAV
   { MP_CMD_DVDNAV, "dvdnav", { ARG_STRING } },

input/input.h
@@ -120,8 +120,6 @@ enum mp_command_type {
     MP_CMD_LOOP,
     MP_CMD_BALANCE,
     MP_CMD_SUB_SCALE,
-    MP_CMD_TV_TELETEXT_ADD_DEC,
-    MP_CMD_TV_TELETEXT_GO_LINK,
     MP_CMD_TV_START_SCAN,
     MP_CMD_SUB_SOURCE,
     MP_CMD_SUB_FILE,

libmpcodecs/dec_teletext.h
@@ -1,148 +1,0 @@
-/*
- * Teletext support
- *
- * Copyright (C) 2007 Vladimir Voroshilov <voroshil@gmail.com>
- *
- * This file is part of MPlayer.
- *
- * MPlayer is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * MPlayer is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with MPlayer; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
-
-#ifndef MPLAYER_DEC_TELETEXT_H
-#define MPLAYER_DEC_TELETEXT_H
-
-struct tt_param {
-    char *device;  ///< teletext device
-    int format;    ///< teletext display format
-    int page;      ///< start teletext page
-    int lang;      ///< primary language code
-};
-
-#define VBI_CONTROL_FALSE              0
-#define VBI_CONTROL_TRUE               1
-#define VBI_CONTROL_UNKNOWN           -1
-
-int teletext_control(void* p, int cmd, void *arg);
-
-/*
-  TELETEXT controls (through teletext_control() )
-   NOTE:
-    _SET_ should be _GET_ +1
-   _STEP_ should be _GET_ +2
-*/
-#define TV_VBI_CONTROL_GET_MODE        0x510   ///< get current mode teletext
-#define TV_VBI_CONTROL_SET_MODE        0x511   ///< on/off grab teletext
-
-#define TV_VBI_CONTROL_GET_PAGE        0x513   ///< get grabbed teletext page
-#define TV_VBI_CONTROL_SET_PAGE        0x514   ///< set grab teletext page number
-#define TV_VBI_CONTROL_STEP_PAGE       0x515   ///< step grab teletext page number
-
-#define TV_VBI_CONTROL_GET_SUBPAGE     0x516   ///< get grabbed teletext page
-#define TV_VBI_CONTROL_SET_SUBPAGE     0x517   ///< set grab teletext page number
-
-#define TV_VBI_CONTROL_GET_FORMAT      0x519   ///< get teletext format
-#define TV_VBI_CONTROL_SET_FORMAT      0x51a   ///< set teletext format
-
-#define TV_VBI_CONTROL_GET_HALF_PAGE   0x51c   ///< get current half page
-#define TV_VBI_CONTROL_SET_HALF_PAGE   0x51d   ///< switch half page
-
-#define TV_VBI_CONTROL_IS_CHANGED      0x540   ///< teletext page is changed
-#define TV_VBI_CONTROL_MARK_UNCHANGED  0x541   ///< teletext page is changed
-
-#define TV_VBI_CONTROL_ADD_DEC         0x550   ///< add page number with dec
-#define TV_VBI_CONTROL_GO_LINK         0x551   ///< go link (1..6) NYI
-#define TV_VBI_CONTROL_GET_VBIPAGE     0x552   ///< get vbi_image for grabbed teletext page
-#define TV_VBI_CONTROL_RESET           0x553   ///< vbi reset
-#define TV_VBI_CONTROL_START           0x554   ///< vbi start
-#define TV_VBI_CONTROL_STOP            0x555   ///< vbi stop
-#define TV_VBI_CONTROL_DECODE_PAGE     0x556   ///< decode vbi page
-#define TV_VBI_CONTROL_GET_NETWORKNAME 0x557   ///< get current network name
-#define TV_VBI_CONTROL_DECODE_DVB      0x558   ///< decode DVB teletext
-
-#define VBI_TFORMAT_TEXT    0               ///< text mode
-#define VBI_TFORMAT_BW      1               ///< black&white mode
-#define VBI_TFORMAT_GRAY    2               ///< grayscale mode
-#define VBI_TFORMAT_COLOR   3               ///< color mode (require color_spu patch!)
-
-#define VBI_MAX_PAGES      0x800            ///< max sub pages number
-#define VBI_MAX_SUBPAGES   64               ///< max sub pages number
-
-#define VBI_ROWS    25                      ///< teletext page height in rows
-#define VBI_COLUMNS 40                      ///< teletext page width in chars
-#define VBI_TIME_LINEPOS    26              ///< time line pos in page header
-
-typedef
-enum{
-    TT_FORMAT_OPAQUE=0,       ///< opaque
-    TT_FORMAT_TRANSPARENT,    ///< transparent
-    TT_FORMAT_OPAQUE_INV,     ///< opaque with inverted colors
-    TT_FORMAT_TRANSPARENT_INV ///< transparent with inverted colors
-} teletext_format;
-
-typedef
-enum{
-    TT_ZOOM_NORMAL=0,
-    TT_ZOOM_TOP_HALF,
-    TT_ZOOM_BOTTOM_HALF
-} teletext_zoom;
-
-typedef struct tt_char_s{
-    unsigned int unicode; ///< unicode (utf8) character
-    unsigned char fg;  ///< foreground color
-    unsigned char bg;  ///< background color
-    unsigned char gfx; ///< 0-no gfx, 1-solid gfx, 2-separated gfx
-    unsigned char flh; ///< 0-no flash, 1-flash
-    unsigned char hidden; ///< char is hidden (for subtitle pages)
-    unsigned char ctl; ///< control character
-    unsigned char lng; ///< lang: 0-secondary language,1-primary language
-    unsigned char raw; ///< raw character (as received from device)
-} tt_char;
-
-typedef struct tt_link_s{
-    int pagenum;          ///< page number
-    int subpagenum;       ///< subpage number
-} tt_link_t;
-
-typedef struct tt_page_s{
-    int pagenum;          ///< page number
-    int subpagenum;       ///< subpage number
-    unsigned char primary_lang;   ///< primary language code
-    unsigned char secondary_lang; ///< secondary language code
-    unsigned char active; ///< page is complete and ready for rendering
-    unsigned char flags;  ///< page flags
-    unsigned char raw[VBI_ROWS*VBI_COLUMNS]; ///< page data
-    struct tt_page_s* next_subpage;
-    struct tt_link_s links[6];
-}  tt_page;
-
-#define TT_PGFL_SUPPRESS_HEADER  0x01
-#define TT_PGFL_UPDATE_INDICATOR 0x02
-#define TT_PGFL_INTERRUPTED_SEQ  0x04
-#define TT_PGFL_INHIBIT_DISPLAY  0x08
-#define TT_PGFL_NEWFLASH         0x10
-#define TT_PGFL_SUBTITLE         0x20
-#define TT_PGFL_ERASE_PAGE       0x40
-#define TT_PGFL_MAGAZINE_SERIAL  0x80
-
-typedef struct tt_stream_props_s{
-    int sampling_rate;
-    int samples_per_line;
-    int offset;
-    int count[2];     ///< number of lines in first and second fields
-    int interlaced;   ///< vbi data are interlaced
-    int bufsize;      ///< required buffer size
-} tt_stream_props;
-
-#endif /* MPLAYER_DEC_TELETEXT_H */

commit 4e2fe3724b3bcd61794cf4fca88adf383f5547d4

command: fix crash when setting nonexistent property
Commit ebaaa41f2a4 accidentally removed the final terminator of the
properties array.

command.c
@@ -2249,6 +2249,8 @@ static const m_option_t mp_properties[] = {
     { "tv_hue", mp_property_tv_color, CONF_TYPE_INT,
       M_OPT_RANGE, -100, 100, .offset = TV_COLOR_HUE },
 #endif
+
+    {0},
 };
 
 