842402c6ca61efcb9d681e86e70d9136bd8d2bf7
wm4 <wm4@nowhere>
2012-10-19 18:05:09 +0200

    gl_common: hack to support compilation on FreeBSD
    It seems FreeBSD 10.0-CURRENT as of this time doesn't include the GLX
    extension and header file definitions for creating OpenGL 3.x core
    contexts. Dump some more hacks into gl_header_fixes.h.

--- libvo/gl_header_fixes.h
+++ libvo/gl_header_fixes.h
@@ -243,3 +243,15 @@
 #define GL_FRAMEBUFFER_SRGB               0x8DB9
 #endif
 #endif
+
+// FreeBSD 10.0-CURRENT lacks the GLX_ARB_create_context extension completely
+#ifndef GLX_CONTEXT_MAJOR_VERSION_ARB
+#define GLX_CONTEXT_MAJOR_VERSION_ARB           0x2091
+#define GLX_CONTEXT_MINOR_VERSION_ARB           0x2092
+#define GLX_CONTEXT_FLAGS_ARB                   0x2094
+#define GLX_CONTEXT_PROFILE_MASK_ARB            0x9126
+#define GLX_CONTEXT_DEBUG_BIT_ARB               0x0001
+#define GLX_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB  0x0002
+#define GLX_CONTEXT_CORE_PROFILE_BIT_ARB        0x00000001
+#define GLX_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB 0x00000002
+#endif
