86a5b7a4cc09b69eb108f8ef9fcb01811d7fa879
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2012-10-31 19:01:55 +0000

    cookies: replace sprintf with snprintf
    Use snprintf instead of sprintf.
    
    No good reason beyond paranoia and Coverity complaining about it.
    In a very theoretical, construed case the adds might overflow
    or the environment might change in-between the getenv calls.
    
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@35307 b3059339-0415-0410-9bf9-f77b7e298cf2

--- stream/cookies.c
+++ stream/cookies.c
@@ -207,11 +207,13 @@ static struct cookie_list_type *load_cookies(void)
     if (dir) {
 	while ((ent = readdir(dir)) != NULL) {
 	    if ((ent->d_name)[0] != '.') {
-		buf = malloc(strlen(getenv("HOME")) +
-                             sizeof("/.mozilla/default/") +
-                             strlen(ent->d_name) + sizeof("cookies.txt") + 1);
-		sprintf(buf, "%s/.mozilla/default/%s/cookies.txt",
-			 getenv("HOME"), ent->d_name);
+		const char *home = getenv("HOME");
+		unsigned len = strlen(home) +
+		               sizeof("/.mozilla/default/") +
+		               strlen(ent->d_name) + sizeof("cookies.txt") + 1;
+		buf = malloc(len);
+		snprintf(buf, len, "%s/.mozilla/default/%s/cookies.txt",
+			 home, ent->d_name);
 		list = load_cookies_from(buf, list);
 		free(buf);
 	    }
