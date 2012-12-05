f62c670709402699f8d5bb418f5f361a59f9b60f
Rudolf Polzer <divverent@xonotic.org>
2012-12-05 09:36:20 +0100

    core/cfg-mplayer: fix segfault when using DVD subs
    The option sub-forced-only was accidentally renamed back to
    forcedsubsonly in commit 72205635abe7, causing a segfault in
    mp_property_generic_option due to missing option.

--- core/cfg-mplayer.h
+++ core/cfg-mplayer.h
@@ -497,7 +497,7 @@ const m_option_t common_opts[] = {
     {"sub-delay", &sub_delay, CONF_TYPE_FLOAT, 0, 0.0, 10.0, NULL},
     {"subfps", &sub_fps, CONF_TYPE_FLOAT, 0, 0.0, 10.0, NULL},
     OPT_MAKE_FLAGS("autosub", sub_auto, 0),
-    {"forcedsubsonly", &forced_subs_only, CONF_TYPE_FLAG, 0, 0, 1, NULL},
+    {"sub-forced-only", &forced_subs_only, CONF_TYPE_FLAG, 0, 0, 1, NULL},
     // specify IFO file for VOBSUB subtitle
     {"ifo", &spudec_ifo, CONF_TYPE_STRING, 0, 0, 0, NULL},
     // enable Closed Captioning display
