mpv-player/mpv

commit 3f85094d4e3d7d0ab0dcc088ecaa2ccf9d55bb68

Fix potential bugs and issues, general cleanups
Most of these are reimar fixing issues found by Coverity static
analyzer, and possibly some more cleanup commits independent from
this.

Since these commits are rather noisy, squash them all together.

Try to make code a bit clearer.

git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@35294 b3059339-0415-0410-9bf9-f77b7e298cf2

Conflicts:
	audio/out/ao_alsa.c

Check the correct variable for NULL.

git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@35323 b3059339-0415-0410-9bf9-f77b7e298cf2

Remove pointless unreachable code (the loop condition already checks
the 0xff case).

git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@35325 b3059339-0415-0410-9bf9-f77b7e298cf2

Fix typo that might have caused reading beyond the string end.

git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@35326 b3059339-0415-0410-9bf9-f77b7e298cf2

Do not needlessly use "long" types.

git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@35331 b3059339-0415-0410-9bf9-f77b7e298cf2

Use AV_RB32 to avoid sign extension issues and validate offset before using it.

git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@35332 b3059339-0415-0410-9bf9-f77b7e298cf2

Remove nonsense casts.

git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@35343 b3059339-0415-0410-9bf9-f77b7e298cf2

Fix crash in case sh_audio allocation failed.

git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@35348 b3059339-0415-0410-9bf9-f77b7e298cf2

Fix potential NULL dereference.

git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@35351 b3059339-0415-0410-9bf9-f77b7e298cf2

Conflicts:
	libmpcodecs/ad_ffmpeg.c

Note: Slightly modified.

Fix malloc failure check to check the correct variable.

git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@35353 b3059339-0415-0410-9bf9-f77b7e298cf2

Avoid code duplication and pointless casts.

git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@35363 b3059339-0415-0410-9bf9-f77b7e298cf2

Conflicts:
	stream/tv.c

Error out if an invalid channel list name was specified

stream/cookies.c
@@ -112,44 +112,49 @@ static int parse_line(char **ptr, char *cols[6])
 /* Loads a file into RAM */
 static char *load_file(const char *filename, off_t * length)
 {
-    int fd;
-    char *buffer;
+    int fd = -1;
+    char *buffer = NULL;
 
     mp_msg(MSGT_NETWORK, MSGL_V, "Loading cookie file: %s\n", filename);
 
     fd = open(filename, O_RDONLY);
     if (fd < 0) {
 	mp_msg(MSGT_NETWORK, MSGL_V, "Could not open");
-	return NULL;
+	goto err_out;
     }
 
     *length = lseek(fd, 0, SEEK_END);
 
     if (*length < 0) {
 	mp_msg(MSGT_NETWORK, MSGL_V, "Could not find EOF");
-	return NULL;
+	goto err_out;
     }
 
     if (*length > SIZE_MAX - 1) {
 	mp_msg(MSGT_NETWORK, MSGL_V, "File too big, could not malloc.");
-	return NULL;
+	goto err_out;
     }
 
     lseek(fd, 0, SEEK_SET);
 
     if (!(buffer = malloc(*length + 1))) {
 	mp_msg(MSGT_NETWORK, MSGL_V, "Could not malloc.");
-	return NULL;
+	goto err_out;
     }
 
     if (read(fd, buffer, *length) != *length) {
 	mp_msg(MSGT_NETWORK, MSGL_V, "Read is behaving funny.");
-	return NULL;
+	goto err_out;
     }
     close(fd);
     buffer[*length] = 0;
 
     return buffer;
+
+err_out:
+    if (fd != -1) close(fd);
+    free(buffer);
+    return NULL;
 }
 
 /* Loads a cookies.txt file into a linked list. */
@@ -164,7 +169,7 @@ static struct cookie_list_type *load_cookies_from(const char *filename,
     if (!ptr)
 	return list;
 
-    while (*ptr > 0) {
+    while (*ptr) {
 	char *cols[7];
 	if (parse_line(&ptr, cols)) {
 	    struct cookie_list_type *new;
@@ -178,6 +183,7 @@ static struct cookie_list_type *load_cookies_from(const char *filename,
 	    list = new;
 	}
     }
+    free(ptr);
     return list;
 }
 
@@ -253,6 +259,5 @@ cookies_set(HTTP_header_t * http_hdr, const char *domain, const char *url)
 
     if (found_cookies)
 	http_set_field(http_hdr, buf);
-    else
-	free(buf);
+    free(buf);
 }

audio/decode/ad_lavc.c
@@ -192,11 +192,11 @@ static int init(sh_audio_t *sh_audio)
     mp_msg(MSGT_DECAUDIO, MSGL_V, "INFO: libavcodec \"%s\" init OK!\n",
            lavc_codec->name);
 
-    if (sh_audio->format == 0x3343414D) {
+    if (sh_audio->wf && sh_audio->format == 0x3343414D) {
         // MACE 3:1
         sh_audio->ds->ss_div = 2 * 3; // 1 samples/packet
         sh_audio->ds->ss_mul = 2 * sh_audio->wf->nChannels; // 1 byte*ch/packet
-    } else if (sh_audio->format == 0x3643414D) {
+    } else if (sh_audio->wf && sh_audio->format == 0x3643414D) {
         // MACE 6:1
         sh_audio->ds->ss_div = 2 * 6; // 1 samples/packet
         sh_audio->ds->ss_mul = 2 * sh_audio->wf->nChannels; // 1 byte*ch/packet

audio/filter/af_export.c
@@ -103,9 +103,11 @@ static int control(struct af_instance* af, int cmd, void* arg)
     // Init memory mapping
     s->fd = open(s->filename, O_RDWR | O_CREAT | O_TRUNC, 0640);
     mp_msg(MSGT_AFILTER, MSGL_INFO, "[export] Exporting to file: %s\n", s->filename);
-    if(s->fd < 0)
+    if(s->fd < 0) {
       mp_msg(MSGT_AFILTER, MSGL_FATAL, "[export] Could not open/create file: %s\n",
 	     s->filename);
+      return AF_ERROR;
+    }
 
     // header + buffer
     mapsize = (SIZE_HEADER + (af->data->bps * s->sz * af->data->nch));

audio/out/ao_alsa.c
@@ -818,13 +818,13 @@ static int play(void* data, int len, int flags)
 	mp_tmsg(MSGT_AO,MSGL_INFO,"[AO_ALSA] Trying to reset soundcard.\n");
 	if ((res = snd_pcm_prepare(alsa_handler)) < 0) {
 	  mp_tmsg(MSGT_AO,MSGL_ERR,"[AO_ALSA] pcm prepare error: %s\n", snd_strerror(res));
-	  return 0;
 	  break;
 	}
+	res = 0;
       }
   } while (res == 0);
 
-  return res < 0 ? res : res * bytes_per_sample;
+  return res < 0 ? 0 : res * bytes_per_sample;
 }
 
 /* how many byes are free in the buffer */

core/parser-cfg.c
@@ -49,8 +49,8 @@ int m_config_parse_config_file(m_config_t *config, const char *conffile)
 #define MAX_LINE_LEN    10000
 #define MAX_OPT_LEN     1000
 #define MAX_PARAM_LEN   1500
-    FILE *fp;
-    char *line;
+    FILE *fp = NULL;
+    char *line = NULL;
     char opt[MAX_OPT_LEN + 1];
     char param[MAX_PARAM_LEN + 1];
     char c;             /* for the "" and '' check */
@@ -86,7 +86,6 @@ int m_config_parse_config_file(m_config_t *config, const char *conffile)
 
     if ((fp = fopen(conffile, "r")) == NULL) {
         mp_msg(MSGT_CFGPARSER, MSGL_V, ": %s\n", strerror(errno));
-        free(line);
         ret = 0;
         goto out;
     }
@@ -238,9 +237,10 @@ nextline:
         ;
     }
 
-    free(line);
-    fclose(fp);
 out:
+    free(line);
+    if (fp)
+        fclose(fp);
     config->mode = prev_mode;
     --recursion_depth;
     if (ret < 0) {

demux/demux_ts.c
@@ -362,13 +362,13 @@ static void ts_add_stream(demuxer_t * demuxer, ES_stream_t *es)
 			if (lang && lang[0])
 				mp_msg(MSGT_IDENTIFY, MSGL_V, "ID_AID_%d_LANG=%s\n", es->pid, lang);
 			priv->last_aid++;
-		}
 
-		if(es->extradata && es->extradata_len)
-		{
-			sh->wf = malloc(sizeof(*sh->wf) + es->extradata_len);
-			sh->wf->cbSize = es->extradata_len;
-			memcpy(sh->wf + 1, es->extradata, es->extradata_len);
+			if(es->extradata && es->extradata_len)
+			{
+				sh->wf = malloc(sizeof(*sh->wf) + es->extradata_len);
+				sh->wf->cbSize = es->extradata_len;
+				memcpy(sh->wf + 1, es->extradata, es->extradata_len);
+			}
 		}
 	}
 

stream/asf_streaming.c
@@ -825,8 +825,6 @@ err_out:
 }
 
 static int open_s(stream_t *stream,int mode, void* opts, int* file_format) {
-	URL_t *url;
-
 	stream->streaming_ctrl = streaming_ctrl_new();
 	if( stream->streaming_ctrl==NULL ) {
 		return STREAM_ERROR;
@@ -834,9 +832,7 @@ static int open_s(stream_t *stream,int mode, void* opts, int* file_format) {
         stream->streaming_ctrl->audio_id_ptr = &stream->opts->audio_id;
         stream->streaming_ctrl->video_id_ptr = &stream->opts->video_id;
 	stream->streaming_ctrl->bandwidth = network_bandwidth;
-	url = url_new(stream->url);
-	stream->streaming_ctrl->url = check4proxies(url);
-	url_free(url);
+	stream->streaming_ctrl->url = url_new_with_proxy(stream->url);
 
 	mp_tmsg(MSGT_OPEN, MSGL_INFO, "STREAM_ASF, URL: %s\n", stream->url);
 	if((!strncmp(stream->url, "http", 4)) && (*file_format!=DEMUXER_TYPE_ASF && *file_format!=DEMUXER_TYPE_UNKNOWN)) {

stream/http.c
@@ -223,7 +223,6 @@ static int scast_streaming_start(stream_t *stream) {
 static int nop_streaming_start( stream_t *stream ) {
 	HTTP_header_t *http_hdr = NULL;
 	char *next_url=NULL;
-	URL_t *rd_url=NULL;
 	int fd,ret;
 	if( stream==NULL ) return -1;
 
@@ -253,12 +252,9 @@ static int nop_streaming_start( stream_t *stream ) {
 				ret=-1;
 				next_url = http_get_field( http_hdr, "Location" );
 
-				if (next_url != NULL)
-					rd_url=url_new(next_url);
-
-				if (next_url != NULL && rd_url != NULL) {
+				if (next_url != NULL) {
 					mp_msg(MSGT_NETWORK,MSGL_STATUS,"Redirected: Using this url instead %s\n",next_url);
-							stream->streaming_ctrl->url=check4proxies(rd_url);
+							stream->streaming_ctrl->url=url_new_with_proxy(next_url);
 					ret=nop_streaming_start(stream); //recursively get streaming started
 				} else {
 					mp_msg(MSGT_NETWORK,MSGL_ERR,"Redirection failed\n");
@@ -402,7 +398,7 @@ http_response_parse( HTTP_header_t *http_hdr ) {
 
 	// Get the reason phrase
 	ptr = strstr( hdr_ptr, "\n" );
-	if( hdr_ptr==NULL ) {
+	if( ptr==NULL ) {
 		mp_msg(MSGT_NETWORK,MSGL_ERR,"Malformed answer. Unable to get the reason phrase.\n");
 		return -1;
 	}
@@ -463,24 +459,20 @@ http_response_parse( HTTP_header_t *http_hdr ) {
 
 char *
 http_build_request( HTTP_header_t *http_hdr ) {
-	char *ptr, *uri;
+	char *ptr;
 	int len;
 	HTTP_field_t *field;
 	if( http_hdr==NULL ) return NULL;
         if( http_hdr->uri==NULL ) return NULL;
 
 	if( http_hdr->method==NULL ) http_set_method( http_hdr, "GET");
-
-        uri = malloc(strlen(http_hdr->uri) + 1);
-        if( uri==NULL ) {
-                mp_msg(MSGT_NETWORK,MSGL_ERR,"Memory allocation failed\n");
-                return NULL;
-        }
-        strcpy(uri,http_hdr->uri);
+	if( http_hdr->uri==NULL ) http_set_uri( http_hdr, "/");
+	if( !http_hdr->uri || !http_hdr->method)
+		return NULL;
 
 	//**** Compute the request length
 	// Add the Method line
-	len = strlen(http_hdr->method)+strlen(uri)+12;
+	len = strlen(http_hdr->method)+strlen(http_hdr->uri)+12;
 	// Add the fields
 	field = http_hdr->first_field;
 	while( field!=NULL ) {
@@ -508,7 +500,7 @@ http_build_request( HTTP_header_t *http_hdr ) {
 	//*** Building the request
 	ptr = http_hdr->buffer;
 	// Add the method line
-	ptr += sprintf( ptr, "%s %s HTTP/1.%d\r\n", http_hdr->method, uri, http_hdr->http_minor_version );
+	ptr += sprintf( ptr, "%s %s HTTP/1.%d\r\n", http_hdr->method, http_hdr->uri, http_hdr->http_minor_version );
 	field = http_hdr->first_field;
 	// Add the field
 	while( field!=NULL ) {
@@ -521,7 +513,6 @@ http_build_request( HTTP_header_t *http_hdr ) {
 		memcpy( ptr, http_hdr->body, http_hdr->body_size );
 	}
 
-	free(uri);
 	return http_hdr->buffer;
 }
 
@@ -726,7 +717,7 @@ static int http_streaming_start(stream_t *stream, int* file_format) {
 	do
 	{
 		redirect = 0;
-		if (fd > 0) closesocket(fd);
+		if (fd >= 0) closesocket(fd);
 		fd = http_send_request( url, 0 );
 		if( fd<0 ) {
 			goto err_out;
@@ -856,12 +847,12 @@ static int http_streaming_start(stream_t *stream, int* file_format) {
 	} while( redirect );
 
 err_out:
-	if (fd > 0) closesocket( fd );
+	if (fd >= 0) closesocket( fd );
 	fd = -1;
 	http_free( http_hdr );
 	http_hdr = NULL;
 out:
-	stream->streaming_ctrl->data = (void*)http_hdr;
+	stream->streaming_ctrl->data = http_hdr;
 	stream->fd = fd;
 	return res;
 }
@@ -895,16 +886,13 @@ static int fixup_open(stream_t *stream,int seekable) {
 
 static int open_s1(stream_t *stream,int mode, void* opts, int* file_format) {
 	int seekable=0;
-	URL_t *url;
 
 	stream->streaming_ctrl = streaming_ctrl_new();
 	if( stream->streaming_ctrl==NULL ) {
 		return STREAM_ERROR;
 	}
 	stream->streaming_ctrl->bandwidth = network_bandwidth;
-	url = url_new(stream->url);
-	stream->streaming_ctrl->url = check4proxies(url);
-	url_free(url);
+	stream->streaming_ctrl->url = url_new_with_proxy(stream->url);
 
 	mp_msg(MSGT_OPEN, MSGL_V, "STREAM_HTTP(1), URL: %s\n", stream->url);
 	seekable = http_streaming_start(stream, file_format);
@@ -924,16 +912,13 @@ static int open_s1(stream_t *stream,int mode, void* opts, int* file_format) {
 
 static int open_s2(stream_t *stream,int mode, void* opts, int* file_format) {
 	int seekable=0;
-	URL_t *url;
 
 	stream->streaming_ctrl = streaming_ctrl_new();
 	if( stream->streaming_ctrl==NULL ) {
 		return STREAM_ERROR;
 	}
 	stream->streaming_ctrl->bandwidth = network_bandwidth;
-	url = url_new(stream->url);
-	stream->streaming_ctrl->url = check4proxies(url);
-	url_free(url);
+	stream->streaming_ctrl->url = url_new_with_proxy(stream->url);
 
 	mp_msg(MSGT_OPEN, MSGL_V, "STREAM_HTTP(2), URL: %s\n", stream->url);
 	seekable = http_streaming_start(stream, file_format);

commit 570271c7767e924be3d9d347b04508579dfa4d80

cookies: fix crash
This was broken in 3f85094 (probably merge mistake). I guess nobody ever
uses this feature.

stream/cookies.c
@@ -162,10 +162,10 @@ static struct cookie_list_type *load_cookies_from(const char *filename,
 						  struct cookie_list_type
 						  *list)
 {
-    char *ptr;
+    char *ptr, *file;
     int64_t length;
 
-    ptr = load_file(filename, &length);
+    ptr = file = load_file(filename, &length);
     if (!ptr)
 	return list;
 
@@ -183,7 +183,7 @@ static struct cookie_list_type *load_cookies_from(const char *filename,
 	    list = new;
 	}
     }
-    free(ptr);
+    free(file);
     return list;
 }
 