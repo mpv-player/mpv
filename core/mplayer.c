mpv-player/mpv

commit 630a2b195acb3768b67ad81539fc0117a12c1293

mplayer: don't show bogus audio PTS during hr-seek
This fixes a problem that happened with syncplay.pl [1] when ad_mpg123
was in use, and get_current_time() returning a bogus time position.
This only happens during seeking; the reported time is correct after the
seek is done.

The audio PTS as returned by playing_audio_pts() is simply bogus during
hr-seek. With ad_ffmpeg, it was actually set to MP_NOPTS_VALUE during
seeking, so get_current_time() did a fallback to the video PTS. However,
ad_mpg123 is different and explicitly decodes some audio when resetting
on seek (reasons why it does this unknown and uninvestigated; apparently
it's to reinit libmpg123). As a result, the audio PTS was set to the
start position of the seek (or something similar), which could be very
different from the seek target time.

This confused syncplay. It got the bogus time because it spams the
player with read commands to the "time-pos" property, so this corner
case was hit.

Fix this by making get_current_time() return the seek target time if
hr-seek is active. This should make behavior the same as before commit
3f949cf "mplayer: prefer audio PTS over video PTS for status line".

[1] http://syncplay.pl

core/mplayer.c
@@ -3000,6 +3000,8 @@ double get_current_time(struct MPContext *mpctx)
         return 0;
     if (demuxer->stream_pts != MP_NOPTS_VALUE)
         return demuxer->stream_pts;
+    if (mpctx->hrseek_active)
+        return mpctx->hrseek_pts;
     double apts = playing_audio_pts(mpctx);
     if (apts != MP_NOPTS_VALUE)
         return apts;

commit f9a259e5d5a3aa7eb34a7278e549636b5138655d

mplayer: fix seek display during seeking when playing ordered chapters
The seek bar appeared to be "stuck" to the start of the current chapter.
This is a regression from 630a2b1. This commit assumed that hrseek_pts
would always contain the hrseek target time (when hrseek_active==true).
But this is not always the case: when playing timeline stuff (e.g.
ordered chapters), hrseek framedropping is abused to handle an obscure
corner case, and then hrseek_pts contains something completely unrelated
to the current playback time. See the added comment in mplayer.c and
commit c1232c9.

Fix this by trying something else to get a correct time "during"
hr-seeks. mpctx->restart_playback looks ideal, because it's set while
audio is being synced / audio buffers being filled, so we know that the
audio time is probably bogus while it is set. Let's hope this is
correct.

core/mplayer.c
@@ -2872,6 +2872,12 @@ static int seek(MPContext *mpctx, struct seek_params seek,
     } else
         mpctx->last_seek_pts = MP_NOPTS_VALUE;
 
+    // The hr_seek==false case is for skipping frames with PTS before the
+    // current timeline chapter start. It's not really known where the demuxer
+    // level seek will end up, so the hrseek mechanism is abused to skip all
+    // frames before chapter start by setting hrseek_pts to the chapter start.
+    // It does nothing when the seek is inside of the current chapter, and
+    // seeking past the chapter is handled elsewhere.
     if (hr_seek || mpctx->timeline) {
         mpctx->hrseek_active = true;
         mpctx->hrseek_framedrop = true;
@@ -2958,15 +2964,15 @@ double get_current_time(struct MPContext *mpctx)
         return 0;
     if (demuxer->stream_pts != MP_NOPTS_VALUE)
         return demuxer->stream_pts;
-    if (mpctx->hrseek_active)
-        return mpctx->hrseek_pts;
-    double apts = playing_audio_pts(mpctx);
-    if (apts != MP_NOPTS_VALUE)
-        return apts;
-    if (mpctx->sh_video) {
-        double pts = mpctx->video_pts;
-        if (pts != MP_NOPTS_VALUE)
-            return pts;
+    if (!mpctx->restart_playback) {
+        double apts = playing_audio_pts(mpctx);
+        if (apts != MP_NOPTS_VALUE)
+            return apts;
+        if (mpctx->sh_video) {
+            double pts = mpctx->video_pts;
+            if (pts != MP_NOPTS_VALUE)
+                return pts;
+        }
     }
     if (mpctx->last_seek_pts != MP_NOPTS_VALUE)
         return mpctx->last_seek_pts;