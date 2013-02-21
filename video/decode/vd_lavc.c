mpv-player/mpv

commit 4d016a92c876e98797c362d05468bf27d5a85414

core: redo how codecs are mapped, remove codecs.conf
Use codec names instead of FourCCs to identify codecs. Rewrite how
codecs are selected and initialized. Now each decoder exports a list
of decoders (and the codec it supports) via add_decoders(). The order
matters, and the first decoder for a given decoder is preferred over
the other decoders. E.g. all ad_mpg123 decoders are preferred over
ad_lavc, because it comes first in the mpcodecs_ad_drivers array.
Likewise, decoders within ad_lavc that are enumerated first by
libavcodec (using av_codec_next()) are preferred. (This is actually
critical to select h264 software decoding by default instead of vdpau.
libavcodec and ffmpeg/avconv use the same method to select decoders by
default, so we hope this is sane.)

The codec names follow libavcodec's codec names as defined by
AVCodecDescriptor.name (see libavcodec/codec_desc.c). Some decoders
have names different from the canonical codec name. The AVCodecDescriptor
API is relatively new, so we need a compatibility layer for older
libavcodec versions for codec names that are referenced internally,
and which are different from the decoder name. (Add a configure check
for that, because checking versions is getting way too messy.)

demux/codec_tags.c is generated from the former codecs.conf (minus
"special" decoders like vdpau, and excluding the mappings that are the
same as the mappings libavformat's exported RIFF tables). It contains
all the mappings from FourCCs to codec name. This is needed for
demux_mkv, demux_mpg, demux_avi and demux_asf. demux_lavf will set the
codec as determined by libavformat, while the other demuxers have to do
this on their own, using the mp_set_audio/video_codec_from_tag()
functions. Note that the sh_audio/video->format members don't uniquely
identify the codec anymore, and sh->codec takes over this role.

Replace the --ac/--vc/--afm/--vfm with new --vd/--ad options, which
provide cover the functionality of the removed switched.

video/decode/vd_lavc.c
@@ -35,6 +35,8 @@
 #include "core/mp_msg.h"
 #include "core/options.h"
 #include "core/av_opts.h"
+#include "core/av_common.h"
+#include "core/codecs.h"
 
 #include "compat/mpbswap.h"
 #include "video/fmt-conversion.h"
@@ -45,18 +47,8 @@
 #include "video/filter/vf.h"
 #include "demux/stheader.h"
 #include "demux/demux_packet.h"
-#include "core/codec-cfg.h"
 #include "osdep/numcores.h"
 #include "video/csputils.h"
-
-static const vd_info_t info = {
-    "libavcodec video codecs",
-    "ffmpeg",
-    "",
-    "",
-    "native codecs",
-    .print_name = "libavcodec",
-};
 
 #include "libavcodec/avcodec.h"
 #include "lavc.h"
@@ -67,7 +59,7 @@ static const vd_info_t info = {
 
 #include "core/m_option.h"
 
-static int init_avctx(sh_video_t *sh, AVCodec *lavc_codec, struct hwdec *hwdec);
+static int init_avctx(sh_video_t *sh, const char *decoder, struct hwdec *hwdec);
 static void uninit_avctx(sh_video_t *sh);
 static int get_buffer_hwdec(AVCodecContext *avctx, AVFrame *pic);
 static void release_buffer_hwdec(AVCodecContext *avctx, AVFrame *pic);
@@ -155,70 +147,53 @@ static enum AVDiscard str2AVDiscard(char *str)
     return AVDISCARD_DEFAULT;
 }
 
-static int init(sh_video_t *sh)
+static int init(sh_video_t *sh, const char *decoder)
 {
     vd_ffmpeg_ctx *ctx;
-    AVCodec *lavc_codec = NULL;
-
     ctx = sh->context = talloc_zero(NULL, vd_ffmpeg_ctx);
     ctx->non_dr1_pool = talloc_steal(ctx, mp_image_pool_new(16));
 
-    if (sh->codec->dll) {
-        lavc_codec = avcodec_find_decoder_by_name(sh->codec->dll);
-        if (!lavc_codec) {
-            mp_tmsg(MSGT_DECVIDEO, MSGL_ERR,
-                    "Cannot find codec '%s' in libavcodec...\n",
-                    sh->codec->dll);
-            uninit(sh);
-            return 0;
-        }
-    } else if (sh->libav_codec_id) {
-        lavc_codec = avcodec_find_decoder(sh->libav_codec_id);
-        if (!lavc_codec) {
-            mp_tmsg(MSGT_DECVIDEO, MSGL_INFO, "Libavcodec has no decoder "
-                   "for this codec\n");
-            uninit(sh);
-            return 0;
-        }
-    }
-    if (!lavc_codec) {
-        uninit(sh);
-        return 0;
-    }
-
-    struct hwdec *hwdec = find_hwcodec(sh->opts->hwdec_api, lavc_codec->name);
+    struct hwdec *hwdec = find_hwcodec(sh->opts->hwdec_api, decoder);
     if (hwdec) {
         AVCodec *lavc_hwcodec = avcodec_find_decoder_by_name(hwdec->hw_codec);
         if (lavc_hwcodec) {
-            ctx->software_fallback = lavc_codec;
-            lavc_codec = lavc_hwcodec;
+            ctx->software_fallback_decoder = decoder;
+            decoder = lavc_hwcodec->name;
         } else {
             hwdec = NULL;
             mp_tmsg(MSGT_DECVIDEO, MSGL_WARN, "Using software decoding.\n");
         }
     }
 
-    if (!init_avctx(sh, lavc_codec, hwdec)) {
-        mp_tmsg(MSGT_DECVIDEO, MSGL_ERR, "Error initializing hardware "
-                "decoding, falling back to software decoding.\n");
-        lavc_codec = ctx->software_fallback;
-        ctx->software_fallback = NULL;
-        if (!init_avctx(sh, lavc_codec, NULL)) {
-            uninit(sh);
-            return 0;
+    if (!init_avctx(sh, decoder, hwdec)) {
+        if (ctx->software_fallback_decoder) {
+            mp_tmsg(MSGT_DECVIDEO, MSGL_ERR, "Error initializing hardware "
+                    "decoding, falling back to software decoding.\n");
+            decoder = ctx->software_fallback_decoder;
+            ctx->software_fallback_decoder = NULL;
+            if (!init_avctx(sh, decoder, NULL)) {
+                uninit(sh);
+                return 0;
+            }
         }
     }
     return 1;
 }
 
-static int init_avctx(sh_video_t *sh, AVCodec *lavc_codec, struct hwdec *hwdec)
+static int init_avctx(sh_video_t *sh, const char *decoder, struct hwdec *hwdec)
 {
     vd_ffmpeg_ctx *ctx = sh->context;
     struct lavc_param *lavc_param = &sh->opts->lavc_param;
+    bool mp_rawvideo = false;
 
-    sh->codecname = lavc_codec->long_name;
-    if (!sh->codecname)
-        sh->codecname = lavc_codec->name;
+    if (strcmp(decoder, "mp-rawvideo") == 0) {
+        mp_rawvideo = true;
+        decoder = "rawvideo";
+    }
+
+    AVCodec *lavc_codec = avcodec_find_decoder_by_name(decoder);
+    if (!lavc_codec)
+        return 0;
 
     ctx->do_dr1 = ctx->do_hw_dr1 = 0;
     ctx->pix_fmt = PIX_FMT_NONE;
@@ -264,8 +239,8 @@ static int init_avctx(sh_video_t *sh, AVCodec *lavc_codec, struct hwdec *hwdec)
 
     avctx->flags |= lavc_param->bitexact;
 
-    avctx->coded_width = sh->disp_w;
-    avctx->coded_height = sh->disp_h;
+    avctx->coded_width  = sh->bih ? sh->bih->biWidth  : sh->disp_w;
+    avctx->coded_height = sh->bih ? sh->bih->biHeight : sh->disp_h;
     avctx->workaround_bugs = lavc_param->workaround_bugs;
     if (lavc_param->gray)
         avctx->flags |= CODEC_FLAG_GRAY;
@@ -340,14 +315,6 @@ static int init_avctx(sh_video_t *sh, AVCodec *lavc_codec, struct hwdec *hwdec)
         }
         break;
 
-    case MKTAG('M', 'P', 'v', 'f'):
-        avctx->codec_tag = 0;
-        avctx->pix_fmt = imgfmt2pixfmt(sh->imgfmt);
-        break;
-    case MKTAG('M', 'P', 'r', 'v'):
-        avctx->codec_tag = sh->imgfmt;
-        break;
-
     default:
         if (!sh->bih || sh->bih->biSize <= sizeof(*sh->bih))
             break;
@@ -360,6 +327,11 @@ static int init_avctx(sh_video_t *sh, AVCodec *lavc_codec, struct hwdec *hwdec)
 
     if (sh->bih)
         avctx->bits_per_coded_sample = sh->bih->biBitCount;
+
+    if (mp_rawvideo && sh->format >= IMGFMT_START && sh->format < IMGFMT_END) {
+        avctx->pix_fmt = imgfmt2pixfmt(sh->format);
+        avctx->codec_tag = 0;
+    }
 
     /* open it */
     if (avcodec_open2(avctx, lavc_codec, NULL) < 0) {
@@ -374,8 +346,6 @@ static void uninit_avctx(sh_video_t *sh)
 {
     vd_ffmpeg_ctx *ctx = sh->context;
     AVCodecContext *avctx = ctx->avctx;
-
-    sh->codecname = NULL;
 
     if (avctx) {
         if (avctx->codec && avcodec_close(avctx) < 0)
@@ -661,14 +631,14 @@ static struct mp_image *decode_with_fallback(struct sh_video *sh,
         return mpi;
 
     // Failed hardware decoding? Try again in software.
-    if (ctx->software_fallback) {
+    if (ctx->software_fallback_decoder) {
         uninit_avctx(sh);
         sh->vf_initialized = 0;
         mp_tmsg(MSGT_DECVIDEO, MSGL_ERR, "Error using hardware "
                 "decoding, falling back to software decoding.\n");
-        AVCodec *codec = ctx->software_fallback;
-        ctx->software_fallback = NULL;
-        if (init_avctx(sh, codec, NULL)) {
+        const char *decoder = ctx->software_fallback_decoder;
+        ctx->software_fallback_decoder = NULL;
+        if (init_avctx(sh, decoder, NULL)) {
             mpi = NULL;
             decode(sh, packet, data, len, flags, reordered_pts, &mpi);
             return mpi;
@@ -700,8 +670,16 @@ static int control(sh_video_t *sh, int cmd, void *arg)
     return CONTROL_UNKNOWN;
 }
 
+static void add_decoders(struct mp_decoder_list *list)
+{
+    mp_add_lavc_decoders(list, AVMEDIA_TYPE_VIDEO);
+    mp_add_decoder(list, "lavc", "mp-rawvideo", "mp-rawvideo",
+                   "raw video");
+}
+
 const struct vd_functions mpcodecs_vd_ffmpeg = {
-    .info = &info,
+    .name = "lavc",
+    .add_decoders = add_decoders,
     .init = init,
     .uninit = uninit,
     .control = control,

.gitignore
@@ -10,7 +10,6 @@
 /mpv
 /mpv.app
 /version.h
-/core/codecs.conf.h
 /core/input/input_conf.h
 /tags
 /TAGS

Makefile
@@ -151,10 +151,11 @@ SOURCES = talloc.c \
           audio/out/ao_null.c \
           audio/out/ao_pcm.c \
           core/asxparser.c \
+          core/av_common.c \
           core/av_log.c \
           core/av_opts.c \
           core/bstr.c \
-          core/codec-cfg.c \
+          core/codecs.c \
           core/command.c \
           core/cpudetect.c \
           core/defaultopts.c \
@@ -181,6 +182,7 @@ SOURCES = talloc.c \
           demux/asfheader.c \
           demux/aviheader.c \
           demux/aviprint.c \
+          demux/codec_tags.c \
           demux/demux.c \
           demux/demux_asf.c \
           demux/demux_avi.c \
@@ -343,10 +345,6 @@ mpv$(EXESUF): $(OBJECTS)
 mpv$(EXESUF):
 	$(CC) -o $@ $^ $(EXTRALIBS)
 
-core/codec-cfg.c: core/codecs.conf.h
-core/codecs.conf.h: TOOLS/file2string.pl etc/codecs.conf
-	./$^ >$@
-
 core/input/input.c: core/input/input_conf.h
 core/input/input_conf.h: TOOLS/file2string.pl etc/input.conf
 	./$^ >$@
@@ -442,7 +440,6 @@ clean:
 	-$(RM) $(call ADD_ALL_EXESUFS,mpv)
 	-$(RM) DOCS/man/en/mpv.1
 	-$(RM) version.h
-	-$(RM) core/codecs.conf.h
 	-$(RM) core/input/input_conf.h
 	-$(RM) video/out/vdpau_template.c
 	-$(RM) demux/ebml_types.h demux/ebml_defs.c

audio/decode/ad.c
@@ -32,7 +32,7 @@
 /* Missed vorbis, mad, dshow */
 
 extern const ad_functions_t mpcodecs_ad_mpg123;
-extern const ad_functions_t mpcodecs_ad_ffmpeg;
+extern const ad_functions_t mpcodecs_ad_lavc;
 extern const ad_functions_t mpcodecs_ad_spdif;
 
 const ad_functions_t * const mpcodecs_ad_drivers[] =
@@ -40,7 +40,7 @@ const ad_functions_t * const mpcodecs_ad_drivers[] =
 #ifdef CONFIG_MPG123
   &mpcodecs_ad_mpg123,
 #endif
-  &mpcodecs_ad_ffmpeg,
+  &mpcodecs_ad_lavc,
   &mpcodecs_ad_spdif,
   NULL
 };

audio/decode/ad.h
@@ -19,17 +19,20 @@
 #ifndef MPLAYER_AD_H
 #define MPLAYER_AD_H
 
-#include "core/mpc_info.h"
+#include "core/codecs.h"
 #include "demux/stheader.h"
 
 typedef struct mp_codec_info ad_info_t;
 
+struct mp_decoder_list;
+
 /* interface of video decoder drivers */
 typedef struct ad_functions
 {
-    const ad_info_t *info;
+    const char *name;
+    void (*add_decoders)(struct mp_decoder_list *list);
     int (*preinit)(sh_audio_t *sh);
-    int (*init)(sh_audio_t *sh);
+    int (*init)(sh_audio_t *sh, const char *decoder);
     void (*uninit)(sh_audio_t *sh);
     int (*control)(sh_audio_t *sh,int cmd,void* arg, ...);
     int (*decode_audio)(sh_audio_t *sh, unsigned char *buffer, int minlen,

audio/decode/ad_internal.h
@@ -19,7 +19,7 @@
 #ifndef MPLAYER_AD_INTERNAL_H
 #define MPLAYER_AD_INTERNAL_H
 
-#include "core/codec-cfg.h"
+#include "core/codecs.h"
 #include "audio/format.h"
 
 #include "stream/stream.h"
@@ -28,14 +28,16 @@
 
 #include "ad.h"
 
-static int init(sh_audio_t *sh);
+static void add_decoders(struct mp_decoder_list *list);
+static int init(sh_audio_t *sh, const char *decoder);
 static int preinit(sh_audio_t *sh);
 static void uninit(sh_audio_t *sh);
 static int control(sh_audio_t *sh,int cmd,void* arg, ...);
 static int decode_audio(sh_audio_t *sh,unsigned char *buffer,int minlen,int maxlen);
 
 #define LIBAD_EXTERN(x) const ad_functions_t mpcodecs_ad_##x = {\
-	&info,\
+        #x, \
+	add_decoders, \
 	preinit,\
 	init,\
         uninit,\

audio/decode/ad_lavc.c
@@ -28,6 +28,8 @@
 #include "talloc.h"
 
 #include "config.h"
+#include "core/av_common.h"
+#include "core/codecs.h"
 #include "core/mp_msg.h"
 #include "core/options.h"
 
@@ -37,17 +39,7 @@
 #include "compat/mpbswap.h"
 #include "compat/libav.h"
 
-static const ad_info_t info =
-{
-    "libavcodec audio decoders",
-    "ffmpeg",
-    "",
-    "",
-    "",
-    .print_name = "libavcodec",
-};
-
-LIBAD_EXTERN(ffmpeg)
+LIBAD_EXTERN(lavc)
 
 struct priv {
     AVCodecContext *avctx;
@@ -189,47 +181,25 @@ static int setup_format(sh_audio_t *sh_audio,
     return 0;
 }
 
-static int init(sh_audio_t *sh_audio)
+static int init(sh_audio_t *sh_audio, const char *decoder)
 {
     struct MPOpts *opts = sh_audio->opts;
     AVCodecContext *lavc_context;
     AVCodec *lavc_codec;
 
-    const char *dll = sh_audio->codec->dll;
-
-    if (sh_audio->wf && dll && strcmp(dll, "pcm") == 0) {
-        if (sh_audio->format == MKTAG('M', 'P', 'a', 'f')) {
-            // demuxer_rawaudio convenience (abuses wFormatTag)
-            dll = find_pcm_decoder(af_map, sh_audio->wf->wFormatTag, 0);
-        } else {
-            dll = find_pcm_decoder(tag_map, sh_audio->format,
+    if (sh_audio->wf && strcmp(decoder, "pcm") == 0) {
+        decoder = find_pcm_decoder(tag_map, sh_audio->format,
                                    sh_audio->wf->wBitsPerSample);
-        }
+    } else if (sh_audio->wf && strcmp(decoder, "mp-pcm") == 0) {
+        decoder = find_pcm_decoder(af_map, sh_audio->format, 0);
     }
 
-    if (dll) {
-        lavc_codec = avcodec_find_decoder_by_name(dll);
-        if (!lavc_codec) {
-            mp_tmsg(MSGT_DECAUDIO, MSGL_ERR,
-                    "Cannot find codec '%s' in libavcodec...\n", dll);
-            return 0;
-        }
-    } else if (!sh_audio->libav_codec_id) {
-        mp_tmsg(MSGT_DECAUDIO, MSGL_INFO, "No Libav codec ID known. "
-                "Generic lavc decoder is not applicable.\n");
+    lavc_codec = avcodec_find_decoder_by_name(decoder);
+    if (!lavc_codec) {
+        mp_tmsg(MSGT_DECAUDIO, MSGL_ERR,
+                "Cannot find codec '%s' in libavcodec...\n", decoder);
         return 0;
-    } else {
-        lavc_codec = avcodec_find_decoder(sh_audio->libav_codec_id);
-        if (!lavc_codec) {
-            mp_tmsg(MSGT_DECAUDIO, MSGL_INFO, "Libavcodec has no decoder "
-                   "for this codec\n");
-            return 0;
-        }
     }
-
-    sh_audio->codecname = lavc_codec->long_name;
-    if (!sh_audio->codecname)
-        sh_audio->codecname = lavc_codec->name;
 
     struct priv *ctx = talloc_zero(NULL, struct priv);
     sh_audio->context = ctx;
@@ -303,7 +273,7 @@ static int init(sh_audio_t *sh_audio)
         }
         if (++tries >= 5) {
             mp_msg(MSGT_DECAUDIO, MSGL_ERR,
-                   "ad_ffmpeg: initial decode failed\n");
+                   "ad_lavc: initial decode failed\n");
             uninit(sh_audio);
             return 0;
         }
@@ -328,7 +298,6 @@ static int init(sh_audio_t *sh_audio)
 
 static void uninit(sh_audio_t *sh)
 {
-    sh->codecname = NULL;
     struct priv *ctx = sh->context;
     if (!ctx)
         return;
@@ -493,4 +462,11 @@ static int decode_audio(sh_audio_t *sh_audio, unsigned char *buf, int minlen,
         sh_audio->pts_bytes += size;
     }
     return len;
+}
+
+static void add_decoders(struct mp_decoder_list *list)
+{
+    mp_add_lavc_decoders(list, AVMEDIA_TYPE_AUDIO);
+    mp_add_decoder(list, "lavc", "pcm", "pcm", "Raw PCM");
+    mp_add_decoder(list, "lavc", "mp-pcm", "mp-pcm", "Raw PCM");
 }

audio/decode/ad_mpg123.c
@@ -26,14 +26,6 @@
 
 #include "ad_internal.h"
 
-static const ad_info_t info = {
-    "MPEG 1.0/2.0/2.5 layers I, II, III",
-    "mpg123",
-    "Thomas Orgis",
-    "mpg123.org",
-    "High-performance decoder using libmpg123."
-};
-
 LIBAD_EXTERN(mpg123)
 
 /* Reducing the ifdeffery to two main variants:
@@ -327,7 +319,7 @@ static int reopen_stream(sh_audio_t *sh)
  * Paranoia note: The mpg123_close() on errors is not really necessary,
  * But it ensures that we don't accidentally continue decoding with a
  * bad state (possibly interpreting the format badly or whatnot). */
-static int init(sh_audio_t *sh)
+static int init(sh_audio_t *sh, const char *decoder)
 {
     long rate    = 0;
     int channels = 0;
@@ -486,4 +478,10 @@ static int control(sh_audio_t *sh, int cmd, void *arg, ...)
         break;
     }
     return CONTROL_UNKNOWN;
+}
+
+static void add_decoders(struct mp_decoder_list *list)
+{
+    mp_add_decoder(list, "mpg123", "mp3", "mp3",
+                   "High-performance decoder using libmpg123");
 }

commit 2f6257e2f40a0f559b20cc2989778c6805e892fa

vd_lavc: fix software decoding fallback
The string is deallocated by the callee after initialization, so
fallback at runtime passes a deallocated string to libavcodec, which
results in random crashes. Regression introduced by commit 4d016a9.

video/decode/vd_lavc.c
@@ -157,7 +157,7 @@ static int init(sh_video_t *sh, const char *decoder)
     if (hwdec) {
         AVCodec *lavc_hwcodec = avcodec_find_decoder_by_name(hwdec->hw_codec);
         if (lavc_hwcodec) {
-            ctx->software_fallback_decoder = decoder;
+            ctx->software_fallback_decoder = talloc_strdup(ctx, decoder);
             decoder = lavc_hwcodec->name;
         } else {
             hwdec = NULL;