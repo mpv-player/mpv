39116ecf3135691a6202bda47d6f21b0633c5322
Alexander Preisinger <alexander.preisinger@gmail.com>
2013-04-02 20:50:54 +0200

    wayland: enable alpha support
    It is now possible to show images and videos with alpha information correctly.
    This was disalbed before, because there was a bug that made black parts of
    videos also transparent.

--- video/out/gl_wayland.c
+++ video/out/gl_wayland.c
@@ -101,7 +101,7 @@ static bool egl_create_context(struct vo_wayland_state *wl,
         EGL_RED_SIZE, 1,
         EGL_GREEN_SIZE, 1,
         EGL_BLUE_SIZE, 1,
-        EGL_ALPHA_SIZE, 0,
+        EGL_ALPHA_SIZE, 1,
         EGL_DEPTH_SIZE, 1,
         EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
         EGL_NONE
