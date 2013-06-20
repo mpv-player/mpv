6b808ef8da4299e6d744a3774b1f424b4e5de88f
Rudolf Polzer <divverent@xonotic.org>
2013-06-20 09:07:42 +0200

    ildetect.sh: env vars support

--- TOOLS/vf_dlopen/ildetect.sh
+++ TOOLS/vf_dlopen/ildetect.sh
@@ -9,30 +9,57 @@ case "$0" in
         ;;
 esac
 
-set -e
+: ${MPV:=mpv}
+: ${ILDETECT_MPV:=$MPV}
+: ${ILDETECT_MPV:=$MPV}
+: ${ILDETECT_MPVFLAGS:=-start 40 -end 60}
+: ${ILDETECT_DRY_RUN:=}
+: ${MAKE:=make}
 
-make -C "$MYDIR"
+# exit status:
+# 0 progressive
+# 1 telecine
+# 2 interlaced
+# 8 unknown
+# 15 compile fail
+# 16 detect fail
+# 17+ mpv's status | 16
+
+$MAKE -C "$MYDIR" ildetect.so || exit 15
 
 testfun()
 {
-    ${MPV:-mpv} "$@" \
+    $ILDETECT_MPV "$@" \
         -vf dlopen="$MYDIR/ildetect.so" \
         -o /dev/null -of rawvideo -ofopts-clr -ovc rawvideo -ovcopts-clr -no-audio \
+        $ILDETECT_MPVFLAGS \
         | tee /dev/stderr | grep "^ildetect:"
 }
 
 out=`testfun "$@"`
 case "$out" in
     *"probably: PROGRESSIVE"*)
-        ${MPV:-mpv} "$@"
-        ;;
-    *"probably: INTERLACED"*)
-        ${MPV:-mpv} "$@" -vf-pre yadif
+        [ -n "$ILDETECT_DRY_RUN" ] || $ILDETECT_MPV "$@"
+        r=$?
+        [ $r -eq 0 ] || exit $(($r | 16))
+        exit 0
         ;;
     *"probably: TELECINED"*)
-        ${MPV:-mpv} "$@" -vf-pre pullup
+        [ -n "$ILDETECT_DRY_RUN" ] || $ILDETECT_MPV "$@" -vf-pre pullup
+        r=$?
+        [ $r -eq 0 ] || exit $(($r | 16))
+        exit 1
+        ;;
+    *"probably: INTERLACED"*)
+        [ -n "$ILDETECT_DRY_RUN" ] || $ILDETECT_MPV "$@" -vf-pre yadif
+        r=$?
+        [ $r -eq 0 ] || exit $(($r | 16))
+        exit 2
+        ;;
+    *"probably: "*)
+        exit 8
         ;;
     *)
-        false
+        exit 16
         ;;
 esac
