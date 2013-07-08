mpv-player/mpv

commit 37c5c114af8b4747dfa0664bf967958baa9a1c91

Remove BSD legacy TV/radio support (BT848 stuff)
FreeBSD actually supports V4L2, and V4L2 supports this chip. Also,
this chip is from 1997. Farewell.

stream/stream_radio.c
@@ -35,22 +35,7 @@
 #include <errno.h>
 #include <unistd.h>
 
-#ifdef CONFIG_RADIO_BSDBT848
-#include <sys/param.h>
-#ifdef IOCTL_BT848_H_NAME
-#include IOCTL_BT848_H_NAME
-#endif
-
-#else /* CONFIG_RADIO_BSDBT848 */
-
 #include <linux/types.h>
-
-#ifdef CONFIG_RADIO_V4L2
-#include <linux/videodev2.h>
-#endif
-
-#endif // !IOCTL_BT848_H_NAME
-
 
 #include "stream.h"
 #include "demux/demux.h"
@@ -85,13 +70,7 @@ typedef struct radio_channels_s {
 
 /// default values for options
 radio_param_t stream_radio_defaults={
-#ifdef CONFIG_RADIO_BSDBT848
-    "/dev/tuner0", //device
-    87.50,         //freq_min
-    108.00,        //freq_max
-#else
     "/dev/radio0", //device;
-#endif
     "default",     //driver
     NULL,          //channels
     100,           //volume
@@ -404,118 +383,6 @@ static const radio_driver_t radio_driver_v4l2={
     get_frequency_v4l2
 };
 #endif /* CONFIG_RADIO_V4L2 */
-#ifdef CONFIG_RADIO_BSDBT848
-
-/*****************************************************************
- * \brief get fraction value for using in set_frequency and get_frequency
- * \return STREAM_OK if success, STREAM_ERROR otherwise
- *
- * For *BSD BT848 frac=100
- *
- * Here is a coment from FreeBSD 5.2-RELEASE source code:
- *
- * * Tuner Notes:
- * * Programming the tuner properly is quite complicated.
- * * Here are some notes, based on a FM1246 data sheet for a PAL-I tuner.
- * * The tuner (front end) covers 45.75 MHz - 855.25 MHz and an FM band of
- * * 87.5 MHz to 108.0 MHz.
- *
- * Thus, frequency range is limited to 87.5-108.0, but you can change
- * it, using freq_min and freq_max options
-*/
-static int init_frac_bsdbt848(radio_priv_t* priv){
-    priv->frac=100;
-    priv->rangelow=priv->radio_param->freq_min;
-    priv->rangehigh=priv->radio_param->freq_max;
-    return STREAM_OK;
-}
-
-/*****************************************************************
- * \brief tune card to given frequency
- * \param frequency frequency in MHz
- * \return STREAM_OK if success, STREAM_ERROR otherwise
- */
-static int set_frequency_bsdbt848(radio_priv_t* priv,float frequency){
-    unsigned int freq;
-    freq=frequency*priv->frac;
-    if(ioctl(priv->radio_fd,RADIO_SETFREQ,&freq)<0){
-        mp_tmsg(MSGT_RADIO,MSGL_ERR,"[radio] ioctl set frequency 0x%x (%.2f) failed: %s\n",freq, frequency, strerror(errno));
-        return  STREAM_ERROR;
-    }
-    return STREAM_OK;
-}
-
-/*****************************************************************
- * \brief get current tuned frequency from card
- * \param frequency where to store frequency in MHz
- * \return STREAM_OK if success, STREAM_ERROR otherwise
- */
-static int get_frequency_bsdbt848(radio_priv_t* priv,float* frequency){
-    unsigned int freq;
-    if (ioctl(priv->radio_fd, RADIO_GETFREQ, &freq) < 0) {
-        mp_tmsg(MSGT_RADIO,MSGL_ERR,"[radio] ioctl get frequency failed: %s\n",strerror(errno));
-        return  STREAM_ERROR;
-    }
-    *frequency=((float)freq)/priv->frac;
-    return STREAM_OK;
-}
-
-/*****************************************************************
- * \brief set volume on radio card
- * \param volume volume level (0..100)
- * \return STREAM_OK if success, STREAM_ERROR otherwise
- *
- * *BSD BT848 does not have volume changing abilities, so
- * we will just mute sound if volume=0 and unmute it otherwise.
- */
-static void set_volume_bsdbt848(radio_priv_t* priv,int volume){
-    int audio_flags;
-
-    /*arg must be between 0 and 100*/
-    if (volume > 100) volume = 100;
-    if (volume < 0) volume = 0;
-
-    audio_flags = (volume==0?AUDIO_MUTE:AUDIO_UNMUTE);
-    if (ioctl(priv->radio_fd, BT848_SAUDIO, &audio_flags)<0){
-            mp_tmsg(MSGT_RADIO,MSGL_WARN,"[radio] ioctl set mute failed: %s\n",strerror(errno));
-    }
-}
-
-/*****************************************************************
- * \brief get current volume from radio card
- * \param volume where to store volume level (0..100)
- * \return previous STREAM_OK if success, STREAM_ERROR otherwise
- *
- * *BSD BT848 does not have volume changing abilities, so
- * we will return 0 if sound is muted and 100 otherwise.
- */
-static int get_volume_bsdbt848(radio_priv_t* priv,int* volume){
-    int audio_flags;
-
-    if (ioctl(priv->radio_fd, BT848_GAUDIO, &audio_flags)<0){
-        mp_tmsg(MSGT_RADIO,MSGL_ERR,"[radio] ioctl get volume failed: %s\n",strerror(errno));
-        return STREAM_ERROR;
-    }
-
-    if (audio_flags & AUDIO_MUTE)
-        *volume=0;
-    else
-        *volume=100;
-
-    return STREAM_OK;
-}
-
-/* bsdbt848 driver info structure */
-static const radio_driver_t radio_driver_bsdbt848={
-    "bsdbt848",
-    _("[radio] Using *BSD BT848 radio interface.\n"),
-    init_frac_bsdbt848,
-    set_volume_bsdbt848,
-    get_volume_bsdbt848,
-    set_frequency_bsdbt848,
-    get_frequency_bsdbt848
-};
-#endif /* CONFIG_RADIO_BSDBT848 */
 
 static inline int init_frac(radio_priv_t* priv){
     return priv->driver->init_frac(priv);
@@ -943,9 +810,6 @@ static int fill_buffer_s(struct stream *s, char *buffer, int max_len){
  when no driver explicitly specified first available will be used
  */
 static const radio_driver_t* radio_drivers[]={
-#ifdef CONFIG_RADIO_BSDBT848
-    &radio_driver_bsdbt848,
-#endif
 #ifdef CONFIG_RADIO_V4L2
     &radio_driver_v4l2,
 #endif

Makefile
@@ -72,7 +72,6 @@ SOURCES-$(STREAM_CACHE)         += stream/cache2.c
 
 SOURCES-$(TV)                   += stream/stream_tv.c stream/tv.c \
                                    stream/frequencies.c stream/tvi_dummy.c
-SOURCES-$(TV_BSDBT848)          += stream/tvi_bsdbt848.c
 
 SOURCES-$(TV_V4L2)              += stream/tvi_v4l2.c stream/audio_in.c
 SOURCES-$(VCD)                  += stream/stream_vcd.c

configure
@@ -305,10 +305,8 @@ Optional features:
   --enable-radio         enable radio interface [disable]
   --enable-radio-capture enable radio capture (through PCI/line-in) [disable]
   --disable-radio-v4l2   disable Video4Linux2 radio interface [autodetect]
-  --disable-radio-bsdbt848   disable BSD BT848 radio interface [autodetect]
   --disable-tv           disable TV interface (TV/DVB grabbers) [enable]
   --disable-tv-v4l2      disable Video4Linux2 TV interface [autodetect]
-  --disable-tv-bsdbt848  disable BSD BT848 interface [autodetect]
   --disable-pvr          disable Video4Linux2 MPEG PVR [autodetect]
   --disable-networking   disable networking [enable]
   --enable-winsock2_h    enable winsock2_h [autodetect]
@@ -454,10 +452,8 @@ _select=yes
 _radio=no
 _radio_capture=no
 _radio_v4l2=auto
-_radio_bsdbt848=auto
 _tv=yes
 _tv_v4l2=auto
-_tv_bsdbt848=auto
 _pvr=auto
 networking=yes
 _winsock2_h=auto
@@ -646,8 +642,6 @@ for ac_option do
   --disable-alsa)       _alsa=no        ;;
   --enable-tv)          _tv=yes         ;;
   --disable-tv)         _tv=no          ;;
-  --enable-tv-bsdbt848)  _tv_bsdbt848=yes ;;
-  --disable-tv-bsdbt848) _tv_bsdbt848=no  ;;
   --enable-tv-v4l2)     _tv_v4l2=yes    ;;
   --disable-tv-v4l2)    _tv_v4l2=no     ;;
   --enable-radio)       _radio=yes      ;;
@@ -656,8 +650,6 @@ for ac_option do
   --disable-radio)      _radio=no       ;;
   --enable-radio-v4l2)  _radio_v4l2=yes ;;
   --disable-radio-v4l2) _radio_v4l2=no  ;;
-  --enable-radio-bsdbt848)      _radio_bsdbt848=yes     ;;
-  --disable-radio-bsdbt848)     _radio_bsdbt848=no      ;;
   --enable-pvr)         _pvr=yes        ;;
   --disable-pvr)        _pvr=no         ;;
   --enable-networking)     networking=yes    ;;
@@ -2727,81 +2719,6 @@ fi
 echores "$_tv"
 
 
-if freebsd || netbsd || openbsd || dragonfly ; then
-  echocheck "*BSD BT848 bt8xx header"
-  _ioctl_bt848_h=no
-  for file in "machine/ioctl_bt848.h" \
-            "dev/bktr/ioctl_bt848.h" \
-            "dev/video/bktr/ioctl_bt848.h" \
-            "dev/ic/bt8xx.h" ; do
-    cat > $TMPC <<EOF
-#include <sys/types.h>
-#include <sys/ioctl.h>
-#include <$file>
-int main(void) { ioctl(0, TVTUNER_GETFREQ, 0); return 0; }
-EOF
-    if cc_check ; then
-     _ioctl_bt848_h=yes
-     _ioctl_bt848_h_name="$file"
-     break;
-    fi
-  done
-  if test "$_ioctl_bt848_h" = yes ; then
-    def_ioctl_bt848_h_name="#define IOCTL_BT848_H_NAME <$_ioctl_bt848_h_name>"
-    res_comment="using $_ioctl_bt848_h_name"
-  else
-    def_ioctl_bt848_h_name="#undef IOCTL_BT848_H_NAME"
-  fi
-  echores "$_ioctl_bt848_h"
-
-  echocheck "*BSD ioctl_meteor.h"
-  _ioctl_meteor_h=no
-  for ioctl_meteor_h_path in "machine/ioctl_meteor.h" "dev/bktr/ioctl_meteor.h" "dev/video/bktr/ioctl_meteor.h" ; do
-    statement_check_broken "sys/types.h" "$ioctl_meteor_h_path" 'ioctl(0, METEORSINPUT, 0)' &&
-      _ioctl_meteor_h=yes && break
-  done
-  if test "$_ioctl_meteor_h" = yes ; then
-    def_ioctl_meteor_h_name="#define IOCTL_METEOR_H_NAME <$ioctl_meteor_h_path>"
-    res_comment="using $ioctl_meteor_h_path"
-  else
-    def_ioctl_meteor_h_name="#undef IOCTL_METEOR_H_NAME"
-  fi
-  echores "$_ioctl_meteor_h"
-
-  echocheck "*BSD BrookTree 848 TV interface"
-  if test "$_tv_bsdbt848" = auto ; then
-    _tv_bsdbt848=no
-    if test "$_tv" = yes ; then
-      cat > $TMPC <<EOF
-#include <sys/types.h>
-$def_ioctl_meteor_h_name
-$def_ioctl_bt848_h_name
-#ifdef IOCTL_METEOR_H_NAME
-#include IOCTL_METEOR_H_NAME
-#endif
-#ifdef IOCTL_BT848_H_NAME
-#include IOCTL_BT848_H_NAME
-#endif
-int main(void) {
- ioctl(0, METEORSINPUT, 0);
- ioctl(0, TVTUNER_GETFREQ, 0);
- return 0;
-}
-EOF
-      cc_check && _tv_bsdbt848=yes
-    fi
-  fi
-  if test "$_tv_bsdbt848" = yes ; then
-    def_tv_bsdbt848='#define CONFIG_TV_BSDBT848 1'
-    inputmodules="tv-bsdbt848 $inputmodules"
-  else
-    def_tv_bsdbt848='#undef CONFIG_TV_BSDBT848'
-    noinputmodules="tv-bsdbt848 $noinputmodules"
-  fi
-  echores "$_tv_bsdbt848"
-fi #if freebsd || netbsd || openbsd || dragonfly
-
-
 echocheck "Video 4 Linux 2 TV interface"
 if test "$_tv_v4l2" = auto ; then
   _tv_v4l2=no
@@ -2860,31 +2777,8 @@ fi
 echores "$_radio_v4l2"
 
 
-if freebsd || netbsd || openbsd || dragonfly &&
-  test "$_radio" = yes && test "$_radio_bsdbt848" = auto ; then
-echocheck "*BSD BrookTree 848 Radio interface"
-   _radio_bsdbt848=no
-    cat > $TMPC <<EOF
-#include <sys/types.h>
-$def_ioctl_bt848_h_name
-#ifdef IOCTL_BT848_H_NAME
-#include IOCTL_BT848_H_NAME
-#endif
-int main(void) { ioctl(0, RADIO_GETFREQ, 0); return 0; }
-EOF
-    cc_check && _radio_bsdbt848=yes
-echores "$_radio_bsdbt848"
-fi #if freebsd || netbsd || openbsd || dragonfly && _radio && _radio_bsdbt848
-
-if test "$_radio_bsdbt848" = yes ; then
-  def_radio_bsdbt848='#define CONFIG_RADIO_BSDBT848 1'
-else
-  def_radio_bsdbt848='#undef CONFIG_RADIO_BSDBT848'
-fi
-
-if test "$_radio_v4l2" = no &&
-   test "$_radio_bsdbt848" = no && test "$_radio" = yes ; then
-    die "Radio driver requires BSD BT848 or V4L2!"
+if test "$_radio_v4l2" = no && test "$_radio" = yes ; then
+    die "Radio driver requires V4L2!"
 fi
 
 echocheck "Video 4 Linux 2 MPEG PVR interface"
@@ -3135,7 +3029,6 @@ RADIO_CAPTURE=$_radio_capture
 RSOUND = $_rsound
 STREAM_CACHE = $_stream_cache
 TV = $_tv
-TV_BSDBT848 = $_tv_bsdbt848
 TV_V4L2 = $_tv_v4l2
 VCD = $_vcd
 VDPAU = $_vdpau
@@ -3290,18 +3183,14 @@ $def_libbs2b
 /* input */
 $def_apple_ir
 $def_apple_remote
-$def_ioctl_bt848_h_name
-$def_ioctl_meteor_h_name
 $def_joystick
 $def_lirc
 $def_lircc
 $def_pvr
 $def_radio
-$def_radio_bsdbt848
 $def_radio_capture
 $def_radio_v4l2
 $def_tv
-$def_tv_bsdbt848
 $def_tv_v4l2
 
 

core/cfg-mplayer.h
@@ -59,10 +59,6 @@ extern const char pp_help[];
 const m_option_t radioopts_conf[]={
     {"device", &stream_radio_defaults.device, CONF_TYPE_STRING, 0, 0 ,0, NULL},
     {"driver", &stream_radio_defaults.driver, CONF_TYPE_STRING, 0, 0 ,0, NULL},
-#ifdef RADIO_BSDBT848_HDR
-    {"freq_min", &stream_radio_defaults.freq_min, CONF_TYPE_FLOAT, 0, 0 ,0, NULL},
-    {"freq_max", &stream_radio_defaults.freq_max, CONF_TYPE_FLOAT, 0, 0 ,0, NULL},
-#endif
     {"channels", &stream_radio_defaults.channels, CONF_TYPE_STRING_LIST, 0, 0 ,0, NULL},
     {"volume", &stream_radio_defaults.volume, CONF_TYPE_INT, CONF_RANGE, 0 ,100, NULL},
     {"adevice", &stream_radio_defaults.adevice, CONF_TYPE_STRING, 0, 0 ,0, NULL},

stream/stream_radio.h
@@ -28,12 +28,6 @@
 typedef struct radio_param_s{
     /** name of radio device file */
     char*   device;
-#ifdef CONFIG_RADIO_BSDBT848
-    /** minimal allowed frequency */
-    float   freq_min;
-    /** maximal allowed frequency */
-    float   freq_max;
-#endif
     /** radio driver (v4l,v4l2) */
     char*   driver;
     /** channels list (see man page) */

stream/tv.c
@@ -57,15 +57,11 @@ char *tv_channel_last_real;
 /* enumerating drivers (like in stream.c) */
 extern const tvi_info_t tvi_info_dummy;
 extern const tvi_info_t tvi_info_v4l2;
-extern const tvi_info_t tvi_info_bsdbt848;
 
 /** List of drivers in autodetection order */
 static const tvi_info_t* tvi_driver_list[]={
 #ifdef CONFIG_TV_V4L2
     &tvi_info_v4l2,
-#endif
-#ifdef CONFIG_TV_BSDBT848
-    &tvi_info_bsdbt848,
 #endif
     &tvi_info_dummy,
     NULL

commit f2ad12a8199013645e9e6a69ea7c5d25ad4c3d61

stream_radio: fix build
This was accidentally broken with 37c5c11 and has been nroken for
5 months.

Does anyone (want to) use this at all?

stream/stream_radio.c
@@ -37,6 +37,10 @@
 
 #include <linux/types.h>
 
+#ifdef CONFIG_RADIO_V4L2
+#include <linux/videodev2.h>
+#endif
+
 #include "stream.h"
 #include "demux/demux.h"
 #include "core/m_struct.h"