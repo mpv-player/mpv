4fd06a78aecf6b52ae28170cfa05ba0d36e75265
wm4 <wm4@nowhere>
2013-07-18 13:45:48 +0200

    sws_utils: remove some old code

--- video/sws_utils.h
+++ video/sws_utils.h
@@ -14,12 +14,6 @@ struct mp_csp_details;
 // Guaranteed to be a power of 2 and > 1.
 #define SWS_MIN_BYTE_ALIGN 16
 
-void sws_getFlagsAndFilterFromCmdLine(int *flags, SwsFilter **srcFilterParam,
-                                      SwsFilter **dstFilterParam);
-struct SwsContext *sws_getContextFromCmdLine(int srcW, int srcH, int srcFormat,
-                                             int dstW, int dstH,
-                                             int dstFormat);
-
 bool mp_sws_supported_format(int imgfmt);
 
 void mp_image_swscale(struct mp_image *dst, struct mp_image *src,
