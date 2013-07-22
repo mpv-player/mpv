mpv-player/mpv

commit 9a83d03c5d8d6f73fd41707eb9775be1af3d3daf

av_log: change how ffmpeg log messages are formatted
Don't print the ffmpeg context pointer as %p. This is usually useless
and confusing. Prefix all messages with "ffmpeg" to make clear that
ffmpeg is printing these messages, and not us.

If libavcodec is from Libav, use "libav" as prefix instead. (In theory,
FFmpeg/Libav libraries could be mixed, but I don't think that is
actually possible in practice.)

core/av_log.c
@@ -97,10 +97,15 @@ static int extract_msg_type_from_ctx(void *ptr)
     return MSGT_FIXME;
 }
 
+#if LIBAVCODEC_VERSION_MICRO >= 100
+#define LIB_PREFIX "ffmpeg"
+#else
+#define LIB_PREFIX "libav"
+#endif
+
 static void mp_msg_av_log_callback(void *ptr, int level, const char *fmt,
                                    va_list vl)
 {
-    static bool print_prefix = 1;
     AVClass *avc = ptr ? *(AVClass **)ptr : NULL;
     int mp_level = av_log_level_to_mp_level(level);
     int type = extract_msg_type_from_ctx(ptr);
@@ -108,9 +113,8 @@ static void mp_msg_av_log_callback(void *ptr, int level, const char *fmt,
     if (!mp_msg_test(type, mp_level))
         return;
 
-    if (print_prefix && avc)
-        mp_msg(type, mp_level, "[%s @ %p]", avc->item_name(ptr), avc);
-    print_prefix = fmt[strlen(fmt) - 1] == '\n';
+    mp_msg(type, mp_level, "[%s/%s] ", LIB_PREFIX,
+           avc ? avc->item_name(ptr) : "?");
 
     mp_msg_va(type, mp_level, fmt, vl);
 }

commit a48f7a546c8044decfd68409663530d6e9d06a14

av_log: restore handling of prefixes and line breaks
Commit 9a83d03 accidentally removed this. (Overlooked "static"?)

The handling of this rather sucks. Maybe a better solution will be
possible once we clean up the mp_msg code.

core/av_log.c
@@ -103,6 +103,8 @@ static int extract_msg_type_from_ctx(void *ptr)
 #define LIB_PREFIX "libav"
 #endif
 
+static bool print_prefix = true;
+
 static void mp_msg_av_log_callback(void *ptr, int level, const char *fmt,
                                    va_list vl)
 {
@@ -113,8 +115,11 @@ static void mp_msg_av_log_callback(void *ptr, int level, const char *fmt,
     if (!mp_msg_test(type, mp_level))
         return;
 
-    mp_msg(type, mp_level, "[%s/%s] ", LIB_PREFIX,
-           avc ? avc->item_name(ptr) : "?");
+    if (print_prefix) {
+        mp_msg(type, mp_level, "[%s/%s] ", LIB_PREFIX,
+               avc ? avc->item_name(ptr) : "?");
+    }
+    print_prefix = fmt[strlen(fmt) - 1] == '\n';
 
     mp_msg_va(type, mp_level, fmt, vl);
 }