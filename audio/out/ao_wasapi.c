f06f3592e0a9ca973a8b6652c78976266890c6ce
Jonathan Yong <10walls@gmail.com>
2013-07-30 21:33:00 +0800

    Fix some warnings

--- audio/out/ao_wasapi.c
+++ audio/out/ao_wasapi.c
@@ -294,7 +294,7 @@ static int set_ao_format(struct wasapi_state *state,
         wformat.Format.wBitsPerSample, wformat.SubFormat.Data1 == 3);
 
     if (wformat.SubFormat.Data1 != 1 && wformat.SubFormat.Data1 != 3) {
-        mp_msg(MSGT_AO, MSGL_ERR, "ao-wasapi: unknown SubFormat %d\n",
+        mp_msg(MSGT_AO, MSGL_ERR, "ao-wasapi: unknown SubFormat %lu\n",
             wformat.SubFormat.Data1);
         return 0;
     }
@@ -798,7 +798,7 @@ static int enumerate_devices(void) {
     CoUninitialize();
     return 0;
 exit_label:
-    mp_msg(MSGT_AO, MSGL_ERR, "Error enumerating devices: HRESULT %08x \"%s\"\n",
+    mp_msg(MSGT_AO, MSGL_ERR, "Error enumerating devices: HRESULT %08lx \"%s\"\n",
         hr, explain_err(hr));
     CoUninitialize();
     return 1;
@@ -1044,7 +1044,7 @@ static void thread_feed(wasapi_state *state,int force_feed)
     return;
 exit_label:
     EnterCriticalSection(&state->print_lock);
-    mp_msg(MSGT_AO, MSGL_ERR, "ao-wasapi: thread_feed fails with %"PRIx32"!\n", hr);
+    mp_msg(MSGT_AO, MSGL_ERR, "ao-wasapi: thread_feed fails with %lx!\n", hr);
     LeaveCriticalSection(&state->print_lock);
     return;
 }
@@ -1107,7 +1107,7 @@ static void thread_uninit(wasapi_state *state)
     ExitThread(0);
 }
 
-static unsigned int __stdcall ThreadLoop(void *lpParameter)
+static DWORD __stdcall ThreadLoop(void *lpParameter)
 {
     struct ao *ao = lpParameter;
     int feedwatch = 0;
