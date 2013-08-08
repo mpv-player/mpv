d7345c4f510cea96f2deed3d4a81b98868c78216
Nikoli <nikoli@lavabit.com>
2013-08-08 10:35:41 +0400

    Fix version for releases, also show it before git commit
    For releases version output is:
    mpv 0.1.1 (C) 2000-2013 mpv/MPlayer/mplayer2 projects
    
    When building from git:
    mpv 0.1.1-git-d892236 (C) 2000-2013 mpv/MPlayer/mplayer2 projects

--- version.sh
+++ version.sh
@@ -4,16 +4,16 @@ export LC_ALL=C
 
 test "$1" && extra="-$1"
 
+# releases extract the version number from the VERSION file
+version=$(cat VERSION 2> /dev/null)
+test $version || version=UNKNOWN
+
 # Extract revision number from file used by daily tarball snapshots
 # or from "git describe" output
 git_revision=$(cat snapshot_version 2> /dev/null)
 test $git_revision || test ! -e .git || git_revision=`git rev-parse --short HEAD`
 git_revision=$(expr "$git_revision" : v*'\(.*\)')
-test $git_revision || git_revision=UNKNOWN
-
-# releases extract the version number from the VERSION file
-version=$(cat VERSION 2> /dev/null)
-test $version || version=git-$git_revision
+test $git_revision && version=$version-git-$git_revision
 
 NEW_REVISION="#define VERSION \"${version}${extra}\""
 OLD_REVISION=$(head -n 1 version.h 2> /dev/null)
