683e58066e65e4bca43ff22cf1ef9f4481810266
Alexander Preisinger <alexander.preisinger@gmail.com>
2013-09-09 18:37:33 +0200

    wayland: update license headers

--- video/out/wayland_common.c
+++ video/out/wayland_common.c
@@ -1,23 +1,21 @@
 /*
- * This file is part of MPlayer.
+ * This file is part of mpv video player.
  * Copyright © 2008 Kristian Høgsberg
  * Copyright © 2012-2013 Collabora, Ltd.
- * Copyright © 2012-2013 Scott Moreau <oreaus@gmail.com>
- * Copyright © 2012-2013 Alexander Preisinger <alexander.preisinger@gmail.com>
+ * Copyright © 2013 Alexander Preisinger <alexander.preisinger@gmail.com>
  *
- * MPlayer is free software; you can redistribute it and/or modify
+ * mpv is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * MPlayer is distributed in the hope that it will be useful,
+ * mpv is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along
- * with MPlayer; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * with mpv.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdio.h>
