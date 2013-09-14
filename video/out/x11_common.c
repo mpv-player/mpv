0e372c9405d07a7b3d3c49c3384f25eba7e58497
Andreas Sinz <andreas.sinz@aon.at>
2013-09-14 16:35:37 +0200

    x11/common: print message according to probing status

--- video/out/x11_common.c
+++ video/out/x11_common.c
@@ -462,7 +462,6 @@ int vo_x11_init(struct vo *vo)
 
     x11->display = XOpenDisplay(dispName);
     if (!x11->display) {
-        MP_ERR(x11, "couldn't open the X11 display (%s)!\n", dispName);
         if (!dispName[0]) {
             MP_MSG(x11, vo->probing ? MSGL_V : MSGL_ERR,
                    "vo: couldn't open the X11 display (%s)!\n", dispName);
