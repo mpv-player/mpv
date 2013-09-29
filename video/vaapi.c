mpv-player/mpv

commit b8382aabb338db49b1cc0344a7ec386fa7f7f1c2

vaapi: make vaDeriveImage() less verbose
This can just be not supported, so making it look like a real error
doesn't make much sense.

video/vaapi.c
@@ -341,7 +341,7 @@ static VAImage *va_surface_image_alloc(struct va_surface *surface,
     va_surface_image_destroy(surface);
 
     VAStatus status = vaDeriveImage(p->display, surface->id, &p->image);
-    if (check_va_status(status, "vaDeriveImage()")) {
+    if (status != VA_STATUS_SUCCESS) {
         /* vaDeriveImage() is supported, check format */
         if (p->image.format.fourcc == format->fourcc &&
                 p->image.width == surface->w && p->image.height == surface->h) {

commit 4e1d65983a94a85e6ba4e1248b321aaaf053391e

vaapi: fix inverted condition
How embarrassing. This could make --hwdec=vaapi-copy as well as
screenshots with vo_vaapi randomly fail. Regression since
commit b8382aa.

video/vaapi.c
@@ -341,7 +341,7 @@ static VAImage *va_surface_image_alloc(struct va_surface *surface,
     va_surface_image_destroy(surface);
 
     VAStatus status = vaDeriveImage(p->display, surface->id, &p->image);
-    if (status != VA_STATUS_SUCCESS) {
+    if (status == VA_STATUS_SUCCESS) {
         /* vaDeriveImage() is supported, check format */
         if (p->image.format.fourcc == format->fourcc &&
                 p->image.width == surface->w && p->image.height == surface->h) {
@@ -349,11 +349,11 @@ static VAImage *va_surface_image_alloc(struct va_surface *surface,
             VA_VERBOSE("Using vaDeriveImage()\n");
         } else {
             vaDestroyImage(p->display, p->image.image_id);
-            p->image.image_id = VA_INVALID_ID;
             status = VA_STATUS_ERROR_OPERATION_FAILED;
         }
     }
     if (status != VA_STATUS_SUCCESS) {
+        p->image.image_id = VA_INVALID_ID;
         status = vaCreateImage(p->display, format, surface->w, surface->h,
                                &p->image);
         if (!check_va_status(status, "vaCreateImage()")) {