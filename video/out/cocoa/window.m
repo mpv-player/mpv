mpv-player/mpv

commit bc49957458870cc9fb19a3450f26ba625ade1e54

cocoa: constraint the window position a little more
The intention of this is to not make the window go outside the screen when
changing dimensions from 2x to .5x.

video/out/cocoa/window.m
@@ -15,6 +15,8 @@
  * with mpv.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <libavutil/common.h>
+
 #include "mpvcore/input/keycodes.h"
 
 #include "osdep/macosx_application.h"
@@ -121,8 +123,11 @@
 - (NSRect)constrainFrameRect:(NSRect)nf toScreen:(NSScreen *)screen
 {
     NSRect s = [[self screen] visibleFrame];
-    if (nf.origin.y + nf.size.height > s.origin.y + s.size.height)
+    if (nf.origin.y + nf.size.height > s.origin.y + s.size.height) {
+        nf.size.height = s.size.height;
         nf.origin.y = s.origin.y + s.size.height - nf.size.height;
+    }
+
     return nf;
 }
 

commit e4d23dd01d68e2ef0f0ffd4f49277afb15432208

cocoa: apply the more invasive constraining only with cmd+1/2/3
Regression from bc49957
Fixes #321

video/out/cocoa/window.m
@@ -31,6 +31,7 @@
 @implementation MpvVideoWindow {
     NSSize _queued_video_size;
     bool   _fs_resize_scheduled;
+    bool   _recenter_window_during_constraint;
 }
 
 @synthesize adapter = _adapter;
@@ -117,6 +118,7 @@
     CGFloat dx = (f.size.width  - ns.width) / 2;
     CGFloat dy = (f.size.height - ns.height - [self titleHeight]) / 2;
     NSRect nf  = NSRectFromCGRect(CGRectInset(NSRectToCGRect(f), dx, dy));
+    self->_recenter_window_during_constraint = true;
     [self setFrame:nf display:NO animate:NO];
 }
 
@@ -124,11 +126,17 @@
 {
     NSRect s = [[self screen] visibleFrame];
     if (nf.origin.y + nf.size.height > s.origin.y + s.size.height) {
-        nf.size.height = s.size.height;
+        if (self->_recenter_window_during_constraint)
+            nf.size.height = s.size.height;
         nf.origin.y = s.origin.y + s.size.height - nf.size.height;
     }
-
     return nf;
+}
+
+- (void)setFrame:(NSRect)frame display:(BOOL)display animate:(BOOL)animate
+{
+    [super setFrame:frame display:display animate:animate];
+    self->_recenter_window_during_constraint = false;
 }
 
 - (void)queueNewVideoSize:(NSSize)new_size