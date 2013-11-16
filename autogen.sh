7560c0c46d399f0f5c5d12ce522005b0ebabb517
wm4 <wm4@nowhere>
2013-11-16 23:18:51 +0100

    build: verify downloaded waf binary
    I don't feel particularly good about executing automatically downloaded
    files from the internet without verification. Verify the downloaded
    executable with sha256sum, which is part of GNU coreutils, and should be
    ubiquitous enough.
    
    Now it should be impossible to run a downloaded file other than what the
    hash stored in autogen.sh indicates, which in turn is bound to the git
    commit hash.
    
    If she256sum is not available, print a warning, and don't make waf
    executable.

--- autogen.sh
+++ autogen.sh
@@ -2,5 +2,20 @@
 #
 # This script simply downloads waf to the project's root directory
 
-curl https://waf.googlecode.com/files/waf-1.7.13 > waf
-chmod +x waf
+WAFRELEASE=waf-1.7.13
+SHA256HASH=03cc750049350ee01cdbc584b70924e333fcc17ba4a2d04648dab1535538a873
+
+curl https://waf.googlecode.com/files/$WAFRELEASE > waf
+if test -x $(which sha256sum) ; then
+    if echo "$SHA256HASH waf" | sha256sum --strict --check - ; then
+        echo "Checksum verified."
+    else
+        rm -f waf
+        echo "The checksum of the downloaded file does not match!"
+        echo "Please download and verify the file manually."
+        exit 1
+    fi
+    chmod +x waf
+else
+    echo "sha256sum not found. It's up to you to verify the downloaded file."
+fi
