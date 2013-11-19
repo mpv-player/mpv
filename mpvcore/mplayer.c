253623d9e8679d3539f1071d469f7994b0fe88a4
wm4 <wm4@nowhere>
2013-11-19 22:16:56 +0100

    player: deselect video track if initialization fails
    This didn't have any consequences, other than suddenly reinitializing
    video when it works again (such as with EDL timeline mixing video and
    audio-only files).
    
    Conflicts:
    	mpvcore/player/video.c

--- mpvcore/mplayer.c
+++ mpvcore/mplayer.c
@@ -2481,6 +2481,7 @@ err_out:
 no_video:
     uninit_player(mpctx, INITIALIZED_VCODEC | (opts->force_vo ? 0 : INITIALIZED_VO));
     cleanup_demux_stream(mpctx, STREAM_VIDEO);
+    mpctx->current_track[STREAM_VIDEO] = NULL;
     handle_force_window(mpctx, true);
     mp_tmsg(MSGT_CPLAYER, MSGL_INFO, "Video: no video\n");
     return 0;
