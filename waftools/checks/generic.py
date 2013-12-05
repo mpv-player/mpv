mpv-player/mpv

commit bf90317ad14dde82f16a0f0f3d2561d38ac970ff

build: remove execute kwarg to check_cc when cross-compiling
This prevents waf from running test programs after compilation. A better
approach would be to only remove this option if the check actually errors,
but we are using this only for Lua anyway.

waftools/checks/generic.py
@@ -1,6 +1,7 @@
 import os
 from inflectors import DependencyInflector
 from waflib.ConfigSet import ConfigSet
+from waflib import Utils
 
 __all__ = [
     "check_pkg_config", "check_cc", "check_statement", "check_libs",
@@ -21,6 +22,12 @@ def __merge_options__(dependency_identifier, *args):
     [options_accu.update(arg) for arg in args if arg]
     return options_accu
 
+def _filter_cc_arguments(ctx, opts):
+    if ctx.env.DEST_OS != Utils.unversioned_sys_platform():
+        # cross compiling, remove execute=True if present
+        if opts['execute'] == True:
+            opts['execute'] = False
+    return opts
 
 def check_libs(libs, function):
     libs = [None] + libs
@@ -42,7 +49,7 @@ def check_statement(header, statement, **kw_ext):
                                  {'fragment':fragment},
                                  __define_options__(dependency_identifier),
                                  kw_ext, kw)
-        return ctx.check_cc(**opts)
+        return ctx.check_cc(**_filter_cc_arguments(ctx, opts))
     return fn
 
 def check_cc(**kw_ext):
@@ -50,7 +57,7 @@ def check_cc(**kw_ext):
         options = __merge_options__(dependency_identifier,
                                     __define_options__(dependency_identifier),
                                     kw_ext, kw)
-        return ctx.check_cc(**options)
+        return ctx.check_cc(**_filter_cc_arguments(ctx, options))
     return fn
 
 def check_pkg_config(*args, **kw_ext):

waftools/checks/custom.py
@@ -61,7 +61,7 @@ def check_lua(ctx, dependency_identifier):
             [lv for lv in lua_versions if lv[0] == ctx.options.LUA_VER]
 
     for lua_version, pkgconfig_query in lua_versions:
-       if compose_checks(
+        if compose_checks(
             check_pkg_config(pkgconfig_query, uselib_store=lua_version),
             check_cc(fragment=fragment,
                      use=[lua_version] + quvi_lib_storage,

commit 5cb8439015f533ee93f4a4c9442e9c36de62b2f0

build: fix regression in cross-compilation
Regression was introduced in bf90317ad in an attempt to fix the Lua check.

waftools/checks/generic.py
@@ -25,7 +25,7 @@ def __merge_options__(dependency_identifier, *args):
 def _filter_cc_arguments(ctx, opts):
     if ctx.env.DEST_OS != Utils.unversioned_sys_platform():
         # cross compiling, remove execute=True if present
-        if opts['execute'] == True:
+        if getattr(opts, 'execute', None) == True:
             opts['execute'] = False
     return opts
 