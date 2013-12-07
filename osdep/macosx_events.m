mpv-player/mpv

commit 6fb020f5de1487484712e7113db0e86dd97481bd

options: add option to disable using right Alt key as Alt Gr
mpv was hardcoded to always consider the right Alt key as Alt Gr, but there
are parituclar combinations of platforms and keyboard layouts where it's more
convenient to treat the right Alt as a keyboard modifier just like the left
one.

Fixes #388

osdep/macosx_events.m
@@ -174,6 +174,11 @@ void cocoa_put_key_with_modifiers(int keycode, int modifiers)
     HIDRemote *_remote;
 }
 
+- (BOOL)useAltGr
+{
+    return mp_input_use_alt_gr(mpv_shared_app().inputContext);
+}
+
 - (void)startAppleRemote
 {
     dispatch_async(dispatch_get_main_queue(), ^{
@@ -287,7 +292,8 @@ void cocoa_put_key_with_modifiers(int keycode, int modifiers)
         mask |= MP_KEY_MODIFIER_SHIFT;
     if (cocoaModifiers & NSControlKeyMask)
         mask |= MP_KEY_MODIFIER_CTRL;
-    if (LeftAltPressed(cocoaModifiers))
+    if (LeftAltPressed(cocoaModifiers) ||
+        RightAltPressed(cocoaModifiers) && ![self useAltGr])
         mask |= MP_KEY_MODIFIER_ALT;
     if (cocoaModifiers & NSCommandKeyMask)
         mask |= MP_KEY_MODIFIER_META;
@@ -333,7 +339,7 @@ void cocoa_put_key_with_modifiers(int keycode, int modifiers)
 
     NSString *chars;
 
-    if (RightAltPressed([event modifierFlags]))
+    if ([self useAltGr] && RightAltPressed([event modifierFlags]))
         chars = [event characters];
     else
         chars = [event charactersIgnoringModifiers];

mpvcore/input/input.c
@@ -550,6 +550,7 @@ struct input_ctx {
     pthread_mutex_t mutex;
     struct mp_log *log;
 
+    bool using_alt_gr;
     bool using_ar;
     bool using_cocoa_media_keys;
 
@@ -635,6 +636,7 @@ const m_option_t mp_input_opts[] = {
     OPT_FLAG("joystick", input.use_joystick, CONF_GLOBAL),
     OPT_FLAG("lirc", input.use_lirc, CONF_GLOBAL),
     OPT_FLAG("lircc", input.use_lircc, CONF_GLOBAL),
+    OPT_FLAG("right-alt-gr", input.use_alt_gr, CONF_GLOBAL),
 #if HAVE_COCOA
     OPT_FLAG("ar", input.use_ar, CONF_GLOBAL),
     OPT_FLAG("media-keys", input.use_media_keys, CONF_GLOBAL),
@@ -2387,6 +2389,10 @@ struct input_ctx *mp_input_init(struct mpv_global *global)
     }
 #endif
 
+    if (input_conf->use_alt_gr) {
+        ictx->using_alt_gr = true;
+    }
+
 #if HAVE_COCOA
     if (input_conf->use_ar) {
         cocoa_init_apple_remote();
@@ -2527,4 +2533,9 @@ unsigned int mp_input_get_mouse_event_counter(struct input_ctx *ictx)
     int ret = ictx->mouse_event_counter;
     input_unlock(ictx);
     return ret;
+}
+
+bool mp_input_use_alt_gr(struct input_ctx *ictx)
+{
+    return ictx->using_alt_gr;
 }

mpvcore/input/input.h
@@ -287,6 +287,10 @@ void mp_input_wakeup(struct input_ctx *ictx);
 // Interruptible usleep:  (used by demux)
 int mp_input_check_interrupt(struct input_ctx *ictx, int time);
 
+// If this returns true, use Right Alt key as Alt Gr to produce special
+// characters. If false, count Right Alt as the modifier Alt key.
+bool mp_input_use_alt_gr(struct input_ctx *ictx);
+
 extern int async_quit_request;
 
 #endif /* MPLAYER_INPUT_H */

mpvcore/options.c
@@ -873,6 +873,7 @@ const struct MPOpts mp_default_opts = {
         .use_joystick = 1,
         .use_lirc = 1,
         .use_lircc = 1,
+        .use_alt_gr = 1,
 #if HAVE_COCOA
         .use_ar = 1,
         .use_media_keys = 1,

mpvcore/options.h
@@ -251,6 +251,7 @@ typedef struct MPOpts {
         int use_joystick;
         int use_lirc;
         int use_lircc;
+        int use_alt_gr;
         int use_ar;
         int use_media_keys;
         int default_bindings;

video/out/vo_sdl.c
@@ -495,11 +495,13 @@ static void check_events(struct vo *vo)
         case SDL_TEXTINPUT: {
             int sdl_mod = SDL_GetModState();
             int mpv_mod = 0;
-            // we ignore KMOD_LSHIFT, KMOD_RSHIFT and KMOD_RALT because
-            // these are already factored into ev.text.text
+            // we ignore KMOD_LSHIFT, KMOD_RSHIFT and KMOD_RALT (if
+            // mp_input_use_alt_gr() is true) because these are already
+            // factored into ev.text.text
             if (sdl_mod & (KMOD_LCTRL | KMOD_RCTRL))
                 mpv_mod |= MP_KEY_MODIFIER_CTRL;
-            if (sdl_mod & KMOD_LALT)
+            if ((sdl_mod & KMOD_LALT) ||
+                (sdl_mod & KMOD_RALT) && !mp_input_use_alt_gr(vo->input_ctx))
                 mpv_mod |= MP_KEY_MODIFIER_ALT;
             if (sdl_mod & (KMOD_LGUI | KMOD_RGUI))
                 mpv_mod |= MP_KEY_MODIFIER_META;

video/out/w32_common.c
@@ -203,7 +203,7 @@ static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam,
             // Windows enables Ctrl+Alt when AltGr (VK_RMENU) is pressed.
             // E.g. AltGr+9 on a German keyboard would yield Ctrl+Alt+[
             // Warning: wine handles this differently. Don't test this on wine!
-            if (key_state(vo, VK_RMENU))
+            if (key_state(vo, VK_RMENU) && mp_input_use_alt_gr(vo->input_ctx))
                 mods &= ~(MP_KEY_MODIFIER_CTRL | MP_KEY_MODIFIER_ALT);
             // Apparently Ctrl+A to Ctrl+Z is special cased, and produces
             // character codes from 1-26. Work it around.

commit b454cc0ca34424101f7e4ba922983cbc44976641

macosx_events: fix null dereference on uninit
Bug introduced by commit 6fb020f5. It doesn't always happen, since it is
caused by the playloop and cocoa UI code running in separate threads.

Fixes #398.

osdep/macosx_events.m
@@ -176,7 +176,10 @@ void cocoa_put_key_with_modifiers(int keycode, int modifiers)
 
 - (BOOL)useAltGr
 {
-    return mp_input_use_alt_gr(mpv_shared_app().inputContext);
+    if (mpv_shared_app().inputContext)
+        return mp_input_use_alt_gr(mpv_shared_app().inputContext);
+    else
+        return YES;
 }
 
 - (void)startAppleRemote