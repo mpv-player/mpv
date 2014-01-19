ef006dcae6b07a8abb7fed847fd4a9e647109584
wm4 <wm4@nowhere>
2014-01-19 20:38:01 +0100

    stream: print stream_read_line warnings by default
    This is probably ok. Probing could hit this case very often, since it'll
    mean running this function on potentially binary data, but on the other
    hand, probing usually uses a memory stream (to limit the amount of data
    read), and memory streams have s->log silenced (details see
    open_memory_stream()).

--- stream/stream.c
+++ stream/stream.c
@@ -899,7 +899,7 @@ unsigned char *stream_read_line(stream_t *s, unsigned char *mem, int max,
         // Reserve 1 byte of ptr for terminating \0.
         int l = read_characters(s, &mem[read], max - read - 1, utf16);
         if (l < 0 || memchr(&mem[read], '\0', l)) {
-            MP_VERBOSE(s, "error reading line\n");
+            MP_WARN(s, "error reading line\n");
             s->eof = false;
             return NULL;
         }
