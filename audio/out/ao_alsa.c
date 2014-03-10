5f788a7812185cf69cc29e2fe6e5dcf27c0165f4
wm4 <wm4@nowhere>
2014-03-10 01:18:10 +0100

    ao_alsa: fix return value for volume operations with spdif
    This AO pretended to support volume operations when in spdif passthrough
    mode, but actually did nothing. This is wrong: at least the GET
    operations must write their argument. Signal that volume is unsupported
    instead.
    
    This was probably a hack to prevent insertion of volume filters or so,
    but it didn't work anyway, while recovering after failed volume filter
    insertion does work, so this is not needed at all.

--- audio/out/ao_alsa.c
+++ audio/out/ao_alsa.c
@@ -100,7 +100,7 @@ static int control(struct ao *ao, enum aocontrol cmd, void *arg)
         float f_multi;
 
         if (AF_FORMAT_IS_IEC61937(ao->format))
-            return CONTROL_TRUE;
+            return CONTROL_FALSE;
 
         //allocate simple id
         snd_mixer_selem_id_alloca(&sid);
