5506c8d0f6450133d33e940fcdeaecc25564092a
wm4 <wm4@nowhere>
2014-03-16 09:53:31 +0100

    ad_lavc: remove deprecated downmixing by channel count
    Downmixing by channel layout now hopefully works with all supported
    libavcodec versions.

--- audio/decode/ad_lavc.c
+++ audio/decode/ad_lavc.c
@@ -212,10 +212,6 @@ static int init(struct dec_audio *da, const char *decoder)
     if (opts->downmix) {
         lavc_context->request_channel_layout =
             mp_chmap_to_lavc(&mpopts->audio_output_channels);
-        // Compatibility for Libav 9
-        av_opt_set_int(lavc_context, "request_channels",
-                       mpopts->audio_output_channels.num,
-                       AV_OPT_SEARCH_CHILDREN);
     }
 
     // Always try to set - option only exists for AC3 at the moment
