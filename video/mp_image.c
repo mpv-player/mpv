fdeda359f7b060ec0a926eccb86c01b736a1ee16
wm4 <wm4@nowhere>
2014-03-28 23:13:41 +0100

    mp_image: add missing field to mp_image_params_equals()
    This is pretty obscure, so it didn't matter much. It still breaks
    switching output levels at runtime, because the video output is not
    reinitialized with the new params.

--- video/mp_image.c
+++ video/mp_image.c
@@ -439,6 +439,7 @@ bool mp_image_params_equals(const struct mp_image_params *p1,
            p1->d_w == p2->d_w && p1->d_h == p2->d_h &&
            p1->colorspace == p2->colorspace &&
            p1->colorlevels == p2->colorlevels &&
+           p1->outputlevels == p2->outputlevels &&
            p1->chroma_location == p2->chroma_location;
 }
 
