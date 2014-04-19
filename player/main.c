0879db960dbbddf50a58ca8dac49e428658fc1c9
wm4 <wm4@nowhere>
2014-04-19 22:31:35 +0200

    encode: don't resume playback when encoding
    The previous commit doesn't handle additionally loaded config files,
    such as the playback resume mechanism.

--- player/main.c
+++ player/main.c
@@ -394,6 +394,7 @@ int mp_initialize(struct MPContext *mpctx)
         m_config_set_option0(mpctx->mconfig, "keep-open", "no");
         m_config_set_option0(mpctx->mconfig, "force-window", "no");
         m_config_set_option0(mpctx->mconfig, "gapless-audio", "yes");
+        m_config_set_option0(mpctx->mconfig, "resume-playback", "no");
         mp_input_enable_section(mpctx->input, "encode", MP_INPUT_EXCLUSIVE);
     }
 #endif
