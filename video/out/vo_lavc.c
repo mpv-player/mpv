cb411ac52093569d831f2673f18be89ff438b9ba
wm4 <wm4@nowhere>
2014-04-30 22:24:48 +0200

    vo_lavc: remove code with no effect
    This field will also removed in the future.

--- video/out/vo_lavc.c
+++ video/out/vo_lavc.c
@@ -527,7 +527,6 @@ static int control(struct vo *vo, uint32_t request, void *data)
 }
 
 const struct vo_driver video_out_lavc = {
-    .buffer_frames = false,
     .encode = true,
     .description = "video encoding using libavcodec",
     .name = "lavc",
