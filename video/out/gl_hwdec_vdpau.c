587f42b56c7cba07fadbb3b73032cdd186f76f5f
wm4 <wm4@nowhere>
2014-05-06 20:14:53 +0200

    Fix bogus copyright text
    This file was copied from gl_hwdec_vaglx.c to have a basic skeleton, but
    no actual code is left from it.

--- video/out/gl_hwdec_vdpau.c
+++ video/out/gl_hwdec_vdpau.c
@@ -1,8 +1,6 @@
 /*
  * This file is part of mpv.
  *
- * Parts based on the MPlayer VA-API patch (see vo_vaapi.c).
- *
  * mpv is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
