c4cbf6b5408204d76dc4abe7c511637c25b8a649
wm4 <wm4@nowhere>
2014-05-15 23:57:59 +0200

    x11: remove unused stuff
    Unfortunately, it looks like some Motif functionality is still needed
    to allow for --no-border.

--- video/out/x11_common.c
+++ video/out/x11_common.c
@@ -100,10 +100,7 @@
 
 #define MWM_HINTS_FUNCTIONS     (1L << 0)
 #define MWM_HINTS_DECORATIONS   (1L << 1)
-#define MWM_HINTS_INPUT_MODE    (1L << 2)
-#define MWM_HINTS_STATUS        (1L << 3)
 
-#define MWM_FUNC_ALL            (1L << 0)
 #define MWM_FUNC_RESIZE         (1L << 1)
 #define MWM_FUNC_MOVE           (1L << 2)
 #define MWM_FUNC_MINIMIZE       (1L << 3)
@@ -111,20 +108,6 @@
 #define MWM_FUNC_CLOSE          (1L << 5)
 
 #define MWM_DECOR_ALL           (1L << 0)
-#define MWM_DECOR_BORDER        (1L << 1)
-#define MWM_DECOR_RESIZEH       (1L << 2)
-#define MWM_DECOR_TITLE         (1L << 3)
-#define MWM_DECOR_MENU          (1L << 4)
-#define MWM_DECOR_MINIMIZE      (1L << 5)
-#define MWM_DECOR_MAXIMIZE      (1L << 6)
-
-#define MWM_INPUT_MODELESS 0
-#define MWM_INPUT_PRIMARY_APPLICATION_MODAL 1
-#define MWM_INPUT_SYSTEM_MODAL 2
-#define MWM_INPUT_FULL_APPLICATION_MODAL 3
-#define MWM_INPUT_APPLICATION_MODAL MWM_INPUT_PRIMARY_APPLICATION_MODAL
-
-#define MWM_TEAROFF_WINDOW      (1L << 0)
 
 typedef struct
 {
@@ -238,7 +221,6 @@ static int x11_errorhandler(Display *display, XErrorEvent *event)
     mp_verbose(log, "Error code: %x, request code: %x, minor code: %x\n",
                event->error_code, event->request_code, event->minor_code);
 
-//    abort();
     return 0;
 }
 
