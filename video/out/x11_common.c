d04ce8a7c230c8deb81fdf2a0c17d70c499ba77e
wm4 <wm4@nowhere>
2014-05-26 21:54:00 +0200

    x11: fix restoring position when leaving fullscreen
    Accidentally broken in commit 7163bf7d by inverting the condition.

--- video/out/x11_common.c
+++ video/out/x11_common.c
@@ -1412,7 +1412,8 @@ static void vo_x11_fullscreen(struct vo *vo)
 
     if (x11->wm_type & vo_wm_FULLSCREEN) {
         x11_set_ewmh_state(x11, "_NET_WM_STATE_FULLSCREEN", x11->fs);
-        if (x11->fs && (x11->pos_changed_during_fs || x11->size_changed_during_fs))
+        if (!x11->fs && (x11->pos_changed_during_fs ||
+                         x11->size_changed_during_fs))
         {
             vo_x11_move_resize(vo, x11->pos_changed_during_fs,
                                    x11->size_changed_during_fs,
