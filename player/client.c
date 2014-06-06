4279712d1ed22faa7df0bcae281976590744d711
wm4 <wm4@nowhere>
2014-06-06 17:26:01 +0200

    client API: don't use the mpv config files by default
    This was always intended this way, and even documented in client.h. Due
    to an oversight it was never actually implemented.
    
    The intention is that mpv embedded in applications and "real mpv" don't
    conflict. An API user can undo this by setting the "config" option to
    "yes", if using the user's mpv config is desired.

--- player/client.c
+++ player/client.c
@@ -302,6 +302,7 @@ mpv_handle *mpv_create(void)
     mpv_handle *ctx = mp_new_client(mpctx->clients, "main");
     if (ctx) {
         // Set some defaults.
+        mpv_set_option_string(ctx, "config", "no");
         mpv_set_option_string(ctx, "idle", "yes");
         mpv_set_option_string(ctx, "terminal", "no");
         mpv_set_option_string(ctx, "osc", "no");
