0ed48f5ec939c50ebd843a17197a7169db33c25b
wm4 <wm4@nowhere>
2014-08-11 17:01:20 +0200

    player: remove cache status from video OSD

--- player/osd.c
+++ player/osd.c
@@ -451,9 +451,6 @@ static void sadd_osd_status(char **buffer, struct MPContext *mpctx, bool full)
             saddf(buffer, " / ");
             sadd_hhmmssff(buffer, get_time_length(mpctx), fractions);
             sadd_percentage(buffer, get_percent_pos(mpctx));
-            float cache = mp_get_cache_percent(mpctx);
-            if (cache >= 0)
-                saddf(buffer, " Cache: %.2f%%", cache);
         }
     }
 }
