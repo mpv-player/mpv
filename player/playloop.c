mpv-player/mpv

commit c269fa1609c1bcdfb5be36ded8d963627f6ed12a

player: fix quit handling when seeking past EOF while paused
The player didn't quit when seeking past EOF in audio-only mode while
paused. The only case when we don't want to quit is when the last video
frame is displayed while paused.

This logic was probably broken a while ago, but I'm not exactly sure.

CC: @mpv-player/stable

Conflicts:
	player/playloop.c

player/playloop.c
@@ -899,12 +899,11 @@ void run_playloop(struct MPContext *mpctx)
     update_osd_msg(mpctx);
     update_subtitles(mpctx);
 
-    /* If we're paused, don't end playback yet. But if video is enabled, is EOF,
-     * and we don't have a video frame, then the user probably seeked outside
-     * of the video, and we want to quit. */
-    bool prevent_eof = mpctx->paused;
-    if (mpctx->d_video && mpctx->video_status == STATUS_EOF)
-        prevent_eof &= mpctx->video_out && mpctx->video_out->hasframe;
+    /* Don't quit while paused and we're displaying the last video frame. On the
+     * other hand, if we don't have a video frame, then the user probably seeked
+     * outside of the video, and we do want to quit. */
+    bool prevent_eof =
+        mpctx->paused && mpctx->video_out && vo_has_frame(mpctx->video_out);
     /* Handles terminating on end of playback (or switching to next segment).
      *
      * It's possible for the user to simultaneously switch both audio

commit 41f2ad33e5e7741935f6ac9cefb8b117fbee1467

player: fix build
This fixes a build failure introduced by c269fa1.

player/playloop.c
@@ -903,7 +903,7 @@ void run_playloop(struct MPContext *mpctx)
      * other hand, if we don't have a video frame, then the user probably seeked
      * outside of the video, and we do want to quit. */
     bool prevent_eof =
-        mpctx->paused && mpctx->video_out && vo_has_frame(mpctx->video_out);
+        mpctx->paused && mpctx->video_out && mpctx->video_out->hasframe;
     /* Handles terminating on end of playback (or switching to next segment).
      *
      * It's possible for the user to simultaneously switch both audio