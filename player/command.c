cc5ae90c40cf705911f3cf8d1d84c02a01f544d7
Otto Modinos <ottomodinos@gmail.com>
2014-09-12 04:55:58 +0300

    Refactor a bit

--- player/command.c
+++ player/command.c
@@ -2492,11 +2492,16 @@ static int mp_property_playlist_pos(void *ctx, struct m_property *prop,
         return M_PROPERTY_UNAVAILABLE;
 
     switch (action) {
-    case M_PROPERTY_GET: {
+    case M_PROPERTY_GET
+    case M_PROPERTY_PRINT: {
         int pos = playlist_entry_to_index(pl, pl->current);
         if (pos < 0)
             return M_PROPERTY_UNAVAILABLE;
-        *(int *)arg = pos;
+
+        if (action == M_PROPERTY_GET)
+            *(int *)arg = pos;
+        else
+            *(char **)arg = talloc_asprintf(NULL, "%d", pos + 1);
         return M_PROPERTY_OK;
     }
     case M_PROPERTY_SET: {
@@ -2514,13 +2519,6 @@ static int mp_property_playlist_pos(void *ctx, struct m_property *prop,
             .max = playlist_entry_count(pl) - 1,
         };
         *(struct m_option *)arg = opt;
-        return M_PROPERTY_OK;
-    }
-    case M_PROPERTY_PRINT: {
-        int pos = playlist_entry_to_index(pl, pl->current);
-        if (pos < 0)
-            return M_PROPERTY_UNAVAILABLE;
-        *(char **)arg = talloc_asprintf(NULL, "%d", pos + 1);
         return M_PROPERTY_OK;
     }
     }
