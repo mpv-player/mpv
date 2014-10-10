mpv-player/mpv

commit e1e8b07cfa04584000f7d203e5a8b71cb7b3f0a8

player: reset some VO state when video is terminated
Useful for idle mode or if video is switched off during playback, and
--force-window is used.

player/playloop.c
@@ -810,6 +810,9 @@ void handle_force_window(struct MPContext *mpctx, bool reconfig)
             .d_w = w, .d_h = h,
         };
         vo_reconfig(vo, &p, 0);
+        vo_control(vo, VOCTRL_SET_CURSOR_VISIBILITY, &(bool){true});
+        vo_control(vo, VOCTRL_RESTORE_SCREENSAVER, NULL);
+        vo_set_paused(vo, true);
         vo_redraw(vo);
         mp_notify(mpctx, MPV_EVENT_VIDEO_RECONFIG, NULL);
     }

commit 3fc9f9d1324468d09c915a0ac5b74e0dcdb5b8d7

player: don't mess up cursor visibility state
Manually setting can break things forever, because it puts the VO cursor
state out of sync with the remembered state by handle_cursor_autohide().
Use the normal autohide code during idle mode too instead. (Originally
the idea was to make the cursor always visible in idle mode, but not so
important.)

Regression since e1e8b07c. Fixes #1166.

CC: @mpv-player/stable

player/playloop.c
@@ -838,7 +838,6 @@ void handle_force_window(struct MPContext *mpctx, bool reconfig)
             .d_w = w, .d_h = h,
         };
         vo_reconfig(vo, &p, 0);
-        vo_control(vo, VOCTRL_SET_CURSOR_VISIBILITY, &(bool){true});
         vo_control(vo, VOCTRL_RESTORE_SCREENSAVER, NULL);
         vo_set_paused(vo, true);
         vo_redraw(vo);
@@ -991,6 +990,7 @@ void mp_idle(struct MPContext *mpctx)
     mp_wait_events(mpctx, mpctx->sleeptime);
     mpctx->sleeptime = 100.0;
     mp_process_input(mpctx);
+    handle_cursor_autohide(mpctx);
     update_osd_msg(mpctx);
     handle_osd_redraw(mpctx);
 }