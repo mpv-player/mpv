mpv-player/mpv

commit 33b57f55573e658b3af6c6e8ff3188c8f959e82e

player: improve audio time display
This commit fixes a "cosmetic" user interface issue. Instead of
displaying the interpolated seek time on OSD, show the actual audio
time.

This is rather silly: when seeking in audio-only mode, it takes some
iterations until audio is "ready", but on the other hand, the audio
state machine is rather fickle, and fixing this cosmetic issue would be
intrusive. So just add a hack that paints over the ugly behavior as
perceived by the user. Probably the lesser evil.

It doesn't happen if video is enabled, because that mode sets the
current time immediately to video PTS. (Audio has to be synced to video,
so the code is a bit more complex.)

Fixes #1233.

player/audio.c
@@ -429,7 +429,7 @@ static bool get_sync_samples(struct MPContext *mpctx, int *skip)
     return true;
 }
 
-void fill_audio_out_buffers(struct MPContext *mpctx, double endpts)
+static void do_fill_audio_out_buffers(struct MPContext *mpctx, double endpts)
 {
     struct MPOpts *opts = mpctx->opts;
     struct dec_audio *d_audio = mpctx->d_audio;
@@ -587,6 +587,15 @@ void fill_audio_out_buffers(struct MPContext *mpctx, double endpts)
         if (ao_eof_reached(mpctx->ao) || opts->gapless_audio)
             mpctx->audio_status = STATUS_EOF;
     }
+}
+
+void fill_audio_out_buffers(struct MPContext *mpctx, double endpts)
+{
+    do_fill_audio_out_buffers(mpctx, endpts);
+    // Run audio playback state machine again to display the actual audio PTS
+    // as current time on OSD in audio-only mode in most situations.
+    if (mpctx->audio_status == STATUS_SYNCING)
+        do_fill_audio_out_buffers(mpctx, endpts);
 }
 
 // Drop data queued for output, or which the AO is currently outputting.

commit 0ed77ca7e2445dd13a4ddfb9523774bbeecc7a81

audio: fix busy loop when seeking while paused
When playing paused, the amount of decoded audio is limited to a small
amount (1 sample), because we don't write any audio to the AO when
paused. The small amount could trigger the case of the wanted audio
being too far in the future in the PTS sync code, which set the audio
status to STATUS_DRAINING, which in turn triggered the EOF code in the
next iteration. This was ok, but unfortunately, this triggered another
retry in order to check resuming from EOF by setting the status to
STATUS_SYNCING, which in turn lead to the busy loop by alternating
between the 2 states. So don't try resyncing while paused.

Since the PTS syncing code also calls ao_reset(), this could cause the
pulseaudio daemon to consume some CPU time as well.

This was caused by commit 33b57f55. Before that, the playloop was merely
run more often, but didn't cause any problems.

Fixes #1288.

player/audio.c
@@ -514,8 +514,10 @@ static void do_fill_audio_out_buffers(struct MPContext *mpctx, double endpts)
     // restart audio properly. This helps with video files where audio starts
     // later. Retrying is needed to get the correct sync PTS.
     if (mpctx->audio_status >= STATUS_DRAINING && status == AD_OK) {
-        mpctx->audio_status = STATUS_SYNCING;
-        mpctx->sleeptime = 0;
+        if (!mpctx->paused) {
+            mpctx->audio_status = STATUS_SYNCING;
+            mpctx->sleeptime = 0;
+        }
         return; // retry on next iteration
     }
 