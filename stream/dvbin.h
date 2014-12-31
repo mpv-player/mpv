7fe5d8c78e32bd7f609d410c3b769060630553fb
Oliver Freyermuth <o.freyermuth@googlemail.com>
2014-12-31 02:20:14 +0100

    dvb: Extend understanding of VDR channel config: stream_id, inversion.
    Now also "stream ID" (for DVB-S2) and "inversion" are understood.
    The parameter-string can also provide information on FEC, rolloff etc. For DVB-S, "auto" which mpv uses by default should be fine, I can also confirm it works. For non-DVB-S cards, it might be useful to also parse this information in case of a vdr-channel list.
    As I have no such hardware and thus would have to do it blindly, I added a FIXME.
    Mostly complete vdr-channels.conf format documentation is at http://www.vdr-wiki.de/wiki/index.php/Channels.conf (german only).

--- stream/dvbin.h
+++ stream/dvbin.h
@@ -64,6 +64,7 @@ typedef struct {
         char                            pol;
         int                             tpid, dpid1, dpid2, progid, ca, pids[DMX_FILTER_SIZE], pids_cnt;
         bool                            is_dvb_s2;
+        int                             stream_id;
         fe_spectral_inversion_t         inv;
         fe_modulation_t                 mod;
         fe_transmit_mode_t              trans;
