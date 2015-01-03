cb9b59858d1fb9b6e670f42082e7880c6bdb080f
wm4 <wm4@nowhere>
2015-01-03 14:49:48 +0100

    TOOLS/lua/observe-all: add a warning
    This is just natural, but it's also not that obvious.

--- TOOLS/lua/observe-all.lua
+++ TOOLS/lua/observe-all.lua
@@ -1,4 +1,8 @@
 -- Test script for property change notification mechanism.
+-- Note that watching/reading some properties can be very expensive, or
+-- require the player to synchronously wait on network (when playing
+-- remote files), so you should in general only watch properties you
+-- are interested in.
 
 local utils = require("mp.utils")
 
