c757a06845dbc5406f722bc129d153dc7620084f
wm4 <wm4@nowhere>
2015-01-14 22:16:36 +0100

    ao_alsa: fix a small memory leak

--- audio/out/ao_alsa.c
+++ audio/out/ao_alsa.c
@@ -555,6 +555,8 @@ static int init_device(struct ao *ao)
         } else {
             CHECK_ALSA_WARN("Channel map setup failed");
         }
+
+        free(alsa_chmap);
     }
 
     snd_pcm_chmap_t *alsa_chmap = snd_pcm_get_chmap(p->alsa);
