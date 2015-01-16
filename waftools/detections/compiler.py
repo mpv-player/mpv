e93c62520ec86b8d99c9fd61885ca5c98887e909
James Ross-Gowan <rossymiles@gmail.com>
2015-01-16 16:20:42 +1100

    win32: make sure __STRICT_ANSI__ is not defined
    __STRICT_ANSI__ disables functions and definitions that aren't in ANSI
    C. Unfortunately this includes j1(), which is used by the new
    ewa_lanczos code. Cygwin's CFLAGS already unset __STRICT_ANSI__, but it
    should be unset for both Cygwin and MinGW.

--- waftools/detections/compiler.py
+++ waftools/detections/compiler.py
@@ -48,7 +48,8 @@ def __add_clang_flags__(ctx):
                        "-Wno-tautological-constant-out-of-range-compare" ]
 
 def __add_mswin_flags__(ctx):
-    ctx.env.CFLAGS += ['-D_WIN32_WINNT=0x600', '-DUNICODE', '-DCOBJMACROS']
+    ctx.env.CFLAGS += ['-D_WIN32_WINNT=0x600', '-DUNICODE', '-DCOBJMACROS',
+                       '-U__STRICT_ANSI__']
 
 def __add_mingw_flags__(ctx):
     __add_mswin_flags__(ctx)
@@ -58,7 +59,6 @@ def __add_mingw_flags__(ctx):
 def __add_cygwin_flags__(ctx):
     __add_mswin_flags__(ctx)
     ctx.env.CFLAGS += ['-mwin32']
-    ctx.env.CFLAGS += ['-U__STRICT_ANSI__']
 
 __compiler_map__ = {
     '__GNUC__':  __add_gcc_flags__,
