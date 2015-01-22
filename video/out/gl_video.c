9e6091a5b11e7218a84d23bfa9847ccbb265d53d
wm4 <wm4@nowhere>
2015-01-22 19:25:16 +0100

    vo_opengl: guarantee correct reinitialization on setting options
    At least the scale_sep_fbo could have been uninitialized or initialized
    incorrectly when switching between scalers (e.g. from bilinear to
    lanczos). Calling check_resize() should take care of this.

--- video/out/gl_video.c
+++ video/out/gl_video.c
@@ -2483,6 +2483,7 @@ void gl_video_set_options(struct gl_video *p, struct gl_video_opts *opts)
 
     check_gl_features(p);
     reinit_rendering(p);
+    check_resize(p);
 }
 
 void gl_video_get_colorspace(struct gl_video *p, struct mp_image_params *params)
