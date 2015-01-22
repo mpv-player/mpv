86f4fcf1e2b7dc6a4aba343465a5bf153d37c7f9
Vítor Galvão <info@vitorgalvao.com>
2015-01-22 21:26:14 +0000

    youtube-dl_mpv.sh: use curly braces and double quotes consistently

--- TOOLS/youtube-dl_mpv.sh
+++ TOOLS/youtube-dl_mpv.sh
@@ -6,17 +6,17 @@ cookies_dir="$(mktemp -d /tmp/youtube-dl_mpv.XXXX)"
 cookies_file="${cookies_dir}/cookies"
 user_agent="$(youtube-dl --dump-user-agent)" # or set whatever you want
 
-video_url="$(youtube-dl \
-    --user-agent="$user_agent" \
-    --cookies="$cookies_file" \
+video_url=$(youtube-dl \
+    --user-agent="${user_agent}" \
+    --cookies="${cookies_file}" \
     --get-url \
-    "$@")"
+    "$@")
 
 set -f
 ${MPV:-mpv} \
     --cookies \
-    --cookies-file="$cookies_file" \
-    --user-agent="$user_agent" \
-    -- $video_url
+    --cookies-file="${cookies_file}" \
+    --user-agent="${user_agent}" \
+    -- "${video_url}"
 
-rm -rf "$cookies_dir"
+rm -rf "${cookies_dir}"
