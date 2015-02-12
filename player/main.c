440ba3363966b6fa4394ca79074b5ba8a0f3a5a6
wm4 <wm4@nowhere>
2015-02-13 00:18:54 +0100

    encoding: fixed-vo option was removed
    (cherry picked from commit 96547a810e87d06121704ed67e3cf44430752c67)

--- player/main.c
+++ player/main.c
@@ -401,7 +401,6 @@ int mp_initialize(struct MPContext *mpctx)
         }
         m_config_set_option0(mpctx->mconfig, "vo", "lavc");
         m_config_set_option0(mpctx->mconfig, "ao", "lavc");
-        m_config_set_option0(mpctx->mconfig, "fixed-vo", "yes");
         m_config_set_option0(mpctx->mconfig, "keep-open", "no");
         m_config_set_option0(mpctx->mconfig, "force-window", "no");
         m_config_set_option0(mpctx->mconfig, "gapless-audio", "yes");
