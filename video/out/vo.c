c7790b4526aaef08cd76f135dc2ea2dd62fab045
wm4 <wm4@nowhere>
2015-03-10 17:17:15 +0100

    vo: hide opengl-cb in --vo=help
    Avoids stupid questions.

--- video/out/vo.c
+++ video/out/vo.c
@@ -166,7 +166,7 @@ static bool get_desc(struct m_obj_desc *dst, int index)
         .priv_size = vo->priv_size,
         .priv_defaults = vo->priv_defaults,
         .options = vo->options,
-        .hidden = vo->encode,
+        .hidden = vo->encode || !strcmp(vo->name, "opengl-cb"),
         .p = vo,
     };
     return true;
