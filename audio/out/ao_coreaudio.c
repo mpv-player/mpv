ab3a64ee4c485bf45bcf187c7c562c308ad2d990
wm4 <wm4@nowhere>
2015-04-01 01:10:23 +0200

    ao_coreaudio: do not signal per-app volume
    CoreAudio doesn't seem to have this concept. The volume is reset the
    next time audio is opened.

--- audio/out/ao_coreaudio.c
+++ audio/out/ao_coreaudio.c
@@ -118,8 +118,6 @@ static int control(struct ao *ao, enum aocontrol cmd, void *arg)
         return set_volume(ao, arg);
     case AOCONTROL_HAS_SOFT_VOLUME:
         return CONTROL_TRUE;
-    case AOCONTROL_HAS_PER_APP_VOLUME:
-        return CONTROL_TRUE;
     }
     return CONTROL_UNKNOWN;
 }
