ad9bce2a5ca62f6a64f65fe79ae170edc0e05da4
wm4 <wm4@nowhere>
2015-05-08 14:24:20 +0200

    ao_alsa: log requested numbers of channels if ALSA rejects them

--- audio/out/ao_alsa.c
+++ audio/out/ao_alsa.c
@@ -501,11 +501,12 @@ static int init_device(struct ao *ao, bool second_try)
     }
 
     if (num_channels != ao->channels.num) {
+        int req = ao->channels.num;
         mp_chmap_from_channels_alsa(&ao->channels, num_channels);
         if (!mp_chmap_is_valid(&ao->channels))
             mp_chmap_from_channels(&ao->channels, 2);
-        MP_ERR(ao, "Couldn't get requested number of channels, fallback to %s.\n",
-               mp_chmap_to_str(&ao->channels));
+        MP_ERR(ao, "Couldn't get requested number of channels (%d), fallback "
+               "to %s.\n", req, mp_chmap_to_str(&ao->channels));
     }
 
     // Some ALSA drivers have broken delay reporting, so disable the ALSA
