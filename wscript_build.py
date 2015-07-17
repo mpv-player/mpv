mpv-player/mpv

commit 4730e0aabab1cdb1e08cb0473f4236394d064149

build: make mpv.rc depend on version.h

wscript_build.py
@@ -418,6 +418,10 @@ def build(ctx):
                 ctx.path.find_node('osdep/mpv.rc'),
                 ctx.path.find_node(node))
 
+        ctx.add_manual_dependency(
+            ctx.path.find_node('osdep/mpv.rc'),
+            ctx.bldnode.find_node('version.h'))
+
     if ctx.dependency_satisfied('cplayer') or ctx.dependency_satisfied('test'):
         ctx(
             target       = "objects",

commit 7ba403002fb6eab9a710c7170c1927380e2df0f5

build: fix windows compilation after clean
broken since 4730e0aabab

wscript_build.py
@@ -418,9 +418,11 @@ def build(ctx):
                 ctx.path.find_node('osdep/mpv.rc'),
                 ctx.path.find_node(node))
 
-        ctx.add_manual_dependency(
-            ctx.path.find_node('osdep/mpv.rc'),
-            ctx.bldnode.find_node('version.h'))
+        version = ctx.bldnode.find_node('version.h')
+        if version:
+            ctx.add_manual_dependency(
+                ctx.path.find_node('osdep/mpv.rc'),
+                version)
 
     if ctx.dependency_satisfied('cplayer') or ctx.dependency_satisfied('test'):
         ctx(