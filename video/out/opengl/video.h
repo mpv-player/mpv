7706bdc150d4e9c880e525e30e83d2ea4d636b71
Bin Jin <bjin1990@gmail.com>
2015-10-24 14:28:40 +0000

    vo_opengl: remove source shader leftover
    The source shader was removed after deband was introduced.

--- video/out/opengl/video.h
+++ video/out/opengl/video.h
@@ -92,7 +92,6 @@ struct gl_video_opts {
     struct m_color background;
     int interpolation;
     int blend_subs;
-    char *source_shader;
     char *scale_shader;
     char **pre_shaders;
     char **post_shaders;
