03013e0fd73cde9b0dc7f2d65387491000ee894b
rr- <mkurczew@gmail.com>
2015-11-07 23:22:38 +0100

    vo_drm: relicense to LGPL
    Also removed authorship information (as per convention seen in other
    files)

--- video/out/drm_common.c
+++ video/out/drm_common.c
@@ -1,8 +1,6 @@
 /*
  * This file is part of mpv.
  *
- * by rr- <rr-@sakuya.pl>
- *
  * mpv is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -15,6 +13,11 @@
  *
  * You should have received a copy of the GNU General Public License along
  * with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can alternatively redistribute this file and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  */
 
 #include <errno.h>
