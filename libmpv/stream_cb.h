f3ade10492e18e57a08049f2e57b38d3704bcbe4
Aman Gupta <aman@tmm1.net>
2016-01-01 15:16:44 -0800

    stream_cb: split up callbacks and document each one

--- libmpv/stream_cb.h
+++ libmpv/stream_cb.h
@@ -38,17 +38,19 @@ extern "C" {
  *
  * This API can be used to make mpv read from a stream with a custom
  * implementation. This interface is inspired by funopen on BSD and
- * fopencookie on linux. The stream is backed by five callbacks: read,
- * write, seek, size and close.
+ * fopencookie on linux. The stream is backed by user-defined callbacks
+ * which can implement customized open, read, seek, size and close behaviors.
  *
  * Usage
  * -----
  *
- * Initialize the sub-api with mpv_get_sub_api(), then register your stream
- * callbacks using mpv_stream_cb_set_callbacks()
+ * Create the sub-api with mpv_get_sub_api(), then initialize it with
+ * mpv_stream_cb_init(). Register your stream callbacks with the
+ * mpv_stream_cb_set_*_fn(). Note that your custom callbacks must not
+ * invoke libmpv APIs as that would cause a deadlock.
  *
  * Once registered, you can `loadfile cb://myfile`. Your open_fn will be invoked
- * with the URI and must return an opaque pointer. This pointer will be passed as
+ * with the URI and must return an opaque cookie. This cookie will be passed as
  * the first argument to all the remaining stream callbacks.
  */
 
@@ -60,12 +62,60 @@ extern "C" {
  */
 typedef struct mpv_stream_cb_context mpv_stream_cb_context;
 
-typedef void *(*mpv_stream_cb_open_fn)(void *, char *);
-typedef int64_t (*mpv_stream_cb_read_fn)(void *, char *, uint64_t);
-typedef int64_t (*mpv_stream_cb_write_fn)(void *, char *, uint64_t);
-typedef int64_t (*mpv_stream_cb_seek_fn)(void *, int64_t, int);
-typedef int64_t (*mpv_stream_cb_size_fn)(void *);
-typedef void (*mpv_stream_cb_close_fn)(void *);
+/**
+ * Open callback used to implement a custom stream.
+ *
+ * @param user_data opaque user data provided via mpv_stream_cb_init()
+ * @param uri name of the stream to be opened
+ * @return opaque cookie identifing the newly opened stream
+ * @return NULL if the URI cannot be opened.
+ */
+typedef void *(*mpv_stream_cb_open_fn)(void *user_data, char *uri);
+
+/**
+ * Read callback used to implement a custom stream.
+ * The semantics of the callback match read(2).
+ *
+ * @param cookie opaque cookie identifying the stream,
+ *               returned from mpv_stream_cb_open_fn
+ * @param buf buffer to read data into
+ * @param size of the buffer
+ * @return number of bytes read into the buffer
+ * @return 0 on EOF
+ * @return -1 on error
+ */
+typedef int64_t (*mpv_stream_cb_read_fn)(void *cookie, char *buf, uint64_t nbytes);
+
+/**
+ * Seek callback used to implement a custom stream.
+ * The semantics of this callback match lseek(2).
+ *
+ * @param cookie opaque cookie identifying the stream,
+ *               returned from mpv_stream_cb_open_fn
+ * @param offset number of bytes to increment or decrement from
+                 the current stream position
+ * @param whence one of SEEK_SET, SEEK_CUR or SEEK_END
+ * @return the resulting offset of the stream
+ * @return -1 if the seek is not allowed or failed
+ */
+typedef int64_t (*mpv_stream_cb_seek_fn)(void *cookie, int64_t offset, int whence);
+
+/**
+ * Size callback used to implement a custom stream.
+ *
+ * @param cookie opaque cookie identifying the stream,
+ *               returned from mpv_stream_cb_open_fn
+ * @return the total size in bytes of the stream
+ */
+typedef int64_t (*mpv_stream_cb_size_fn)(void *cookie);
+
+/**
+ * Close callback used to implement a custom stream.
+ *
+ * @param cookie opaque cookie identifying the stream,
+ *               returned from mpv_stream_cb_open_fn
+ */
+typedef void (*mpv_stream_cb_close_fn)(void *cookie);
 
 /**
  * Initialize the stream callback module.
@@ -76,24 +126,36 @@ typedef void (*mpv_stream_cb_close_fn)(void *);
 void mpv_stream_cb_init(mpv_stream_cb_context *ctx, void *user_data);
 
 /**
- * Set the callbacks that are invoked to manipulate the underlying stream.
- * The callbacks specified here should not call into libmpv APIs as that
- * would cause a deadlock.
- *
- * @param open_fn callback invoked to open a stream given a URI
- * @param read_fn callback invoked to read from the stream
- * @param write_fn callback invoked to write from the stream
- * @param seek_fn callback invoked to seek the stream
- * @param size_fn callback invoked to get the size of a stream
- * @param close_fn callback invoked to close the stream
+ * Set the open callback for the custom stream.
  */
-void mpv_stream_cb_set_callbacks(mpv_stream_cb_context *ctx,
-                                 mpv_stream_cb_open_fn open_fn,
-                                 mpv_stream_cb_read_fn read_fn,
-                                 mpv_stream_cb_write_fn write_fn,
-                                 mpv_stream_cb_seek_fn seek_fn,
-                                 mpv_stream_cb_size_fn size_fn,
-                                 mpv_stream_cb_close_fn close_fn);
+void mpv_stream_cb_set_open_fn(mpv_stream_cb_context *ctx,
+                               mpv_stream_cb_open_fn fn);
+
+/**
+ * Set the read callback for the custom stream.
+ */
+void mpv_stream_cb_set_read_fn(mpv_stream_cb_context *ctx,
+                               mpv_stream_cb_read_fn fn);
+
+/**
+ * Set the seek callback for the custom stream.
+ * This callback is optional, and can be omitted if the underlying
+ * stream is not seekable.
+ */
+void mpv_stream_cb_set_seek_fn(mpv_stream_cb_context *ctx,
+                               mpv_stream_cb_seek_fn fn);
+
+/* Set the size callback for the custom stream.
+ * This callback is optional, and can be omitted if the underlying
+ * stream does not have a known size.
+ */
+void mpv_stream_cb_set_size_fn(mpv_stream_cb_context *ctx,
+                               mpv_stream_cb_size_fn fn);
+
+/* Set the close callback fro the custom stream.
+ */
+void mpv_stream_cb_set_close_fn(mpv_stream_cb_context *ctx,
+                                mpv_stream_cb_close_fn fn);
 
 #ifdef __cplusplus
 }
--- stream/stream_cb.c
+++ stream/stream_cb.c
@@ -21,7 +21,6 @@ struct mpv_stream_cb_context {
     void *user_data;
     mpv_stream_cb_open_fn open_fn;
     mpv_stream_cb_read_fn read_fn;
-    mpv_stream_cb_write_fn write_fn;
     mpv_stream_cb_seek_fn seek_fn;
     mpv_stream_cb_size_fn size_fn;
     mpv_stream_cb_close_fn close_fn;
@@ -39,20 +38,34 @@ void mpv_stream_cb_init(mpv_stream_cb_context *ctx, void *user_data)
     ctx->user_data = user_data;
 }
 
-void mpv_stream_cb_set_callbacks(mpv_stream_cb_context *ctx,
-                                 mpv_stream_cb_open_fn open_fn,
-                                 mpv_stream_cb_read_fn read_fn,
-                                 mpv_stream_cb_write_fn write_fn,
-                                 mpv_stream_cb_seek_fn seek_fn,
-                                 mpv_stream_cb_size_fn size_fn,
-                                 mpv_stream_cb_close_fn close_fn)
+void mpv_stream_cb_set_open_fn(mpv_stream_cb_context *ctx,
+                               mpv_stream_cb_open_fn fn)
 {
-    ctx->open_fn = open_fn;
-    ctx->read_fn = read_fn;
-    ctx->write_fn = write_fn;
-    ctx->seek_fn = seek_fn;
-    ctx->size_fn = size_fn;
-    ctx->close_fn = close_fn;
+    ctx->open_fn = fn;
+}
+
+void mpv_stream_cb_set_read_fn(mpv_stream_cb_context *ctx,
+                               mpv_stream_cb_read_fn fn)
+{
+    ctx->read_fn = fn;
+}
+
+void mpv_stream_cb_set_seek_fn(mpv_stream_cb_context *ctx,
+                               mpv_stream_cb_seek_fn fn)
+{
+    ctx->seek_fn = fn;
+}
+
+void mpv_stream_cb_set_size_fn(mpv_stream_cb_context *ctx,
+                               mpv_stream_cb_size_fn fn)
+{
+    ctx->size_fn = fn;
+}
+
+void mpv_stream_cb_set_close_fn(mpv_stream_cb_context *ctx,
+                                mpv_stream_cb_close_fn fn)
+{
+    ctx->close_fn = fn;
 }
 
 struct priv {
@@ -66,12 +79,6 @@ static int fill_buffer(stream_t *s, char *buffer, int max_len)
     return (int)p->stream_cb_ctx->read_fn(p->cookie, buffer, (size_t)max_len);
 }
 
-static int write_buffer(stream_t *s, char *buffer, int len)
-{
-    struct priv *p = s->priv;
-    return (int)p->stream_cb_ctx->write_fn(p->cookie, buffer, len);
-}
-
 static int seek(stream_t *s, int64_t newpos)
 {
     struct priv *p = s->priv;
@@ -83,6 +90,8 @@ static int control(stream_t *s, int cmd, void *arg)
     struct priv *p = s->priv;
     switch (cmd) {
     case STREAM_CTRL_GET_SIZE: {
+        if (!p->stream_cb_ctx->size_fn)
+            break;
         off_t size = p->stream_cb_ctx->size_fn(p->cookie);
         if (size != (off_t)-1) {
             *(int64_t *)arg = size;
@@ -114,6 +123,8 @@ static int open_cb(stream_t *stream)
         }
 
         p->cookie = p->stream_cb_ctx->open_fn(p->stream_cb_ctx->user_data, stream->url);
+        if (!p->cookie)
+            return STREAM_ERROR;
     } else {
         return STREAM_ERROR;
     }
@@ -124,7 +135,6 @@ static int open_cb(stream_t *stream)
     }
     stream->fast_skip = true;
     stream->fill_buffer = fill_buffer;
-    stream->write_buffer = write_buffer;
     stream->control = control;
     stream->read_chunk = 64 * 1024;
     stream->close = s_close;
@@ -136,5 +146,4 @@ const stream_info_t stream_info_cb = {
     .name = "stream_callback",
     .open = open_cb,
     .protocols = (const char*const[]){ "cb", NULL },
-    .can_write = 1
 };
