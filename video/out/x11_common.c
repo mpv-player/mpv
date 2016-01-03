fa315705be4e2850895d19aee6f16d1a1817ad08
wm4 <wm4@nowhere>
2016-01-03 15:50:00 +0100

    x11: silence xdg-screensaver failure message

--- video/out/x11_common.c
+++ video/out/x11_common.c
@@ -497,8 +497,8 @@ static void *screensaver_thread(void *arg)
         char *args[] = {"xdg-screensaver", "reset", NULL};
         int status = mp_subprocess(args, NULL, NULL, NULL, NULL, &(char*){0});
         if (status) {
-            MP_WARN(x11, "Disabling screensaver failed (%d). Make sure the "
-                    "xdg-screensaver script is installed.\n", status);
+            MP_VERBOSE(x11, "Disabling screensaver failed (%d). Make sure the "
+                            "xdg-screensaver script is installed.\n", status);
             break;
         }
     }
