cbda46370c45604465eff55b979b02880cd46b25
wm4 <wm4@nowhere>
2016-01-12 23:50:17 +0100

    stream: stream_read_complete() reads from current pos, not 0
    (Well, I hope no caller really relied on this anyway.)

--- stream/stream.c
+++ stream/stream.c
@@ -914,7 +914,7 @@ struct bstr stream_read_complete(struct stream *s, void *talloc_ctx,
     int total_read = 0;
     int padding = 1;
     char *buf = NULL;
-    int64_t size = stream_get_size(s);
+    int64_t size = stream_get_size(s) - stream_tell(s);
     if (size > max_size)
         return (struct bstr){NULL, 0};
     if (size > 0)
