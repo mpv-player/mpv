0d3a67ab2cb9edf51643529b10af9bd8b11b1c78
wm4 <wm4@nowhere>
2016-01-19 18:40:31 +0100

    filter_kernels.h: adjust the license
    Make it consistent with filter_kernels.c.
    
    See #2688.

--- video/out/filter_kernels.h
+++ video/out/filter_kernels.h
@@ -1,18 +1,12 @@
 /*
  * This file is part of mpv.
  *
- * mpv is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This file can be distributed under the 3-clause license ("New BSD License").
  *
- * mpv is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ * You can alternatively redistribute the non-Glumpy parts of this file and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  */
 
 #ifndef MPLAYER_FILTER_KERNELS_H
