mpv-player/mpv

commit fd339e3f53996efd2dae9525990da433d1e1bf89

vd_lavc: avoid calling flush on an unopened AVCodecContext
It can be "dangerous". In particular, the decoder might have failed to
initialize, and is now in a broken state. avcodec_flush_buffers() is not
expected to be called in this state, and could trigger undefined
behavior.

video/decode/vd_lavc.c
@@ -457,6 +457,11 @@ static void init_avctx(struct dec_video *vd, const char *decoder,
 
 error:
     MP_ERR(vd, "Could not open codec.\n");
+    // Free it here to avoid attempting to flush+close.
+    if (ctx->avctx) {
+        av_freep(&ctx->avctx->extradata);
+        av_freep(&ctx->avctx);
+    }
     uninit_avctx(vd);
 }
 
@@ -483,23 +488,21 @@ static void flush_all(struct dec_video *vd)
 static void uninit_avctx(struct dec_video *vd)
 {
     vd_ffmpeg_ctx *ctx = vd->priv;
-    AVCodecContext *avctx = ctx->avctx;
 
     flush_all(vd);
     av_frame_free(&ctx->pic);
 
-    if (avctx) {
-        if (avctx->codec && avcodec_close(avctx) < 0)
+    if (ctx->avctx) {
+        if (avcodec_close(ctx->avctx) < 0)
             MP_ERR(vd, "Could not close codec.\n");
 
-        av_freep(&avctx->extradata);
+        av_freep(&ctx->avctx->extradata);
+        av_freep(&ctx->avctx);
     }
 
     if (ctx->hwdec && ctx->hwdec->uninit)
         ctx->hwdec->uninit(ctx);
     ctx->hwdec = NULL;
-
-    av_freep(&ctx->avctx);
 
     ctx->hwdec_failed = false;
     ctx->hwdec_fail_count = 0;

commit 1f083a8cd7be437aff64905ba8bf2d47a41c6dbe

vd_lavc: fix use after free in some hwdecs
fd339e3f53996efd2dae9525990da433d1e1bf89 introduced a regression that caused
segfault while uniniting dxva2 decoder (and possibly vdpau too). The problem was
that it freed the avctx earlier, before calling the backend-specific uninit
which referenced it. This commit moves freeing the avctx back to where it was.

video/decode/vd_lavc.c
@@ -497,12 +497,13 @@ static void uninit_avctx(struct dec_video *vd)
             MP_ERR(vd, "Could not close codec.\n");
 
         av_freep(&ctx->avctx->extradata);
-        av_freep(&ctx->avctx);
     }
 
     if (ctx->hwdec && ctx->hwdec->uninit)
         ctx->hwdec->uninit(ctx);
     ctx->hwdec = NULL;
+
+    av_freep(&ctx->avctx);
 
     ctx->hwdec_failed = false;
     ctx->hwdec_fail_count = 0;