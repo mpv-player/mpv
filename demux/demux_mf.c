a75f40e0d73d8d3686477aedbb51462c72d693fd
Jan Ekström <jeebjp@gmail.com>
2016-02-09 02:09:21 +0200

    demux_mf: only use glob() if it is available
    The only other place where glob() is used is windows-specific and
    for windows mpv includes its own glob wrapper.

--- demux/demux_mf.c
+++ demux/demux_mf.c
@@ -108,6 +108,7 @@ static mf_t *open_mf_pattern(void *talloc_ctx, struct mp_log *log, char *filenam
 
     char *fname = talloc_size(mf, strlen(filename) + 32);
 
+#if HAVE_GLOB || HAVE_GLOB_WIN32_REPLACEMENT
     if (!strchr(filename, '%')) {
         strcpy(fname, filename);
         if (!strchr(filename, '*'))
@@ -130,6 +131,7 @@ static mf_t *open_mf_pattern(void *talloc_ctx, struct mp_log *log, char *filenam
         globfree(&gg);
         goto exit_mf;
     }
+#endif
 
     mp_info(log, "search expr: %s\n", filename);
 
