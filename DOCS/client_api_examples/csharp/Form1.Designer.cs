463e0363130c6b2616c065d4bc6d953ccc81e9b4
Nikolaj Olsson <nikse.dk@gmail.com>
2016-02-28 12:17:59 +0100

    Added client api c# example

---
+++ DOCS/client_api_examples/csharp/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace mpv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.textBoxVideoSampleFileName = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonPlayPause = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonLoadVideo = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(13, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(530, 317);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonLoad.Location = new System.Drawing.Point(13, 359);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(146, 23);
+            this.buttonLoad.TabIndex = 3;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // textBoxVideoSampleFileName
+            // 
+            this.textBoxVideoSampleFileName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBoxVideoSampleFileName.Location = new System.Drawing.Point(166, 362);
+            this.textBoxVideoSampleFileName.Name = "textBoxVideoSampleFileName";
+            this.textBoxVideoSampleFileName.Size = new System.Drawing.Size(323, 20);
+            this.textBoxVideoSampleFileName.TabIndex = 4;
+            this.textBoxVideoSampleFileName.Text = "sample.mp4";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(560, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(388, 290);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(560, 369);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // buttonPlayPause
+            // 
+            this.buttonPlayPause.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonPlayPause.Location = new System.Drawing.Point(561, 332);
+            this.buttonPlayPause.Name = "buttonPlayPause";
+            this.buttonPlayPause.Size = new System.Drawing.Size(95, 23);
+            this.buttonPlayPause.TabIndex = 7;
+            this.buttonPlayPause.Text = "Play/pause";
+            this.buttonPlayPause.UseVisualStyleBackColor = true;
+            this.buttonPlayPause.Click += new System.EventHandler(this.buttonPlayPause_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonStop.Location = new System.Drawing.Point(665, 332);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(95, 23);
+            this.buttonStop.TabIndex = 8;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonLoadVideo
+            // 
+            this.buttonLoadVideo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonLoadVideo.Location = new System.Drawing.Point(496, 359);
+            this.buttonLoadVideo.Name = "buttonLoadVideo";
+            this.buttonLoadVideo.Size = new System.Drawing.Size(47, 23);
+            this.buttonLoadVideo.TabIndex = 10;
+            this.buttonLoadVideo.Text = "...";
+            this.buttonLoadVideo.UseVisualStyleBackColor = true;
+            this.buttonLoadVideo.Click += new System.EventHandler(this.buttonLoadVideo_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 394);
+            this.Controls.Add(this.buttonLoadVideo);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonPlayPause);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxVideoSampleFileName);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.textBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button buttonLoad;
+        private TextBox textBoxVideoSampleFileName;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Button buttonPlayPause;
+        private Button buttonStop;
+        private Button buttonLoadVideo;
+        private OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ DOCS/client_api_examples/csharp/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace mpv
+{
+    public partial class Form1 : Form
+    {
+        private IntPtr _libMpvDll;
+        private IntPtr _mpvHandle;
+
+        #region Win32 API
+
+        // Win32 API functions for dynamically loading DLLs
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Ansi, BestFitMapping = false)]
+        internal static extern IntPtr LoadLibrary(string dllToLoad);
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Ansi, BestFitMapping = false)]
+        internal static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
+
+        #endregion Win32 API
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate IntPtr MpvCreate();
+        private MpvCreate _mpvCreate;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int MpvInitialize(IntPtr mpvHandle);
+        private MpvInitialize _mpvInitialize;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int MpvCommand(IntPtr mpvHandle, [MarshalAs(UnmanagedType.LPArray)] string[] args);
+        private MpvCommand _mpvCommand;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int MpvTerminateDestroy(IntPtr mpvHandle);
+        private MpvTerminateDestroy _mpvTerminateDestroy;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate IntPtr MpvWaitEvent(IntPtr mpvHandle, double wait);
+        private MpvWaitEvent _mpvWaitEvent;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int MpvSetOption(IntPtr mpvHandle, byte[] name, int format, ref uint data);
+        private MpvSetOption _mpvSetOption;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int MpvSetOptionString(IntPtr mpvHandle, byte[] name, byte[] value);
+        private MpvSetOptionString _mpvSetOptionString;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int MpvGetPropertystring(IntPtr mpvHandle, byte[] name, int format, ref IntPtr data);
+        private MpvGetPropertystring _mpvGetPropertyString;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int MpvGetPropertyDouble(IntPtr mpvHandle, byte[] name, int format, ref double data);
+        private MpvGetPropertyDouble _mpvGetPropertyDouble;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int MpvSetProperty(IntPtr mpvHandle, byte[] name, int format, ref byte[] data);
+        private MpvSetProperty _mpvSetProperty;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private object GetDllType(Type type, string name)
+        {
+            IntPtr address = GetProcAddress(_libMpvDll, name);
+            if (address != IntPtr.Zero)
+                return Marshal.GetDelegateForFunctionPointer(address, type);
+            return null;
+        }
+
+        private void LoadMvpDynamic()
+        {
+            _libMpvDll = LoadLibrary("mpv-1.dll");
+            _mpvCreate = (MpvCreate)GetDllType(typeof(MpvCreate), "mpv_create");
+            _mpvInitialize = (MpvInitialize)GetDllType(typeof(MpvInitialize), "mpv_initialize");
+            _mpvTerminateDestroy = (MpvTerminateDestroy)GetDllType(typeof(MpvTerminateDestroy), "mpv_terminate_destroy");
+            _mpvWaitEvent = (MpvWaitEvent)GetDllType(typeof(MpvWaitEvent), "mpv_wait_event");
+            _mpvCommand = (MpvCommand)GetDllType(typeof(MpvCommand), "mpv_command");
+            _mpvSetOption = (MpvSetOption)GetDllType(typeof(MpvSetOption), "mpv_set_option");
+            _mpvSetOptionString = (MpvSetOptionString)GetDllType(typeof(MpvSetOptionString), "mpv_set_option_string");
+            _mpvGetPropertyString = (MpvGetPropertystring)GetDllType(typeof(MpvGetPropertystring), "mpv_get_property");
+            _mpvGetPropertyDouble = (MpvGetPropertyDouble)GetDllType(typeof(MpvGetPropertyDouble), "mpv_get_property");
+            _mpvSetProperty = (MpvSetProperty)GetDllType(typeof(MpvSetProperty), "mpv_set_property");
+        }
+
+        public void Pause()
+        {
+            if (_mpvHandle == IntPtr.Zero)
+                return;
+
+            int MPV_FORMAT_STRING = 1;
+            var bytes = Encoding.UTF8.GetBytes("yes\0");
+            _mpvSetProperty(_mpvHandle, Encoding.UTF8.GetBytes("pause\0"), MPV_FORMAT_STRING, ref bytes);
+        }
+
+        private void Play()
+        {
+            if (_mpvHandle == IntPtr.Zero)
+                return;
+
+            int MPV_FORMAT_STRING = 1;
+            var bytes = Encoding.UTF8.GetBytes("no\0");
+            _mpvSetProperty(_mpvHandle, Encoding.UTF8.GetBytes("pause\0"), MPV_FORMAT_STRING, ref bytes);
+        }
+
+        public bool IsPaused()
+        {
+            if (_mpvHandle == IntPtr.Zero)
+                return true;
+
+            int mpvFormatString = 1;
+            IntPtr lpBuffer = Marshal.AllocHGlobal(10);
+            _mpvGetPropertyString(_mpvHandle, Encoding.UTF8.GetBytes("pause\0"), mpvFormatString, ref lpBuffer);
+            string str = Marshal.PtrToStringAnsi(lpBuffer);
+            return str == "yes";
+        }
+      
+        public double GetTime()
+        {
+            if (_mpvHandle == IntPtr.Zero)
+                return 0;
+
+            int mpvFormatDouble = 5;
+            double time = 0;
+            _mpvGetPropertyDouble(_mpvHandle, Encoding.UTF8.GetBytes("time-pos\0"), mpvFormatDouble, ref time);
+            return time;
+        }
+
+        public void SetTime(double value)
+        {
+            if (_mpvHandle == IntPtr.Zero)
+                return;
+
+            string[] args = { "seek", value.ToString(CultureInfo.InvariantCulture), "absolute", null };
+            _mpvCommand(_mpvHandle, args);
+        }
+
+        public double GetDuration()
+        {
+            if (_mpvHandle == IntPtr.Zero)
+                return 0;
+
+            int mpvFormatDouble = 5;
+            double d = 0;
+            _mpvGetPropertyDouble(_mpvHandle, Encoding.UTF8.GetBytes("duration\0"), mpvFormatDouble, ref d);
+            return d;
+        }
+
+        public double GetWidth()
+        {
+            if (_mpvHandle == IntPtr.Zero)
+                return 0;
+
+            int mpvFormatDouble = 5;
+            double d = 0;
+            _mpvGetPropertyDouble(_mpvHandle, Encoding.UTF8.GetBytes("width\0"), mpvFormatDouble, ref d);
+            return d;
+        }
+
+        public double GetHeight()
+        {
+            if (_mpvHandle == IntPtr.Zero)
+                return 0;
+
+            int mpvFormatDouble = 5;
+            double d = 0;
+            _mpvGetPropertyDouble(_mpvHandle, Encoding.UTF8.GetBytes("height\0"), mpvFormatDouble, ref d);
+            return d;
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            if (_mpvHandle != IntPtr.Zero)
+                _mpvTerminateDestroy(_mpvHandle);
+
+            LoadMvpDynamic();
+            if (_libMpvDll == IntPtr.Zero)
+                return;
+
+            _mpvHandle = _mpvCreate.Invoke();
+            if (_mpvHandle == IntPtr.Zero)
+                return;
+
+            _mpvInitialize.Invoke(_mpvHandle);
+
+            _mpvSetOptionString(_mpvHandle, Encoding.UTF8.GetBytes("vo\0"), Encoding.UTF8.GetBytes("direct3d\0")); 
+            _mpvSetOptionString(_mpvHandle, Encoding.UTF8.GetBytes("keep-open\0"), Encoding.UTF8.GetBytes("always\0")); 
+
+            int mpvFormatInt64 = 4;
+            uint windowId = (uint)pictureBox1.Handle.ToInt64();
+            _mpvSetOption(_mpvHandle, Encoding.UTF8.GetBytes("wid\0"), mpvFormatInt64, ref windowId);
+
+            string[] args = { "loadfile", textBoxVideoSampleFileName.Text, null };
+            _mpvCommand(_mpvHandle, args);
+
+            textBox1.AppendText("start playing... " + Environment.NewLine);
+            while (true)
+            {
+                label1.Text = String.Format("{0:00.000} / {1:00.000}    {2}x{3}", GetTime(), GetDuration(), GetWidth(), GetHeight());
+                label1.Refresh();
+                Application.DoEvents();
+
+                if (_mpvHandle == IntPtr.Zero)
+                    return;
+
+                var eventId = _mpvWaitEvent(_mpvHandle, 0);
+                var s2 = Convert.ToInt64(Marshal.PtrToStructure(eventId, typeof(int)));
+                if (s2 != 0)
+                    textBox1.AppendText("EventId: " + GetEventName(Convert.ToInt64(s2)) + Environment.NewLine);
+
+                if (s2 == 1)
+                    return; // SHUTDOWN
+            }
+        }
+
+        private object GetEventName(long toInt64)
+        {
+            switch (toInt64)
+            {
+                case 1:
+                    return "MPV_EVENT_SHUTDOWN";
+                case 2:
+                    return "MPV_EVENT_LOG_MESSAGE";
+                case 3:
+                    return "MPV_EVENT_GET_PROPERTY_REPLY";
+                case 4:
+                    return "MPV_EVENT_SET_PROPERTY_REPLY";
+                case 5:
+                    return "MPV_EVENT_COMMAND_REPLY";
+                case 6:
+                    return "MPV_EVENT_START_FILE";
+                case 7:
+                    return "MPV_EVENT_END_FILE";
+                case 8:
+                    return "MPV_EVENT_FILE_LOADED";
+                case 9:
+                    return "MPV_EVENT_TRACKS_CHANGED";
+                case 10:
+                    return "MPV_EVENT_TRACK_SWITCHED";
+                case 11:
+                    return "MPV_EVENT_IDLE";
+                case 12:
+                    return "MPV_EVENT_PAUSE";
+                case 13:
+                    return "MPV_EVENT_UNPAUSE";
+                case 14:
+                    return "MPV_EVENT_TICK";
+                case 15:
+                    return "MPV_EVENT_SCRIPT_INPUT_DISPATCH";
+                case 16:
+                    return "MPV_EVENT_CLIENT_MESSAGE";
+                case 17:
+                    return "MPV_EVENT_VIDEO_RECONFIG";
+                case 18:
+                    return "MPV_EVENT_AUDIO_RECONFIG";
+                case 19:
+                    return "MPV_EVENT_METADATA_UPDATE";
+                case 20:
+                    return "MPV_EVENT_SEEK";
+                case 21:
+                    return "MPV_EVENT_PLAYBACK_RESTART";
+                case 22:
+                    return "MPV_EVENT_PROPERTY_CHANGE";
+                case 23:
+                    return "MPV_EVENT_CHAPTER_CHANGE";
+                case 24:
+                    return "MPV_EVENT_QUEUE_OVERFLOW";
+            }
+            return "Uknwown " + toInt64;
+        }
+
+        private void buttonPlayPause_Click(object sender, EventArgs e)
+        {
+            if (IsPaused())
+                Play();
+            else
+                Pause();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            Pause();
+            SetTime(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetTime(1);
+        }
+
+        private void buttonLoadVideo_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = String.Empty;
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                textBoxVideoSampleFileName.Text = openFileDialog1.FileName;
+                buttonPlay_Click(null, null);
+            }
+        }        
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_mpvHandle != IntPtr.Zero)
+                _mpvTerminateDestroy(_mpvHandle);
+        }
+    }
+}
