3c59f54ec8155ed9e58c996451851640b36c8cd4
wm4 <wm4@nowhere>
2016-03-11 17:22:50 +0100

    build: be less strict about line endings
    This is a shitty hack, but also not terribly offensive.

--- wscript_build.py
+++ wscript_build.py
@@ -486,9 +486,9 @@ def build(ctx):
         if build_shared:
             waftoolsdir = os.path.join(os.path.dirname(__file__), "waftools")
             ctx.load("syms", tooldir=waftoolsdir)
-        vre = '^#define MPV_CLIENT_API_VERSION MPV_MAKE_VERSION\((.*), (.*)\)$'
+        vre = '#define MPV_CLIENT_API_VERSION MPV_MAKE_VERSION\((.*), (.*)\)'
         libmpv_header = ctx.path.find_node("libmpv/client.h").read()
-        major, minor = re.search(vre, libmpv_header, re.M).groups()
+        major, minor = re.search(vre, libmpv_header).groups()
         libversion = major + '.' + minor + '.0'
 
         def _build_libmpv(shared):
