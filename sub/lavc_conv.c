a606b92f4a6fab12e94ad687f65d4d65f52b6d59
wm4 <wm4@nowhere>
2016-04-06 12:36:13 +0200

    sub: interpret "text" subtitles as srt
    Most players will interpret HTML-style tags (aka srt) in almost any kind
    of text subtitles; make mpv do this too.

--- sub/lavc_conv.c
+++ sub/lavc_conv.c
@@ -47,6 +47,9 @@ static const char *get_lavc_format(const char *format)
     // For the hack involving parse_webvtt().
     if (format && strcmp(format, "webvtt-webm") == 0)
         format = "webvtt";
+    // Most text subtitles are srt/html style anyway.
+    if (format && strcmp(format, "text") == 0)
+        format = "subrip";
     return format;
 }
 
