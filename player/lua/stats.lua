bced0288fc47b06fd12de8eb8faac070db982cc7
Julian <MyFakeAcc.4@googlemail.com>
2016-06-07 17:47:12 +0900

    stats: use redraw_delay for timer period
    Previously the delay was slightly shortened but a user explicitly
    setting a specific delay most likely expects the stats to refresh
    in exactly the frequency he desired.

--- player/lua/stats.lua
+++ player/lua/stats.lua
@@ -288,7 +288,7 @@ function b(t)
 end
 
 
-local timer = mp.add_periodic_timer(o.redraw_delay - 0.1, function() print_stats(o.redraw_delay) end)
+local timer = mp.add_periodic_timer(o.redraw_delay, function() print_stats(o.redraw_delay + 1) end)
 timer:kill()
 
 function toggle_stats()
@@ -297,7 +297,7 @@ function toggle_stats()
         mp.osd_message("", 0)
     else
         timer:resume()
-        print_stats(o.redraw_delay)
+        print_stats(o.redraw_delay + 1)
     end
 end
 
