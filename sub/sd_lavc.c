689cf04571109ea12b3cddc61a6eb43ceb768c63
wm4 <wm4@nowhere>
2016-06-18 10:49:41 +0200

    sd_lavc: fix typo

--- sub/sd_lavc.c
+++ sub/sd_lavc.c
@@ -289,7 +289,7 @@ static void read_sub_bitmaps(struct sd *sd, struct sub *sub)
         b->bitmap = sub->data->planes[0] + pos.y * b->stride + pos.x * 4;
 
         sub->src_w = FFMAX(sub->src_w, b->x + b->w);
-        sub->src_h = FFMAX(sub->src_h, b->h + b->h);
+        sub->src_h = FFMAX(sub->src_h, b->y + b->h);
 
         assert(r->nb_colors > 0);
         assert(r->nb_colors <= 256);
