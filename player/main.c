1298c29dc7cb78d2f7375cf7e1667e7bc129cc09
wm4 <wm4@nowhere>
2016-07-02 18:26:55 +0200

    player: add missing space to error message

--- player/main.c
+++ player/main.c
@@ -425,7 +425,7 @@ int mp_initialize(struct MPContext *mpctx, char **options)
         MP_FATAL(mpctx, "\nmpv was compiled against a different version of "
                  "FFmpeg/Libav than the shared\nlibrary it is linked against. "
                  "This is most likely a broken build and could\nresult in "
-                 "misbehavior and crashes.\n\nmpv does not support this"
+                 "misbehavior and crashes.\n\nmpv does not support this "
                  "configuration and will not run - rebuild mpv instead.\n");
         return -1;
     }
