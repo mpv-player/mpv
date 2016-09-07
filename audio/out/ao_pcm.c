5a7b1ff4c0cb176ee94995646d310f6ffd60fe61
wm4 <wm4@nowhere>
2016-09-07 12:54:33 +0200

    ao_pcm: remove some useless messages
    The first one is printed even if the user disabled video (or there's no
    video), so just remove it. The second one uses deprecated sub-option
    syntax, so remove that as well.

--- audio/out/ao_pcm.c
+++ audio/out/ao_pcm.c
@@ -152,8 +152,6 @@ static int init(struct ao *ao)
             priv->outputfilename,
             priv->waveheader ? "WAVE" : "RAW PCM", ao->samplerate,
             ao->channels.num, af_fmt_to_str(ao->format));
-    MP_INFO(ao, "Info: Faster dumping is achieved with --no-video\n");
-    MP_INFO(ao, "Info: To write WAVE files use --ao=pcm:waveheader (default).\n");
 
     priv->fp = fopen(priv->outputfilename, priv->append ? "ab" : "wb");
     if (!priv->fp) {
