2f1eb49e9f130c728c09a3458ff8449bc571c0c0
wm4 <wm4@nowhere>
2016-09-07 13:42:55 +0200

    sub: actually apply text alignment options to non-ASS subtitles
    This is a bug fix, and the text alignment functionality probably got
    lost sometime along the way.
    
    For ASS subtitles, this could have unintended consequences, so it's hard
    to get right - thus it's not applied to ASS subtitles.
    
    For other text subtitles, this should be fine, though. It still works on
    ASS subtitles as promised by the manpage if --no-sub-ass is used.

--- sub/sd_ass.c
+++ sub/sd_ass.c
@@ -329,6 +329,8 @@ static void configure_ass(struct sd *sd, struct mp_osd_res *dim,
         set_force_flags |= ASS_OVERRIDE_BIT_STYLE | ASS_OVERRIDE_BIT_FONT_SIZE;
     if (opts->ass_style_override == 4)
         set_force_flags |= ASS_OVERRIDE_BIT_FONT_SIZE;
+    if (converted)
+        set_force_flags |= ASS_OVERRIDE_BIT_ALIGNMENT;
     ass_set_selective_style_override_enabled(priv, set_force_flags);
     ASS_Style style = {0};
     mp_ass_set_style(&style, 288, opts->sub_text_style);
