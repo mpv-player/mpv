a655b7a25d2fac280d37f4d52cb2e76120842426
Akemi <der.richter@gmx.de>
2016-09-24 04:54:27 +0200

    cocoa: fix fullscreen regression on 10.11 and newer Fixes #3364.

--- video/out/opengl/context_cocoa.c
+++ video/out/opengl/context_cocoa.c
@@ -54,7 +54,6 @@ static CGLError test_gl_version(struct vo *vo,
     CGLPixelFormatAttribute attrs[] = {
         kCGLPFAOpenGLProfile,
         (CGLPixelFormatAttribute) version,
-        kCGLPFADoubleBuffer,
         kCGLPFAAccelerated,
         #if MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_8
         // leave this as the last entry of the array to not break the fallback
