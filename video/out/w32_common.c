544110bf430f34b75d86dd0ea416f8c5e5f9386d
pavelxdd <pavel.otchertsov@gmail.com>
2016-12-12 19:39:00 +0300

    win32: change the log level of 'move window' msg
    It does more harm than good, since it spams terminal a lot

--- video/out/w32_common.c
+++ video/out/w32_common.c
@@ -727,7 +727,7 @@ static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam,
         signal_events(w32, VO_EVENT_WIN_STATE);
 
         update_display_info(w32);  // if we moved between monitors
-        MP_VERBOSE(w32, "move window: %d:%d\n", w32->window_x, w32->window_y);
+        MP_DBG(w32, "move window: %d:%d\n", w32->window_x, w32->window_y);
         break;
     }
     case WM_SIZE: {
