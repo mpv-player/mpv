2a42e58f99ab1f4c6adc9c239ed6e520fa1daeda
Jan Janssen <medhefgo@web.de>
2017-03-17 16:49:28 +0100

    af_drc: remove
    Remove low quality drc filter. Anyone whishing to have dynamic range
    compression should use the much more powerful acompressor ffmpeg filter:
    
        mpv --af=lavfi=[acompressor] INPUT
    
    Or with parameters:
    
        mpv --af=lavfi=[acompressor=threshold=-25dB:ratio=3:makeup=8dB] INPUT
    
    Refer to https://ffmpeg.org/ffmpeg-filters.html#acompressor for a full
    list of supported parameters.

--- audio/filter/af.c
+++ audio/filter/af.c
@@ -36,7 +36,6 @@ extern const struct af_info af_info_format;
 extern const struct af_info af_info_volume;
 extern const struct af_info af_info_equalizer;
 extern const struct af_info af_info_pan;
-extern const struct af_info af_info_drc;
 extern const struct af_info af_info_lavcac3enc;
 extern const struct af_info af_info_lavrresample;
 extern const struct af_info af_info_scaletempo;
@@ -50,7 +49,6 @@ static const struct af_info *const filter_list[] = {
     &af_info_volume,
     &af_info_equalizer,
     &af_info_pan,
-    &af_info_drc,
     &af_info_lavcac3enc,
     &af_info_lavrresample,
 #if HAVE_RUBBERBAND
