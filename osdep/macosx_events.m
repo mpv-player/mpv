mpv-player/mpv

commit 063ca8f0fe48c281944c5307b1e8bbb11bb22822

osx: fix key input in certain circumstances
for a reason i can just assume some key events can vanish from the
event chain and mpv seems unresponsive.

after quite some testing i could confirm that the events are present at
the first entry point of the event chain, the sendEvent method of the
Application, and that they vanish at a point afterwards. now we use
that entry point to grab keyDown and keyUp events. we also stop
propagating those key events to prevent the no key input' error sound.
if we ever need the key events somewhere down the event chain we need
to start propagating them again. though this is not necessary currently.

osdep/macosx_events.m
@@ -55,7 +55,6 @@
 - (NSEvent *)handleKey:(NSEvent *)event;
 - (void)setMpvHandle:(struct mpv_handle *)ctx;
 - (void)readEvents;
-- (void)startEventMonitor;
 - (void)startAppleRemote;
 - (void)stopAppleRemote;
 - (void)startMediaKeys;
@@ -121,11 +120,6 @@ static int convert_key(unsigned key, unsigned charcode)
     if (mpkey)
         return mpkey;
     return charcode;
-}
-
-void cocoa_start_event_monitor(void)
-{
-    [[EventsResponder sharedInstance] startEventMonitor];
 }
 
 void cocoa_init_apple_remote(void)
@@ -347,19 +341,6 @@ void cocoa_set_mpv_handle(struct mpv_handle *ctx)
     }
 }
 
-- (void)startEventMonitor
-{
-    [NSEvent addLocalMonitorForEventsMatchingMask:NSEventMaskKeyDown|NSEventMaskKeyUp
-                                          handler:^(NSEvent *event) {
-        BOOL equivalent = [[NSApp mainMenu] performKeyEquivalent:event];
-        if (equivalent) {
-            return (NSEvent *)nil;
-        } else {
-            return [self handleKey:event];
-        }
-    }];
-}
-
 - (void)startAppleRemote
 {
 
@@ -525,6 +506,17 @@ void cocoa_set_mpv_handle(struct mpv_handle *ctx)
         [self handleMPKey:key withMask:[self keyModifierMask:event]];
 
     return nil;
+}
+
+- (bool)processKeyEvent:(NSEvent *)event
+{
+    if ((event.type == NSEventTypeKeyDown || event.type == NSEventTypeKeyUp) &&
+        ![[NSApp mainMenu] performKeyEquivalent:event])
+    {
+        [self handleKey:event];
+        return true;
+    }
+    return false;
 }
 
 - (void)handleFilesArray:(NSArray *)files

etc/builtin.conf
@@ -24,7 +24,6 @@ stop-playback-on-init-failure=yes
 # OSX/Cocoa global input hooks
 input-appleremote=no
 input-media-keys=no
-input-app-events=no
 
 [encoding]
 vo=lavc

input/input.c
@@ -168,7 +168,6 @@ struct input_opts {
     int use_alt_gr;
     int use_appleremote;
     int use_media_keys;
-    int use_app_events;
     int default_bindings;
     int enable_mouse_movements;
     int vo_key_input;
@@ -193,7 +192,6 @@ const struct m_sub_options input_config = {
 #if HAVE_COCOA
         OPT_FLAG("input-appleremote", use_appleremote, 0),
         OPT_FLAG("input-media-keys", use_media_keys, 0),
-        OPT_FLAG("input-app-events", use_app_events, M_OPT_FIXED),
 #endif
         OPT_FLAG("window-dragging", allow_win_drag, 0),
         OPT_REPLACED("input-x11-keyboard", "input-vo-keyboard"),
@@ -210,7 +208,6 @@ const struct m_sub_options input_config = {
 #if HAVE_COCOA
         .use_appleremote = 1,
         .use_media_keys = 1,
-        .use_app_events = 1,
 #endif
         .default_bindings = 1,
         .vo_key_input = 1,
@@ -1297,11 +1294,6 @@ void mp_input_load_config(struct input_ctx *ictx)
             parse_config_file(ictx, files[n], false);
         talloc_free(tmp);
     }
-
-#if HAVE_COCOA
-    if (ictx->opts->use_app_events)
-        cocoa_start_event_monitor();
-#endif
 
 #if defined(__MINGW32__)
     if (ictx->global->opts->input_file && *ictx->global->opts->input_file)

osdep/macosx_application.m
@@ -81,8 +81,8 @@ static void terminate_cocoa_application(void)
 
 - (void)sendEvent:(NSEvent *)event
 {
-    [super sendEvent:event];
-
+    if (![_eventsResponder processKeyEvent:event])
+        [super sendEvent:event];
     [_eventsResponder wakeup];
 }
 

osdep/macosx_compat.h
@@ -37,8 +37,6 @@ static const NSEventType NSEventTypeSystemDefined = NSSystemDefined;
 static const NSEventType NSEventTypeKeyDown = NSKeyDown;
 static const NSEventType NSEventTypeKeyUp = NSKeyUp;
 
-static const NSEventMask NSEventMaskKeyDown = NSKeyDownMask;
-static const NSEventMask NSEventMaskKeyUp = NSKeyUpMask;
 static const NSEventMask NSEventMaskLeftMouseUp = NSLeftMouseUpMask;
 
 #if (MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_10)

osdep/macosx_events.h
@@ -28,8 +28,6 @@ void cocoa_put_key(int keycode);
 void cocoa_put_key_with_modifiers(int keycode, int modifiers);
 void cocoa_put_key_event(void *event);
 
-void cocoa_start_event_monitor(void);
-
 void cocoa_init_apple_remote(void);
 void cocoa_uninit_apple_remote(void);
 

osdep/macosx_events_objc.h
@@ -42,4 +42,6 @@ struct input_ctx;
 
 - (void)handleFilesArray:(NSArray *)files;
 
+- (bool)processKeyEvent:(NSEvent *)event;
+
 @end

video/out/cocoa/events_view.m
@@ -124,16 +124,6 @@
 
 - (BOOL)resignFirstResponder { return YES; }
 
-- (void)keyDown:(NSEvent *)event
-{
-    [self.adapter putKeyEvent:event];
-}
-
-- (void)keyUp:(NSEvent *)event
-{
-    [self.adapter putKeyEvent:event];
-}
-
 - (BOOL)canHideCursor
 {
     return !self.hasMouseDown && [self containsMouseLocation]

commit 265547eb941bcf48d2c8c9f9e8fbadcec1514a98

osx: fix event propagation of menu bar item key shortcuts
a small logic error in 063ca8f broke the filtering of the menu bar item
key shortcuts. they where called several times per event because the
propagation wasn't stopped properly.

osdep/macosx_events.m
@@ -510,10 +510,9 @@ void cocoa_set_mpv_handle(struct mpv_handle *ctx)
 
 - (bool)processKeyEvent:(NSEvent *)event
 {
-    if ((event.type == NSEventTypeKeyDown || event.type == NSEventTypeKeyUp) &&
-        ![[NSApp mainMenu] performKeyEquivalent:event])
-    {
-        [self handleKey:event];
+    if (event.type == NSEventTypeKeyDown || event.type == NSEventTypeKeyUp){
+        if (![[NSApp mainMenu] performKeyEquivalent:event])
+            [self handleKey:event];
         return true;
     }
     return false;