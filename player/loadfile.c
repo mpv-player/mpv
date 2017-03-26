3eb6607e5e3137e7801c669952f715b626f7afb0
wm4 <wm4@nowhere>
2017-03-26 14:13:36 +0200

    player: make new stream output more consistent with other output
    It seems we generally skip the space, such as seen on the AO init line.

--- player/loadfile.c
+++ player/loadfile.c
@@ -137,12 +137,12 @@ static void print_stream(struct MPContext *mpctx, struct track *t)
         if (s && s->codec->disp_w)
             APPEND(b, " %dx%d", s->codec->disp_w, s->codec->disp_h);
         if (s && s->codec->fps)
-            APPEND(b, " %.3f Hz", s->codec->fps);
+            APPEND(b, " %.3ffps", s->codec->fps);
     } else if (t->type == STREAM_AUDIO) {
         if (s && s->codec->channels.num)
-            APPEND(b, " %d ch", s->codec->channels.num);
+            APPEND(b, " %dch", s->codec->channels.num);
         if (s && s->codec->samplerate)
-            APPEND(b, " %d Hz", s->codec->samplerate);
+            APPEND(b, " %dHz", s->codec->samplerate);
     }
     APPEND(b, ")");
     if (t->is_external)
