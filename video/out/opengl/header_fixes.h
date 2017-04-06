ebecf9c2d6b662cbc2c0076d994d2669a143b1c7
wm4 <wm4@nowhere>
2017-04-06 08:40:17 +0200

    vo_opengl: header_fixes.h: merge IOS GLES block
    It appears we expect IOS to provide GLES 3. The IOS block contains all
    symbols from the GLES block. Weirdly not all, so it's possible that some
    symbols will be redefined, which is annoying, but harmless. I don't have
    an iOS setup to test, otherwise it's likely that a modification of the
    IOS include statements would take care of this.

--- video/out/opengl/header_fixes.h
+++ video/out/opengl/header_fixes.h
@@ -67,7 +67,9 @@
 #define GL_BACK_LEFT                      0x0402
 #endif
 
-#if HAVE_STANDARD_GLES3
+// Desktop symbols we need in addition to standard GLES 3.0 symbols.
+// Might also include GLES 3.1, 3.2, etc. symbols.
+#if HAVE_STANDARD_GLES3 || HAVE_IOS_GL
 #define GL_UNSIGNED_BYTE_3_3_2            0x8032
 #define GL_UNSIGNED_BYTE_2_3_3_REV        0x8362
 #define GL_UNSIGNED_SHORT_1_5_5_5_REV     0x8366
@@ -91,23 +93,6 @@
 #define GL_LUMINANCE16_ALPHA16 0x8048
 #define GL_UNSIGNED_SHORT_8_8_APPLE 0x85BA
 #define GL_UNSIGNED_SHORT_8_8_REV_APPLE 0x85BB
-#endif
-
-#if HAVE_IOS_GL
-#define GL_WRITE_ONLY GL_WRITE_ONLY_OES
-#define GL_TEXTURE_1D 0x0DE0
-#define GL_R16 0x822A
-#define GL_RG16 0x822C
-#define GL_RGB10 0x8052
-#define GL_RGB16 0x8054
-#define GL_RGBA12 0x805A
-#define GL_RGBA16 0x805B
-#define GL_LUMINANCE8 GL_LUMINANCE8_EXT
-#define GL_LUMINANCE8_ALPHA8 GL_LUMINANCE8_ALPHA8_EXT
-#define GL_LUMINANCE16 0x8042
-#define GL_LUMINANCE16_ALPHA16 0x8048
-#define GL_TEXTURE_RED_SIZE 0x805C
-#define GL_TEXTURE_LUMINANCE_SIZE 0x8060
 #endif
 
 // GL_ARB_timer_query and EXT_disjoint_timer_query
