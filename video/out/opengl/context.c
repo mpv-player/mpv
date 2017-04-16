bba08e38ff9842c4edf9da6a1d54066a01acd982
wm4 <wm4@nowhere>
2017-04-16 02:35:12 +0200

    vo_opengl: move X11 backends before Wayland
    Wayland is still too amateurish, and multiple features don't work,
    including critical ones. There is no solution in sight, so prefer X11.
    (Which seems to mostly work ok via xwayland.)
    
    Once all problems are solved, the defaults can be switched back.

--- video/out/opengl/context.c
+++ video/out/opengl/context.c
@@ -65,9 +65,6 @@ static const struct mpgl_driver *const backends[] = {
 #if HAVE_GL_DXINTEROP
     &mpgl_driver_dxinterop,
 #endif
-#if HAVE_GL_WAYLAND
-    &mpgl_driver_wayland,
-#endif
 #if HAVE_GL_X11
     &mpgl_driver_x11_probe,
 #endif
@@ -76,6 +73,9 @@ static const struct mpgl_driver *const backends[] = {
 #endif
 #if HAVE_GL_X11
     &mpgl_driver_x11,
+#endif
+#if HAVE_GL_WAYLAND
+    &mpgl_driver_wayland,
 #endif
 #if HAVE_EGL_DRM
     &mpgl_driver_drm,
