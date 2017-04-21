b78d2e6d094285baa7a0e4e661f91e9a77541539
wm4 <wm4@nowhere>
2017-04-21 12:43:58 +0200

    demux_mkv: mention non-LGPL code

--- demux/demux_mkv.c
+++ demux/demux_mkv.c
@@ -18,6 +18,8 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Parts under HAVE_GPL are licensed under GNU General Public License.
  */
 
 #include <stdlib.h>
