2fd20f638efef80fa5aae4de04df800ddb09a898
wm4 <wm4@nowhere>
2017-05-05 07:15:17 +0200

    av_log: mark as almost LGPL
    All authors agreed. Some of the code originated from commit
    3c49701490aecb by michael, thus the license change has to be delayed.

--- common/av_log.c
+++ common/av_log.c
@@ -17,6 +17,8 @@
  *
  * You should have received a copy of the GNU General Public License along
  * with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Almost LGPL.
  */
 
 #include <stdlib.h>
