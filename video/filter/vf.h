c0ba8b520f4258539f860e13d1b781f15df701cb
wm4 <wm4@nowhere>
2017-05-11 14:44:46 +0200

    vf.c, vf.h: change license to LGPL
    Most authors have agreed. Like with vo.c, we don't take module
    declarations into consideration (see 0e09533c73dc0).
    
    Notable issues:
    
    115bfb9762: the author has (probably) not agreed. The vf.c changes were
    removed with the removal of filter DR, though (see c54fc507da8e).
    
    7b25afd742: the same author adds VOCTRLs for deinterlacing switching at
    runtime. Put them under a HAVE_GPL ifdef just to be sure. (It looks like
    we could remove the VOCTRLs immediately, as they're needed only for some
    compatibility things, but no need to do that yet.)
    
    02b199e5e9: the author had a conditional agreement to LGPL, which
    doesn't allow us to change it just yet, but the code added here was
    completely removed anyway. (These days, the pts is passed as mp_image
    field, and put_image is gone.)
    
    3532cd532e: same author, but code removed with DR removal.
    
    f0626e2f8d: same author, but code was moved to mp_image.c.
    
    e5b4b495c3: agreed to LGPLv3+ only, but the code was removed in
    cfa1f9e082 anyway.
    
    086c324692: author was not asked - minor warning fix, but no mpv
    includes malloc.h anymore.
    
    e9d0a1d609: author was not asked - removed again in 33b62af947.
    
    c260a1139d: author could not be reached - but this code was removed when
    mpv changed the image allocation code to essentially use FFmpeg's
    pixdesc.

--- video/filter/vf.h
+++ video/filter/vf.h
@@ -1,24 +1,28 @@
 /*
  * This file is part of mpv.
  *
- * mpv is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * mpv is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * mpv is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Parts under HAVE_GPL are licensed under GNU General Public License.
  */
 
 #ifndef MPLAYER_VF_H
 #define MPLAYER_VF_H
 
 #include <stdbool.h>
+
+#include "config.h"
 
 #include "video/mp_image.h"
 #include "common/common.h"
@@ -145,8 +149,10 @@ enum vf_ctrl {
     VFCTRL_SEEK_RESET = 1,   // reset on picture and PTS discontinuities
     VFCTRL_SET_EQUALIZER,    // set color options (brightness,contrast etc)
     VFCTRL_GET_EQUALIZER,    // get color options (brightness,contrast etc)
+#if HAVE_GPL
     VFCTRL_SET_DEINTERLACE,  // Set deinterlacing status
     VFCTRL_GET_DEINTERLACE,  // Get deinterlacing status
+#endif
     VFCTRL_GET_METADATA,     // Get frame metadata from lavfi filters (e.g., cropdetect)
     /* Hack to make the OSD state object available to vf_sub which
      * access OSD/subtitle state outside of normal OSD draw time. */
