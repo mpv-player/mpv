44d30c986db68ac8575c1e63a72546ac396f859a
wm4 <wm4@nowhere>
2017-06-20 10:14:48 +0200

    input: mention GPL exceptions in license header
    (Just to make our HAVE_GPL business explicit.)

--- input/cmd_list.c
+++ input/cmd_list.c
@@ -13,6 +13,8 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Parts under HAVE_GPL are licensed under GNU General Public License.
  */
 
 #include <limits.h>
