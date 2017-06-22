0788d7961fd3816b1965a6ab90cd8d9994c774fe
wm4 <wm4@nowhere>
2017-06-22 13:16:33 +0200

    build: reduce GLX test fragment
    While it doesn't matter for anything, we don't really need to check for
    GL symbols (neither headers nor for linking).

--- waftools/fragments/gl_x11.c
+++ waftools/fragments/gl_x11.c
@@ -1,6 +1,5 @@
 #include <X11/Xlib.h>
 #include <GL/glx.h>
-#include <GL/gl.h>
 #include <stddef.h>
 
 int main(int argc, char *argv[]) {
@@ -8,8 +7,5 @@ int main(int argc, char *argv[]) {
   glXQueryExtensionsString(NULL, 0);
   glXGetProcAddressARB("");
   glXGetCurrentDisplay();
-  glFinish();
-  (void)GL_RGB32F;          // arbitrary OpenGL 3.0 symbol
-  (void)GL_LUMINANCE16;     // arbitrary OpenGL legacy-only symbol
   return 0;
 }
