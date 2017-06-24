f8cc184134a910a83a05cb9fb2ad596f150bebe5
wm4 <wm4@nowhere>
2017-06-24 10:07:48 +0200

    TOOLS: change license of some scripts involved in build to LGPL
    wscript calls them directly, and thus are probably part of the build
    system. They seem to be fully covered by relicensing agreements.

--- TOOLS/zsh.pl
+++ TOOLS/zsh.pl
@@ -2,6 +2,23 @@
 
 # Generate ZSH completion
 
+#
+# This file is part of mpv.
+#
+# mpv is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# mpv is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 use strict;
 use warnings;
 use warnings FATAL => 'uninitialized';
