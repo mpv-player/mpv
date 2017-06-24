fcbcd1d3c0827128372f9f476a10cbcdd2fef377
wm4 <wm4@nowhere>
2017-06-24 13:19:51 +0200

    demux_mf, stream_mf: change license to LGPL
    cehoyos, who did not agree to the relicensing, added bcb5c78ce3. If
    there was copyright, we consider it gone, because the table changed. It
    does not map file extension to a FourCC anymore, and codecs.conf is
    gone. The new mapping is a libavcodec codec name (happens to be the same
    as the file extension).
    
    The same applies to commits 60ecafec, b749836b, 5b3e3be1. None of these
    authors were contacted. These were before the code was replaced with a
    table (in d0326807). The parts outside of demux_mf.c were removed a long
    time ago. Like in the previous comment, we don't think any copyright
    applies at least to the new code (at least after the FourCC removal).
    
    iive authored 0aa37a0d, which is probably still left in some form, and
    makes demux_mf.c "LGPL 3 or later".
    
    stream_avdevice.c (unrelated) has been marked as LGPL before.

--- demux/demux_mf.c
+++ demux/demux_mf.c
@@ -1,18 +1,24 @@
 /*
  * This file is part of mpv.
  *
- * mpv is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * mpv is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
  *
  * mpv is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The parts making this file LGPL v3 (instead of v2.1 or later) are:
+ *  0aa37a0db23c allow autodetection of pictures type when using mf://@fil...
+ * (iive agreed to LGPL v3+ only.)
+ * Once these changes are not relevant to for copyright anymore (e.g. because
+ * they have been removed), this file will change to LGPLv2.1+.
  */
 
 #include <stdio.h>
