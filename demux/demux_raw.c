f033c488e8acabe5470b9d9e46a9b621138d488b
wm4 <wm4@nowhere>
2017-06-24 13:56:53 +0200

    demux_raw: change license to LGPL
    All relevant authors have agreed.
    
    I'm removing the NV12 FourCC, which was added in f910f3d9 by someone who
    was not contacted for the relicensing. I doubt the remaining code is
    copyrightable (basically all what remains the fact is that NV12 uses the
    same amount of space like YV12), but in this case I feel more
    comfortable removing it.
    
    Might contain some trace amounts of "michael"'s copyright, who agrees
    with LGPL only once the core is relicensed - but with the core already
    mostly relicensed, I'm changing the license header to LGPL, and only
    marking this in the "Copyright" file.

--- demux/demux_raw.c
+++ demux/demux_raw.c
@@ -1,18 +1,18 @@
 /*
  * This file is part of mpv.
  *
- * mpv is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * mpv is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * mpv is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "config.h"
@@ -223,7 +223,6 @@ static int demux_rawvideo_open(demuxer_t *demuxer, enum demux_check check)
         case MKTAG('Y', 'V', '1', '2'):
         case MKTAG('I', '4', '2', '0'):
         case MKTAG('I', 'Y', 'U', 'V'):
-        case MKTAG('N', 'V', '1', '2'):
             bpp = 12;
             break;
         case MKTAG('U', 'Y', 'V', 'Y'):
