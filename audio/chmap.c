8922c7b84a769a938854e0a3aabe1ee397503770
wm4 <wm4@nowhere>
2017-06-24 11:36:10 +0200

    chmap: remove misleading "downmix" channel layout name
    I'm not even sure when/if FFmpeg produces those. It's just confusing. If
    you really need this, you can still use dl-dr. I expect that most use is
    unintentional.
    
    Probably fixes #4545.

--- audio/chmap.c
+++ audio/chmap.c
@@ -95,7 +95,6 @@ static const char *const std_layout_names[][2] = {
     {"7.1(wide-side)",  "fl-fr-fc-lfe-flc-frc-sl-sr"},
     {"7.1(rear)",       "fl-fr-fc-lfe-bl-br-sdl-sdr"}, // not in lavc
     {"octagonal",       "fl-fr-fc-bl-br-bc-sl-sr"},
-    {"downmix",         "dl-dr"},
     {"auto",            ""}, // not in lavc
     {0}
 };
