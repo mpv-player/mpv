46bfa3726f6163cb812425b49a8483c635b6d623
Jagannathan Tiruvallur Eachambadi <jagannathante@gmail.com>
2017-07-16 14:11:24 +0200

    ytdl_hook: add a header to support geo-bypass
    youtube-dl supports bypassing some geographic restrictions by
    setting X-Forwarded-For header when used with geo-bypass and
    geo-bypass-country.

--- player/lua/ytdl_hook.lua
+++ player/lua/ytdl_hook.lua
@@ -41,7 +41,7 @@ local function set_http_headers(http_headers)
     if useragent and not option_was_set("user-agent") then
         mp.set_property("file-local-options/user-agent", useragent)
     end
-    local additional_fields = {"Cookie", "Referer"}
+    local additional_fields = {"Cookie", "Referer", "X-Forwarded-For"}
     for idx, item in pairs(additional_fields) do
         local field_value = http_headers[item]
         if field_value then
