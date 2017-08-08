mpv-player/mpv

commit a434892208559232d108b512f9ed30527e646d9d

build: fix dependencies for Cygwin environment
This replaces previous commit with same intentions. This time, with
proper formating (no tabs in code).

wscript_build.py
@@ -120,9 +120,9 @@ def build(ctx):
     ctx(features = "ebml_definitions", target = "ebml_defs.c")
 
     main_fn_c = ctx.pick_first_matching_dep([
+        ( "osdep/main-fn-unix.c",                "posix" ),
         ( "osdep/main-fn-win.c",                 "win32-desktop" ),
         ( "osdep/main-fn-cocoa.c",               "cocoa" ),
-        ( "osdep/main-fn-unix.c" ),
     ])
 
     getch2_c = ctx.pick_first_matching_dep([
@@ -466,12 +466,12 @@ def build(ctx):
         ( "osdep/path-macosx.m",                 "cocoa" ),
         ( "osdep/path-unix.c"),
         ( "osdep/path-win.c",                    "win32-desktop" ),
-        ( "osdep/path-win.c",                    "os-cygwin" ),
         ( "osdep/path-uwp.c",                    "uwp" ),
         ( "osdep/glob-win.c",                    "glob-win32" ),
         ( "osdep/w32_keyboard.c",                "os-win32" ),
         ( "osdep/w32_keyboard.c",                "os-cygwin" ),
         ( "osdep/windows_utils.c",               "os-win32" ),
+        ( "osdep/windows_utils.c",               "os-cygwin" ),
         ( "osdep/mpv.rc",                        "win32-executable" ),
         ( "osdep/win32/pthread.c",               "win32-internal-pthreads"),
         ( "osdep/android/strnlen.c",             "android"),

wscript
@@ -548,7 +548,7 @@ audio_output_features = [
     }, {
         'name': '--wasapi',
         'desc': 'WASAPI audio output',
-        'deps': ['os-win32'],
+        'deps_any': ['os-win32', 'os-cygwin'],
         'func': check_cc(fragment=load_fragment('wasapi.c')),
     }
 ]

commit 2ad79785cc4ab9a233acede8880a56ca53613de9

build: use main-fn-cocoa.c on OSX
Broken by a434892208.

wscript_build.py
@@ -120,9 +120,9 @@ def build(ctx):
     ctx(features = "ebml_definitions", target = "ebml_defs.c")
 
     main_fn_c = ctx.pick_first_matching_dep([
+        ( "osdep/main-fn-cocoa.c",               "cocoa" ),
         ( "osdep/main-fn-unix.c",                "posix" ),
         ( "osdep/main-fn-win.c",                 "win32-desktop" ),
-        ( "osdep/main-fn-cocoa.c",               "cocoa" ),
     ])
 
     getch2_c = ctx.pick_first_matching_dep([