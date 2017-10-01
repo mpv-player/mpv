2d171ada9f87cfc0deac93a0dd4f6e8d070458d0
Tobias Killer <tokidev@posteo.de>
2017-10-01 09:01:57 +0200

    vo_shablo: fix to make it runnable

--- video/out/vo.c
+++ video/out/vo.c
@@ -64,6 +64,7 @@ extern const struct vo_driver video_out_vaapi;
 extern const struct vo_driver video_out_wayland;
 extern const struct vo_driver video_out_rpi;
 extern const struct vo_driver video_out_tct;
+extern const struct vo_driver video_out_shablo;
 
 const struct vo_driver *const video_out_drivers[] =
 {
@@ -96,6 +97,7 @@ const struct vo_driver *const video_out_drivers[] =
     // should not be auto-selected
     &video_out_image,
     &video_out_tct,
+    &video_out_shablo,
 #if HAVE_CACA
     &video_out_caca,
 #endif
