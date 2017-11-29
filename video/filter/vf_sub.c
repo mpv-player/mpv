194ec2b47bb75391ec71d180ba0f5fd0b5dbec8e
wm4 <wm4@nowhere>
2017-11-29 18:20:29 +0100

    vf_sub, vf_format: change license to LGPL
    They were added to the "to deleted" list and never relicensed, because I
    thought I'd delete them early. But it's possible that they'll stay in
    mpv for a longer time, so relicense them. Still leaving them as
    deprecated and scheduled for removal, so they can still be dropped once
    there is a better way to deal with them, if they get annoying, or if a
    better mechanism is found that makes them unnecessary.
    
    All contributors agreed. There are some minor changes by people who did
    not agree, but these are all not relevant or have been removed.

--- video/filter/vf_sub.c
+++ video/filter/vf_sub.c
@@ -3,19 +3,20 @@
  *
  * This file is part of mpv.
  *
- * mpv is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * mpv is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * mpv is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 #include "config.h"
 
@@ -39,11 +40,6 @@
 #include "video/sws_utils.h"
 
 #include "options/m_option.h"
-
-#include "config.h"
-#if !HAVE_GPL
-#error GPL only
-#endif
 
 struct vf_priv_s {
     int opt_top_margin, opt_bottom_margin;
