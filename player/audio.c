5103b5dc2cfeb6bc980139444e0fd0110f8d4111
wm4 <wm4@nowhere>
2018-01-06 17:19:11 +0100

    player: handle audio playback restart in central playback start code
    No idea why this wasn't done earlier. This makes playback start in audio
    only tracks closer to video-only or video/audio restart. It has the
    consequence that --cache-pause-initial now works for audio-only streams
    too.

--- player/audio.c
+++ player/audio.c
@@ -1225,10 +1225,8 @@ void fill_audio_out_buffers(struct MPContext *mpctx)
     // Even if we're done decoding and syncing, let video start first - this is
     // required, because sending audio to the AO already starts playback.
     if (mpctx->audio_status == STATUS_READY) {
-        if (mpctx->vo_chain && !mpctx->vo_chain->is_coverart &&
-            mpctx->video_status <= STATUS_READY)
-            return;
-        MP_VERBOSE(mpctx, "starting audio playback\n");
+        // Warning: relies on handle_playback_restart() being called afterwards.
+        return;
     }
 
     bool audio_eof = status == AD_EOF;
