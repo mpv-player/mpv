a1ed1f8be09c927994b58399e77e99336ec7f436
wm4 <wm4@nowhere>
2018-05-06 16:17:47 +0200

    thread_pool: make it slightly less dumb
    The existing thread pool code is the most primitive thread pool
    possible. That's fine, but one annoying thing was that it used a static
    number of threads. Make it dynamic, so we don't need to "waste" idle
    threads.
    
    This tries to add threads as needed. If threads are idle for some time,
    destroy them again until a minimum number of threads is reached.
    
    Also change the license to ISC.

--- misc/thread_pool.h
+++ misc/thread_pool.h
@@ -3,8 +3,10 @@
 
 struct mp_thread_pool;
 
-struct mp_thread_pool *mp_thread_pool_create(void *ta_parent, int threads);
-void mp_thread_pool_queue(struct mp_thread_pool *pool, void (*fn)(void *ctx),
+struct mp_thread_pool *mp_thread_pool_create(void *ta_parent, int init_threads,
+                                             int min_threads, int max_threads);
+
+bool mp_thread_pool_queue(struct mp_thread_pool *pool, void (*fn)(void *ctx),
                           void *fn_ctx);
 
 #endif
