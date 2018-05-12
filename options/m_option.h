4dd0d0f6c44b2105de67681993681cdaa82975c1
Philip Sequeira <phsequei@gmail.com>
2018-05-12 11:29:27 -0400

    m_option: fix duplicate flag value

--- options/m_option.h
+++ options/m_option.h
@@ -410,8 +410,8 @@ char *format_file_size(int64_t size);
 
 // The following are also part of the M_OPT_* flags, and are used to update
 // certain groups of options.
-#define UPDATE_OPT_FIRST        (1 << 7)
-#define UPDATE_TERM             (1 << 7)  // terminal options
+#define UPDATE_OPT_FIRST        (1 << 8)
+#define UPDATE_TERM             (1 << 8)  // terminal options
 #define UPDATE_OSD              (1 << 10) // related to OSD rendering
 #define UPDATE_BUILTIN_SCRIPTS  (1 << 11) // osc/ytdl/stats
 #define UPDATE_IMGPAR           (1 << 12) // video image params overrides
