101a790c24c3e81dc660045e7478175faf6ae182
fatalis <fatalis@fatalis.pw>
2018-05-13 09:32:26 -0500

    osc: fix accidentally skipping files when seeking with slider
    When seeking near the end of the file and the next file loads, seeking
    continues on the next file at the same position and then immediately
    the file after that. This patch stops slider seeking when a new file is
    loaded, which is the standard behavior of many other players.

--- player/lua/osc.lua
+++ player/lua/osc.lua
@@ -1538,6 +1538,8 @@ function osc_init()
     end
     osc_param.playresx = osc_param.playresy * osc_param.display_aspect
 
+    -- stop seeking with the slider to prevent skipping files
+    state.active_element = nil
 
 
 
