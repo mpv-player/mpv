9b1c1d302fdb00209e990f7ca17ebe9c95cd5d0b
wm4 <wm4@nowhere>
2018-05-17 15:29:03 +0200

    misc: move some helper code from client.c
    (Slightly oddly function names, because I want to avoid starting them
    with mpv_*, which is reserved for public API.)

--- misc/node.h
+++ misc/node.h
@@ -11,5 +11,7 @@ void node_map_add_int64(struct mpv_node *dst, const char *key, int64_t v);
 void node_map_add_double(struct mpv_node *dst, const char *key, double v);
 void node_map_add_flag(struct mpv_node *dst, const char *key, bool v);
 mpv_node *node_map_get(mpv_node *src, const char *key);
+bool equal_mpv_value(const void *a, const void *b, mpv_format format);
+bool equal_mpv_node(const struct mpv_node *a, const struct mpv_node *b);
 
 #endif
