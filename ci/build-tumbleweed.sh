dee7cdfae75fb88aa45fa6869c70d25c95f1187d
Martin Herkt <lachs0r@srsfckn.biz>
2018-07-05 16:03:32 +0200

    ci: enable libsmbclient

--- ci/build-tumbleweed.sh
+++ ci/build-tumbleweed.sh
@@ -4,13 +4,14 @@ set -e
 ./bootstrap.py
 ./waf configure \
   --enable-cdda          \
-  --enable-dvdread       \
-  --enable-dvdnav        \
-  --enable-libmpv-shared \
-  --enable-zsh-comp      \
-  --enable-manpage-build \
-  --enable-libarchive    \
   --enable-dvbin         \
+  --enable-dvdnav        \
+  --enable-dvdread       \
+  --enable-libarchive    \
+  --enable-libmpv-shared \
+  --enable-libsmbclient  \
+  --enable-manpage-build \
+  --enable-shaderc       \
   --enable-vulkan        \
-  --enable-shaderc
+  --enable-zsh-comp
 ./waf build --verbose
