b74b39dfb550b474624202efd193261a130fe191
Philip Langdale <philipl@overt.org>
2018-12-09 15:34:22 +0800

    vo_gpu: vulkan: Add back context_win for libplacebo
    Feature parity with the original ra_vk obviously requires win32 support,
    so let's put it back in.

--- video/out/gpu/context.c
+++ video/out/gpu/context.c
@@ -50,7 +50,7 @@ extern const struct ra_ctx_fns ra_ctx_vdpauglx;
 
 /* Vulkan */
 extern const struct ra_ctx_fns ra_ctx_vulkan_wayland;
-//extern const struct ra_ctx_fns ra_ctx_vulkan_win;
+extern const struct ra_ctx_fns ra_ctx_vulkan_win;
 extern const struct ra_ctx_fns ra_ctx_vulkan_xlib;
 
 /* Direct3D 11 */
@@ -105,11 +105,9 @@ static const struct ra_ctx_fns *contexts[] = {
 // Vulkan contexts:
 #if HAVE_VULKAN
 
-/*
 #if HAVE_WIN32_DESKTOP
     &ra_ctx_vulkan_win,
 #endif
-*/
 #if HAVE_WAYLAND
     &ra_ctx_vulkan_wayland,
 #endif
