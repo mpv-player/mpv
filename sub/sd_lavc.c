829308da0824cb6bd9c1f40642eab1977201f224
wm4 <wm4@nowhere>
2019-01-20 17:29:33 +0100

    sd_lavc: fix some obscure UB
    UB-sanitizer complains that we shift bits into the sign (when a is
    used). Change it to unsigned, which in theory is more correct and
    silences the warning.
    
    Doesn't matter in practice, both the "bug" and the fix have 0 impact.

--- sub/sd_lavc.c
+++ sub/sd_lavc.c
@@ -147,10 +147,10 @@ static void convert_pal(uint32_t *colors, size_t count, bool gray)
 {
     for (int n = 0; n < count; n++) {
         uint32_t c = colors[n];
-        int b = c & 0xFF;
-        int g = (c >> 8) & 0xFF;
-        int r = (c >> 16) & 0xFF;
-        int a = (c >> 24) & 0xFF;
+        uint32_t b = c & 0xFF;
+        uint32_t g = (c >> 8) & 0xFF;
+        uint32_t r = (c >> 16) & 0xFF;
+        uint32_t a = (c >> 24) & 0xFF;
         if (gray)
             r = g = b = (r + g + b) / 3;
         // from straight to pre-multiplied alpha
