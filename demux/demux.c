mpv-player/mpv

commit fec3a4792a40a0228188eea4ccca0ce2135ab703

demux: cleaner mutex usage
The demuxer layer can start a thread to decouple the rest of the player
from blocking I/O (such as network accesses). But this particular
function does not support running with the thread enabled. The mutex use
within it is only since thread_work() may temporarily unlock the mutex,
and unlocking an unlocked mutex is not allowed. Most of the rest of the
code still does proper locking, even if it's pointless and effectively
single-threaded.

To make this look slightly cleaner, extend the mutex around the rest of
the code (like threaded code would have to do). This is mostly a
cosmetic change.

demux/demux.c
@@ -2002,26 +2002,26 @@ int demux_read_packet_async(struct sh_stream *sh, struct demux_packet **out_pkt)
 struct demux_packet *demux_read_any_packet(struct demuxer *demuxer)
 {
     struct demux_internal *in = demuxer->in;
+    pthread_mutex_lock(&in->lock);
     assert(!in->threading); // doesn't work with threading
+    struct demux_packet *out_pkt = NULL;
     bool read_more = true;
     while (read_more && !in->blocked) {
         bool all_eof = true;
         for (int n = 0; n < in->num_streams; n++) {
             in->reading = true; // force read_packet() to read
-            struct demux_packet *out_pkt = NULL;
             int r = dequeue_packet(in->streams[n]->ds, &out_pkt);
             if (r > 0)
-                return out_pkt;
+                break;
             if (r == 0)
                 all_eof = false;
         }
         // retry after calling this
-        pthread_mutex_lock(&in->lock); // lock only because thread_work unlocks
         read_more = thread_work(in);
         read_more &= !all_eof;
-        pthread_mutex_unlock(&in->lock);
     }
-    return NULL;
+    pthread_mutex_unlock(&in->lock);
+    return out_pkt;
 }
 
 void demuxer_help(struct mp_log *log)

commit 95b17119e5c5c01a7b4015ad70e3dedb996f299a

demux: fix edl/ordered chapter operation
I refactored this in a previous commit, and my testing was insufficient.
This appeared to work - by coincidence this demuxed the video stream in
some tests files (so it appeared to work to me), but all other packets
were discarded and leaked.

Fixes: fec3a4792a4

demux/demux.c
@@ -2377,7 +2377,7 @@ struct demux_packet *demux_read_any_packet(struct demuxer *demuxer)
             in->reading = true; // force read_packet() to read
             int r = dequeue_packet(in->streams[n]->ds, &out_pkt);
             if (r > 0)
-                break;
+                goto done;
             if (r == 0)
                 all_eof = false;
         }
@@ -2385,6 +2385,7 @@ struct demux_packet *demux_read_any_packet(struct demuxer *demuxer)
         read_more = thread_work(in);
         read_more &= !all_eof;
     }
+done:
     pthread_mutex_unlock(&in->lock);
     return out_pkt;
 }