mpv-player/mpv

commit f93826afa5387ea27dba7af50c68bf74e5b41a17

ad_lavc: skip fully skipped frames
Fixes stupid messages with a opus/mkv test file that had an absurdly
huge codec delay.

This file fully skips several frames at the start. ad_lavc.c trimmed
these frames to 0 samples and returned them. The next layer
(f_decoder_wrapper.c) saw discontinuous PTS values, because the PTS
values increased by a frame, but amounted to 0 audio samples. This was
harmless, but logged PTS discontinuity errors.

audio/decode/ad_lavc.c
@@ -254,7 +254,8 @@ static bool receive_frame(struct mp_filter *da, struct mp_frame *out)
         priv->trim_samples -= trim;
     }
 
-    *out = MAKE_FRAME(MP_FRAME_AUDIO, mpframe);
+    if (mp_aframe_get_size(mpframe) > 0)
+        *out = MAKE_FRAME(MP_FRAME_AUDIO, mpframe);
 
     av_frame_unref(priv->avframe);
 

commit 3fc61af5d6636b01308473e52cd5e9ac09ad3863

ad_lavc: fix minor memory leak
In commit f93826afa5387ea27d I didn't consider that the frame isn't
released properly.

Fixes: f93826afa5387ea27d

audio/decode/ad_lavc.c
@@ -254,8 +254,11 @@ static bool receive_frame(struct mp_filter *da, struct mp_frame *out)
         priv->trim_samples -= trim;
     }
 
-    if (mp_aframe_get_size(mpframe) > 0)
+    if (mp_aframe_get_size(mpframe) > 0) {
         *out = MAKE_FRAME(MP_FRAME_AUDIO, mpframe);
+    } else {
+        talloc_free(mpframe);
+    }
 
     av_frame_unref(priv->avframe);
 