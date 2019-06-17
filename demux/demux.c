77e4b215ea0415a1c2389e79b2a4761179931909
Anton Kindestam <antonki@kth.se>
2019-06-17 13:16:52 +0200

    Revert "demux: fix seek range update after head packets are pruned"
    This reverts commit b275232141f569d14d34b263ac059a77225222cc.
    
    This is to make the commit:
      "demux: mess with seek range updates and pruning"
      (wm4/mpv_backup commit 52851a72cd511bca5d8b72e38d594bb8f4e61a93)
    apply, which fixes the same thing as the reverted commit.

--- demux/demux.c
+++ demux/demux.c
@@ -1715,6 +1715,8 @@ static void prune_old_packets(struct demux_internal *in)
                 }
                 prev = prev->next;
             }
+
+            update_seek_ranges(range);
         }
 
         bool done = false;
@@ -1722,8 +1724,6 @@ static void prune_old_packets(struct demux_internal *in)
             done = queue->next_prune_target == queue->head;
             remove_head_packet(queue);
         }
-
-        update_seek_ranges(range);
 
         if (range != in->current_range && range->seek_start == MP_NOPTS_VALUE)
             free_empty_cached_ranges(in);
