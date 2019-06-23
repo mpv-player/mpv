mpv-player/mpv

commit 47bb8bc1a890237b9d861bd066b7ca2b62406e26

demux: turn some redundant assignments into asserts
demux_packet.next should not be used outside of demux.c, and in this
case it's a packet that was just passed to demux.c from the outside.

demux_packet.stream is already set by the demuxer, and this is assured
by the add_packet_locked() caller.

demux/demux.c
@@ -1890,6 +1890,10 @@ static void add_packet_locked(struct sh_stream *stream, demux_packet_t *dp)
         talloc_free(dp);
         return;
     }
+
+    assert(dp->stream == stream->index);
+    assert(!dp->next);
+
     struct demux_internal *in = ds->in;
 
     in->after_seek = false;
@@ -1942,9 +1946,6 @@ static void add_packet_locked(struct sh_stream *stream, demux_packet_t *dp)
     queue->last_dts = dp->dts;
     ds->global_correct_pos &= queue->correct_pos;
     ds->global_correct_dts &= queue->correct_dts;
-
-    dp->stream = stream->index;
-    dp->next = NULL;
 
     // (keep in mind that even if the reader went out of data, the queue is not
     // necessarily empty due to the backbuffer)

commit 816d82fdfd738bc00cec27a14d1b6449fed5913b

demux: fix assert on closed captions
An earlier commit (see below) changed add_packet_locked() to expect this
field to be set correctly, instead of assigning to it. The closed
captions case was overlooked, and is rather obscure anyway.

Fixes: 47bb8bc1a890

demux/demux.c
@@ -1249,6 +1249,7 @@ void demuxer_feed_caption(struct sh_stream *stream, demux_packet_t *dp)
     dp->keyframe = true;
     dp->pts = MP_ADD_PTS(dp->pts, -in->ts_offset);
     dp->dts = MP_ADD_PTS(dp->dts, -in->ts_offset);
+    dp->stream = sh->index;
     add_packet_locked(sh, dp);
     pthread_mutex_unlock(&in->lock);
 }