221b2f50103418d70eee6f346d09ca2955bf9128
Jan Ekström <jeebjp@gmail.com>
2019-07-21 21:14:47 +0300

    travis: rework scripts to re-enable macOS
    * Adds a script to clone and build FFmpeg as well as
      to configure and build mpv itself. Currently only used
      for macOS and contain hard-coded macOS specific options.
    * Still works with the Linux containers.
    * Moves our language back to "c" from "generic"
    * Defines our Linux distribution as "bionic" to get the latest
      Ubuntu base distribution to be the runner for our containers.
    * Adds the homebrew add-on for macOS package installation for
      dependencies. Installs everything required but FFmpeg, as we want
      to have our own FFmpeg snapshots.

---
+++ ci/build-macos.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+FFMPEG_SYSROOT="${HOME}/deps/sysroot"
+MPV_INSTALL_PREFIX="${HOME}/out/mpv"
+MPV_VARIANT="${TRAVIS_OS_NAME}"
+
+if [[ -d "./build/${MPV_VARIANT}" ]] ; then
+    rm -rf "./build/${MPV_VARIANT}"
+fi
+
+if [[ ! -e "./waf" ]] ; then
+    python3 ./bootstrap.py
+fi
+
+PKG_CONFIG_PATH="${FFMPEG_SYSROOT}/lib/pkgconfig/" CC="${CC}" CXX="${CXX}" python3 \
+  ./waf configure \
+    --variant="${MPV_VARIANT}" \
+    --prefix="${MPV_INSTALL_PREFIX}" \
+    --enable-{gl,iconv,lcms2,libass,libass-osd,libmpv-shared,lua,jpeg,plain-gl,zlib} \
+    --enable-{apple-remote,cocoa,coreaudio,gl-cocoa,macos-cocoa-cb,macos-touchbar,videotoolbox-gl}
+
+python3 ./waf build --variant="${MPV_VARIANT}" -j4
+
+python3 ./waf install --variant="${MPV_VARIANT}"
