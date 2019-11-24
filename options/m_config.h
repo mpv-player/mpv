d123af34b5a10f4ff6dc011bc462da68c94ef9fa
wm4 <wm4@nowhere>
2019-11-25 00:52:30 +0100

    m_config: discourage mp_read_option_raw()
    This function is dangerous, because it disables the already basic/week
    type checking the option system has at all. I'm tend towards thinking
    that all of its uses should be replaced.

--- options/m_config.h
+++ options/m_config.h
@@ -338,6 +338,7 @@ void *mp_get_config_group(void *ta_parent, struct mpv_global *global,
 // Read a single global option in a thread-safe way. For multiple options,
 // use m_config_cache. The option must exist and match the provided type (the
 // type is used as a sanity check only). Performs semi-expensive lookup.
+// Warning: new code must not use this.
 void mp_read_option_raw(struct mpv_global *global, const char *name,
                         const struct m_option_type *type, void *dst);
 
