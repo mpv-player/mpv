6a88e7463e9f759121b109be16cd4bd911355c86
wm4 <wm4@nowhere>
2019-11-30 17:37:56 +0100

    build: require at least GL 2.0 headers for GLX
    The previous hack for fixing #7201 requires this, but it wasn't checked.
    It's easy to check, so do it. (Yes, we could just have required OpenGL
    3.2 headers and skipped the earlier fix.)
    
    For #7201.

--- waftools/fragments/gl_x11.c
+++ waftools/fragments/gl_x11.c
@@ -2,6 +2,10 @@
 #include <GL/glx.h>
 #include <stddef.h>
 
+#ifndef GL_VERSION_2_0
+#error "At least GL 2.0 headers needed."
+#endif
+
 int main(int argc, char *argv[]) {
   glXCreateContext(NULL, NULL, NULL, True);
   glXQueryExtensionsString(NULL, 0);
