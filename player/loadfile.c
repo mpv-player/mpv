fd9a9f893a84cbc0910acbd6c1e114355a974c27
xiangxiaojun <2436915040@qq.com>
2020-07-13 16:42:48 +0800

    code optimization

--- player/loadfile.c
+++ player/loadfile.c
@@ -1753,10 +1753,10 @@ struct playlist_entry *mp_next_file(struct MPContext *mpctx, int direction,
         while (next && next->playback_short)
             next = playlist_entry_get_rel(next, -1);
         // Always allow jumping to first file
-        if (!next && mpctx->opts->loop_times == 1){
+        if (!next && mpctx->opts->loop_times == 1) {
             next = playlist_get_first(mpctx->playlist);
             while (next && next->playback_short)
-                next = playlist_entry_get_rel(next, 1);
+                next = playlist_entry_get_rel(next, -1);
         }
     }
     if (!next && mpctx->opts->loop_times != 1) {
