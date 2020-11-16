8b4773cc56c7cf4f5cf92765f85e54f8da84436d
Avi Halachmi (:avih) <avihpit@yahoo.com>
2020-11-16 18:15:18 +0200

    lua/js: mp.get_mouse_pos: use the mouse-pos property
    mp.get_mouse_pos() is undocumented and is no longer required - the
    property can be used officially by any client now, however, osc.lua
    uses it, and also some user scripts learnt to rely on it, so we keep
    it - as a trivial wrapper around the new mouse-pos property.

--- player/lua.c
+++ player/lua.c
@@ -991,16 +991,6 @@ static int script_raw_abort_async_command(lua_State *L)
     return 0;
 }
 
-static int script_get_mouse_pos(lua_State *L)
-{
-    struct MPContext *mpctx = get_mpctx(L);
-    int px, py;
-    mp_input_get_mouse_pos(mpctx->input, &px, &py);
-    lua_pushnumber(L, px);
-    lua_pushnumber(L, py);
-    return 2;
-}
-
 static int script_get_time(lua_State *L)
 {
     struct script_ctx *ctx = get_ctx(L);
@@ -1242,7 +1232,6 @@ static const struct fn_entry main_fns[] = {
     AF_ENTRY(set_property_native),
     FN_ENTRY(raw_observe_property),
     FN_ENTRY(raw_unobserve_property),
-    FN_ENTRY(get_mouse_pos),
     FN_ENTRY(get_time),
     FN_ENTRY(input_set_section_mouse_area),
     FN_ENTRY(format_time),
