af264029483656e04458a678849165f0340433ab
der richter <der.richter@gmx.de>
2020-12-13 17:13:18 +0100

    mac: use visible frame rectangle for window geometry calculation
    currently we use the whole screen rectangle to calculate the window
    geometry. this doesn't take the menu bar or the Dock into account.
    
    by default use the visible screen rectangle instead. this is also a
    change in behaviour, since the window can't be placed outside of this
    rectangle anymore. also add an option to change to the old behaviour,
    because it can still be useful in certain cases, like placing the window
    directly underneath the menu bar when used a desktop background.
    
    Fixes #8272

--- osdep/macosx_application.h
+++ osdep/macosx_application.h
@@ -21,6 +21,11 @@
 #include "osdep/macosx_menubar.h"
 #include "options/m_option.h"
 
+enum {
+    FRAME_VISIBLE = 0,
+    FRAME_WHOLE,
+};
+
 struct macos_opts {
     int macos_title_bar_style;
     int macos_title_bar_appearance;
@@ -29,6 +34,7 @@ struct macos_opts {
     int macos_fs_animation_duration;
     int macos_force_dedicated_gpu;
     int macos_app_activation_policy;
+    int macos_geometry_calculation;
     int cocoa_cb_sw_renderer;
     int cocoa_cb_10bit_context;
 };
