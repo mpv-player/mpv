mpv-player/mpv

commit 55d7f9ded197d82d172b7baf74b1a07640361ae8

player: add automatic loading of external cover art files
Picks up files like "cover.jpg". It's made part of normal external file
loading, so I'm adding 3 new options that are direct equivalents for the
options that control loading of external subtitle and audio files. Even
though I bet nobody wants them and they just increase confusion... I
guess the world is actually hell, so this outcome should be fine.

It prefers non-specific external files like "cover.jpg" over embedded
cover art. Not sure if that's wanted or unwanted.

There's some pain over explicitly marking such files as external
pictures. This is basically an optimization: in most cases, a heuristic
would treat an image file loaded with --external-file the same (it's a
heuristic because ffmpeg can't tell us whether something is an image or
a video). However, even with this heuristic, it would decode the cover
art picture again on each seek, which would essentially slow down
seeking in audio files. This bothered me greatly, which is why I'm
adding these additional options at all, and bothered with the previous
commit.

Fixes: #3056

player/loadfile.c
@@ -827,6 +827,8 @@ int mp_add_external_file(struct MPContext *mpctx, char *filename,
         t->external_filename = talloc_strdup(t, filename);
         t->no_default = sh->type != filter;
         t->no_auto_select = t->no_default;
+        // filter==STREAM_VIDEO always means cover art.
+        t->attached_picture = t->type == STREAM_VIDEO && filter == STREAM_VIDEO;
         if (first_num < 0 && (filter == STREAM_TYPE_COUNT || sh->type == filter))
             first_num = mpctx->num_tracks - 1;
     }
@@ -859,14 +861,15 @@ static void open_external_files(struct MPContext *mpctx, char **files,
 // See mp_add_external_file() for meaning of cancel parameter.
 void autoload_external_files(struct MPContext *mpctx, struct mp_cancel *cancel)
 {
-    if (mpctx->opts->sub_auto < 0 && mpctx->opts->audiofile_auto < 0)
+    struct MPOpts *opts = mpctx->opts;
+
+    if (opts->sub_auto < 0 && opts->audiofile_auto < 0 && opts->coverart_auto < 0)
         return;
-    if (!mpctx->opts->autoload_files || strcmp(mpctx->filename, "-") == 0)
+    if (!opts->autoload_files || strcmp(mpctx->filename, "-") == 0)
         return;
 
     void *tmp = talloc_new(NULL);
-    struct subfn *list = find_external_files(mpctx->global, mpctx->filename,
-                                             mpctx->opts);
+    struct subfn *list = find_external_files(mpctx->global, mpctx->filename, opts);
     talloc_steal(tmp, list);
 
     int sc[STREAM_TYPE_COUNT] = {0};
@@ -887,6 +890,8 @@ void autoload_external_files(struct MPContext *mpctx, struct mp_cancel *cancel)
             goto skip;
         if (list[i].type == STREAM_AUDIO && !sc[STREAM_VIDEO])
             goto skip;
+        if (list[i].type == STREAM_VIDEO && (sc[STREAM_VIDEO] || !sc[STREAM_AUDIO]))
+            goto skip;
         int first = mp_add_external_file(mpctx, filename, list[i].type, cancel);
         if (first < 0)
             goto skip;
@@ -894,6 +899,8 @@ void autoload_external_files(struct MPContext *mpctx, struct mp_cancel *cancel)
         for (int n = first; n < mpctx->num_tracks; n++) {
             struct track *t = mpctx->tracks[n];
             t->auto_loaded = true;
+            t->attached_picture =
+                t->type == STREAM_VIDEO && list[i].type == STREAM_VIDEO;
             if (!t->lang)
                 t->lang = talloc_strdup(t, lang);
         }
@@ -1364,6 +1371,7 @@ static void load_external_opts_thread(void *p)
     load_chapters(mpctx);
     open_external_files(mpctx, mpctx->opts->audio_files, STREAM_AUDIO);
     open_external_files(mpctx, mpctx->opts->sub_name, STREAM_SUB);
+    open_external_files(mpctx, mpctx->opts->coverart_files, STREAM_VIDEO);
     open_external_files(mpctx, mpctx->opts->external_files, STREAM_TYPE_COUNT);
     autoload_external_files(mpctx, mpctx->playback_abort);
 

options/options.c
@@ -512,8 +512,6 @@ static const m_option_t mp_opts[] = {
 #endif
 
     // demuxer.c - select audio/sub file/demuxer
-    {"audio-files", OPT_PATHLIST(audio_files), .flags = M_OPT_FILE},
-    {"audio-file", OPT_CLI_ALIAS("audio-files-append")},
     {"demuxer", OPT_STRING(demuxer_name)},
     {"audio-demuxer", OPT_STRING(audio_demuxer_name)},
     {"sub-demuxer", OPT_STRING(sub_demuxer_name)},
@@ -573,15 +571,24 @@ static const m_option_t mp_opts[] = {
 
     {"sub-files", OPT_PATHLIST(sub_name), .flags = M_OPT_FILE},
     {"sub-file", OPT_CLI_ALIAS("sub-files-append")},
+    {"audio-files", OPT_PATHLIST(audio_files), .flags = M_OPT_FILE},
+    {"audio-file", OPT_CLI_ALIAS("audio-files-append")},
+    {"cover-art-files", OPT_PATHLIST(coverart_files), .flags = M_OPT_FILE},
+    {"cover-art-file", OPT_CLI_ALIAS("covert-art-files-append")},
+
     {"sub-file-paths", OPT_PATHLIST(sub_paths), .flags = M_OPT_FILE},
     {"audio-file-paths", OPT_PATHLIST(audiofile_paths), .flags = M_OPT_FILE},
+
     {"external-files", OPT_PATHLIST(external_files), .flags = M_OPT_FILE},
     {"external-file", OPT_CLI_ALIAS("external-files-append")},
     {"autoload-files", OPT_FLAG(autoload_files)},
+
     {"sub-auto", OPT_CHOICE(sub_auto,
         {"no", -1}, {"exact", 0}, {"fuzzy", 1}, {"all", 2})},
     {"audio-file-auto", OPT_CHOICE(audiofile_auto,
         {"no", -1}, {"exact", 0}, {"fuzzy", 1}, {"all", 2})},
+    {"cover-art-auto", OPT_CHOICE(coverart_auto,
+        {"no", -1}, {"fuzzy", 1})},
 
     {"", OPT_SUBSTRUCT(subs_rend, mp_subtitle_sub_opts)},
     {"", OPT_SUBSTRUCT(subs_filt, mp_sub_filter_opts)},
@@ -1002,6 +1009,7 @@ static const struct MPOpts mp_default_opts = {
     .pitch_correction = 1,
     .sub_auto = 0,
     .audiofile_auto = -1,
+    .coverart_auto = 1,
     .osd_bar_visible = 1,
     .screenshot_template = "mpv-shot%n",
     .play_dir = 1,

options/options.h
@@ -291,10 +291,12 @@ typedef struct MPOpts {
     char **sub_name;
     char **sub_paths;
     char **audiofile_paths;
+    char **coverart_files;
     char **external_files;
     int autoload_files;
     int sub_auto;
     int audiofile_auto;
+    int coverart_auto;
     int osd_bar_visible;
 
     int w32_priority;

player/external_files.c
@@ -42,6 +42,25 @@ static const char *const audio_exts[] = {"mp3", "aac", "mka", "dts", "flac",
                                          "wv",
                                          NULL};
 
+// Stolen from: vlc/-/blob/master/modules/meta_engine/folder.c#L40
+static const char *const cover_files[] = {
+    "Folder.jpg",
+    "Folder.png",
+    "AlbumArtSmall.jpg",
+    "AlbumArt.jpg",
+    "Album.jpg",
+    ".folder.png",
+    "cover.jpg",
+    "cover.png",
+    "cover.gif",
+    "front.jpg",
+    "front.png",
+    "front.gif",
+    "front.bmp",
+    "thumb.jpg",
+    NULL
+};
+
 static bool test_ext_list(bstr ext, const char *const *list)
 {
     for (int n = 0; list[n]; n++) {
@@ -57,6 +76,15 @@ static int test_ext(bstr ext)
         return STREAM_SUB;
     if (test_ext_list(ext, audio_exts))
         return STREAM_AUDIO;
+    return -1;
+}
+
+static int test_filename(bstr fname)
+{
+    for (int n = 0; cover_files[n]; n++) {
+        if (bstrcasecmp(bstr0(cover_files[n]), fname) == 0)
+            return STREAM_VIDEO;
+    }
     return -1;
 }
 
@@ -159,6 +187,8 @@ static void append_dir_subtitles(struct mpv_global *global, struct MPOpts *opts,
 
         // check what it is (most likely)
         int type = test_ext(tmp_fname_ext);
+        if (type < 0)
+            type = test_filename(dename);
         char **langs = NULL;
         int fuzz = -1;
         switch (type) {
@@ -169,6 +199,9 @@ static void append_dir_subtitles(struct mpv_global *global, struct MPOpts *opts,
         case STREAM_AUDIO:
             langs = opts->stream_lang[type];
             fuzz = opts->audiofile_auto;
+            break;
+        case STREAM_VIDEO:
+            fuzz = opts->coverart_auto;
             break;
         }
 
@@ -208,6 +241,10 @@ static void append_dir_subtitles(struct mpv_global *global, struct MPOpts *opts,
         // doesn't contain the movie name
         // don't try in the mplayer subtitle directory
         if (!limit_fuzziness && fuzz >= 2)
+            prio |= 1;
+
+        // cover art: just accept it
+        if (type == STREAM_VIDEO && fuzz >= 1)
             prio |= 1;
 
         mp_dbg(log, "Potential external file: \"%s\"  Priority: %d\n",

player/external_files.h
@@ -21,7 +21,7 @@
 #include <stdbool.h>
 
 struct subfn {
-    int type; // STREAM_SUB/STREAM_AUDIO
+    int type; // STREAM_SUB/STREAM_AUDIO/STREAM_VIDEO(coverart)
     int priority;
     char *fname;
     char *lang;

commit 3c642ad669c477f70dd8da394792924d69c4702d

player: fixed external video add attached_picture
Fixes bug introduced in commit 55d7f9ded197d82d172b7baf74b1a07640361ae8
Player assumed all external video tracks added were artwork
(video_track->attached_picture=true), but this is rarely the case when
using the command 'video-add'.
This had the added side effect of only playing the first frame of
dynamically added video tracks.

There was a fundamental problem in assuming all additional
externally loaded video was coverart. To overcome this I've removed the
main offending line and added a few necessary lines to specify if the
file is artwork.

Now attached_picture will only equal true if the file was explicitly
loaded within autoload_external_files, passed as argument in
--cover-art-files or --cover-art-file, or via the command video-add with
the new extra flag.

Fixes #8358

player/command.c
@@ -5282,6 +5282,9 @@ static void cmd_track_add(void *p)
         char *lang = cmd->args[3].v.s;
         if (lang && lang[0])
             t->lang = talloc_strdup(t, lang);
+        int extra = cmd->args[4].v.i;
+        int is_albumart = extra & 1;
+        t->attached_picture = t->type == STREAM_VIDEO && is_albumart;
     }
 
     if (mpctx->playback_initialized)
@@ -5322,8 +5325,10 @@ static void cmd_track_reload(void *p)
 
     if (t && t->is_external && t->external_filename) {
         char *filename = talloc_strdup(NULL, t->external_filename);
+        bool is_coverart = t->attached_picture;
         mp_remove_track(mpctx, t);
         nt_num = mp_add_external_file(mpctx, filename, type, cmd->abort->cancel);
+        t->attached_picture = is_coverart;
         talloc_free(filename);
     }
 
@@ -6070,6 +6075,8 @@ const struct mp_cmd_def mp_cmds[] = {
                 .flags = MP_CMD_OPT_ARG},
             {"title", OPT_STRING(v.s), .flags = MP_CMD_OPT_ARG},
             {"lang", OPT_STRING(v.s), .flags = MP_CMD_OPT_ARG},
+            {"extra", OPT_CHOICE(v.i,
+                {"albumart", 1}), .flags = MP_CMD_OPT_ARG},
         },
         .priv = &(const int){STREAM_VIDEO},
         .spawn_thread = true,

player/loadfile.c
@@ -832,8 +832,6 @@ int mp_add_external_file(struct MPContext *mpctx, char *filename,
         t->external_filename = talloc_strdup(t, filename);
         t->no_default = sh->type != filter;
         t->no_auto_select = t->no_default;
-        // filter==STREAM_VIDEO always means cover art.
-        t->attached_picture = t->type == STREAM_VIDEO && filter == STREAM_VIDEO;
         if (first_num < 0 && (filter == STREAM_TYPE_COUNT || sh->type == filter))
             first_num = mpctx->num_tracks - 1;
     }
@@ -1376,7 +1374,10 @@ static void load_external_opts_thread(void *p)
     load_chapters(mpctx);
     open_external_files(mpctx, mpctx->opts->audio_files, STREAM_AUDIO);
     open_external_files(mpctx, mpctx->opts->sub_name, STREAM_SUB);
+    int n = mpctx->num_tracks;
     open_external_files(mpctx, mpctx->opts->coverart_files, STREAM_VIDEO);
+    for (; n < mpctx->num_tracks; n++)
+        mpctx->tracks[n]->attached_picture = true;
     open_external_files(mpctx, mpctx->opts->external_files, STREAM_TYPE_COUNT);
     autoload_external_files(mpctx, mpctx->playback_abort);
 