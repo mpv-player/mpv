ded36a4470a5970693ee0ceb2f43baa5db470cd3
Your Name <you@example.com>
2021-05-11 22:22:33 +0200

    options: add some entries to --display-tags defaults
    Useful for the previous commit.

--- options/options.c
+++ options/options.c
@@ -1032,6 +1032,7 @@ static const struct MPOpts mp_default_opts = {
         "Artist", "Album", "Album_Artist", "Comment", "Composer",
         "Date", "Description", "Genre", "Performer", "Rating",
         "Series", "Title", "Track", "icy-title", "service_name",
+        "Uploader", "Channel_URL",
         NULL
     },
 
