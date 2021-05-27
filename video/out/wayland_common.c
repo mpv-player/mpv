mpv-player/mpv

commit c26d83348b730d083f3be313556039fd31d32505

wayland: shuffle around the render loop again
Take two. f4e89dd went wrong by moving vo_wayland_wait_frame before
start_frame was called. Whether or not this matters depends on the
compositor, but some weird things can happen. Basically, it's a
scheduling issue. vo_wayland_wait_frame queues all events and sends them
to the server to process (with no blocking if presentation time is
available). If mpv changes state while rendering (and this function is
called before every frame is drawn), then that event also gets
dispatched and sent to the compositor. This, in some cases, can cause
some funny behavior because the next frame gets attached to the surface
while the old buffer is getting released. It's safer to call this
function after the swap already happens and well before mpv calls its
next draw. There's no weird scheduling of events, and the compositor log
is more normal.

The second part of this is to fix some stuttering issues. This is mostly
just conjecture, but probably what was happening was this thing called
"composition". The easiest way to see this is to play a video on the
default audio sync mode (probably easiest to see on a typical 23.976
video). Have that in a window and float it over firefox (floating
windows are bloat on a tiling wm anyway). Then in firefox, do some short
bursts of smooth scrolling (likely uses egl). Some stutter in video
rendering could be observed, particularly in panning shots.

Compositors are supposed to prevent tearing so what likely was happening
was that the compositor was simply holding the buffer a wee bit longer
to make sure it happened in sync with the smooth scrolling. Because the
mpv code waits precisely on presentation time, the loop would timeout on
occasion instead of receiving the frame callback. This would then lead
to a skipped frame when rendering and thus causing stuttering.

The fix is simple: just only count consecutive timeouts as not receiving

video/out/wayland_common.c
@@ -1865,7 +1865,7 @@ void vo_wayland_wakeup(struct vo *vo)
     (void)write(wl->wakeup_pipe[1], &(char){0}, 1);
 }
 
-bool vo_wayland_wait_frame(struct vo_wayland_state *wl)
+void vo_wayland_wait_frame(struct vo_wayland_state *wl)
 {
     int64_t vblank_time = 0;
     struct pollfd fds[1] = {
@@ -1917,27 +1917,25 @@ bool vo_wayland_wait_frame(struct vo_wayland_state *wl)
     }
 
     /* If the compositor does not have presentatiom time, we cannot be sure
-     * that this wait is accurate. Do some crap with wl_display_roundtrip
-     * and randomly assume that if timeouts > refresh rate, the window is
-     * hidden. This is neccesary otherwise we may mistakeningly skip rendering.*/
-    if (!wl->presentation) {
-        if (wl_display_get_error(wl->display) == 0)
-            wl_display_roundtrip(wl->display);
-        if (wl->frame_wait) {
-            if (wl->timeout_count > ((1 / (double)vblank_time) * 1e6)) {
-                return false;
-            } else {
-                wl->timeout_count += 1;
-                return true;
-            }
-        } else {
-            wl->timeout_count = 0;
-            return true;
-        }
+     * that this wait is accurate. Do a hacky block with wl_display_roundtrip. */
+    if (!wl->presentation && !wl_display_get_error(wl->display))
+        wl_display_roundtrip(wl->display);
 
+    if (wl->frame_wait) {
+        // Only consider consecutive missed callbacks.
+        if (wl->timeout_count > 1) {
+            wl->render = false;
+            return;
+        } else {
+            wl->timeout_count += 1;
+            wl->render = true;
+            return;
+        }
     }
 
-    return !wl->frame_wait;
+    wl->timeout_count = 0;
+    wl->render = true;
+    return;
 }
 
 void vo_wayland_wait_events(struct vo *vo, int64_t until_time_us)

video/out/opengl/context_wayland.c
@@ -63,12 +63,9 @@ static bool wayland_egl_start_frame(struct ra_swapchain *sw, struct ra_fbo *out_
 {
     struct ra_ctx *ctx = sw->ctx;
     struct vo_wayland_state *wl = ctx->vo->wl;
-    bool render = true;
-
-    if (!wl->opts->disable_vsync)
-        render = vo_wayland_wait_frame(wl);
-
+    bool render = wl->render || wl->opts->disable_vsync;
     wl->frame_wait = true;
+
     return render ? ra_gl_ctx_start_frame(sw, out_fbo) : false;
 }
 
@@ -79,6 +76,9 @@ static void wayland_egl_swap_buffers(struct ra_swapchain *sw)
     struct vo_wayland_state *wl = ctx->vo->wl;
 
     eglSwapBuffers(p->egl_display, p->egl_surface);
+
+    if (!wl->opts->disable_vsync)
+        vo_wayland_wait_frame(wl);
 
     if (wl->presentation)
         wayland_sync_swap(wl);

video/out/vo_wlshm.c
@@ -218,13 +218,10 @@ static void draw_image(struct vo *vo, struct mp_image *src)
     struct priv *p = vo->priv;
     struct vo_wayland_state *wl = vo->wl;
     struct buffer *buf;
-    bool callback = true;
-
-    if (!wl->opts->disable_vsync)
-        callback = vo_wayland_wait_frame(wl);
-
+    bool render = wl->render || wl->opts->disable_vsync;
     wl->frame_wait = true;
-    if (!callback)
+
+    if (!render)
         return;
 
     buf = p->free_buffers;
@@ -276,6 +273,9 @@ static void flip_page(struct vo *vo)
     wl_surface_damage(wl->surface, 0, 0, mp_rect_w(wl->geometry),
                       mp_rect_h(wl->geometry));
     wl_surface_commit(wl->surface);
+
+    if (!wl->opts->disable_vsync)
+        vo_wayland_wait_frame(wl);
 
     if (wl->presentation)
         wayland_sync_swap(wl);

video/out/vulkan/context_wayland.c
@@ -32,18 +32,19 @@ struct priv {
 static bool wayland_vk_start_frame(struct ra_ctx *ctx)
 {
     struct vo_wayland_state *wl = ctx->vo->wl;
-    bool render = true;
-
-    if (!wl->opts->disable_vsync)
-        render = vo_wayland_wait_frame(wl);
-
+    bool render = wl->render || wl->opts->disable_vsync;
     wl->frame_wait = true;
+
     return render;
 }
 
 static void wayland_vk_swap_buffers(struct ra_ctx *ctx)
 {
     struct vo_wayland_state *wl = ctx->vo->wl;
+
+    if (!wl->opts->disable_vsync)
+        vo_wayland_wait_frame(wl);
+
     if (wl->presentation)
         wayland_sync_swap(wl);
 }

video/out/wayland_common.h
@@ -74,6 +74,7 @@ struct vo_wayland_state {
     int reduced_width;
     int reduced_height;
     bool frame_wait;
+    bool render;
     bool state_change;
     bool toplevel_configured;
     bool activated;
@@ -152,7 +153,7 @@ int last_available_sync(struct vo_wayland_state *wl);
 void vo_wayland_uninit(struct vo *vo);
 void vo_wayland_wakeup(struct vo *vo);
 void vo_wayland_wait_events(struct vo *vo, int64_t until_time_us);
-bool vo_wayland_wait_frame(struct vo_wayland_state *wl);
+void vo_wayland_wait_frame(struct vo_wayland_state *wl);
 void vo_wayland_set_opaque_region(struct vo_wayland_state *wl, int alpha);
 void wayland_sync_swap(struct vo_wayland_state *wl);
 void vo_wayland_sync_shift(struct vo_wayland_state *wl);

commit e2dda3f5f9a7c14a713307dc94fc9cb8657fea01

wayland: refactor dispatching events
This was originally just a bugfix for a race condition, but the scope
expanded a bit. Currently, the wayland code does a prepare_read ->
dispatch_pending -> display_flush -> read_events -> dispatch_pending
routine that's basically straight from the wayland client API
documentation. This essentially just queues up all the wayland events
mpv has and dispatches them to the compositor. We do this for blocking
purposes on every frame we render. A very similar thing is done for
wait_events from the VO.

This code can pretty easily be unified and split off into a separate
function, vo_wayland_dispatch_events. vo_wayland_wait_frame will call
this function in a while loop (which will break either on timeout or if
we receive frame callback from the compositor). wait_events needs to
just check this in case we get some state change on the wakeup_pipe
(i.e. waking up from the paused state).

As for the actual bugfix part of this, it's a slight regression from
c26d833. The toplevel config event always forced a redraw when a surface
became activated again. This is for something like displaying cover art
on a music file. If the window was originally out of view and then later
brought back into focus, no picture would be rendered (i.e. the window
is just black). That's because something like cover art is just 1 frame
and the VO stops doing any other additional rendering. If you miss that
1 frame, nothing would show up ever again. The fix in this case is to
always just force a redraw when the mpv window comes back into view.

Well with the aforementioned commit, we stopped doing
wl_display_roundtrip calls on every frame. That means we no longer do
roundtrip blocking calls. We just be sure to queue up all of the events
we have and then dispatch them. Because wayland is fundamentally an
asynchronous protocol, there's no guarantee what order these events
would be processed in. This meant that on occasion, a

video/out/wayland_common.c
@@ -1181,6 +1181,7 @@ static void handle_toplevel_config(void *data, struct xdg_toplevel *toplevel,
 
             if (wl->activated) {
                 /* If the surface comes back into view, force a redraw. */
+                vo_wayland_wait_frame(wl);
                 wl->pending_vo_events |= VO_EVENT_EXPOSE;
             }
         }
@@ -1783,6 +1784,36 @@ int vo_wayland_control(struct vo *vo, int *events, int request, void *arg)
     return VO_NOTIMPL;
 }
 
+static void vo_wayland_dispatch_events(struct vo_wayland_state *wl, int nfds, int timeout)
+{
+    struct pollfd fds[2] = {
+        {.fd = wl->display_fd,     .events = POLLIN },
+        {.fd = wl->wakeup_pipe[0], .events = POLLIN },
+    };
+
+    while (wl_display_prepare_read(wl->display) != 0)
+        wl_display_dispatch_pending(wl->display);
+    wl_display_flush(wl->display);
+
+    poll(fds, nfds, timeout);
+
+    if (fds[0].revents & (POLLERR | POLLHUP | POLLNVAL)) {
+        MP_FATAL(wl, "Error occurred on the display fd, closing\n");
+        wl_display_cancel_read(wl->display);
+        close(wl->display_fd);
+        wl->display_fd = -1;
+        mp_input_put_key(wl->vo->input_ctx, MP_KEY_CLOSE_WIN);
+    } else {
+        wl_display_read_events(wl->display);
+    }
+
+    if (fds[0].revents & POLLIN)
+        wl_display_dispatch_pending(wl->display);
+
+    if (fds[1].revents & POLLIN)
+        mp_flush_wakeup_pipe(wl->wakeup_pipe[0]);
+}
+
 void vo_wayland_set_opaque_region(struct vo_wayland_state *wl, int alpha)
 {
     const int32_t width = wl->scaling * mp_rect_w(wl->geometry);
@@ -1868,10 +1899,6 @@ void vo_wayland_wakeup(struct vo *vo)
 void vo_wayland_wait_frame(struct vo_wayland_state *wl)
 {
     int64_t vblank_time = 0;
-    struct pollfd fds[1] = {
-        {.fd = wl->display_fd,     .events = POLLIN },
-    };
-
     /* We need some vblank interval to use for the timeout in
      * this function. The order of preference of values to use is:
      * 1. vsync duration from presentation time
@@ -1895,28 +1922,14 @@ void vo_wayland_wait_frame(struct vo_wayland_state *wl)
     int64_t finish_time = mp_time_us() + vblank_time;
 
     while (wl->frame_wait && finish_time > mp_time_us()) {
-
         int poll_time = ceil((double)(finish_time - mp_time_us()) / 1000);
         if (poll_time < 0) {
             poll_time = 0;
         }
-
-        while (wl_display_prepare_read(wl->display) != 0)
-            wl_display_dispatch_pending(wl->display);
-        wl_display_flush(wl->display);
-
-        poll(fds, 1, poll_time);
-
-        if (fds[0].revents & (POLLERR | POLLHUP | POLLNVAL)) {
-            wl_display_cancel_read(wl->display);
-        } else {
-            wl_display_read_events(wl->display);
-        }
-
-        wl_display_dispatch_pending(wl->display);
+        vo_wayland_dispatch_events(wl, 1, poll_time);
     }
 
-    /* If the compositor does not have presentatiom time, we cannot be sure
+    /* If the compositor does not have presentation time, we cannot be sure
      * that this wait is accurate. Do a hacky block with wl_display_roundtrip. */
     if (!wl->presentation && !wl_display_get_error(wl->display))
         wl_display_roundtrip(wl->display);
@@ -1945,33 +1958,8 @@ void vo_wayland_wait_events(struct vo *vo, int64_t until_time_us)
     if (wl->display_fd == -1)
         return;
 
-    struct pollfd fds[2] = {
-        {.fd = wl->display_fd,     .events = POLLIN },
-        {.fd = wl->wakeup_pipe[0], .events = POLLIN },
-    };
-
     int64_t wait_us = until_time_us - mp_time_us();
     int timeout_ms = MPCLAMP((wait_us + 999) / 1000, 0, 10000);
 
-    while (wl_display_prepare_read(wl->display) != 0)
-        wl_display_dispatch_pending(wl->display);
-    wl_display_flush(wl->display);
-
-    poll(fds, 2, timeout_ms);
-
-    if (fds[0].revents & (POLLERR | POLLHUP | POLLNVAL)) {
-        MP_FATAL(wl, "Error occurred on the display fd, closing\n");
-        wl_display_cancel_read(wl->display);
-        close(wl->display_fd);
-        wl->display_fd = -1;
-        mp_input_put_key(vo->input_ctx, MP_KEY_CLOSE_WIN);
-    } else {
-        wl_display_read_events(wl->display);
-    }
-
-    if (fds[0].revents & POLLIN)
-        wl_display_dispatch_pending(wl->display);
-
-    if (fds[1].revents & POLLIN)
-        mp_flush_wakeup_pipe(wl->wakeup_pipe[0]);
+    vo_wayland_dispatch_events(wl, 2, timeout_ms);
 }