9de023785c915a1aa7f88e921eeab24d5705dcde
rcombs <rcombs@rcombs.me>
2021-06-27 01:30:24 -0500

    sub: fix subs/lyrics on music files with sub-past-video-end=no
    Regressed in 11423acf3

--- player/sub.c
+++ player/sub.c
@@ -115,7 +115,9 @@ static bool update_subtitle(struct MPContext *mpctx, double video_pts,
     // quite different, because normally subtitles are redrawn on new video
     // frames, using the video frames' timestamps.
     if (mpctx->video_out && mpctx->video_status == STATUS_EOF &&
-        mpctx->opts->subs_rend->sub_past_video_end) {
+        (mpctx->opts->subs_rend->sub_past_video_end ||
+         !mpctx->current_track[0][STREAM_VIDEO] ||
+         mpctx->current_track[0][STREAM_VIDEO]->attached_picture)) {
         if (osd_get_force_video_pts(mpctx->osd) != video_pts) {
             osd_set_force_video_pts(mpctx->osd, video_pts);
             osd_query_and_reset_want_redraw(mpctx->osd);
