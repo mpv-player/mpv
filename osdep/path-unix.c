d72da407db8f2a7eefce558824118444d8fd96c7
Andreas Rammhold <andreas@rammhold.de>
2021-07-24 13:50:06 +0200

    path: support looking up config files via XDG_CONFIG_DIRS
    This adds a fallback to XDG_CONFIG_DIRS if neither ~/.config/mpv nor
    ~/.mpv exist.
    
    XDG_CONFIG_DIRS is a colon (:) separated list of paths just like PATH
    where the first entry in the list has the highest priority. We append
    `/mpv` to each of these directories and set mpv_home to that value, if
    it exists.
    
    If none of the XDG_CONFIG_DIRS exist the previous default is used.

--- osdep/path-unix.c
+++ osdep/path-unix.c
@@ -32,6 +32,7 @@ static void path_init(void)
 {
     char *home = getenv("HOME");
     char *xdg_dir = getenv("XDG_CONFIG_HOME");
+    char *xdg_config_dirs = getenv("XDG_CONFIG_DIRS");
 
     if (xdg_dir && xdg_dir[0]) {
         snprintf(mpv_home, sizeof(mpv_home), "%s/mpv", xdg_dir);
@@ -48,6 +49,44 @@ static void path_init(void)
     if (mp_path_exists(old_home) && !mp_path_exists(mpv_home)) {
         snprintf(mpv_home, sizeof(mpv_home), "%s", old_home);
         old_home[0] = '\0';
+        return;
+    }
+
+    // no previous mpv_home was found so continue searching through XDG_CONFIG_DIRS
+    if (!mp_path_exists(mpv_home)) {
+        char tmp[512];
+        char *token;
+        char *buf;
+        char *original_buf;
+        if (!xdg_config_dirs || !xdg_config_dirs[0]) {
+            // If the XDG_CONFIG_DIRS variable is empty or unset default to
+            // /etc/xdg as per spec.
+            xdg_config_dirs = "/etc/xdg";
+        }
+
+        // create a copy of the xdg_config_dirs var as strsep is mutating the original value
+        buf = original_buf = strdup(xdg_config_dirs);
+        if (buf == NULL)
+            return;
+
+        // For each colon (:) delimited path in the variable search for an mpv directory.
+        // The first directory that was found is set as mpv_home.
+        while ((token = strsep(&buf, ":"))) {
+            if (snprintf(tmp, sizeof(tmp), "%s/mpv", token) >= sizeof(tmp)) {
+                // new path doesn't fit in the buffer, use previous match
+                break;
+            }
+
+            if (mp_path_exists(tmp)) {
+                // copy to the destination buffer, we don't have to
+                // care about the return value of snprintf as the size
+                // has already been checked above.
+                (void) snprintf(mpv_home, sizeof(mpv_home), "%s", tmp);
+                break;
+            }
+        }
+
+        free(original_buf);
     }
 }
 
