mpv-player/mpv

commit 24357cb7b5a1a8ca4407c2e6d9316f07a7a5ef0d

wayland: cleanup handle_toplevel_config
The source of many geometry woes. There's some loosely related toplevel
things that should be cleaned up/fixed. First of all,
VO_EVENT_LIVE_RESIZING is actually completely useless. It might have
been useful sometime in the past, but there's no point. It doesn't
"speed up" resizing in any way and appears to be originally for cocoa.
Just remove it.

Way back in the day, toplevel_width/height was added as a workaround for
when we got uncoorperative (i.e. wrong) width/height coordinates from
the compositor in this event. Basically it could happen due to numerous
reasons but a lack of atomic commits was part of the reason and also
mpv's geometry handling then was a lot rougher. We *shouldn't* need this
workaround anymore. The width/height values are only used exactly when
we need them. If mpv sets geometry on its own, it should still be the
right dimensions.

Related to the above, mpv never actually propertly handled the case
where width or height was equal to 0. According to the xdg-shell spec,
"If the width or height arguments are zero, it means the client should
decided its own window dimension." An example of a compositor doing this
is weston. It's, unsurprisingly, broken. Getting out of fullscreen or a
maximized state does not restore the old window size like it should. The
right way to handle this is to just return near the end of the function
if we have a 0 for either argument and before any geometry is set
(wl->geometry's width or height can never be zero). Luckily, state
changes are already being detected so they just trigger the goto when
needed.

Finally, e2c24ad mistakenly removed the VO_EVENT_EXPOSE. There are edge
cases where this is needed and it's safer to just force a redraw here
when the window gets activated again. Just force wl->hidden to false
first and then trigger the expose.

video/out/wayland_common.c
@@ -796,7 +796,6 @@ static void handle_toplevel_config(void *data, struct xdg_toplevel *toplevel,
             is_fullscreen = true;
             break;
         case XDG_TOPLEVEL_STATE_RESIZING:
-            wl->pending_vo_events |= VO_EVENT_LIVE_RESIZING;
             break;
         case XDG_TOPLEVEL_STATE_ACTIVATED:
             is_activated = true;
@@ -839,15 +838,12 @@ static void handle_toplevel_config(void *data, struct xdg_toplevel *toplevel,
             wl->focused = !wl->focused;
             wl->pending_vo_events |= VO_EVENT_FOCUS;
         }
+        /* Just force a redraw to be on the safe side. */
+        if (wl->activated) {
+            wl->hidden = false;
+            wl->pending_vo_events |= VO_EVENT_EXPOSE;
+        }
     }
-
-    if (!(wl->pending_vo_events & VO_EVENT_LIVE_RESIZING))
-        vo_query_and_reset_events(wl->vo, VO_EVENT_LIVE_RESIZING);
-
-    int old_toplevel_width = wl->toplevel_width;
-    int old_toplevel_height = wl->toplevel_height;
-    wl->toplevel_width = width;
-    wl->toplevel_height = height;
 
     if (wl->scale_change) {
         wl_surface_set_buffer_scale(wl->surface, wl->scaling);
@@ -862,12 +858,12 @@ static void handle_toplevel_config(void *data, struct xdg_toplevel *toplevel,
         }
     }
 
-    if (old_toplevel_width == wl->toplevel_width && old_toplevel_height == wl->toplevel_height)
+    if (width == 0 || height == 0)
         return;
 
     if (!is_fullscreen && !is_maximized) {
         if (vo_opts->keepaspect && vo_opts->keepaspect_window) {
-            if (abs(wl->toplevel_width - old_toplevel_width) > abs(wl->toplevel_height - old_toplevel_height)) {
+            if (width > height) {
                 double scale_factor = (double)width / wl->reduced_width;
                 width = wl->reduced_width * scale_factor;
             } else {

video/out/wayland_common.h
@@ -50,8 +50,6 @@ struct vo_wayland_state {
     int gcd;
     int reduced_height;
     int reduced_width;
-    int toplevel_width;
-    int toplevel_height;
 
     /* State */
     bool activated;

commit 19c4ae004a14617c8ecff191119723d0b7731b0c

wayland: keep track of toplevel width/height again
Regression from 24357cb. It's ugly but unfortunately keeping tracking of
the last toplevel width and height really is the best way to solve this
problem and removing it was a mistake. Compositors don't always send
width/height coordinates of the actual window. The easiest way to
trigger this is by changing window-scale/current-window-scale and then
unfocusing the window. The compositor will send another toplevel
configure event with coordinates of the window before the resize. Maybe
compositors could be smarter but multiple ones do this (sway, gnome,
plasma), so just keep the old workaround. The only difference this time
is that the toplevel width/height is saved at the very beginning which
also covers the case where it equals 0 (i.e. weston).

video/out/wayland_common.c
@@ -782,6 +782,11 @@ static void handle_toplevel_config(void *data, struct xdg_toplevel *toplevel,
     struct mp_vo_opts *vo_opts = wl->vo_opts;
     struct mp_rect old_geometry = wl->geometry;
 
+    int old_toplevel_width = wl->toplevel_width;
+    int old_toplevel_height = wl->toplevel_height;
+    wl->toplevel_width = width;
+    wl->toplevel_height = height;
+
     /* Don't do anything here if we haven't finished setting geometry. */
     if (mp_rect_w(wl->geometry) == 0 || mp_rect_h(wl->geometry) == 0)
         return;
@@ -858,7 +863,8 @@ static void handle_toplevel_config(void *data, struct xdg_toplevel *toplevel,
         }
     }
 
-    if (width == 0 || height == 0)
+    if (old_toplevel_width == wl->toplevel_width &&
+        old_toplevel_height == wl->toplevel_height)
         return;
 
     if (!is_fullscreen && !is_maximized) {

video/out/wayland_common.h
@@ -50,6 +50,8 @@ struct vo_wayland_state {
     int gcd;
     int reduced_height;
     int reduced_width;
+    int toplevel_width;
+    int toplevel_height;
 
     /* State */
     bool activated;