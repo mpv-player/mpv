629b63740611ddb9072487988320a9ddf4bb2fa7
rcombs <rcombs@rcombs.me>
2021-11-13 17:32:51 -0600

    vd_lavc: enable hwdec for prores by default

--- video/decode/vd_lavc.c
+++ video/decode/vd_lavc.c
@@ -136,7 +136,7 @@ const struct m_sub_options vd_lavc_conf = {
         .framedrop = AVDISCARD_NONREF,
         .dr = 1,
         .hwdec_api = "no",
-        .hwdec_codecs = "h264,vc1,hevc,vp8,vp9,av1",
+        .hwdec_codecs = "h264,vc1,hevc,vp8,vp9,av1,prores",
         // Maximum number of surfaces the player wants to buffer. This number
         // might require adjustment depending on whatever the player does;
         // for example, if vo_gpu increases the number of reference surfaces for
