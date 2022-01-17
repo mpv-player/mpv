22b0bac28e6c51651dedc7c270fc009309f2cb6b
Philip Langdale <philipl@overt.org>
2022-01-17 13:45:47 -0800

    ao/pipewire: Add copyright header
    Sometimes the most obvious things can be missed.
    
    Reflects authorship described in the original commit.
    
    * https://github.com/mpv-player/mpv/pull/7902
    * https://github.com/Oschowa/mpv/commit/fddb143282fa74425a8a6f29c9566e51777759d0
    * https://github.com/mpv-player/mpv/pull/9587

--- audio/out/ao_pipewire.c
+++ audio/out/ao_pipewire.c
@@ -1,3 +1,24 @@
+/*
+ * PipeWire audio output driver.
+ * Copyright (C) 2021 Thomas Weißschuh <thomas@t-8ch.de>
+ * Copyright (C) 2021 Oschowa <oschowa@web.de>
+ * Copyright (C) 2020 Andreas Kempf <aakempf@gmail.com>
+ *
+ * This file is part of mpv.
+ *
+ * mpv is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * mpv is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <pipewire/pipewire.h>
 #include <spa/param/audio/format-utils.h>
