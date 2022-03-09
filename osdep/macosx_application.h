70b26263645bbf205cb3be5be91bf7257999c10e
eko5624 <562433403@qq.com>
2022-03-09 13:23:10 +0800

    Update macosx_application.h

--- osdep/macosx_application.h
+++ osdep/macosx_application.h
@@ -26,6 +26,39 @@ enum {
     FRAME_WHOLE,
 };
 
+enum {
+    MAC_CSP_AUTO = -1,
+    MAC_CSP_DISPLAY_P3, //macOS 10.11.2+
+    MAC_CSP_DISPLAY_P3_HLG, //macOS 10.14.6+
+    MAC_CSP_DISPLAY_P3_PQ_EOTF, //macOS 10.14.6–10.15.4
+    MAC_CSP_DCIP3, //macOS 10.11+
+    MAC_CSP_ITUR_2020, //macOS 10.11+
+    MAC_CSP_ITUR_709, //macOS 10.11+
+
+    MAC_CSP_SRGB, //macOS 10.5+
+    MAC_CSP_LINEAR_SRGB, //macOS 10.12+
+    MAC_CSP_GENERIC_RGB_LINEAR, //macOS 10.5+
+    MAC_CSP_ADOBE_RGB1998, //macOS 10.5+
+
+    // no documentation?
+    MAC_CSP_DISPLAY_P3_PQ, //macOS 10.15.4+
+
+
+    // extended formats with values below 0.0 and above 1.0, useless?
+    MAC_CSP_EXTENDED_LINEAR_DISPLAY_P3, //macOS 10.14.3+
+    MAC_CSP_EXTENDED_SRGB, //macOS 10.12+
+    MAC_CSP_EXTENDED_LINEAR_SRGB, //macOS 10.12+
+    MAC_CSP_EXTENDED_LINEAR_ITUR_2020, //macOS 10.14.3+
+    // pixel values between 0.0 and 12.0
+    MAC_CSP_ITUR_2020_HLG, //macOS 10.15.6–11.0
+    // pixel value of 1.0 is assumed to be 100 nits
+    MAC_CSP_ITUR_2020_PQ_EOTF, //macOS 10.14.6–10.15.4
+
+    // no documentation?
+    MAC_CSP_EXTENDED_DISPLAY_P3, //macOS 11.0+
+    MAC_CSP_EXTENDED_ITUR_2020, //macOS 11.0+
+};
+
 struct macos_opts {
     int macos_title_bar_style;
     int macos_title_bar_appearance;
@@ -35,6 +68,7 @@ struct macos_opts {
     int macos_force_dedicated_gpu;
     int macos_app_activation_policy;
     int macos_geometry_calculation;
+    int macos_output_csp;
     int cocoa_cb_sw_renderer;
     int cocoa_cb_10bit_context;
 };
