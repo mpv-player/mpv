b1639ee561c7d62a8797bf8700bbec563e78d0c9
Aaron Boxer <boxerab@protonmail.com>
2022-05-26 15:49:26 -0400

    vo: allow vaapi_wayland and vaapi_x11 to coexist

--- video/out/vo.c
+++ video/out/vo.c
@@ -93,9 +93,10 @@ const struct vo_driver *const video_out_drivers[] =
 #if HAVE_SDL2_VIDEO
     &video_out_sdl,
 #endif
-#if HAVE_VAAPI_WAYLAND
+#if HAVE_VAAPI_WAYLAND && HAVE_MEMFD_CREATE
     &video_out_vaapi_wayland,
-#elif HAVE_VAAPI_X11 && HAVE_GPL
+#endif
+#if HAVE_VAAPI_X11 && HAVE_GPL
     &video_out_vaapi,
 #endif
 #if HAVE_X11
