1840ee42620243fdc493eeddf6603ad2982b0ebf
Dudemanguy <random342@airmail.cc>
2022-10-26 23:23:49 -0500

    vo_dmabuf_wayland: use special ra_ctx_create_by_name
    vo_dmabuf_wayland has its own ra and context so it can handle all the
    different hwdec correctly. Unfortunately, this API was pretty clearly
    designed with vo_gpu/vo_gpu_next in mind and has a lot of concepts that
    don't make sense for vo_dmabuf_wayland. We still want to bolt on a
    ra_ctx, but instead let's rework the ra_ctx logic a little bit. First,
    this introduces a hidden bool within the ra_ctx_fns that is used to hide
    the wldmabuf context from users as an option (unlike the other usual
    contexts). We also want to make sure that hidden contexts wouldn't be
    autoprobed in the usual ra_ctx_create, so we be sure to skip those in
    that function. Additionally, let's create a new ra_ctx_create_by_name
    function which does exactly what says. It specifically selects a context
    based on a passed string. This function has no probing or option logic
    is simplified just for what vo_dmabuf_wayland needs. The api/context
    validations functions are modified just a little bit to make sure hidden
    contexts are skipped and the documentation is updated to remove this
    entries. Fixes #10793.

--- video/out/wldmabuf/context_wldmabuf.c
+++ video/out/wldmabuf/context_wldmabuf.c
@@ -39,6 +39,7 @@ static bool init(struct ra_ctx *ctx)
 const struct ra_ctx_fns ra_ctx_wldmabuf = {
     .type               = "none",
     .name               = "wldmabuf",
+    .hidden             = true,
     .init               = init,
     .uninit             = uninit,
 };
