mpv-player/mpv

commit eb4da3400a1c37eea7b258b00297e8b8fcdd8db4

ci/msys2: switch to Python 3.11 to fix crashes during build
There is a long-standing bug with random crashes of Python 3.10 on CI.

See:
https://github.com/python/cpython/issues/105400
https://github.com/msys2/MINGW-packages/issues/11864
https://github.com/msys2/MINGW-packages/issues/17415

.github/workflows/build.yml
@@ -271,7 +271,7 @@ jobs:
             meson:p
             ninja:p
             pkgconf:p
-            python:p
+            python3.11:p
             rst2pdf:p
             rubberband:p
             shaderc:p

ci/build-msys2.sh
@@ -2,6 +2,7 @@
 
 if [ "$1" = "meson" ]; then
     meson setup build            \
+      --native-file=ci/msys2-meson.txt \
       -D cdda=enabled            \
       -D d3d-hwaccel=enabled     \
       -D d3d11=enabled           \

commit 9f597f188adb618ed84d3f05f1c1837399053179

ci/msys2: double down on running meson through python3.11
Run not only scripts inside build system, but also meson itself with
Python 3.11.

Fixes: eb4da3400a1c37eea7b258b00297e8b8fcdd8db4

.github/workflows/build.yml
@@ -255,7 +255,6 @@ jobs:
             libjpeg-turbo:p
             libplacebo:p
             lua51:p
-            meson:p
             ninja:p
             pkgconf:p
             python3.11:p
@@ -280,6 +279,7 @@ jobs:
       - name: Run meson tests
         id: tests
         run: |
+          source ./venv/bin/activate
           meson test -C build
 
       - name: Print meson test log

ci/build-msys2.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -e
 
 if [ "$1" = "meson" ]; then
+    python3.11 -m venv venv
+    source ./venv/bin/activate
+    python -m pip install meson
     meson setup build            \
-      --native-file=ci/msys2-meson.txt \
       -D cdda=enabled            \
       -D d3d-hwaccel=enabled     \
       -D d3d11=enabled           \