7e57c5eec6eaf3596cbb4b04bf08815da1b8b2a8
Dudemanguy <random342@airmail.cc>
2023-08-10 20:15:04 -0500

    vf_vapoursynth: save display resolution as a variable
    mpv has a generic method for getting the display resolution, so we can
    save it in vf_vapoursynth without too much trouble. Unfortunately, the
    resolution won't actually be available in many cases (like my own)
    because the windowing backend doesn't actually know it yet. It looks
    like at least windows always returns the default monitor (maybe we
    should do something similar for x11 and wayland), so there's at least
    some value. Of course, this still has a bunch of pitfalls like not being
    able to cope with multi monitor setups at all but so does display_fps.
    As an aside, the vapoursynth API this uses apparently requires R26 (an
    ancient version anyway), so bump the build to compensate for this.
    Fixes #11510

--- filters/filter.h
+++ filters/filter.h
@@ -398,6 +398,7 @@ struct mp_stream_info {
     void *priv; // for use by whoever implements the callbacks
 
     double (*get_display_fps)(struct mp_stream_info *i);
+    void   (*get_display_res)(struct mp_stream_info *i, int *res);
 
     struct mp_hwdec_devices *hwdec_devs;
     struct osd_state *osd;
