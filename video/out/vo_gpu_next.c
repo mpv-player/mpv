mpv-player/mpv

commit a8192eda6cfc909fc9f5f62e36523b53c0300eff

vo_gpu_next: add --libplacebo-opts
To help test not-yet-exposed options, and for debugging purposes.

video/out/vo_gpu_next.c
@@ -145,6 +145,7 @@ struct priv {
     const struct pl_hook **hooks; // storage for `params.hooks`
     const struct pl_filter_config *frame_mixer;
     enum mp_csp_levels output_levels;
+    char **raw_opts;
 
     struct pl_icc_profile icc_profile;
     char *icc_path;
@@ -794,6 +795,11 @@ static void update_options(struct vo *vo)
     pars->color_adjustment.saturation = cparams.saturation;
     pars->color_adjustment.gamma = cparams.gamma;
     p->output_levels = cparams.levels_out;
+
+#if PL_API_VER >= 309
+    for (char **kv = p->raw_opts; kv && kv[0]; kv += 2)
+        pl_options_set_str(pars, kv[0], kv[1]);
+#endif
 }
 
 static void apply_target_contrast(struct priv *p, struct pl_color_space *color)
@@ -2085,6 +2091,7 @@ const struct vo_driver video_out_gpu_next = {
         {"target-lut", OPT_STRING(target_lut.opt), .flags = M_OPT_FILE},
         {"target-colorspace-hint", OPT_BOOL(target_hint)},
         // No `target-lut-type` because we don't support non-RGB targets
+        {"libplacebo-opts", OPT_KEYVALUELIST(raw_opts)},
         {0}
     },
 };

commit 7ae4396ea55f555b0dc2c61d5fcd65f3c4e52b80

vo_gpu_next: fix typo in video_screenshot()
Undefined behavior (bad initializer). Somehow didn't trigger a warning
on my end...

Fixes: a8192eda6cfc909fc9f5f62e36523b53c0300eff

video/out/vo_gpu_next.c
@@ -1177,7 +1177,7 @@ static bool update_auto_profile(struct priv *p, int *events)
 static void video_screenshot(struct vo *vo, struct voctrl_screenshot *args)
 {
     struct priv *p = vo->priv;
-    pl_options pars = pars;
+    pl_options pars = p->pars;
     pl_gpu gpu = p->gpu;
     pl_tex fbo = NULL;
     args->res = NULL;