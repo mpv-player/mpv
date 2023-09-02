mpv-player/mpv

commit 15252e13d5bb367a8f86ffbc5474488b841dc96c

{zsh,bash}-completion: run mpv with --no-config
Reading the config when we're just interested in the option list
(not sure why mpv does that anyway) only has the potential to mess
this process up when it prints errors or the user has funny options
like msg-level or log-file set, so avoid doing so.

etc/_mpv.zsh
@@ -42,7 +42,7 @@ function _mpv_generate_arguments {
   local -a option_aliases=()
 
   local list_options_line
-  for list_options_line in "${(@f)$($~words[1] --list-options)}"; do
+  for list_options_line in "${(@f)$($~words[1] --no-config --list-options)}"; do
 
     [[ $list_options_line =~ $'^[ \t]+--([^ \t]+)[ \t]*(.*)' ]] || continue
 

etc/mpv.bash-completion
@@ -18,7 +18,7 @@
 #
 
 # Cache all the mpv options
-_mpv_options=$(mpv --list-options)
+_mpv_options=$(mpv --no-config --list-options)
 
 _mpv_get_args()
 {

commit 5bd51aaa449bb53faa0fb00f468a5a9ac2e2f333

{zsh,bash}-completion: add missing --no-config flags
Seemingly forgotten, even though the intent was to add them.

Fixes: 15252e13d5bb367a8f86ffbc5474488b841dc96c

etc/_mpv.zsh
@@ -150,7 +150,7 @@ function _mpv_generate_arguments {
 function _mpv_generate_protocols {
   _mpv_completion_protocols=()
   local list_protos_line
-  for list_protos_line in "${(@f)$($~words[1] --list-protocols)}"; do
+  for list_protos_line in "${(@f)$($~words[1] --no-config --list-protocols)}"; do
     if [[ $list_protos_line =~ $'^[ \t]+(.*)' ]]; then
       _mpv_completion_protocols+="$match[1]"
     fi
@@ -211,7 +211,7 @@ case $state in
     esac
     local -a values
     local current
-    for current in "${(@f)$($~words[1] --${option_name}=help)}"; do
+    for current in "${(@f)$($~words[1] --no-config --${option_name}=help)}"; do
       [[ $current =~ $pattern ]] || continue;
       local name=${match[name_group]//:/\\:} desc=${match[desc_group]}
       if [[ -n $desc ]]; then

etc/mpv.bash-completion
@@ -42,7 +42,7 @@ _mpv_get_args()
         _filedir 2>/dev/null || COMPREPLY=($(compgen -f))
         return 0
       else
-        candidates=($(mpv $1=help | grep -v ':' | awk '{print $1;}'))
+        candidates=($(mpv --no-config $1=help | grep -v ':' | awk '{print $1;}'))
         candidates+=("help")
       fi
       ;;
@@ -50,16 +50,16 @@ _mpv_get_args()
       candidates=("yes" "no" "help")
       ;;
     Choices:|Object)
-      candidates=($(mpv $1=help | grep -v ':' | awk '{print $1;}'))
+      candidates=($(mpv --no-config $1=help | grep -v ':' | awk '{print $1;}'))
       candidates+=("help")
       ;;
     Image)
-      candidates=($(mpv $1=help))
+      candidates=($(mpv --no-config $1=help))
       candidates=("${candidates[@]:2}")
       candidates+=("help")
       ;;
     Profile)
-      candidates=($(mpv $1= | grep -v ':' | awk '{print $1;}'))
+      candidates=($(mpv --no-config $1= | grep -v ':' | awk '{print $1;}'))
       ;;
     *)
       # There are other categories; some of which we could do something smarter