mpv-player/mpv

commit 12ffce0f224056f91a20c9f0b197f4973931efbe

vo_gpu: lower default deband threshold
The previous default was found to be too aggressive for most video.
Change to a lower value to prevent destroying too much detail.

video/out/gpu/video_shaders.c
@@ -946,7 +946,7 @@ struct deband_opts {
 
 const struct deband_opts deband_opts_def = {
     .iterations = 1,
-    .threshold = 64.0,
+    .threshold = 32.0,
     .range = 16.0,
     .grain = 48.0,
 };

commit 62535f179ca36b7b6c98a19b9607bc270b170894

vo_gpu: match libplacebo debanding defaults
The defaults were awful and horribly regressed many files while also not
fixing banding on files that actually needed it, sometimes even
*increasing* banding due to the low threshold.

Fixes: 12ffce0f224056f91a20c9f0b197f4973931efbe
See-Also: haasn/libplacebo@e1e43376d16d5112ee1254534664b0b85110139b

video/out/gpu/video_shaders.c
@@ -942,9 +942,9 @@ static void prng_init(struct gl_shader_cache *sc, AVLFG *lfg)
 
 const struct deband_opts deband_opts_def = {
     .iterations = 1,
-    .threshold = 32.0,
+    .threshold = 48.0,
     .range = 16.0,
-    .grain = 48.0,
+    .grain = 32.0,
 };
 
 #define OPT_BASE_STRUCT struct deband_opts