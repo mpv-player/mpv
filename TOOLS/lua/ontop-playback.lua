a4b9426c83c24f7d0239cf25f6c90436061b2222
KFB <kungfubeaner@users.noreply.github.com>
2023-09-24 16:57:12 -0500

    Update ontop-playback.lua
    This version addresses some scenarios not addressed by the previous version. Update:  changed 'ontop' to true for the value

--- TOOLS/lua/ontop-playback.lua
+++ TOOLS/lua/ontop-playback.lua
@@ -1,19 +1,31 @@
 --makes mpv disable ontop when pausing and re-enable it again when resuming playback
 --please note that this won't do anything if ontop was not enabled before pausing
 
-local was_ontop = false
+was_ontop = mp.get_property_native("ontop")
+
+function f_load(event)
+    if was_ontop then
+        mp.set_property_native("ontop", true)
+    end
+end
+
+mp.register_event("file-loaded", f_load)
 
 mp.observe_property("pause", "bool", function(name, value)
-    local ontop = mp.get_property_native("ontop")
+
+local ontop = mp.get_property_native("ontop")
+local is_idle = mp.get_property_native("playback-abort")
     if value then
-        if ontop then
+        mp.set_property_native("ontop", false)
+        was_ontop = true
+    else
+        if is_idle then
             mp.set_property_native("ontop", false)
             was_ontop = true
+        else
+            if was_ontop then
+                mp.set_property_native("ontop", true)
+            end
         end
-    else
-        if was_ontop and not ontop then
-            mp.set_property_native("ontop", true)
-        end
-        was_ontop = false
     end
 end)
