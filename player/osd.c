fedb27e7286f5e7df799391f9a8e4e29a641d4a9
Kacper Michajłow <kasper93@gmail.com>
2023-10-20 20:57:04 +0200

    Revert "player: cut off status line on terminal width"
    No longer needed, wrapped status line is supported now. Also this didn't
    work correctly if status were decorated with module name or time.
    
    This reverts commit ab6fac43b4bc34949bd9c4da8e911fc9f3489a32.

--- player/osd.c
+++ player/osd.c
@@ -134,11 +134,6 @@ static void term_osd_set_status_lazy(struct MPContext *mpctx, const char *text)
 {
     talloc_free(mpctx->term_osd_status);
     mpctx->term_osd_status = talloc_strdup(mpctx, text);
-
-    int w = 80, h = 24;
-    terminal_get_size(&w, &h);
-    if (strlen(mpctx->term_osd_status) > w && !strchr(mpctx->term_osd_status, '\n'))
-        mpctx->term_osd_status[w] = '\0';
 }
 
 static void add_term_osd_bar(struct MPContext *mpctx, char **line, int width)
