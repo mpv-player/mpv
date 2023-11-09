95e3ba0e81e03f216502c0261cdc930830331570
Dan <dan@wadoku.de>
2023-11-09 22:31:44 +0100

    mac: replace `NSScreen.displayName` with system provided `NSScreen.localizedName`

--- osdep/macos/swift_extensions.swift
+++ osdep/macos/swift_extensions.swift
@@ -28,37 +28,6 @@ extension NSScreen {
             return deviceDescription[.screenNumber] as? CGDirectDisplayID ?? 0
         }
     }
-
-    public var displayName: String? {
-        get {
-            var name: String? = nil
-            var object: io_object_t
-            var iter = io_iterator_t()
-            let matching = IOServiceMatching("IODisplayConnect")
-            let result = IOServiceGetMatchingServices(kIOMasterPortDefault, matching, &iter)
-
-            if result != KERN_SUCCESS || iter == 0 { return nil }
-
-            repeat {
-                object = IOIteratorNext(iter)
-                if let info = IODisplayCreateInfoDictionary(object, IOOptionBits(kIODisplayOnlyPreferredName)).takeRetainedValue() as? [String:AnyObject],
-                    (info[kDisplayVendorID] as? UInt32 == CGDisplayVendorNumber(displayID) &&
-                    info[kDisplayProductID] as? UInt32 == CGDisplayModelNumber(displayID) &&
-                    info[kDisplaySerialNumber] as? UInt32 ?? 0 == CGDisplaySerialNumber(displayID))
-                {
-                    if let productNames = info["DisplayProductName"] as? [String:String],
-                       let productName = productNames.first?.value
-                    {
-                        name = productName
-                        break
-                    }
-                }
-            } while object != 0
-
-            IOObjectRelease(iter)
-            return name
-        }
-    }
 }
 
 extension NSColor {
