44858bf5c6894485da869f9d7fc320fd9db8c402
Dudemanguy <random342@airmail.cc>
2023-11-27 13:12:06 -0600

    ci: remove leftover libplacebo subproject options
    486bb93dfa90bf948cff8d77ad685f54d0928531 removed the wrap which made
    these specific options obsolete.

--- ci/build-tumbleweed.sh
+++ ci/build-tumbleweed.sh
@@ -3,7 +3,6 @@ set -e
 
 meson setup build \
   --werror        \
-  -Dlibplacebo:werror=false \
   -Dc_args="-Wno-error=deprecated -Wno-error=deprecated-declarations" \
   -Db_sanitize=address,undefined \
   -Dcdda=enabled          \
