5a75f575400256b9d7c8ade3fedc7d7f10a5fd5d
nanahi <130121847+na-na-hi@users.noreply.github.com>
2024-01-02 16:24:41 -0500

    w32_common: add function to control window transparency state
    Use the DWM API to enable and disable compositor transparency.

--- video/out/w32_common.h
+++ video/out/w32_common.h
@@ -32,5 +32,6 @@ int vo_w32_control(struct vo *vo, int *events, int request, void *arg);
 void vo_w32_config(struct vo *vo);
 HWND vo_w32_hwnd(struct vo *vo);
 void vo_w32_run_on_thread(struct vo *vo, void (*cb)(void *ctx), void *ctx);
+void vo_w32_set_transparency(struct vo *vo, bool enable);
 
 #endif /* MPLAYER_W32_COMMON_H */
