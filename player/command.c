57d01b7d9e214ed6aabccaeda3d170d11db341e8
Dudemanguy <random342@airmail.cc>
2024-01-19 22:46:49 -0600

    player/command: reselect track after UPDATE_SUB_HARD
    The lingering cache needs to be cleared so the packets don't stay
    forever on the screen past their welcome. Do this by simply refreshing
    the stream. Fixes #13148.

--- player/command.c
+++ player/command.c
@@ -6980,6 +6980,8 @@ void mp_option_change_callback(void *ctx, struct m_config_option *co, int flags,
                                       (void *)(uintptr_t)flags);
                 if (ret == CONTROL_OK && flags & (UPDATE_SUB_FILT | UPDATE_SUB_HARD))
                     sub_redecode_cached_packets(sub);
+                if (track->selected)
+                    reselect_demux_stream(mpctx, track, true);
             }
         }
         osd_changed(mpctx->osd);
