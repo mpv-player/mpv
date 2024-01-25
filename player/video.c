mpv-player/mpv

commit b0e6ac380f6dd1a3196de0b2426f71a3e3f68bff

Revert "player/video: loosen logic checks for adjust_sync"
This reverts commit cb2b579f61764452652c6cf5c6a94ae5e67c77ed.

This breaks files where the audio starts much later after the video
since mpv reads the first audio packet even if it isn't supposed to
start yet, resulting in the audio_status being STATUS_READY for the
entire time mpv is "waiting" for the first audio packet to be played.

player/video.c
@@ -343,7 +343,7 @@ static void adjust_sync(struct MPContext *mpctx, double v_pts, double frame_time
 {
     struct MPOpts *opts = mpctx->opts;
 
-    if (mpctx->audio_status == STATUS_EOF)
+    if (mpctx->audio_status != STATUS_PLAYING)
         return;
 
     mpctx->delay -= frame_time;
@@ -388,7 +388,7 @@ static void handle_new_frame(struct MPContext *mpctx)
         }
     }
     mpctx->time_frame += frame_time / mpctx->video_speed;
-    if (frame_time)
+    if (mpctx->video_status >= STATUS_PLAYING)
         adjust_sync(mpctx, pts, frame_time);
     MP_TRACE(mpctx, "frametime=%5.3f\n", frame_time);
 }

commit ff209a831ebb8667e40d50f63dea9136a1473d40

video: adjust avsync when draining audio
The full revert in b0e6ac380f was actually not needed, doing so causes
a single frame to drop when mpv is draining audio after a discontinuity
(e.g. seek). Calculating A-V sync change while draining audio actually
makes sense in hindsight, and probably should've been the original
commit.

This actually gets us the benefits of cb2b579f61 without breaking files
where the audio doesn't start together with the video.

Fixes: b0e6ac380f ("Revert "player/video: loosen logic checks for adjust_sync"")

player/video.c
@@ -343,7 +343,7 @@ static void adjust_sync(struct MPContext *mpctx, double v_pts, double frame_time
 {
     struct MPOpts *opts = mpctx->opts;
 
-    if (mpctx->audio_status != STATUS_PLAYING)
+    if (mpctx->audio_status == STATUS_EOF)
         return;
 
     mpctx->delay -= frame_time;