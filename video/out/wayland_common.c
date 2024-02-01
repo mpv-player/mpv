mpv-player/mpv

commit f54ad8eb055771a080f079964504cb0f32fc4014

wayland: add mouse buttons and fix axis scaling
Previously, the only mouse buttons supported in wayland were left,
right, and middle click. This adds the thumb back/forward buttons as
valid bindings. Also it removes the old, default behavior of always
sending a right click if an unrecognized mouse button is clicked.
In a related but different fix, the magnitude of an axis event in
wayland is not important to mpv since it internally handles all scaling.
The only thing we care about is getting the sign when the event occurs.

video/out/wayland_common.c
@@ -137,10 +137,30 @@ static void pointer_handle_button(void *data, struct wl_pointer *wl_pointer,
     state = state == WL_POINTER_BUTTON_STATE_PRESSED ? MP_KEY_STATE_DOWN
                                                      : MP_KEY_STATE_UP;
 
-    button = button == BTN_LEFT   ? MP_MBTN_LEFT :
-             button == BTN_MIDDLE ? MP_MBTN_MID  : MP_MBTN_RIGHT;
+    switch (button) {
+    case BTN_LEFT:
+        button = MP_MBTN_LEFT;
+        break;
+    case BTN_MIDDLE:
+        button = MP_MBTN_MID;
+        break;
+    case BTN_RIGHT:
+        button = MP_MBTN_RIGHT;
+        break;
+    case BTN_SIDE:
+        button = MP_MBTN_BACK;
+        break;
+    case BTN_EXTRA:
+        button = MP_MBTN_FORWARD;
+        break;
+    default:
+        button = 0;
+        break;
+    }
 
-    mp_input_put_key(wl->vo->input_ctx, button | state);
+    if (button) {
+        mp_input_put_key(wl->vo->input_ctx, button | state);
+    }
 
     if (!mp_input_test_dragging(wl->vo->input_ctx, wl->mouse_x, wl->mouse_y) &&
         (button == MP_MBTN_LEFT) && (state == MP_KEY_STATE_DOWN))
@@ -151,7 +171,7 @@ static void pointer_handle_axis(void *data, struct wl_pointer *wl_pointer,
                                 uint32_t time, uint32_t axis, wl_fixed_t value)
 {
     struct vo_wayland_state *wl = data;
-    double val = wl_fixed_to_double(value)*0.1;
+    double val = wl_fixed_to_double(value)/abs(wl_fixed_to_double(value));
     switch (axis) {
     case WL_POINTER_AXIS_VERTICAL_SCROLL:
         if (value > 0)

commit b287089638c29c46e6a9254b664fda80462b1929

wayland_common: properly handle high resolution scrolling
Commit f54ad8eb055771a080f079964504cb0f32fc4014 broke high resolution
scrolling because one logical mouse click is generated for every scroll
event, no matter the magnitude. This makes scrolling on trackpad way
too fast.

Revert the axis scaling change in that commit and clamp the resulting
value between -1 and 1 to make sure mouse wheel scrolling works as
intended on compositors which send a value larger than 10 for these events.

video/out/wayland_common.c
@@ -295,7 +295,12 @@ static void pointer_handle_axis(void *data, struct wl_pointer *wl_pointer,
 {
     struct vo_wayland_state *wl = data;
 
-    double val = wl_fixed_to_double(value) < 0 ? -1 : 1;
+    // The axis value is specified in logical coordinates, but the exact value emitted
+    // by one mouse wheel click is unspecified. In practice, most compositors use either
+    // 10 (GNOME, Weston) or 15 (wlroots, same as libinput) as the value.
+    // Divide the value by 10 and clamp it between -1 and 1 so that mouse wheel clicks
+    // work as intended on all compositors while still allowing high resolution trackpads.
+    double val = MPCLAMP(wl_fixed_to_double(value) / 10.0, -1, 1);
     switch (axis) {
     case WL_POINTER_AXIS_VERTICAL_SCROLL:
         if (value > 0)