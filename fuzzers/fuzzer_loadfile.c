47dbc3a74e09d2815c5649dc4cc0c7cf822392bf
Kacper Michajłow <kasper93@gmail.com>
2024-02-03 05:40:06 +0100

    fuzzers: add new fuzzer targets
    fuzzer_set_property.c:
    
    fuzz mpv_set_property in both initialized and non-initialized state.
    Useful for user provided values sanitization test. I've already seen
    some memory leaks in parsing code, good to drill it.
    
    fuzzer_loadfile.c:
    
    mpv_command "loadfile" test. Good for testing demuxers, decoding and
    playback loop. Sadly in headless mode we can't really test AO and VO,
    but at least all the code around can be fuzzed. Especially our custom
    demuxers like demux_mkv.
    
    fuzzer_loadfile_direct.c:
    
    Similar to loadfile above, but instead of saving the data to file, it
    passes the fuzz input in the command. Generated protocol specific
    versions (mf:// and memory:// for now) and generic one.
    
    Nothing really complex, but good start and even those few targets should
    give good coverage of the most common code paths in libmpv.

---
+++ fuzzers/fuzzer_loadfile.c
@@ -0,0 +1,71 @@
+/*
+ * This file is part of mpv.
+ *
+ * mpv is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * mpv is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+
+#include <libmpv/client.h>
+
+#include "common.h"
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    if (size == 0)
+        return -1;
+
+    char filename[15 + 10 + 1];
+    sprintf(filename, "/tmp/libfuzzer.%d", getpid());
+
+    FILE *fp = fopen(filename, "wb");
+    if (!fp)
+        exit(1);
+
+    if (fwrite(data, size, 1, fp) != 1)
+        exit(1);
+
+    if (fclose(fp))
+        exit(1);
+
+    mpv_handle *ctx = mpv_create();
+    if (!ctx)
+        exit(1);
+
+    check_error(mpv_set_option_string(ctx, "vo", "null"));
+    check_error(mpv_set_option_string(ctx, "ao", "null"));
+    check_error(mpv_set_option_string(ctx, "ao-null-untimed", "yes"));
+    check_error(mpv_set_option_string(ctx, "untimed", "yes"));
+    check_error(mpv_set_option_string(ctx, "video-osd", "no"));
+    check_error(mpv_set_option_string(ctx, "msg-level", "all=trace"));
+
+    check_error(mpv_initialize(ctx));
+
+    const char *cmd[] = {"loadfile", filename, NULL};
+    check_error(mpv_command(ctx, cmd));
+
+    while (1) {
+        mpv_event *event = mpv_wait_event(ctx, 10000);
+        if (event->event_id == MPV_EVENT_IDLE)
+            break;
+    }
+
+    mpv_terminate_destroy(ctx);
+
+    return 0;
+}
---
+++ fuzzers/fuzzer_set_property.c
@@ -0,0 +1,89 @@
+/*
+ * This file is part of mpv.
+ *
+ * mpv is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * mpv is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libmpv/client.h>
+
+#include "common.h"
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    size_t value_len;
+    switch (MPV_FORMAT)
+    {
+    case MPV_FORMAT_STRING:
+        value_len = strnlen(data, size);
+        if (!value_len || value_len == size)
+            return -1;
+        value_len += 1;
+        break;
+    case MPV_FORMAT_FLAG:
+        value_len = sizeof(int);
+        break;
+    case MPV_FORMAT_INT64:
+        value_len = sizeof(int64_t);
+        break;
+    case MPV_FORMAT_DOUBLE:
+        value_len = sizeof(double);
+        break;
+    default:
+        exit(1);
+        break;
+    }
+
+    // at least two bytes for the name
+    if (size < value_len + 2)
+        return -1;
+
+    const char *name = (const char *)data + value_len;
+    size_t name_len = strnlen(name, size - value_len);
+    if (!name_len || name_len != size - value_len - 1)
+        return -1;
+
+    mpv_handle *ctx = mpv_create();
+    if (!ctx)
+        exit(1);
+
+#if MPV_RUN
+    check_error(mpv_set_option_string(ctx, "vo", "null"));
+    check_error(mpv_set_option_string(ctx, "ao", "null"));
+    check_error(mpv_set_option_string(ctx, "ao-null-untimed", "yes"));
+    check_error(mpv_set_option_string(ctx, "untimed", "yes"));
+    check_error(mpv_set_option_string(ctx, "video-osd", "no"));
+    check_error(mpv_set_option_string(ctx, "msg-level", "all=trace"));
+
+    check_error(mpv_initialize(ctx));
+#endif
+
+    const void *value = data;
+    mpv_set_property(ctx, name, MPV_FORMAT, &value);
+
+#if MPV_RUN
+    check_error(mpv_set_option_string(ctx, "audio-files", "av://lavfi:sine=d=0.1"));
+    const char *cmd[] = {"loadfile", "av://lavfi:yuvtestsrc=d=0.1", NULL};
+    check_error(mpv_command(ctx, cmd));
+
+    while (1) {
+        mpv_event *event = mpv_wait_event(ctx, 10000);
+        if (event->event_id == MPV_EVENT_IDLE)
+            break;
+    }
+#endif
+
+    mpv_terminate_destroy(ctx);
+
+    return 0;
+}
