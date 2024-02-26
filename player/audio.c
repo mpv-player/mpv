72648bbdcaef9c1fecefba4379492bcc65a46c73
Dudemanguy <random342@airmail.cc>
2024-02-25 22:50:10 -0600

    player/audio: remove misleading comment about delay
    This came up in #13571. playing_audio_pts does not include mpctx->delay
    contray to what that implies. The function is meant to only offset the
    written audio pts by whatever the internal AO buffer may be.
    mpctx->delay is a combination from both the audio and video code, so it
    should not be used here. What is wanted is purely the audio pts.
    b74c09efbf7c6969fc053265f72cc0501b840ce1, a very controversial commit to
    say the least, was what introduced this comment, so removing is probably
    OK.

--- player/audio.c
+++ player/audio.c
@@ -829,7 +829,7 @@ void audio_start_ao(struct MPContext *mpctx)
     double pts = MP_NOPTS_VALUE;
     if (!get_sync_pts(mpctx, &pts))
         return;
-    double apts = playing_audio_pts(mpctx); // (basically including mpctx->delay)
+    double apts = playing_audio_pts(mpctx);
     if (pts != MP_NOPTS_VALUE && apts != MP_NOPTS_VALUE && pts < apts &&
         mpctx->video_status != STATUS_EOF)
     {
