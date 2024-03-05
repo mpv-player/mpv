mpv-player/mpv

commit df758880e26ccdb084e2ad9dd33d8c1835ac4a3f

path: don't override "cache" and "state" paths with configdir
This reverts commit 576e86bfa18a0a79e591988ad93ed009edc40849 (functionally).

Right now, the --config-dir option silently causes all watch_later and cache
files to be written in the --config-dir as well. This is pretty uninitutive
and also not desirable in most cases so get rid of this.
libmpv users will have to set the corresponding options or env vars if they
want to keep the old behaviour.

options/path.c
@@ -64,12 +64,6 @@ static const char *const config_dirs[] = {
     "exe_dir",
     "global",
 };
-// types that configdir replaces (if set)
-// These are not part of any fallback order so need to be overriden separately.
-static const char *const config_dir_replaces[] = {
-    "state",
-    "cache",
-};
 
 // Return a platform specific path using a path type as defined in osdep/path.h.
 // Keep in mind that the only way to free the return value is freeing talloc_ctx
@@ -85,10 +79,6 @@ static const char *mp_get_platform_path(void *talloc_ctx,
         for (int n = 0; n < MP_ARRAY_SIZE(config_dirs); n++) {
             if (strcmp(config_dirs[n], type) == 0)
                 return (n == 0 && global->configdir[0]) ? global->configdir : NULL;
-        }
-        for (int n = 0; n < MP_ARRAY_SIZE(config_dir_replaces); n++) {
-            if (strcmp(config_dir_replaces[n], type) == 0)
-                return global->configdir[0] ? global->configdir : NULL;
         }
     }
 

commit 8e54f1e5a047bd2954079ed540893a71632627d4

path: don't load any files if --no-config is passed
`--no-config` should prevent loading any user files, whether it be
config, cache, watch_later state etc. This functionality was changed by
df758880e26c because internally `--no-config` is equivalent to passing
`--config-dir=""` which resulted in cache and state being auto-detected
even if `--no-config` was passed.

Fixes: df758880e26c ("path: don't override "cache" and "state" paths with configdir")

options/path.c
@@ -76,10 +76,14 @@ static const char *mp_get_platform_path(void *talloc_ctx,
     assert(talloc_ctx);
 
     if (global->configdir) {
+        // Return NULL for all platform paths if --no-config is passed
+        if (!global->configdir[0])
+            return NULL;
+
         // force all others to NULL, only first returns the path
         for (int n = 0; n < MP_ARRAY_SIZE(config_dirs); n++) {
             if (strcmp(config_dirs[n], type) == 0)
-                return (n == 0 && global->configdir[0]) ? global->configdir : NULL;
+                return (n == 0) ? global->configdir : NULL;
         }
     }
 