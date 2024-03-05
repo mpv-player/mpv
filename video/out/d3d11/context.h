ef38fbef25f56616ed2e310ce1fa5594ae61d3e6
llyyr <llyyr@yukari.in>
2024-03-06 01:22:54 +0530

    Revert "vo_gpu_next: respect d3d11 swapchain output format preference"
    Next commit will enable this unconditionally for both d3d11 and vulkan.
    
    This reverts commit 41259db9523bede8c656c7a8664484e0e4befa02.

--- video/out/d3d11/context.h
+++ video/out/d3d11/context.h
@@ -7,7 +7,3 @@
 // Get the underlying D3D11 swap chain from an RA context. The returned swap chain is
 // refcounted and must be released by the caller.
 IDXGISwapChain *ra_d3d11_ctx_get_swapchain(struct ra_ctx *ra);
-
-// Returns true if an 8-bit output format is explicitly requested for
-// d3d11-output-format for an RA context.
-bool ra_d3d11_ctx_prefer_8bit_output_format(struct ra_ctx *ra);
