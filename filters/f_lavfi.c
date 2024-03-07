78447c4b91634aa91dcace1cc6a9805fb93b9252
Dudemanguy <random342@airmail.cc>
2024-03-07 14:12:15 -0600

    filters/f_lavfi: handle removed AV_OPT_TYPE_CHANNEL_LAYOUT
    See: https://github.com/FFmpeg/FFmpeg/commit/65ddc74988245a01421a63c5cffa4d900c47117c

--- filters/f_lavfi.c
+++ filters/f_lavfi.c
@@ -1034,7 +1034,11 @@ static const char *get_avopt_type_name(enum AVOptionType type)
     case AV_OPT_TYPE_VIDEO_RATE:        return "fps";
     case AV_OPT_TYPE_DURATION:          return "duration";
     case AV_OPT_TYPE_COLOR:             return "color";
+#if LIBAVUTIL_VERSION_MAJOR < 59
     case AV_OPT_TYPE_CHANNEL_LAYOUT:    return "channellayout";
+#else
+    case AV_OPT_TYPE_CHLAYOUT:          return "channellayout";
+#endif
     case AV_OPT_TYPE_BOOL:              return "bool";
     case AV_OPT_TYPE_CONST: // fallthrough
     default:
