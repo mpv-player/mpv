b480daad88706b216bceb79f5c78f55c05ce65ae
der richter <der.richter@gmx.de>
2024-03-19 22:25:39 +0100

    mac/option: make option helper none optional
    gets rid of some unwrapping boilerplate and nil coalescing operators.

--- video/out/mac/view.swift
+++ video/out/mac/view.swift
@@ -19,7 +19,7 @@ import Cocoa
 
 class View: NSView, CALayerDelegate {
     unowned var common: Common
-    var option: OptionHelper? { get { return common.option } }
+    var option: OptionHelper { get { return common.option } }
     var input: InputHelper? { get { return common.input } }
 
     var tracker: NSTrackingArea?
