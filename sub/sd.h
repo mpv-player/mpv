4022270878e1d6c672e7aa010abeda2aeb8ce670
Kacper Michajłow <kasper93@gmail.com>
2024-03-23 09:43:30 +0100

    sub/lavc_conv: take sd context as a parameter for lavc_conv_create
    Will be useful for future commits.

--- sub/sd.h
+++ sub/sd.h
@@ -51,8 +51,7 @@ struct sd_functions {
 
 // lavc_conv.c
 struct lavc_conv;
-struct lavc_conv *lavc_conv_create(struct mp_log *log,
-                                   const struct mp_codec_params *mp_codec);
+struct lavc_conv *lavc_conv_create(struct sd *sd);
 char *lavc_conv_get_extradata(struct lavc_conv *priv);
 char **lavc_conv_decode(struct lavc_conv *priv, struct demux_packet *packet,
                         double *sub_pts, double *sub_duration);
