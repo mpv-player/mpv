1ce3f701241c3e23455bfeea2a649fb25e2c5a61
der richter <der.richter@gmx.de>
2024-04-02 22:24:58 +0200

    mac/window: fix unfs window size retrieval
    the unfsContentFrame wasn't updated when externally resized leading to
    a wrong unfs window size afterwards. update it on windowDidResize event
    when not in fs, not animating and not live resizing.

--- video/out/mac/view.swift
+++ video/out/mac/view.swift
@@ -165,6 +165,7 @@ class View: NSView, CALayerDelegate {
     }
 
     override func magnify(with event: NSEvent) {
+        common.window?.isAnimating = event.phase != .ended
         event.phase == .ended ? common.windowDidEndLiveResize() : common.windowWillStartLiveResize()
         common.window?.addWindowScale(Double(event.magnification))
     }
