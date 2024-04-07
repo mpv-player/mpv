mpv-player/mpv

commit 024e0cd4c1405a41edd6a8b302ec6b747bc60ea3

options: only apply sub-visibility to primary subs
Previously, the sub-visibility option changed the visibility of all
subtitles including secondary ones. This meant that it was not possible
to only display secondary subtitles while hiding the primary ones. This
modifies the sub-visibility option so that it only affects primary
subtitles which allows only secondary subtitles to be displayed.

sub/dec_sub.c
@@ -334,8 +334,6 @@ struct sub_bitmaps *sub_get_bitmaps(struct dec_sub *sub, struct mp_osd_res dim,
 {
     pthread_mutex_lock(&sub->lock);
 
-    struct mp_subtitle_opts *opts = sub->opts;
-
     pts = pts_to_subtitle(sub, pts);
 
     sub->last_vo_pts = pts;
@@ -344,7 +342,7 @@ struct sub_bitmaps *sub_get_bitmaps(struct dec_sub *sub, struct mp_osd_res dim,
     struct sub_bitmaps *res = NULL;
 
     if (!(sub->end != MP_NOPTS_VALUE && pts >= sub->end) &&
-        opts->sub_visibility && sub->sd->driver->get_bitmaps)
+        sub->sd->driver->get_bitmaps)
         res = sub->sd->driver->get_bitmaps(sub->sd, dim, format, pts);
 
     pthread_mutex_unlock(&sub->lock);
@@ -451,6 +449,11 @@ void sub_set_play_dir(struct dec_sub *sub, int dir)
     pthread_mutex_lock(&sub->lock);
     sub->play_dir = dir;
     pthread_mutex_unlock(&sub->lock);
+}
+
+bool sub_is_primary_visible(struct dec_sub *sub)
+{
+    return !!sub->opts->sub_visibility;
 }
 
 bool sub_is_secondary_visible(struct dec_sub *sub)

sub/dec_sub.h
@@ -51,6 +51,7 @@ void sub_reset(struct dec_sub *sub);
 void sub_select(struct dec_sub *sub, bool selected);
 void sub_set_recorder_sink(struct dec_sub *sub, struct mp_recorder_sink *sink);
 void sub_set_play_dir(struct dec_sub *sub, int dir);
+bool sub_is_primary_visible(struct dec_sub *sub);
 bool sub_is_secondary_visible(struct dec_sub *sub);
 
 int sub_control(struct dec_sub *sub, enum sd_ctrl cmd, void *arg);

sub/osd.c
@@ -291,7 +291,7 @@ static struct sub_bitmaps *render_object(struct osd_state *osd,
     check_obj_resize(osd, osdres, obj);
 
     if (obj->type == OSDTYPE_SUB) {
-        if (obj->sub)
+        if (obj->sub && sub_is_primary_visible(obj->sub))
             res = sub_get_bitmaps(obj->sub, obj->vo_res, format, video_pts);
     } else if (obj->type == OSDTYPE_SUB2) {
         if (obj->sub && sub_is_secondary_visible(obj->sub))

commit 8633594c31e5bf7c472a8eaee6e1d4be141afde9

dec_sub: fix locking for sub_is_{primary,secondary}_visible
These public functions should use locks to keep its usage
consistent with input.c.

Fixes: 024e0cd4c1405a41edd6a8b302ec6b747bc60ea3

sub/dec_sub.c
@@ -548,10 +548,16 @@ void sub_set_play_dir(struct dec_sub *sub, int dir)
 
 bool sub_is_primary_visible(struct dec_sub *sub)
 {
-    return sub->shared_opts->sub_visibility[0];
+    mp_mutex_lock(&sub->lock);
+    bool ret = sub->shared_opts->sub_visibility[0];
+    mp_mutex_unlock(&sub->lock);
+    return ret;
 }
 
 bool sub_is_secondary_visible(struct dec_sub *sub)
 {
-    return sub->shared_opts->sub_visibility[1];
+    mp_mutex_lock(&sub->lock);
+    bool ret = sub->shared_opts->sub_visibility[1];
+    mp_mutex_unlock(&sub->lock);
+    return ret;
 }