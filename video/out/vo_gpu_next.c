mpv-player/mpv

commit a9354b36caed24d13987cad3a87b16227cc36f2c

vo_gpu_next: set target resolution to crop value
Use rect of the actual image instead of FBO size which includes
margins.

video/out/vo_gpu_next.c
@@ -1134,8 +1134,8 @@ static void draw_frame(struct vo *vo, struct vo_frame *frame)
     p->target_params = (struct mp_image_params){
         .imgfmt_name = swframe.fbo->params.format
                         ? swframe.fbo->params.format->name : NULL,
-        .w = swframe.fbo->params.w,
-        .h = swframe.fbo->params.h,
+        .w = mp_rect_w(target.crop),
+        .h = mp_rect_h(target.crop),
         .color = target.color,
         .repr = target.repr,
         .rotate = target.rotation,

commit b3112a3c646f4d778d45b49906f085b37e339781

vo_gpu_next: rotate target crop
Turns out libplacebo uses unrotated target crop in relation to source.
Use dst rect from VO, instead of extracting it from pl_frame, to avoid
another unrotating operation.

Fixes: a9354b36ca

video/out/vo_gpu_next.c
@@ -1134,8 +1134,8 @@ static void draw_frame(struct vo *vo, struct vo_frame *frame)
     p->target_params = (struct mp_image_params){
         .imgfmt_name = swframe.fbo->params.format
                         ? swframe.fbo->params.format->name : NULL,
-        .w = mp_rect_w(target.crop),
-        .h = mp_rect_h(target.crop),
+        .w = mp_rect_w(p->dst),
+        .h = mp_rect_h(p->dst),
         .color = target.color,
         .repr = target.repr,
         .rotate = target.rotation,