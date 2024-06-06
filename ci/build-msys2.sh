7ad9821df47cfe06c28c074d7656a17103cac9d3
Kacper Michajłow <kasper93@gmail.com>
2024-06-06 23:32:46 +0200

    ci/msys2: re-enable 32-bit builds
    The 32-bit builds were disabled, because some packages started
    disappearing. MSYS2 is slowly phasing out 32-bit support, but since we
    still support it we have to test it somehow. Recent brakage with calling
    convention mismatch would be spotted a lot quicker. So enable it, but
    with some, not available, packages disabled.
    
    This reverts a56d5c7fa1f20d917a179be894580a4c66c374dc

--- ci/build-msys2.sh
+++ ci/build-msys2.sh
@@ -7,20 +7,21 @@ meson setup build            \
   -D d3d-hwaccel=enabled     \
   -D d3d11=enabled           \
   -D dvdnav=enabled          \
-  -D egl-angle-lib=enabled   \
-  -D egl-angle-win32=enabled \
   -D jpeg=enabled            \
   -D lcms2=enabled           \
   -D libarchive=enabled      \
   -D libbluray=enabled       \
   -D libmpv=true             \
   -D lua=enabled             \
-  -D pdf-build=enabled       \
-  -D rubberband=enabled      \
   -D shaderc=enabled         \
   -D spirv-cross=enabled     \
   -D tests=true              \
   -D uchardet=enabled        \
   -D vapoursynth=enabled
+
+if [[ "$SYS" != "clang32" && "$SYS" != "mingw32" ]]; then
+   meson configure build -D{egl-angle-lib,egl-angle-win32,pdf-build,rubberband}=enabled
+fi
+
 meson compile -C build
 ./build/mpv.com -v --no-config
