02714ceb1febf96c431d87e2ccee41b0557bdbb7
Kacper Michajłow <kasper93@gmail.com>
2024-06-07 20:39:32 +0200

    ci/msys2: compact build script

--- ci/build-msys2.sh
+++ ci/build-msys2.sh
@@ -1,27 +1,17 @@
 #!/bin/sh -e
 
-meson setup build            \
-  --werror                   \
-  -Dc_args="-Wno-error=deprecated -Wno-error=deprecated-declarations" \
-  -D cdda=enabled            \
-  -D d3d-hwaccel=enabled     \
-  -D d3d11=enabled           \
-  -D dvdnav=enabled          \
-  -D jpeg=enabled            \
-  -D lcms2=enabled           \
-  -D libarchive=enabled      \
-  -D libbluray=enabled       \
-  -D libmpv=true             \
-  -D lua=enabled             \
-  -D shaderc=enabled         \
-  -D spirv-cross=enabled     \
-  -D tests=true              \
-  -D uchardet=enabled        \
-  -D vapoursynth=enabled
+args=(
+  --werror
+  -Dc_args='-Wno-error=deprecated -Wno-error=deprecated-declarations'
+  -D{cdda,d3d-hwaccel,d3d11,dvdnav,jpeg,lcms2,libarchive}=enabled
+  -D{libbluray,lua,shaderc,spirv-cross,uchardet,vapoursynth}=enabled
+  -D{libmpv,tests}=true
+)
 
-if [[ "$SYS" != "clang32" && "$SYS" != "mingw32" ]]; then
-   meson configure build -D{egl-angle-lib,egl-angle-win32,pdf-build,rubberband}=enabled
-fi
+[[ "$SYS" != "clang32" && "$SYS" != "mingw32" ]] && args+=(
+  -D{egl-angle-lib,egl-angle-win32,pdf-build,rubberband}=enabled
+)
 
+meson setup build "${args[@]}"
 meson compile -C build
 ./build/mpv.com -v --no-config
