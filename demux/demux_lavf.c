mpv-player/mpv

commit 4ef7931266759560a860e66e01b75622a0a3996b

demux_lavf: drop jpeg frames that are at a nonzero pos
There are jpg files out there that have extra embedded metadata
(pictures from smartphones commonly have an embedded gain map). ffmpeg
doesn't currently support this, so mpv currently sees the extra metadata
as essentially another frame. This results in weird video-like behavior.
Until ffmpeg support this more properly, we can work around this by
simply discarding extra packets and not sending the new frame to the
internal playloop. If demux_lavf detects that the stream is a
single-frame jpg, then we only accepts the packet at pos 0. Anything
else is discarded. Ref #13192.

demux/demux_lavf.c
@@ -1255,6 +1255,12 @@ static bool demux_lavf_read_packet(struct demuxer *demux,
     struct sh_stream *stream = info->sh;
     AVStream *st = priv->avfc->streams[pkt->stream_index];
 
+    // Never send additional frames for streams that are a single frame jpeg.
+    if (stream->image && !strcmp(priv->avif->name, "jpeg_pipe") && pkt->pos != 0) {
+        av_packet_unref(pkt);
+        return true;
+    }
+
     if (!demux_stream_is_selected(stream)) {
         av_packet_unref(pkt);
         return true; // don't signal EOF if skipping a packet

commit 172ac8c970a88d2b4e69f8ea5770553d6bb8d403

demux_lavf: fix null derefence of stream variable
demux_stream_is_selected() already checks if stream is valid, so move
the image format hack check after that.

Fixes: 4ef7931266759560a860e66e01b75622a0a3996b

demux/demux_lavf.c
@@ -1260,15 +1260,15 @@ static bool demux_lavf_read_packet(struct demuxer *demux,
     struct sh_stream *stream = info->sh;
     AVStream *st = priv->avfc->streams[pkt->stream_index];
 
+    if (!demux_stream_is_selected(stream)) {
+        av_packet_unref(pkt);
+        return true; // don't signal EOF if skipping a packet
+    }
+
     // Never send additional frames for streams that are a single frame.
     if (stream->image && priv->format_hack.first_frame_only && pkt->pos != 0) {
         av_packet_unref(pkt);
         return true;
-    }
-
-    if (!demux_stream_is_selected(stream)) {
-        av_packet_unref(pkt);
-        return true; // don't signal EOF if skipping a packet
     }
 
     struct demux_packet *dp = new_demux_packet_from_avpacket(pkt);