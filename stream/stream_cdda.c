111571bd0510e751b4c8764a7e1e5a8f49150567
Dudemanguy <random342@airmail.cc>
2024-06-18 12:07:46 -0500

    options: remove some options with .deprecation_message
    In both cases, setting these options did nothing other than give you a
    warning that they may be removed in the future. Remove them now.

--- stream/stream_cdda.c
+++ stream/stream_cdda.c
@@ -62,7 +62,6 @@ typedef struct cdda_params {
     int paranoia_mode;
     int sector_size;
     int search_overlap;
-    int toc_bias;
     int toc_offset;
     bool skip;
     char *device;
@@ -78,8 +77,6 @@ const struct m_sub_options stream_cdda_conf = {
         {"paranoia", OPT_INT(paranoia_mode), M_RANGE(0, 2)},
         {"sector-size", OPT_INT(sector_size), M_RANGE(1, 100)},
         {"overlap", OPT_INT(search_overlap), M_RANGE(0, 75)},
-        {"toc-bias", OPT_INT(toc_bias),
-            .deprecation_message = "toc-bias is no longer used"},
         {"toc-offset", OPT_INT(toc_offset)},
         {"skip", OPT_BOOL(skip)},
         {"span-a", OPT_INT(span[0])},
