2887b2e64c1004da5caebb98b91235ebf4931f93
Kacper Michajłow <kasper93@gmail.com>
2024-06-21 19:22:29 +0200

    meson: increase FFmpeg minimum required version to 6.1

--- ci/build-openbsd.sh
+++ ci/build-openbsd.sh
@@ -8,7 +8,24 @@ git clone https://code.videolan.org/videolan/libplacebo.git \
     --recurse-submodules --shallow-submodules \
     --depth=1 --recurse-submodules subprojects/libplacebo
 
+# FFmpeg on openBSD (4.4.4) is too old; use a subproject
+cat <<EOF > subprojects/ffmpeg.wrap
+[wrap-git]
+url = https://gitlab.freedesktop.org/gstreamer/meson-ports/ffmpeg.git
+revision = meson-6.1
+depth = 1
+[provide]
+libavcodec = libavcodec_dep
+libavdevice = libavdevice_dep
+libavfilter = libavfilter_dep
+libavformat = libavformat_dep
+libavutil = libavutil_dep
+libswresample = libswresample_dep
+libswscale = libswscale_dep
+EOF
+
 meson setup build \
+    -Dffmpeg:vulkan=auto \
     -Dlibmpv=true \
     -Dlua=enabled \
     -Dopenal=enabled \
