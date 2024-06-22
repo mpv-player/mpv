9e1271260f33ffb8c7ff352c2bfc7122e0a0c988
Kacper Michajłow <kasper93@gmail.com>
2024-06-22 14:58:07 +0200

    ad_spdif: fix lavf version check
    Fixes: 62b1bad755bb6141c5a704741bda8a4da6dfcde5

--- audio/decode/ad_spdif.c
+++ audio/decode/ad_spdif.c
@@ -54,7 +54,7 @@ struct spdifContext {
     struct mp_decoder public;
 };
 
-#if LIBAVCODEC_VERSION_MAJOR < 61
+#if LIBAVFORMAT_VERSION_MAJOR < 61
 static int write_packet(void *p, uint8_t *buf, int buf_size)
 #else
 static int write_packet(void *p, const uint8_t *buf, int buf_size)
