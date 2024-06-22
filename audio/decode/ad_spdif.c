mpv-player/mpv

commit 62b1bad755bb6141c5a704741bda8a4da6dfcde5

ad_spdif: handle const buf pointee in avio_alloc_context
ffmpeg recently changed this field to be const which causes our CI to
fail on newer versions.

See: https://github.com/FFmpeg/FFmpeg/commit/2a68d945cd74265bb71c3d38b7a2e7f7d7e87be5

audio/decode/ad_spdif.c
@@ -59,7 +59,11 @@ struct spdifContext {
     struct mp_decoder public;
 };
 
+#if LIBAVCODEC_VERSION_MAJOR < 61
 static int write_packet(void *p, uint8_t *buf, int buf_size)
+#else
+static int write_packet(void *p, const uint8_t *buf, int buf_size)
+#endif
 {
     struct spdifContext *ctx = p;
 

commit 3e65e546df4e425df223a9e412924fe644587674

ad_spdif: fix lavf version check
Fixes: 62b1bad755bb6141c5a704741bda8a4da6dfcde5

audio/decode/ad_spdif.c
@@ -54,7 +54,7 @@ struct spdifContext {
     struct mp_decoder public;
 };
 
-#if LIBAVCODEC_VERSION_MAJOR < 61
+#if LIBAVFORMAT_VERSION_MAJOR < 61
 static int write_packet(void *p, uint8_t *buf, int buf_size)
 #else
 static int write_packet(void *p, const uint8_t *buf, int buf_size)