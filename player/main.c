799137a87cf9437ed3ec40bc245c1a16373d8b36
Kacper Michajłow <kasper93@gmail.com>
2024-06-23 16:44:45 +0200

    fuzzers: disallow include command in more principal way
    We should never allow include command for fuzzers and it can be
    triggered also by direct set property.

--- player/main.c
+++ player/main.c
@@ -227,6 +227,9 @@ static bool handle_help_options(struct MPContext *mpctx)
 
 static int cfg_include(void *ctx, char *filename, int flags)
 {
+#ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
+    return 1;
+#endif
     struct MPContext *mpctx = ctx;
     char *fname = mp_get_user_path(NULL, mpctx->global, filename);
     int r = m_config_parse_config_file(mpctx->mconfig, mpctx->global, fname, NULL, flags);
