mpv-player/mpv

commit 1225bcbd4112d2f5880c7273f90f56725123fe8a

fuzzer_set_property: don't run mpv if set property failed
We can avoid testing this cases, hopefully there are no side-effects
when set property fails.

fuzzers/fuzzer_set_property.c
@@ -67,11 +67,15 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     check_error(mpv_initialize(ctx));
 #endif
 
+    int ret;
     if (MPV_FORMAT == MPV_FORMAT_STRING) {
-        mpv_set_property_string(ctx, name, (void *)data);
+        ret = mpv_set_property_string(ctx, name, (void *)data);
     } else {
-        mpv_set_property(ctx, name, MPV_FORMAT, (void *)data);
+        ret = mpv_set_property(ctx, name, MPV_FORMAT, (void *)data);
     }
+
+    if (ret != MPV_ERROR_SUCCESS)
+        return 0;
 
 #if MPV_RUN
     check_error(mpv_set_option_string(ctx, "ao-null-untimed", "yes"));

commit 0f39cb4f7b95d443715cb55a5da5ecb240f8166c

fuzzer_set_property: destroy mpv also on error
Shouldn't matter much and in fact even LSAN doesn't complain, but we
should clean it correctly.

Fixes: 1225bcbd4112d2f5880c7273f90f56725123fe8a

fuzzers/fuzzer_set_property.c
@@ -75,7 +75,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     }
 
     if (ret != MPV_ERROR_SUCCESS)
-        return 0;
+        goto done;
 
 #if MPV_RUN
     check_error(mpv_set_option_string(ctx, "ao-null-untimed", "yes"));
@@ -93,6 +93,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     }
 #endif
 
+done:
     mpv_terminate_destroy(ctx);
 
     return 0;