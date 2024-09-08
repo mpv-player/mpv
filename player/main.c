mpv-player/mpv

commit c1282d4d43be8fb8bbc8529b22804d288d59038a

player/main: move terminal_uninit to the end
It is strange to do terminal_uninit() and still use terminal after. Even
tho it has no side-effects.

player/main.c
@@ -184,16 +184,17 @@ void mp_destroy(struct MPContext *mpctx)
     cocoa_set_input_context(NULL);
 #endif
 
-    if (cas_terminal_owner(mpctx, mpctx)) {
-        terminal_uninit();
-        cas_terminal_owner(mpctx, NULL);
-    }
-
     mp_input_uninit(mpctx->input);
 
     uninit_libav(mpctx->global);
 
     mp_msg_uninit(mpctx->global);
+
+    if (cas_terminal_owner(mpctx, mpctx)) {
+        terminal_uninit();
+        cas_terminal_owner(mpctx, NULL);
+    }
+
     assert(!mpctx->num_abort_list);
     talloc_free(mpctx->abort_list);
     mp_mutex_destroy(&mpctx->abort_lock);

commit e8609f8b3fc4258c4eeeeb632eb8a99ffbf23d49

player/main: uninit input after terminal uninit
The terminal input thread holds the input_ctx reference.
Fixes: c1282d4d43be8fb8bbc8529b22804d288d59038a

player/main.c
@@ -187,8 +187,6 @@ void mp_destroy(struct MPContext *mpctx)
     cocoa_set_input_context(NULL);
 #endif
 
-    mp_input_uninit(mpctx->input);
-
     uninit_libav(mpctx->global);
 
     mp_msg_uninit(mpctx->global);
@@ -197,6 +195,8 @@ void mp_destroy(struct MPContext *mpctx)
         terminal_uninit();
         cas_terminal_owner(mpctx, NULL);
     }
+
+    mp_input_uninit(mpctx->input);
 
     assert(!mpctx->num_abort_list);
     talloc_free(mpctx->abort_list);