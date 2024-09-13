e41ee0524c78b5af2c9c8c89045c78c2dcc42f16
Kacper Michajłow <kasper93@gmail.com>
2024-09-13 16:51:44 +0200

    f_auto_filters: don't set interlaced-only for userdeint
    Not wanted apparently: https://github.com/mpv-player/mpv/pull/14822#discussion_r1758134111

--- filters/f_auto_filters.c
+++ filters/f_auto_filters.c
@@ -94,7 +94,6 @@ static void deint_process(struct mp_filter *f)
             mp_create_user_filter(f, MP_OUTPUT_CHAIN_VIDEO, "vdpaupp", args);
     } else if (img->imgfmt == IMGFMT_D3D11) {
         char *args[] = {"deint", "yes",
-                        "interlaced-only", "yes",
                         "parity", field_parity, NULL};
         p->sub.filter =
             mp_create_user_filter(f, MP_OUTPUT_CHAIN_VIDEO, "d3d11vpp", args);
@@ -110,7 +109,6 @@ static void deint_process(struct mp_filter *f)
             mp_create_user_filter(f, MP_OUTPUT_CHAIN_VIDEO, "bwdif_vulkan", args);
     } else if (img->imgfmt == IMGFMT_VAAPI) {
         char *args[] = {"deint", "motion-adaptive",
-                        "interlaced-only", "yes",
                         "parity", field_parity, NULL};
         p->sub.filter =
             mp_create_user_filter(f, MP_OUTPUT_CHAIN_VIDEO, "vavpp", args);
