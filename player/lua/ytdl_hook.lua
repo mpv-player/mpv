mpv-player/mpv

commit ec966fb866f4676139ea5a104c4d52ee154d81be

ytdl_hook: add some more yt-dlp tags
These might be useful

player/lua/ytdl_hook.lua
@@ -43,10 +43,29 @@ end
 
 -- youtube-dl JSON name to mpv tag name
 local tag_list = {
+    ["artist"]          = "artist",
+    ["album"]           = "album",
+    ["album_artist"]    = "album_artist",
+    ["composer"]        = "composer",
+    ["upload_date"]     = "date",
+    ["genre"]           = "genre",
+    ["series"]          = "series",
+    ["track"]           = "title",
+    ["track_number"]    = "track",
     ["uploader"]        = "uploader",
     ["channel_url"]     = "channel_url",
-    -- these titles tend to be a bit too long, so hide them on the terminal
-    -- (default --display-tags does not include this name)
+
+    -- These tags are not displayed by default, but can be shown with
+    -- --display-tags
+    ["playlist"]        = "ytdl_playlist",
+    ["playlist_index"]  = "ytdl_playlist_index",
+    ["playlist_title"]  = "ytdl_playlist_title",
+    ["playlist_id"]     = "ytdl_playlist_id",
+    ["chapters"]        = "ytdl_chapters",
+    ["season"]          = "ytdl_season",
+    ["episode"]         = "ytdl_episode",
+    ["is_live"]         = "ytdl_is_live",
+    ["release_year"]    = "ytdl_release_year",
     ["description"]     = "ytdl_description",
     -- "title" is handled by force-media-title
     -- tags don't work with all_formats=yes

commit d6886355b86d821f5805764ef39a395ac56f9050

ytdl_hook: fix typo (chapters -> chapter)
Fixes: ec966fb866f4676139ea5a104c4d52ee154d81be

player/lua/ytdl_hook.lua
@@ -61,7 +61,7 @@ local tag_list = {
     ["playlist_index"]  = "ytdl_playlist_index",
     ["playlist_title"]  = "ytdl_playlist_title",
     ["playlist_id"]     = "ytdl_playlist_id",
-    ["chapters"]        = "ytdl_chapters",
+    ["chapter"]         = "ytdl_chapter",
     ["season"]          = "ytdl_season",
     ["episode"]         = "ytdl_episode",
     ["is_live"]         = "ytdl_is_live",