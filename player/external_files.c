mpv-player/mpv

commit d384a6b79303ef21b7330555e968a99d59a7c5b0

external_files: allow specifying --cover-art-whitelist filenames
Fixes https://github.com/mpv-player/mpv/discussions/14520.

player/external_files.c
@@ -30,20 +30,6 @@
 #include "options/path.h"
 #include "external_files.h"
 
-// Stolen from: vlc/-/blob/master/modules/meta_engine/folder.c#L40
-// sorted by priority (descending)
-static const char *const cover_files[] = {
-    "AlbumArt",
-    "Album",
-    "cover",
-    "front",
-    "AlbumArtSmall",
-    "Folder",
-    ".folder",
-    "thumb",
-    NULL
-};
-
 // Needed for mp_might_be_subtitle_file
 char **sub_exts;
 
@@ -70,11 +56,13 @@ static int test_ext(MPOpts *opts, bstr ext)
     return -1;
 }
 
-static int test_cover_filename(bstr fname)
+static int test_cover_filename(bstr fname, char **cover_files)
 {
     for (int n = 0; cover_files[n]; n++) {
         if (bstrcasecmp(bstr0(cover_files[n]), fname) == 0) {
-            return MP_ARRAY_SIZE(cover_files) - n;
+            size_t size = n;
+            while (cover_files[++size]);
+            return size - n;
         }
     }
     return 0;
@@ -198,8 +186,8 @@ static void append_dir_subtitles(struct mpv_global *global, struct MPOpts *opts,
         if (bstr_find(tmp_fname_trim, f_fname_trim) >= 0 && fuzz >= 1)
             prio |= 2; // contains the movie name
 
-        if (type == STREAM_VIDEO && opts->coverart_whitelist && prio == 0)
-            prio = test_cover_filename(tmp_fname_trim);
+        if (type == STREAM_VIDEO && prio == 0)
+            prio = test_cover_filename(tmp_fname_trim, opts->coverart_whitelist);
 
         // doesn't contain the movie name
         // don't try in the mplayer subtitle directory

options/options.c
@@ -709,7 +709,7 @@ static const m_option_t mp_opts[] = {
     {"cover-art-auto", OPT_CHOICE(coverart_auto,
         {"no", -1}, {"exact", 0}, {"fuzzy", 1}, {"all", 2})},
     {"cover-art-auto-exts", OPT_STRINGLIST(coverart_auto_exts)},
-    {"cover-art-whitelist", OPT_BOOL(coverart_whitelist)},
+    {"cover-art-whitelist", OPT_STRINGLIST(coverart_whitelist)},
 
     {"", OPT_SUBSTRUCT(subs_rend, mp_subtitle_sub_opts)},
     {"", OPT_SUBSTRUCT(subs_shared, mp_subtitle_shared_sub_opts)},
@@ -1030,7 +1030,6 @@ static const struct MPOpts mp_default_opts = {
     .playback_speed = 1.,
     .pitch_correction = true,
     .audiofile_auto = -1,
-    .coverart_whitelist = true,
     .osd_bar_visible = true,
     .screenshot_template = "mpv-shot%n",
     .play_dir = 1,
@@ -1084,6 +1083,20 @@ static const struct MPOpts mp_default_opts = {
         "utf-8",
         "utf8",
         "vtt",
+        NULL
+    },
+
+    // Stolen from: vlc/-/blob/master/modules/meta_engine/folder.c#L40
+    // sorted by priority (descending)
+    .coverart_whitelist = (char *[]){
+        "AlbumArt",
+        "Album",
+        "cover",
+        "front",
+        "AlbumArtSmall",
+        "Folder",
+        ".folder",
+        "thumb",
         NULL
     },
 

options/options.h
@@ -334,7 +334,7 @@ typedef struct MPOpts {
     char **audiofile_auto_exts;
     int coverart_auto;
     char **coverart_auto_exts;
-    bool coverart_whitelist;
+    char **coverart_whitelist;
     bool osd_bar_visible;
 
     int w32_priority;

commit e3460444c90fb412c2271e159eb37328d9dec88c

player/external_files: fix null deref when cover-art-whitelist is empty
This can happen when using --cover-art-whitelist-clr.

Fixes: d384a6b79303ef21b7330555e968a99d59a7c5b0

player/external_files.c
@@ -47,7 +47,7 @@ static int test_ext(MPOpts *opts, bstr ext)
 
 static int test_cover_filename(bstr fname, char **cover_files)
 {
-    for (int n = 0; cover_files[n]; n++) {
+    for (int n = 0; cover_files && cover_files[n]; n++) {
         if (bstrcasecmp(bstr0(cover_files[n]), fname) == 0) {
             size_t size = n;
             while (cover_files[++size]);