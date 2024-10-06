aab5edde3640865ac29b56916cbd65462013b573
Kacper Michajłow <kasper93@gmail.com>
2024-10-06 18:09:40 +0200

    Revert "ci/msys2: disable some features for 32-bit build"
    No longer needed after 232919336d567b70330c43a41920238a56e100da.
    
    This reverts commit 883a45fa3eb7347431c246d831cf8ece12da8676.

--- ci/build-msys2.sh
+++ ci/build-msys2.sh
@@ -3,14 +3,13 @@
 args=(
   --werror
   -Dc_args='-Wno-error=deprecated -Wno-error=deprecated-declarations'
-  -D{d3d-hwaccel,d3d11,dvdnav,jpeg,lcms2,libarchive}=enabled
-  -D{libbluray,lua,shaderc,spirv-cross}=enabled
+  -D{cdda,d3d-hwaccel,d3d11,dvdnav,jpeg,lcms2,libarchive}=enabled
+  -D{libbluray,lua,shaderc,spirv-cross,uchardet,vapoursynth}=enabled
   -D{libmpv,tests}=true
 )
 
 [[ "$SYS" != "clang32" && "$SYS" != "mingw32" ]] && args+=(
-  -D{cdda,egl-angle-lib,egl-angle-win32,pdf-build}=enabled
-  -D{rubberband,uchardet,vapoursynth,win32-smtc}=enabled
+  -D{egl-angle-lib,egl-angle-win32,pdf-build,rubberband,win32-smtc}=enabled
 )
 
 meson setup build "${args[@]}"
