1adb0f6fc770d100f4c869eb24e91dd873b69b4a
Dudemanguy <random342@airmail.cc>
2024-10-16 17:27:13 -0500

    ci: refactor ci so common args are picked from one location
    Mostly. It is annoying if we want to add some flag/option to all the cis
    and then have to do it manually. Things easily get missed that way.
    Skipped powershell because of course I'm not going to deal with that.

--- ci/build-msys2.sh
+++ ci/build-msys2.sh
@@ -1,17 +1,16 @@
-#!/bin/sh -e
+#!/bin/bash -e
+
+. ./ci/build-common.sh
 
 args=(
-  --werror
-  -Dc_args='-Wno-error=deprecated -Wno-error=deprecated-declarations'
   -D{cdda,d3d-hwaccel,d3d11,dvdnav,jpeg,lcms2,libarchive}=enabled
   -D{libbluray,lua,shaderc,spirv-cross,uchardet,vapoursynth}=enabled
-  -D{libmpv,tests}=true
 )
 
 [[ "$SYS" != "mingw32" ]] && args+=(
   -D{egl-angle-lib,egl-angle-win32,pdf-build,rubberband,win32-smtc}=enabled
 )
 
-meson setup build "${args[@]}"
+meson setup build $common_args "${args[@]}"
 meson compile -C build
 ./build/mpv.com -v --no-config
