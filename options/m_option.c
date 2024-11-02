mpv-player/mpv

commit 1f5a67d8fae69ebc19d1655035676ee66e22b0aa

options/m_option: limit string list to 100 per operation for fuzzing

options/m_option.c
@@ -1317,6 +1317,13 @@ static int str_list_add(char **add, int n, void *dst, int pre)
     for (ln = 0; lst && lst[ln]; ln++)
         /**/;
 
+#ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
+    if (ln >= 100) {
+        talloc_free(add);
+        return 0;
+    }
+#endif
+
     lst = talloc_realloc(NULL, lst, char *, n + ln + 1);
 
     if (pre) {
@@ -1447,6 +1454,10 @@ static int parse_str_list_impl(struct mp_log *log, const m_option_t *opt,
     n = 0;
 
     while (1) {
+#ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
+        if (n >= 100)
+            break;
+#endif
         struct bstr el = get_nextsep(&str, separator, 1);
         res[n] = bstrdup0(NULL, el);
         n++;

commit 14d16392272390b551fa709a25f2ff32f80c1af6

options/m_option: free all list elements when removing them
Elements are not parented to the add list, as they are directly copied
to the target list. Therefore, we need to clean them up manually.

Fixes: 1f5a67d8fae69ebc19d1655035676ee66e22b0aa

options/m_option.c
@@ -1319,6 +1319,8 @@ static int str_list_add(char **add, int n, void *dst, int pre)
 
 #ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
     if (ln >= 100) {
+        while (n--)
+            talloc_free(add[n]);
         talloc_free(add);
         return 0;
     }