45c1c586809b00f093e07eeaffda01372c94f0c5
der richter <der.richter@gmx.de>
2024-11-17 16:02:43 +0100

    mac/compat: fix compatibility for deployment targets lower than SDK ver
    the swift parts can't be nicely encapsulated in the compat file because
    it lags proper target version compile time checks.
    
    Fixes #15292

--- video/out/mac/common.swift
+++ video/out/mac/common.swift
@@ -281,13 +281,16 @@ class Common: NSObject {
     }
 
     func initLightSensor() {
-        let srv = IOServiceGetMatchingService(kIOMainPortDefault, IOServiceMatching("AppleLMUController"))
+        let mainPort: mach_port_t
+        if #available(macOS 12.0, *) { mainPort = kIOMainPortDefault } else { mainPort = kIOMasterPortDefault }
+
+        let srv = IOServiceGetMatchingService(mainPort, IOServiceMatching("AppleLMUController"))
         if srv == IO_OBJECT_NULL {
             log.verbose("Can't find an ambient light sensor")
             return
         }
 
-        lightSensorIOPort = IONotificationPortCreate(kIOMainPortDefault)
+        lightSensorIOPort = IONotificationPortCreate(mainPort)
         IONotificationPortSetDispatchQueue(lightSensorIOPort, queue)
         var n = io_object_t()
         IOServiceAddInterestNotification(lightSensorIOPort, srv, kIOGeneralInterest, lightSensorCallback,
