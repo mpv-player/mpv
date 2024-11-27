mpv-player/mpv

commit e1d30c4c5a7443d08c7d4c183644917a340954b2

clipboard: add clipboard API
This adds a clipboard API with multiple backend and format support.
--clipboard-enable option can be toggled at runtime to turn native
clipboard on and off.

player/clipboard/clipboard.c
@@ -1,0 +1,108 @@
+/*
+ * This file is part of mpv.
+ *
+ * mpv is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * mpv is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common/common.h"
+#include "options/m_config.h"
+#include "player/core.h"
+#include "clipboard.h"
+
+struct clipboard_opts {
+    bool enabled;
+};
+
+#define OPT_BASE_STRUCT struct clipboard_opts
+const struct m_sub_options clipboard_conf = {
+    .opts = (const struct m_option[]) {
+        {"enable", OPT_BOOL(enabled), .flags = UPDATE_CLIPBOARD},
+        {0}
+    },
+    .defaults = &(const struct clipboard_opts) {
+        .enabled = true,
+    },
+    .size = sizeof(struct clipboard_opts)
+};
+
+// backend list
+extern const struct clipboard_backend clipboard_backend_win32;
+extern const struct clipboard_backend clipboard_backend_vo;
+
+static const struct clipboard_backend *const clipboard_backend_list[] = {
+#if HAVE_WIN32_DESKTOP
+    &clipboard_backend_win32,
+#endif
+    &clipboard_backend_vo,
+};
+
+struct clipboard_ctx *mp_clipboard_create(struct clipboard_init_params *params,
+                                          struct mpv_global *global)
+{
+    struct clipboard_ctx *cl = talloc_ptrtype(NULL, cl);
+    *cl = (struct clipboard_ctx) {
+        .log = mp_log_new(cl, global->log, "clipboard"),
+        .global = global,
+    };
+
+    for (int i = 0; i < MP_ARRAY_SIZE(clipboard_backend_list); i++) {
+        const struct clipboard_backend *backend = clipboard_backend_list[i];
+        if (backend->init(cl, params) == CLIPBOARD_SUCCESS) {
+            MP_VERBOSE(cl, "Initialized %s clipboard backend.\n", backend->name);
+            cl->backend = backend;
+            goto success;
+        }
+    }
+
+    MP_WARN(cl, "Failed to initialize any clipboard backend.\n");
+    talloc_free(cl);
+    return NULL;
+success:
+    return cl;
+}
+
+void mp_clipboard_destroy(struct clipboard_ctx *cl)
+{
+    if (cl && cl->backend->uninit)
+        cl->backend->uninit(cl);
+    talloc_free(cl);
+}
+
+int mp_clipboard_get_data(struct clipboard_ctx *cl, struct clipboard_access_params *params,
+                          struct clipboard_data *out, void *talloc_ctx)
+{
+    if (cl && cl->backend->get_data)
+        return cl->backend->get_data(cl, params, out, talloc_ctx);
+    return CLIPBOARD_FAILED;
+}
+
+int mp_clipboard_set_data(struct clipboard_ctx *cl, struct clipboard_access_params *params,
+                          struct clipboard_data *data)
+{
+    if (cl && cl->backend->set_data)
+        return cl->backend->set_data(cl, params, data);
+    return CLIPBOARD_FAILED;
+}
+
+void reinit_clipboard(struct MPContext *mpctx)
+{
+    mp_clipboard_destroy(mpctx->clipboard);
+    mpctx->clipboard = NULL;
+
+    struct clipboard_opts *opts = mp_get_config_group(NULL, mpctx->global, &clipboard_conf);
+    if (opts->enabled) {
+        mpctx->clipboard = mp_clipboard_create(&(struct clipboard_init_params){0}, mpctx->global);
+    }
+    talloc_free(opts);
+}

player/clipboard/clipboard.h
@@ -1,0 +1,88 @@
+/*
+ * This file is part of mpv.
+ *
+ * mpv is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * mpv is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "common/common.h"
+#include "common/global.h"
+#include "video/mp_image.h"
+
+struct clipboard_ctx;
+struct MPContext;
+
+enum clipboard_data_type {
+    CLIPBOARD_DATA_TEXT,
+    CLIPBOARD_DATA_IMAGE,
+};
+
+enum clipboard_target {
+    CLIPBOARD_TARGET_CLIPBOARD,
+    CLIPBOARD_TARGET_PRIMARY_SELECTION,
+};
+
+enum clipboard_result {
+    CLIPBOARD_SUCCESS = 0,
+    CLIPBOARD_FAILED = -1,
+};
+
+struct clipboard_data {
+    enum clipboard_data_type type;
+    union {
+        char *text;
+        struct mp_image *image;
+    } u;
+};
+
+struct clipboard_init_params {
+    int flags;
+};
+
+struct clipboard_access_params {
+    int flags;
+    enum clipboard_data_type type;
+    enum clipboard_target target;
+};
+
+struct clipboard_backend {
+    const char *name;
+    const char *desc;
+
+    // Return 0 on success, otherwise -1
+    int (*init)(struct clipboard_ctx *cl, struct clipboard_init_params *params);
+    void (*uninit)(struct clipboard_ctx *cl);
+    int (*get_data)(struct clipboard_ctx *cl, struct clipboard_access_params *params,
+                    struct clipboard_data *out, void *talloc_ctx);
+    int (*set_data)(struct clipboard_ctx *cl, struct clipboard_access_params *params,
+                    struct clipboard_data *data);
+};
+
+struct clipboard_ctx {
+    const struct clipboard_backend *backend; // clipboard description structure
+    struct mp_log *log;
+    void *priv;   // backend-specific internal data
+    struct mpv_global *global;
+};
+
+struct clipboard_ctx *mp_clipboard_create(struct clipboard_init_params *params,
+                                          struct mpv_global *global);
+void mp_clipboard_destroy(struct clipboard_ctx *cl);
+int mp_clipboard_get_data(struct clipboard_ctx *cl, struct clipboard_access_params *params,
+                          struct clipboard_data *out, void *talloc_ctx);
+int mp_clipboard_set_data(struct clipboard_ctx *cl, struct clipboard_access_params *params,
+                          struct clipboard_data *data);
+
+void reinit_clipboard(struct MPContext *mpctx);

meson.build
@@ -173,6 +173,10 @@ sources = files(
     'player/sub.c',
     'player/video.c',
 
+    ## clipboard
+    'player/clipboard/clipboard.c',
+    'player/clipboard/clipboard-vo.c',
+
     ## Streams
     'stream/cookies.c',
     'stream/stream.c',
@@ -257,7 +261,7 @@ sources = files(
     ## tree_allocator
     'ta/ta.c',
     'ta/ta_talloc.c',
-    'ta/ta_utils.c'
+    'ta/ta_utils.c',
 )
 
 
@@ -525,6 +529,7 @@ if features['win32-desktop']
         sources += files('osdep/subprocess-dummy.c')
     endif
     sources += files('input/ipc-win.c',
+                     'player/clipboard/clipboard-win.c',
                      'osdep/language-win.c',
                      'osdep/terminal-win.c',
                      'video/out/w32_common.c',

options/m_option.h
@@ -458,7 +458,8 @@ char *format_file_size(int64_t size);
 #define UPDATE_SUB_EXTS         (1 << 23) // update internal list of sub exts
 #define UPDATE_VIDEO            (1 << 24) // force redraw if needed
 #define UPDATE_VO               (1 << 25) // reinit the VO
-#define UPDATE_OPT_LAST         (1 << 25)
+#define UPDATE_CLIPBOARD        (1 << 26) // reinit the clipboard
+#define UPDATE_OPT_LAST         (1 << 26)
 
 // All bits between _FIRST and _LAST (inclusive)
 #define UPDATE_OPTS_MASK \

options/options.c
@@ -89,6 +89,7 @@ extern const struct m_obj_list vo_obj_list;
 extern const struct m_sub_options ao_conf;
 
 extern const struct m_sub_options dvd_conf;
+extern const struct m_sub_options clipboard_conf;
 
 extern const struct m_sub_options opengl_conf;
 extern const struct m_sub_options vulkan_conf;
@@ -869,6 +870,8 @@ static const m_option_t mp_opts[] = {
     {"", OPT_SUBSTRUCT(resample_opts, resample_conf)},
 
     {"", OPT_SUBSTRUCT(input_opts, input_config)},
+
+    {"clipboard", OPT_SUBSTRUCT(clipboard_opts, clipboard_conf)},
 
     {"", OPT_SUBSTRUCT(vo, vo_sub_opts)},
     {"", OPT_SUBSTRUCT(demux_opts, demux_conf)},

options/options.h
@@ -358,6 +358,8 @@ typedef struct MPOpts {
 
     struct input_opts *input_opts;
 
+    struct clipboard_opts *clipboard_opts;
+
     struct encode_opts *encode_opts;
 
     char *ipc_path;

player/clipboard/clipboard-vo.c
@@ -1,0 +1,44 @@
+/*
+ * This file is part of mpv.
+ *
+ * mpv is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * mpv is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common/common.h"
+#include "clipboard.h"
+
+static int init(struct clipboard_ctx *cl, struct clipboard_init_params *params)
+{
+    return CLIPBOARD_FAILED;
+}
+
+static int get_data(struct clipboard_ctx *cl, struct clipboard_access_params *params,
+                    struct clipboard_data *out, void *talloc_ctx)
+{
+    return CLIPBOARD_FAILED;
+}
+
+static int set_data(struct clipboard_ctx *cl, struct clipboard_access_params *params,
+                    struct clipboard_data *data)
+{
+    return CLIPBOARD_FAILED;
+}
+
+const struct clipboard_backend clipboard_backend_vo = {
+    .name = "vo",
+    .desc = "VO clipboard",
+    .init = init,
+    .get_data = get_data,
+    .set_data = set_data,
+};

player/clipboard/clipboard-win.c
@@ -1,0 +1,45 @@
+/*
+ * This file is part of mpv.
+ *
+ * mpv is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * mpv is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <windows.h>
+#include "common/common.h"
+#include "clipboard.h"
+
+static int init(struct clipboard_ctx *cl, struct clipboard_init_params *params)
+{
+    return CLIPBOARD_FAILED;
+}
+
+static int get_data(struct clipboard_ctx *cl, struct clipboard_access_params *params,
+                    struct clipboard_data *out, void *talloc_ctx)
+{
+    return CLIPBOARD_FAILED;
+}
+
+static int set_data(struct clipboard_ctx *cl, struct clipboard_access_params *params,
+                    struct clipboard_data *data)
+{
+    return CLIPBOARD_FAILED;
+}
+
+const struct clipboard_backend clipboard_backend_win32 = {
+    .name = "win32",
+    .desc = "Windows clipboard",
+    .init = init,
+    .get_data = get_data,
+    .set_data = set_data,
+};

commit ebaac8caf19e2a9cbedfc9409fb692b50e394b44

player/clipboard/clipboard: don't include global.h in header
This header should not be included except when really needed.

Fixes: e1d30c4c5a7443d08c7d4c183644917a340954b2

player/clipboard/clipboard.c
@@ -15,10 +15,12 @@
  * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "clipboard.h"
+
 #include "common/common.h"
+#include "common/global.h"
 #include "options/m_config.h"
 #include "player/core.h"
-#include "clipboard.h"
 
 struct clipboard_opts {
     bool enabled;

player/clipboard/clipboard.h
@@ -18,11 +18,11 @@
 #pragma once
 
 #include "common/common.h"
-#include "common/global.h"
-#include "video/mp_image.h"
 
 struct clipboard_ctx;
+struct mp_image;
 struct MPContext;
+struct mpv_global;
 
 #define CLIPBOARD_INIT_ENABLE_MONITORING (1 << 0)
 