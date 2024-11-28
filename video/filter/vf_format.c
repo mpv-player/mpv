mpv-player/mpv

commit e2365bfeceeef8939d159780b607698d05073910

vf_format: set original params when converting format
Otherwise it will restore to a format before vf_format conversion when
restoring params. Only do this when the out format isn't dolbyvision.

video/filter/vf_format.c
@@ -84,6 +84,11 @@ static void set_params(struct vf_format_opts *p, struct mp_image_params *out,
             out->light = MP_CSP_LIGHT_AUTO;
         }
     }
+    if (p->colormatrix != PL_COLOR_SYSTEM_DOLBYVISION && !p->dovi) {
+        out->primaries_orig = out->color.primaries;
+        out->transfer_orig = out->color.transfer;
+        out->sys_orig = out->repr.sys;
+    }
     if (p->sig_peak)
         out->color.hdr = (struct pl_hdr_metadata){ .max_luma = p->sig_peak * MP_REF_WHITE };
     if (p->light)

commit e129d9346c8464406ea42a425668c397a7a34756

vf_format: fix format=dolbyvision=no metadata stripping
If p->colormatrix is 0 if not set by user, so it would always override
_orig parameters with Dolby Vision ones and restore that erroneously.

Fixes: e2365bfeceeef8939d159780b607698d05073910

video/filter/vf_format.c
@@ -84,7 +84,7 @@ static void set_params(struct vf_format_opts *p, struct mp_image_params *out,
             out->light = MP_CSP_LIGHT_AUTO;
         }
     }
-    if (p->colormatrix != PL_COLOR_SYSTEM_DOLBYVISION && !p->dovi) {
+    if (out->repr.sys != PL_COLOR_SYSTEM_DOLBYVISION) {
         out->primaries_orig = out->color.primaries;
         out->transfer_orig = out->color.transfer;
         out->sys_orig = out->repr.sys;