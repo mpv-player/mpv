4747865fefbc057e0c7998c361fe41ca2e9d9d4f
Kacper Michajłow <kasper93@gmail.com>
2024-12-09 00:39:44 +0100

    player/command: continue hook processing on errors
    If a hook fails to invoke or a client has been removed during hook
    processing, attempt to continue with the next hook if possible.
    
    Before this commit, any failure during hook processing would cause the
    hook processing to stop.
    
    This issue can be reproduced when auto_profiles.lua registers an on_load
    hook and exits afterward because no profiles are registered. In such a
    state, the next hook would fail and not invoke.
    
    There are many scenarios where a hook client may be broken,
    unresponsive, or disabled, so it makes sense to continue processing
    subsequent hooks.
    
    The regression we observed was caused by e2284fba1 and 5dc404741, which
    made auto_profiles.lua register hooks and then exit, leaving stale hooks
    registered. However, the root cause is not limited to the
    auto_profiles.lua case.
    
    Fixes: #15465

--- player/command.c
+++ player/command.c
@@ -196,8 +196,9 @@ bool mp_hook_test_completion(struct MPContext *mpctx, char *type)
         if (h->active && strcmp(h->type, type) == 0) {
             if (!mp_client_id_exists(mpctx, h->client_id)) {
                 MP_WARN(mpctx, "client removed during hook handling\n");
+                // Trigger completion of this hook and continue with the next one.
+                mp_hook_continue(mpctx, h->client_id, h->seq);
                 hook_remove(mpctx, h);
-                break;
             }
             return false;
         }
@@ -229,14 +230,21 @@ static int invoke_hook_handler(struct MPContext *mpctx, struct hook_handler *h)
     return r;
 }
 
-static int run_next_hook_handler(struct MPContext *mpctx, char *type, int index)
+static int run_next_hook_handler(struct MPContext *mpctx, char *type, int start)
 {
     struct command_ctx *cmd = mpctx->command_ctx;
 
-    for (int n = index; n < cmd->num_hooks; n++) {
+    for (int n = start; n < cmd->num_hooks; n++) {
         struct hook_handler *h = cmd->hooks[n];
-        if (strcmp(h->type, type) == 0)
-            return invoke_hook_handler(mpctx, h);
+        if (strcmp(h->type, type) == 0) {
+            int ret = invoke_hook_handler(mpctx, h);
+            // Repeat until the hook is successfully started or none are left.
+            if (ret < 0) {
+                --n;
+                continue;
+            }
+            return ret;
+        }
     }
 
     mp_wakeup_core(mpctx); // finished hook
@@ -247,10 +255,7 @@ static int run_next_hook_handler(struct MPContext *mpctx, char *type, int index)
 // caller needs to use mp_hook_test_completion() to check whether they're done.
 void mp_hook_start(struct MPContext *mpctx, char *type)
 {
-    while (run_next_hook_handler(mpctx, type, 0) < 0) {
-        // We can repeat this until all broken clients have been removed, and
-        // hook processing is successfully started.
-    }
+    run_next_hook_handler(mpctx, type, 0);
 }
 
 int mp_hook_continue(struct MPContext *mpctx, int64_t client_id, uint64_t id)
