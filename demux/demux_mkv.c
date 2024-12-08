mpv-player/mpv

commit 84ee84abb8bd13a50101a896f451c8853f4706dd

demux_mkv: propagate global stream side data via codecpar
The first AVPacket method has been deprecated in FFmpeg, and only
the HEVC decoder would read this information from there. The native
AV1 decoder as well as the libdav1d wrapper only support the newer
propagation method via codec context's side data. {ad,sd,vd}_lavc
call mp_set_avctx_codec_headers which copies the side data from
codecpar to codec context, so no changes are required on the
receiving end.

demux/demux_mkv.c
@@ -1688,7 +1688,34 @@ static int demux_mkv_open_video(demuxer_t *demuxer, mkv_track_t *track)
         sh_v->rotate = rotate;
     }
 
+    // our vessel to transport global track side data to decoder context;
+    // this gets called in all lavc decoders through `mp_set_avctx_codec_headers`
+    // and its failure causes decoding failure. Thus if this call fails here, it is
+    // likely that decoding of this track would also lead to an error.
+    sh_v->lav_codecpar = mp_codec_params_to_av(sh_v);
+    if (!sh_v->lav_codecpar) {
+        MP_ERR(demuxer, "Failed to create codec parameters for track %d!",
+               track->tnum);
+        goto done;
+    }
+
     if (track->dovi_config) {
+        size_t dovi_size;
+        AVDOVIDecoderConfigurationRecord *dovi = av_dovi_alloc(&dovi_size);
+        MP_HANDLE_OOM(dovi);
+
+        memcpy(dovi, track->dovi_config, dovi_size);
+
+        if (!av_packet_side_data_add(&sh_v->lav_codecpar->coded_side_data,
+                                     &sh_v->lav_codecpar->nb_coded_side_data,
+                                     AV_PKT_DATA_DOVI_CONF,
+                                     dovi, dovi_size, 0))
+        {
+            MP_ERR(demuxer, "Failed to attach Dolby Vision configuration record to "
+                   "codec parameters for track %d!\n", track->tnum);
+            av_free(dovi);
+        }
+
         sh_v->dovi = true;
         sh_v->dv_level = track->dovi_config->dv_level;
         sh_v->dv_profile = track->dovi_config->dv_profile;
@@ -2995,18 +3022,6 @@ static int handle_block(demuxer_t *demuxer, struct block_info *block_info)
                     int64_t id = add->n_block_add_id ? add->block_add_id : 1;
                     demux_packet_add_blockadditional(dp, id,
                         add->block_additional.start, add->block_additional.len);
-                }
-            }
-            if (track->dovi_config) {
-                size_t dovi_size;
-                AVDOVIDecoderConfigurationRecord *dovi = av_dovi_alloc(&dovi_size);
-                MP_HANDLE_OOM(dovi);
-                memcpy(dovi, track->dovi_config, dovi_size);
-                if (av_packet_add_side_data(dp->avpacket,
-                                            AV_PKT_DATA_DOVI_CONF,
-                                            (uint8_t *)dovi, dovi_size) < 0)
-                {
-                    av_free(dovi);
                 }
             }
 

commit 0ecc6a099f8a6ba41b6086e14e463a0c3368e66f

demux_mkv: fix memory leak of codec params
Fixes: 84ee84abb8bd13a50101a896f451c8853f4706dd

demux/demux_mkv.c
@@ -1559,6 +1559,11 @@ static const char *const mkv_video_tags[][2] = {
     {0}
 };
 
+static void avcodec_par_destructor(void *p)
+{
+    avcodec_parameters_free(p);
+}
+
 static int demux_mkv_open_video(demuxer_t *demuxer, mkv_track_t *track)
 {
     unsigned char *extradata = NULL;
@@ -1692,7 +1697,9 @@ static int demux_mkv_open_video(demuxer_t *demuxer, mkv_track_t *track)
     // this gets called in all lavc decoders through `mp_set_avctx_codec_headers`
     // and its failure causes decoding failure. Thus if this call fails here, it is
     // likely that decoding of this track would also lead to an error.
-    sh_v->lav_codecpar = mp_codec_params_to_av(sh_v);
+    struct AVCodecParameters **lavp = talloc_ptrtype(track, lavp);
+    talloc_set_destructor(lavp, avcodec_par_destructor);
+    *lavp = sh_v->lav_codecpar = mp_codec_params_to_av(sh_v);
     if (!sh_v->lav_codecpar) {
         MP_ERR(demuxer, "Failed to create codec parameters for track %d!",
                track->tnum);
@@ -2077,11 +2084,6 @@ static const char *const mkv_sub_tag[][2] = {
     { "S_ARIBSUB",          "arib_caption"},
     {0}
 };
-
-static void avcodec_par_destructor(void *p)
-{
-    avcodec_parameters_free(p);
-}
 
 static int demux_mkv_open_sub(demuxer_t *demuxer, mkv_track_t *track)
 {