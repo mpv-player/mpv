mpv-player/mpv

commit e2284fba180fe33f399835f1d1afdb7871ebef4c

command: add load-config-file
Unlike set include mpv.conf, this works after playback has started. It
can be used to auto reload the configuration, e.g. in vim:
autocmd BufWritePost ~/.config/mpv/mpv.conf silent !echo load-config-file %:p | socat - /tmp/mpvsocket

Partially fixes #6362.

player/command.c
@@ -55,6 +55,7 @@
 #include "options/m_option.h"
 #include "options/m_property.h"
 #include "options/m_config_frontend.h"
+#include "options/parse_configfile.h"
 #include "osdep/getpid.h"
 #include "video/out/gpu/context.h"
 #include "video/out/vo.h"
@@ -6270,6 +6271,23 @@ static void cmd_apply_profile(void *p)
     }
 }
 
+static void cmd_load_config_file(void *p)
+{
+    struct mp_cmd_ctx *cmd = p;
+    struct MPContext *mpctx = cmd->mpctx;
+
+    char *config_file = cmd->args[0].v.s;
+    int r = m_config_parse_config_file(mpctx->mconfig, mpctx->global,
+                                       config_file, NULL, 0);
+
+    if (r < 1) {
+        cmd->success = false;
+        return;
+    }
+
+    mp_notify_property(mpctx, "profile-list");
+}
+
 static void cmd_load_script(void *p)
 {
     struct mp_cmd_ctx *cmd = p;
@@ -6806,6 +6824,8 @@ const struct mp_cmd_def mp_cmds[] = {
         {"mode", OPT_CHOICE(v.i, {"apply", 0}, {"restore", 1}),
             .flags = MP_CMD_OPT_ARG}, }
     },
+
+    { "load-config-file", cmd_load_config_file, {{"filename", OPT_STRING(v.s)}} },
 
     { "load-script", cmd_load_script, {{"filename", OPT_STRING(v.s)}} },
 

player/lua/auto_profiles.lua
@@ -164,8 +164,8 @@ local function compile_cond(name, s)
     return chunk
 end
 
-local function load_profiles()
-    for i, v in ipairs(mp.get_property_native("profile-list")) do
+local function load_profiles(profiles_property)
+    for _, v in ipairs(profiles_property) do
         local cond = v["profile-cond"]
         if cond and #cond > 0 then
             local profile = {
@@ -182,17 +182,25 @@ local function load_profiles()
     end
 end
 
-load_profiles()
+mp.observe_property("profile-list", "native", function (_, profiles_property)
+    profiles = {}
+    watched_properties = {}
+    cached_properties = {}
+    properties_to_profiles = {}
+    mp.unobserve_property(on_property_change)
 
-if #profiles < 1 and mp.get_property("load-auto-profiles") == "auto" then
-    -- make it exit immediately
-    _G.mp_event_loop = function() end
-    return
-end
+    load_profiles(profiles_property)
+
+    if #profiles < 1 and mp.get_property("load-auto-profiles") == "auto" then
+        -- make it exit immediately
+        _G.mp_event_loop = function() end
+        return
+    end
+
+    on_idle() -- re-evaluate all profiles immediately
+end)
 
 mp.register_idle(on_idle)
 for _, name in ipairs({"on_load", "on_preloaded", "on_before_start_file"}) do
     mp.add_hook(name, 50, on_hook)
 end
-
-on_idle() -- re-evaluate all profiles immediately

commit 3f5fc3895e1e05c5357fe85ba24aff483450b599

player/command: continue hook processing on errors
If a hook fails to invoke or a client has been removed during hook
processing, attempt to continue with the next hook if possible.

Before this commit, any failure during hook processing would cause the
hook processing to stop.

This issue can be reproduced when auto_profiles.lua registers an on_load
hook and exits afterward because no profiles are registered. In such a
state, the next hook would fail and not invoke.

There are many scenarios where a hook client may be broken,
unresponsive, or disabled, so it makes sense to continue processing
subsequent hooks.

The regression we observed was caused by e2284fba1 and 5dc404741, which
made auto_profiles.lua register hooks and then exit, leaving stale hooks
registered. However, the root cause is not limited to the
auto_profiles.lua case.

Fixes: #15465

player/command.c
@@ -196,8 +196,9 @@ bool mp_hook_test_completion(struct MPContext *mpctx, char *type)
         if (h->active && strcmp(h->type, type) == 0) {
             if (!mp_client_id_exists(mpctx, h->client_id)) {
                 MP_WARN(mpctx, "client removed during hook handling\n");
+                // Trigger completion of this hook and continue with the next one.
+                mp_hook_continue(mpctx, h->client_id, h->seq);
                 hook_remove(mpctx, h);
-                break;
             }
             return false;
         }
@@ -229,14 +230,21 @@ static int invoke_hook_handler(struct MPContext *mpctx, struct hook_handler *h)
     return r;
 }
 
-static int run_next_hook_handler(struct MPContext *mpctx, char *type, int index)
+static int run_next_hook_handler(struct MPContext *mpctx, char *type, int start)
 {
     struct command_ctx *cmd = mpctx->command_ctx;
 
-    for (int n = index; n < cmd->num_hooks; n++) {
+    for (int n = start; n < cmd->num_hooks; n++) {
         struct hook_handler *h = cmd->hooks[n];
-        if (strcmp(h->type, type) == 0)
-            return invoke_hook_handler(mpctx, h);
+        if (strcmp(h->type, type) == 0) {
+            int ret = invoke_hook_handler(mpctx, h);
+            // Repeat until the hook is successfully started or none are left.
+            if (ret < 0) {
+                --n;
+                continue;
+            }
+            return ret;
+        }
     }
 
     mp_wakeup_core(mpctx); // finished hook
@@ -247,10 +255,7 @@ static int run_next_hook_handler(struct MPContext *mpctx, char *type, int index)
 // caller needs to use mp_hook_test_completion() to check whether they're done.
 void mp_hook_start(struct MPContext *mpctx, char *type)
 {
-    while (run_next_hook_handler(mpctx, type, 0) < 0) {
-        // We can repeat this until all broken clients have been removed, and
-        // hook processing is successfully started.
-    }
+    run_next_hook_handler(mpctx, type, 0);
 }
 
 int mp_hook_continue(struct MPContext *mpctx, int64_t client_id, uint64_t id)