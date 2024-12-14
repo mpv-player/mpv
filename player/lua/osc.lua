d4a119994c95792f9a5a37c3197de23c1a37dd3a
Guido Cella <guido@guidocella.xyz>
2024-12-14 18:34:14 +0100

    osc.lua: add cache_enabled() helper

--- player/lua/osc.lua
+++ player/lua/osc.lua
@@ -451,6 +451,10 @@ local function get_touchtimeout()
     return state.touchtime + (get_hidetimeout() / 1000) - mp.get_time()
 end
 
+local function cache_enabled()
+    return state.cache_state and #state.cache_state["seekable-ranges"] > 0
+end
+
 local function reset_margins()
     if state.using_video_margins then
         for _, mopt in ipairs(margins_opts) do
@@ -1912,23 +1916,15 @@ local function osc_init()
         end
     end
     ne.slider.seekRangesF = function()
-        if user_opts.seekrangestyle == "none" then
-            return nil
-        end
-        local cache_state = state.cache_state
-        if not cache_state then
+        if user_opts.seekrangestyle == "none" or not cache_enabled() then
             return nil
         end
         local duration = mp.get_property_number("duration")
         if duration == nil or duration <= 0 then
             return nil
         end
-        local ranges = cache_state["seekable-ranges"]
-        if #ranges == 0 then
-            return nil
-        end
         local nranges = {}
-        for _, range in pairs(ranges) do
+        for _, range in pairs(state.cache_state["seekable-ranges"]) do
             nranges[#nranges + 1] = {
                 ["start"] = 100 * range["start"] / duration,
                 ["end"] = 100 * range["end"] / duration,
@@ -2035,13 +2031,10 @@ local function osc_init()
     ne = new_element("cache", "button")
 
     ne.content = function ()
-        local cache_state = state.cache_state
-        if not (cache_state and cache_state["seekable-ranges"] and
-            #cache_state["seekable-ranges"] > 0) then
-            -- probably not a network stream
+        if not cache_enabled() then
             return ""
         end
-        local dmx_cache = cache_state and cache_state["cache-duration"]
+        local dmx_cache = state.cache_state["cache-duration"]
         local thresh = math.min(state.dmx_cache * 0.05, 5)  -- 5% or 5s
         if dmx_cache and math.abs(dmx_cache - state.dmx_cache) >= thresh then
             state.dmx_cache = dmx_cache
