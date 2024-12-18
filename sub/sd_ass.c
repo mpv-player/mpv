mpv-player/mpv

commit f862d3b6cd66abee98f2af21c36255ef7361cb80

sd_ass: fix margins for all styles when overriding PlayResX
Also save old playresx and use it instead of assuming values of things
we know.

sub/sd_ass.c
@@ -550,12 +550,13 @@ static void configure_ass(struct sd *sd, struct mp_osd_res *dim,
         if (override_playres) {
             int vidw = dim->w - (dim->ml + dim->mr);
             int vidh = dim->h - (dim->mt + dim->mb);
+            int old_playresx = track->PlayResX;
             track->PlayResX = track->PlayResY * (double)vidw / MPMAX(vidh, 1);
-            // ffmpeg and mpv use a default PlayResX of 384 when it is not known,
-            // this comes from VSFilter.
-            double fix_margins = track->PlayResX / (double)MP_ASS_FONT_PLAYRESX;
-            track->styles->MarginL = round(track->styles->MarginL * fix_margins);
-            track->styles->MarginR = round(track->styles->MarginR * fix_margins);
+            double fix_margins = track->PlayResX / (double)old_playresx;
+            for (int n = 0; n < track->n_styles; n++) {
+                track->styles[n].MarginL = round(track->styles[n].MarginL * fix_margins);
+                track->styles[n].MarginR = round(track->styles[n].MarginR * fix_margins);
+            }
         }
     }
 }

commit 3f5b076c73ae436fa282afe12ae7cf64d1960bda

sd_ass: fix fix_margins calculation
Saving old playresx method is incorrect after the first
configure after the subtitle is loaded.

Fixes: f862d3b6cd66abee98f2af21c36255ef7361cb80

sub/sd_ass.c
@@ -617,9 +617,10 @@ static void configure_ass(struct sd *sd, struct mp_osd_res *dim,
         if (override_playres) {
             int vidw = dim->w - (dim->ml + dim->mr);
             int vidh = dim->h - (dim->mt + dim->mb);
-            int old_playresx = track->PlayResX;
             track->PlayResX = track->PlayResY * (double)vidw / MPMAX(vidh, 1);
-            double fix_margins = track->PlayResX / (double)old_playresx;
+            // ffmpeg and mpv use a default PlayResX of 384 when it is not known,
+            // this comes from VSFilter.
+            double fix_margins = track->PlayResX / (double)MP_ASS_FONT_PLAYRESX;
             for (int n = 0; n < track->n_styles; n++) {
                 track->styles[n].MarginL = lrint(track->styles[n].MarginL * fix_margins);
                 track->styles[n].MarginR = lrint(track->styles[n].MarginR * fix_margins);