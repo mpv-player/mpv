d9eb9ed844546fe857b921a46582314788ca34b4
Kacper Michajłow <kasper93@gmail.com>
2024-12-25 14:32:11 +0100

    video/sws_utils: remove unused mp_image_swscale
    This has been used before 2c43d2b75a88b8e0e8f0a715f993ffc1c8977d13. Now
    that we have also zimg support, using swscale directly may not be wanted
    depending on the user options.

--- video/sws_utils.c
+++ video/sws_utils.c
@@ -424,16 +424,6 @@ int mp_sws_scale(struct mp_sws_context *ctx, struct mp_image *dst,
     return 0;
 }
 
-int mp_image_swscale(struct mp_image *dst, struct mp_image *src,
-                     int my_sws_flags)
-{
-    struct mp_sws_context *ctx = mp_sws_alloc(NULL);
-    ctx->flags = my_sws_flags;
-    int res = mp_sws_scale(ctx, dst, src);
-    talloc_free(ctx);
-    return res;
-}
-
 int mp_image_sw_blur_scale(struct mp_image *dst, struct mp_image *src,
                            float gblur)
 {
