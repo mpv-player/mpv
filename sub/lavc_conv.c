c5668729be880899ec0cf8faf60edd5dd4ee8d48
Kacper Michajłow <kasper93@gmail.com>
2024-12-28 16:42:15 +0100

    sub/lavc_conv: free avctx correctly on error
    Fixes memory leak on error.

--- sub/lavc_conv.c
+++ sub/lavc_conv.c
@@ -107,7 +107,7 @@ struct lavc_conv *lavc_conv_create(struct sd *sd)
  error:
     MP_FATAL(priv, "Could not open libavcodec subtitle converter\n");
     av_dict_free(&opts);
-    av_free(avctx);
+    avcodec_free_context(&avctx);
     mp_free_av_packet(&priv->avpkt);
     mp_free_av_packet(&priv->avpkt_vtt);
     talloc_free(priv);
