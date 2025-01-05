mpv-player/mpv

commit f744aadb776dc8269ae6f4cc54c239c548e19a49

ao_pulse: dump library version etc.
Might help with debugging.

Unfortunately, there doesn't seem to be a way to get the actual
pulseaudio server version.

audio/out/ao_pulse.c
@@ -295,6 +295,12 @@ static int init(struct ao *ao)
         goto fail;
     }
 
+    MP_VERBOSE(ao, "Library version: %s\n", pa_get_library_version());
+    MP_VERBOSE(ao, "Proto: %lu\n",
+        (long)pa_context_get_protocol_version(priv->context));
+    MP_VERBOSE(ao, "Server proto: %lu\n",
+        (long)pa_context_get_server_protocol_version(priv->context));
+
     pa_context_set_state_callback(priv->context, context_state_cb, ao);
 
     if (pa_context_connect(priv->context, host, 0, NULL) < 0)

commit b229cf21074c8f866230f44268c8662c6f7cab7b

ao/pulse: only print server protocol after connection
The server's protocol version is not available until a connection is
properly established. Currently the value of `PA_INVALID_INDEX` is printed
due to calling `pa_context_get_server_protocol_version()` before connecting.

Fix this by doing the call after the connection has been set up.

Fixes: f744aadb776d ("ao_pulse: dump library version etc.")

audio/out/ao_pulse.c
@@ -316,8 +316,6 @@ static int pa_init_boilerplate(struct ao *ao)
     MP_VERBOSE(ao, "Library version: %s\n", pa_get_library_version());
     MP_VERBOSE(ao, "Proto: %lu\n",
         (long)pa_context_get_protocol_version(priv->context));
-    MP_VERBOSE(ao, "Server proto: %lu\n",
-        (long)pa_context_get_server_protocol_version(priv->context));
 
     pa_context_set_state_callback(priv->context, context_state_cb, ao);
     pa_context_set_subscribe_callback(priv->context, subscribe_cb, ao);
@@ -334,6 +332,9 @@ static int pa_init_boilerplate(struct ao *ao)
             goto fail;
         pa_threaded_mainloop_wait(priv->mainloop);
     }
+
+    MP_VERBOSE(ao, "Server proto: %lu\n",
+        (long)pa_context_get_server_protocol_version(priv->context));
 
     pa_threaded_mainloop_unlock(priv->mainloop);
     return 0;