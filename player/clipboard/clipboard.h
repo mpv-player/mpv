7a533951e06de4e9aabe12379a2f48202ef72130
nanahi <130121847+na-na-hi@users.noreply.github.com>
2025-01-17 20:13:43 -0500

    clipboard: don't store mpv_global context
    It's only used for logging, and further creation of log contexts
    can use the parenting feature instead, so there is no need to
    store it.

--- player/clipboard/clipboard.h
+++ player/clipboard/clipboard.h
@@ -79,7 +79,6 @@ struct clipboard_ctx {
     const struct clipboard_backend *backend; // clipboard description structure
     struct mp_log *log;
     void *priv;   // backend-specific internal data
-    struct mpv_global *global;
     bool monitor;
 };
 
