dc531a938f2d4321d8efa1f02c60bc1f57ebd638
nanahi <130121847+na-na-hi@users.noreply.github.com>
2025-01-18 01:27:23 -0500

    console.lua: use clipboard property for native Wayland backend
    This backend does not require the window focus quirk handling,
    and also supports primary selection.

--- player/lua/console.lua
+++ player/lua/console.lua
@@ -1179,7 +1179,11 @@ local function get_clipboard(clip)
             return res.stdout
         end
     elseif platform == 'wayland' then
-        -- Wayland clipboard is only updated on window focus
+        if mp.get_property('current-clipboard-backend') == 'wayland' then
+            local property = clip and 'clipboard/text' or 'clipboard/text-primary'
+            return mp.get_property(property, '')
+        end
+        -- Wayland VO clipboard is only updated on window focus
         if clip and mp.get_property_native('focused') then
             return mp.get_property('clipboard/text', '')
         end
