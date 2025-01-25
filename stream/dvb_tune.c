422ca6da5429fcdc19775984bf7a47a984bd8862
nanahi <130121847+na-na-hi@users.noreply.github.com>
2025-01-25 01:42:19 -0500

    various: update license headers
    Change MPlayer to mpv and the last sentence like all other files.

--- stream/dvb_tune.c
+++ stream/dvb_tune.c
@@ -7,20 +7,20 @@
    http://svn.mplayerhq.hu/mplayer/trunk/
    $Id$
 
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License
-   as published by the Free Software Foundation; either version 2
-   of the License, or (at your option) any later version.
+   This file is part of mpv.
 
-   This program is distributed in the hope that it will be useful,
+   mpv is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   mpv is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-   Or, point your browser to http://www.gnu.org/copyleft/gpl.html
+   You should have received a copy of the GNU General Public License along
+   with mpv.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
