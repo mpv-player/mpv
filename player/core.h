e2f219952297691503d685f2b8fc50283592f3f7
Dudemanguy <random342@airmail.cc>
2025-02-17 14:12:05 -0600

    player: run all update handlers on init
    The refactoring in bbac628a1b01261766459c6556e36d2443a42534 missed that
    the initial update handler didn't immediately run all the triggered
    callbacks but would instead delay it until the next playloop. This
    subtly breaks things of course like hooks, force window handling, etc.
    Ensure that we immediately run all the callbacks like before on startup.
    
    Fixes bbac628a1b01261766459c6556e36d2443a42534
    Fixes https://github.com/mpv-player/mpv/pull/15828#issuecomment-2663830477

--- player/core.h
+++ player/core.h
@@ -600,6 +600,7 @@ void mp_wakeup_core(struct MPContext *mpctx);
 void mp_wakeup_core_cb(void *ctx);
 void mp_core_lock(struct MPContext *mpctx);
 void mp_core_unlock(struct MPContext *mpctx);
+void handle_option_callbacks(struct MPContext *mpctx);
 double get_relative_time(struct MPContext *mpctx);
 void reset_playback_state(struct MPContext *mpctx);
 void set_pause_state(struct MPContext *mpctx, bool user_pause);
