d26131e15857b9a7178dd215d558c7272fd09912
Dudemanguy <random342@airmail.cc>
2025-02-17 22:18:13 -0600

    wayland: cleanup redundancy and global state in color-management
    Maybe the xx version was different (probably not), but looking over this
    code again it's pretty clear that there is a lot of unneeded
    complication. There's no need to keep track of the image description,
    creator params, etc. globally. This can all be passed around on the fly.
    Also, reset_color_management was redundant because it would execute on
    reconfig and also on the first draw after the reconfig. We don't need
    both so delete the reconfig path. Collapse the various color functions
    into one simple "set_color_management" and let the wayland listeners do
    all the object destroying for us.

--- video/out/wayland_common.h
+++ video/out/wayland_common.h
@@ -92,8 +92,6 @@ struct vo_wayland_state {
     /* color-management */
     struct wp_color_manager_v1 *color_manager;
     struct wp_color_management_surface_v1 *color_surface;
-    struct wp_image_description_v1 *image_description;
-    struct wp_image_description_creator_params_v1 *image_creator_params;
     struct mp_image_params target_params;
     bool supports_icc;
     bool supports_parametric;
@@ -101,7 +99,6 @@ struct vo_wayland_state {
     bool supports_tf_power;
     bool supports_luminances;
     bool supports_display_primaries;
-    bool unsupported_colorspace;
     int primaries_map[PL_COLOR_PRIM_COUNT];
     int transfer_map[PL_COLOR_TRC_COUNT];
 
