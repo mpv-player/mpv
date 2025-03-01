61eaea5ced7636240acabb4a1b9161ee11d73031
Kacper Michajłow <kasper93@gmail.com>
2025-03-01 10:11:09 +0100

    osdep/compiler: fix MP_ASSERT_UNREACHABLE macro for clang
    Depending on the target, clang may not define `__GNUC__`. One example is
    when targeting `x86_64-pc-windows-msvc`.

--- osdep/compiler.h
+++ osdep/compiler.h
@@ -9,24 +9,20 @@
 #define MP_NORETURN __attribute__((noreturn))
 #define MP_FALLTHROUGH __attribute__((fallthrough))
 #define MP_WARN_UNUSED_RESULT __attribute__((warn_unused_result))
+#define MP_ASSERT_UNREACHABLE() (assert(!"unreachable"), __builtin_unreachable())
 #else
 #define PRINTF_ATTRIBUTE(a1, a2)
 #define SCANF_ATTRIBUTE(a1, a2)
 #define MP_NORETURN
 #define MP_FALLTHROUGH do {} while (0)
 #define MP_WARN_UNUSED_RESULT
+#define MP_ASSERT_UNREACHABLE() (assert(!"unreachable"), abort())
 #endif
 
 // Broken crap with __USE_MINGW_ANSI_STDIO
 #if defined(__MINGW32__) && defined(__GNUC__) && !defined(__clang__)
 #undef PRINTF_ATTRIBUTE
 #define PRINTF_ATTRIBUTE(a1, a2) __attribute__ ((format (gnu_printf, a1, a2)))
-#endif
-
-#ifdef __GNUC__
-#define MP_ASSERT_UNREACHABLE() (assert(!"unreachable"), __builtin_unreachable())
-#else
-#define MP_ASSERT_UNREACHABLE() (assert(!"unreachable"), abort())
 #endif
 
 #endif
