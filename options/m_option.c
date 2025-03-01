mpv-player/mpv

commit 877775f84e86d750094ba0901d3c52fb5fa438ae

m_option: add print callback to start/end/length

options/m_option.c
@@ -2431,10 +2431,28 @@ out:
     return 1;
 }
 
+static char *print_rel_time(const m_option_t *opt, const void *val)
+{
+    const struct m_rel_time *t = val;
+    switch(t->type) {
+    case REL_TIME_ABSOLUTE:
+        return talloc_asprintf(NULL, "%g", t->pos);
+    case REL_TIME_RELATIVE:
+        return talloc_asprintf(NULL, "%s%g",
+            (t->pos >= 0) ? "+" : "-", fabs(t->pos));
+    case REL_TIME_CHAPTER:
+        return talloc_asprintf(NULL, "#%g", t->pos);
+    case REL_TIME_PERCENT:
+        return talloc_asprintf(NULL, "%g%%", t->pos);
+    }
+    return talloc_strdup(NULL, "none");
+}
+
 const m_option_type_t m_option_type_rel_time = {
     .name  = "Relative time or percent position",
     .size  = sizeof(struct m_rel_time),
     .parse = parse_rel_time,
+    .print = print_rel_time,
     .copy  = copy_opt,
 };
 

commit 12928532ee14eb35c4a68ea520fdde67849bd77c

options/m_option: fix REL_TIME_CHAPTER printing
The relative time chapter is 1-based even though it stores
0-based values internally, but the printing doesn't consider
that and gives 0-based value.

Change it to use the same base as setting the value so that it prints
the same value as being set.

Fixes: 877775f84e86d750094ba0901d3c52fb5fa438ae

options/m_option.c
@@ -2906,7 +2906,7 @@ static char *print_rel_time(const m_option_t *opt, const void *val)
     case REL_TIME_RELATIVE:
         return talloc_asprintf(NULL, "%+g", t->pos);
     case REL_TIME_CHAPTER:
-        return talloc_asprintf(NULL, "#%g", t->pos);
+        return talloc_asprintf(NULL, "#%g", t->pos + 1);
     case REL_TIME_PERCENT:
         return talloc_asprintf(NULL, "%g%%", t->pos);
     }