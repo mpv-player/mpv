23372644939d8ab967d1bff5ef25fe984b3efe27
Kacper Michajłow <kasper93@gmail.com>
2025-03-02 10:40:05 +0100

    common/common: use mp_require for MP_HANDLE_OOM

--- common/common.h
+++ common/common.h
@@ -167,9 +167,7 @@ char **mp_dup_str_array(void *tctx, char **s);
 // kill the process even with NDEBUG.
 #define MP_HANDLE_OOM(x) do {   \
         void *oom_p_ = (x);     \
-        mp_assert(oom_p_);      \
-        if (!oom_p_)            \
-            abort();            \
+        mp_require(oom_p_);     \
     } while (0)
 
 #ifdef _MSC_VER
