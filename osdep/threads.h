cef7a83daa2a68d708102fa685c49d03f9c20f51
Kacper Michajłow <kasper93@gmail.com>
2025-03-03 12:42:07 +0100

    threads: remove unused recursive mutex support
    After removal of `MP_MUTEX_RECURSIVE` from vdpau.c there are no more
    clients of recursive mutex. Remove all the code related to it. This
    commit can be reverted if in the future it is needed again. For now
    let's not keep dead code.

--- osdep/threads.h
+++ osdep/threads.h
@@ -3,17 +3,6 @@
 
 #include "config.h"
 
-enum mp_mutex_type {
-    MP_MUTEX_NORMAL = 0,
-    MP_MUTEX_RECURSIVE,
-};
-
-#define mp_mutex_init(mutex) \
-    mp_mutex_init_type(mutex, MP_MUTEX_NORMAL)
-
-#define mp_mutex_init_type(mutex, mtype) \
-    mp_mutex_init_type_internal(mutex, mtype)
-
 #if HAVE_WIN32_THREADS
 #include "threads-win32.h"
 #else
