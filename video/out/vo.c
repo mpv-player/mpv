mpv-player/mpv

commit 8798cec7fa14bbeb35c7b711db3a41d336f78a69

vo: replace driver->untimed with VO_CAP_UNTIMED
There's a weird mix and match of ways to set special things about a VO
so let's consolidate it all to use VO_CAP bits. More importantly this
fixes do_redraw not being properly ignored/skipped in the untimed VO
case. This was intended in 8657b8e1f3c247dcba441a4c61e3beef2acc2cd4 but
later the code got reorganized/moved around to make it no longer a no-op
but we just got lucky and there were not any known problems with the
broken logic until 5338f4ba70903e7517bbfc7881e10ff13a912ffc.

video/out/vo.c
@@ -1076,15 +1076,14 @@ static void do_redraw(struct vo *vo)
 {
     struct vo_internal *in = vo->in;
 
-    if (!vo->config_ok || (vo->driver->caps & VO_CAP_NORETAIN))
+    if (!vo->config_ok || (vo->driver->caps & VO_CAP_NORETAIN) ||
+        (vo->driver->caps & VO_CAP_UNTIMED))
         return;
 
     mp_mutex_lock(&in->lock);
     in->request_redraw = false;
     bool full_redraw = in->dropped_frame;
-    struct vo_frame *frame = NULL;
-    if (!vo->driver->untimed)
-        frame = vo_frame_ref(in->current_frame);
+    struct vo_frame *frame = vo_frame_ref(in->current_frame);
     if (frame)
         in->dropped_frame = false;
     struct vo_frame dummy = {0};

player/video.c
@@ -410,7 +410,7 @@ static bool use_video_lookahead(struct MPContext *mpctx)
 {
     return mpctx->video_out &&
            !(mpctx->video_out->driver->caps & VO_CAP_NORETAIN) &&
-           !(mpctx->opts->untimed || mpctx->video_out->driver->untimed) &&
+           !(mpctx->opts->untimed || (mpctx->video_out->driver->caps & VO_CAP_UNTIMED)) &&
            !mpctx->opts->video_latency_hacks;
 }
 
@@ -627,7 +627,8 @@ static void update_avsync_before_frame(struct MPContext *mpctx)
          * If untimed is set always output frames immediately
          * without sleeping.
          */
-        if (mpctx->time_frame < -0.2 || opts->untimed || vo->driver->untimed)
+        if (mpctx->time_frame < -0.2 || opts->untimed ||
+            (vo->driver->caps & VO_CAP_UNTIMED))
             mpctx->time_frame = 0;
     }
 }
@@ -1247,7 +1248,7 @@ void write_video(struct MPContext *mpctx)
     struct vo_frame *frame = vo_frame_ref(&dummy);
 
     double diff = mpctx->past_frames[0].approx_duration;
-    if (opts->untimed || vo->driver->untimed)
+    if (opts->untimed || (vo->driver->caps & VO_CAP_UNTIMED))
         diff = -1; // disable frame dropping and aspects of frame timing
     if (diff >= 0) {
         diff /= mpctx->video_speed;

video/out/vo.h
@@ -202,6 +202,8 @@ enum {
     VO_CAP_NORETAIN     = 1 << 2,
     // VO supports applying film grain
     VO_CAP_FILM_GRAIN   = 1 << 3,
+    // VO is drawn untimed and is never redrawn
+    VO_CAP_UNTIMED      = 1 << 4,
 };
 
 enum {
@@ -323,9 +325,6 @@ struct vo_driver {
 
     // VO_CAP_* bits
     int caps;
-
-    // Disable video timing, push frames as quickly as possible, never redraw.
-    bool untimed;
 
     // The VO is responsible for freeing frames.
     bool frame_owner;

video/out/vo_image.c
@@ -158,7 +158,7 @@ const struct vo_driver video_out_image =
 {
     .description = "Write video frames to image files",
     .name = "image",
-    .untimed = true,
+    .caps = VO_CAP_UNTIMED,
     .priv_size = sizeof(struct priv),
     .preinit = preinit,
     .query_format = query_format,

video/out/vo_lavc.c
@@ -259,8 +259,8 @@ const struct vo_driver video_out_lavc = {
     .encode = true,
     .description = "video encoding using libavcodec",
     .name = "lavc",
+    .caps = VO_CAP_UNTIMED,
     .initially_blocked = true,
-    .untimed = true,
     .priv_size = sizeof(struct priv),
     .preinit = preinit,
     .query_format = query_format,

commit 84c90346e1e07a985e85625c2d9eb59d5913e000

vo: don't burn cpu in paused state for untimed VOs
Fixes VO thread busy looping in paused state for untimed VOs.

One example is vo_lavc (encoding mode), used commonly to extract
thumbnail by scripts.

Example command that was affected:
mpv --ovc=rawvideo --of=image2 --ofopts=update=1 --o=test --pause
file.ext

We have to update request_redraw value to not redraw again. This has
been skipped by early exit before.

This also fixes VO_CAP_NORETAIN case.

Fixes: 8798cec7fa14bbeb35c7b711db3a41d336f78a69

video/out/vo.c
@@ -1076,12 +1076,17 @@ static void do_redraw(struct vo *vo)
 {
     struct vo_internal *in = vo->in;
 
-    if (!vo->config_ok || (vo->driver->caps & VO_CAP_NORETAIN) ||
-        (vo->driver->caps & VO_CAP_UNTIMED))
+    if (!vo->config_ok)
         return;
 
     mp_mutex_lock(&in->lock);
     in->request_redraw = false;
+
+    if (vo->driver->caps & (VO_CAP_NORETAIN | VO_CAP_UNTIMED)) {
+        mp_mutex_unlock(&in->lock);
+        return;
+    }
+
     bool full_redraw = in->dropped_frame;
     struct vo_frame *frame = vo_frame_ref(in->current_frame);
     if (frame)