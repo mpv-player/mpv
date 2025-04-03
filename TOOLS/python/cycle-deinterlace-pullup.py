e02e6f0793a6a304bb989219ef005be6df0e19cf
8lurry <sharifmehedi24@outlook.com>
2025-04-03 17:47:12 +0600

    python: add script cycle-deinterlace-pullup

---
+++ TOOLS/python/cycle-deinterlace-pullup.py
@@ -0,0 +1,50 @@
+# This script cycles between deinterlacing, pullup (inverse
+# telecine), and both filters off. It uses the "deinterlace" property
+# so that a hardware deinterlacer will be used if available.
+#
+# It overrides the default deinterlace toggle keybinding "D"
+# (shift+d), so that rather than merely cycling the "deinterlace" property
+# between on and off, it adds a "pullup" step to the cycle.
+#
+# It provides OSD feedback as to the actual state of the two filters
+# after each cycle step/keypress.
+#
+# Note: if hardware decoding is enabled, pullup filter will likely
+# fail to insert.
+#
+# TODO: It might make sense to use hardware assisted vdpaupp=pullup,
+# if available, but I don't have hardware to test it. Patch welcome.
+
+from mpvclient import mpv
+
+script_name = mpv.name
+pullup_label = "%s-pullup" % script_name
+
+def pullup_on():
+    for vf in mpv.get_property_node("vf"):
+        if vf["label"] == pullup_label:
+            return "yes"
+    return "no"
+
+def do_cycle():
+    if pullup_on() == "yes":
+        # if pullup is on remove it
+        mpv.command_string("vf remove @%s:pullup" % pullup_label)
+        return
+    elif mpv.get_property_string("deinterlace") == "yes":
+        # if deinterlace is on, turn it off and insert pullup filter
+        mpv.set_property_string("deinterlace", "no")
+        mpv.command_string("vf add @%s:pullup" % pullup_label)
+        return
+    else:
+        # if neither is on, turn on deinterlace
+        mpv.set_property_string("deinterlace", "yes")
+        return
+
+
+@mpv.add_binding(key="D", name="cycle-deinterlace-pullup")
+def cycle_deinterlace_pullup_handler():
+    do_cycle()
+    # independently determine current state and give user feedback
+    mpv.osd_message("deinterlace: %s\npullup: %s" % (
+            mpv.get_property_string("deinterlace"), pullup_on()))
