mpv-player/mpv

commit f24611980cab484148e5f50ea0916ba1a7e1ccb6

sub/osd: add osd-shaper and set it to simple by default
This is a change in default for libass with harfbuzz enabled, previously
it would choose the best available shaper.

Harfbuzz isn't really needed for mpv's OSD and it comes with a
significant performance penalty, so use simple instead

options/options.c
@@ -414,6 +414,7 @@ const struct m_sub_options mp_osd_render_sub_opts = {
         {"osd-prune-delay", OPT_DOUBLE(osd_ass_prune_delay), M_RANGE(-1.0, 10000.0)},
         {"osd-glyph-limit", OPT_INT(osd_glyph_limit)},
         {"osd-bitmap-max-size", OPT_INT(osd_bitmap_max_size)},
+        {"osd-shaper", OPT_CHOICE(osd_shaper, {"simple", 0}, {"complex", 1})},
         {0}
     },
     .size = sizeof(OPT_BASE_STRUCT),

options/options.h
@@ -151,6 +151,7 @@ struct mp_osd_render_opts {
     double osd_ass_prune_delay;
     int osd_glyph_limit;
     int osd_bitmap_max_size;
+    int osd_shaper;
 };
 
 typedef struct MPOpts {

sub/osd_libass.c
@@ -128,6 +128,7 @@ static void create_ass_track(struct osd_state *osd, struct osd_object *obj,
     track->WrapStyle = 1; // end-of-line wrapping instead of smart wrapping
     track->Kerning = true;
     track->ScaledBorderAndShadow = true;
+    ass_set_shaper(ass->render, opts->osd_shaper);
 #if LIBASS_VERSION >= 0x01600010
     ass_track_set_feature(track, ASS_FEATURE_WRAP_UNICODE, 1);
 #endif

commit 88545d332c2dcdc23d4b05a3a887de8d735c7ed4

sub/osd: set osd-shaper to complex by default
I'd rather not argue about which is better so go back to status quo,
users can use the more sensible option of "simple" if it doesn't break
anything (which it won't).

Using simple has benefits of growing memory usage slower and much faster
OSD rendering when complex isn't needed.

Simple breaks rendering complex glyphs like Material font icons

Fixes: https://github.com/tomasklaen/uosc/issues/1086
Fixes: f24611980cab ("sub/osd: add osd-shaper and set it to simple by default")

options/options.c
@@ -424,6 +424,7 @@ const struct m_sub_options mp_osd_render_sub_opts = {
         .osd_selected_color = {250, 189, 47, 255},
         .osd_selected_outline_color = {0, 0, 0, 255},
         .osd_ass_prune_delay = -1.0,
+        .osd_shaper = 1,
     },
     .change_flags = UPDATE_OSD,
 };