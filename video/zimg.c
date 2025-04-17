mpv-player/mpv

commit 6a96b15cf48e75a1f2b17dccddf7ada95f3e0fcf

video/csputils: add missing alpha choice

video/zimg.c
@@ -198,6 +198,18 @@ static zimg_color_primaries_e mp_to_z_prim(enum pl_color_primaries prim)
     }
 }
 
+#if HAVE_ZIMG_ALPHA
+static zimg_alpha_type_e pl_to_z_alpha(enum pl_alpha_mode alpha)
+{
+    switch (alpha) {
+    case PL_ALPHA_UNKNOWN:       return ZIMG_ALPHA_STRAIGHT;
+    case PL_ALPHA_INDEPENDENT:   return ZIMG_ALPHA_STRAIGHT;
+    case PL_ALPHA_PREMULTIPLIED: return ZIMG_ALPHA_PREMULTIPLIED;
+    default:                     return ZIMG_ALPHA_NONE;
+    }
+}
+#endif
+
 static void destroy_zimg(struct mp_zimg_context *ctx)
 {
     for (int n = 0; n < ctx->num_states; n++) {
@@ -378,8 +390,7 @@ static bool setup_format(zimg_image_format *zfmt, struct mp_zimg_repack *r,
             r->z_planes[3] = n; // alpha, always plane 4 in zimg
 
 #if HAVE_ZIMG_ALPHA
-            zfmt->alpha = fmt.repr.alpha == PL_ALPHA_PREMULTIPLIED
-                ? ZIMG_ALPHA_PREMULTIPLIED : ZIMG_ALPHA_STRAIGHT;
+            zfmt->alpha = pl_to_z_alpha(fmt.repr.alpha);
 #else
             return false;
 #endif

player/command.c
@@ -2418,10 +2418,16 @@ static int property_imgparams(const struct mp_image_params *p, int action, void 
         for (int i = 0; i < desc.num_planes; i++)
             bpp += desc.bpp[i] >> (desc.xs[i] + desc.ys[i]);
 
+#if PL_API_VER >= 344
+        // If PL_ALPHA_NONE is supported, use it directly, unless in auto mode.
+        if ((desc.flags & MP_IMGFLAG_ALPHA) && alpha == PL_ALPHA_UNKNOWN)
+            alpha = PL_ALPHA_INDEPENDENT;
+#else
         // Alpha type is not supported by FFmpeg, so PL_ALPHA_UNKNOWN may mean alpha
         // is of an unknown type, or simply not present. Normalize to AUTO=no alpha.
         if (!!(desc.flags & MP_IMGFLAG_ALPHA) != (alpha != PL_ALPHA_UNKNOWN))
             alpha = (desc.flags & MP_IMGFLAG_ALPHA) ? PL_ALPHA_INDEPENDENT : PL_ALPHA_UNKNOWN;
+#endif
     }
 
     const struct pl_hdr_metadata *hdr = &p->color.hdr;

video/csputils.c
@@ -120,6 +120,9 @@ const struct m_opt_choice_alternatives pl_alpha_names[] = {
     {"auto",        PL_ALPHA_UNKNOWN},
     {"straight",    PL_ALPHA_INDEPENDENT},
     {"premul",      PL_ALPHA_PREMULTIPLIED},
+#if PL_API_VER >= 344
+    {"none",        PL_ALPHA_NONE},
+#endif
     {0}
 };
 

commit b3acd08ffe7030f31bfaf05c0382bc4fa10ba5bb

video/zimg: set alpha to ZIMG_ALPHA_STRAIGHT in all cases except premul
Fixes: 6a96b15cf48e75a1f2b17dccddf7ada95f3e0fcf ("video/csputils: add missing alpha choice")
Fixes: #16255

video/zimg.c
@@ -198,18 +198,6 @@ static zimg_color_primaries_e mp_to_z_prim(enum pl_color_primaries prim)
     }
 }
 
-#if HAVE_ZIMG_ALPHA
-static zimg_alpha_type_e pl_to_z_alpha(enum pl_alpha_mode alpha)
-{
-    switch (alpha) {
-    case PL_ALPHA_UNKNOWN:       return ZIMG_ALPHA_STRAIGHT;
-    case PL_ALPHA_INDEPENDENT:   return ZIMG_ALPHA_STRAIGHT;
-    case PL_ALPHA_PREMULTIPLIED: return ZIMG_ALPHA_PREMULTIPLIED;
-    default:                     return ZIMG_ALPHA_NONE;
-    }
-}
-#endif
-
 static void destroy_zimg(struct mp_zimg_context *ctx)
 {
     for (int n = 0; n < ctx->num_states; n++) {
@@ -390,7 +378,8 @@ static bool setup_format(zimg_image_format *zfmt, struct mp_zimg_repack *r,
             r->z_planes[3] = n; // alpha, always plane 4 in zimg
 
 #if HAVE_ZIMG_ALPHA
-            zfmt->alpha = pl_to_z_alpha(fmt.repr.alpha);
+            zfmt->alpha = fmt.repr.alpha == PL_ALPHA_PREMULTIPLIED
+                ? ZIMG_ALPHA_PREMULTIPLIED : ZIMG_ALPHA_STRAIGHT;
 #else
             return false;
 #endif