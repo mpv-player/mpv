1d1535ff9124fdeb3c81a2f089551e2cc8404613
Kacper Michajłow <kasper93@gmail.com>
2025-05-22 21:58:10 +0200

    ci/openbsd: bump to 7.7 and remove ffmpeg subproject

--- ci/build-openbsd.sh
+++ ci/build-openbsd.sh
@@ -3,24 +3,6 @@ set -e
 
 . ./ci/build-common.sh
 
-# FFmpeg on openBSD (4.4.4) is too old; use a subproject
-rm -rf subprojects
-mkdir -p subprojects
-cat <<EOF > subprojects/ffmpeg.wrap
-[wrap-git]
-url = https://gitlab.freedesktop.org/gstreamer/meson-ports/ffmpeg.git
-revision = meson-7.1
-depth = 1
-[provide]
-libavcodec = libavcodec_dep
-libavdevice = libavdevice_dep
-libavfilter = libavfilter_dep
-libavformat = libavformat_dep
-libavutil = libavutil_dep
-libswresample = libswresample_dep
-libswscale = libswscale_dep
-EOF
-
 meson setup build $common_args \
   -Dffmpeg:vulkan=auto \
   -Dffmpeg:werror=false \
