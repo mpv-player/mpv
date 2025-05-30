mpv-player/mpv

commit c7c09fdb8b5ccfb91b44cb3d1e5184c92ef7a070

command: fix spammy events when frame stepping forward with seek
The frame-step command also signals on up events. This is only useful
when using the default "play" mode. For seek mode, it triggers another
frame step which is not what you want. Rearrange the logic so the up
event is ignored for the seek flag. The frames check is also not needed
since add_step_frame already handles it.

Fixes 9661a3839ba5af79e5620ce87acedf6cdaa244c1

player/command.c
@@ -5688,7 +5688,10 @@ static void cmd_frame_step(void *p)
         return;
     }
 
-    if (frames > 0 && !flags && cmd->cmd->is_up_down) {
+    if (flags) {
+        if (!cmd->cmd->is_up)
+            add_step_frame(mpctx, frames, flags);
+    } else {
         if (cmd->cmd->is_up) {
             if (mpctx->step_frames < 1)
                 set_pause_state(mpctx, true);
@@ -5699,8 +5702,6 @@ static void cmd_frame_step(void *p)
                 add_step_frame(mpctx, frames, flags);
             }
         }
-    } else {
-        add_step_frame(mpctx, frames, flags);
     }
 }
 

commit b4535b1a93184023cea872fb469775637019a9f5

command: fix frame step with mouse buttons
c7c09fdb8b5ccfb91b44cb3d1e5184c92ef7a070 made frame step with seek flag
ignore key ups. However, for commands with emit_on_up flag set, which can
happen with mouse buttons, command should only be triggered on key ups.
This results in frame-step command with seek flag triggered on mouse down
instead of mouse up, and frame-back-step command not working with mouse.

Fix this by making sure that step is triggered depending on the emit_on_up
flag.

Fixes: c7c09fdb8b5ccfb91b44cb3d1e5184c92ef7a070

player/command.c
@@ -5750,7 +5750,10 @@ static void cmd_frame_step(void *p)
     }
 
     if (flags == 1) {
-        if (!cmd->cmd->is_up)
+        // frame-step command has on_updown set so it is called on both down
+        // and up, but stepping should only be triggered when it matches the
+        // emit_on_up flag.
+        if (cmd->cmd->is_up == cmd->cmd->emit_on_up)
             add_step_frame(mpctx, frames, true);
     } else {
         if (cmd->cmd->is_up) {