7fd423124bbeb3b5eb35c3e27e08fca6082a51a9
Hubert Głuchowski <hubert.gluchowski19@gmail.com>
2025-07-06 19:50:16 +0200

    ci: build with subrandr on mingw64 and msys2

--- ci/build-msys2.sh
+++ ci/build-msys2.sh
@@ -8,9 +8,20 @@ args=(
   -D{egl-angle-lib,egl-angle-win32,pdf-build,rubberband,win32-smtc}=enabled
 )
 
-[[ "$SYS" == "clang64" ]] && args+=(
-  -Db_sanitize=address,undefined
-)
+if [[ "$SYS" == "clang64" ]]; then
+    args+=(
+      -Db_sanitize=address,undefined
+    )
+else # sanitizers are not supported on stable rust yet so clang64 is excluded
+    echo "::group::Building subrandr"
+    build_subrandr "$PWD/subrandr_prefix"
+    echo "::endgroup::"
+    export PKG_CONFIG_PATH="$PWD/subrandr_prefix/lib/pkgconfig:$PKG_CONFIG_PATH"
+    mkdir build
+    # make sure mpv.com will be able to find the DLL
+    cp subrandr_prefix/bin/subrandr-[0-9]*.dll build
+    args+=(-Dsubrandr=enabled)
+fi
 
 meson setup build $common_args "${args[@]}"
 meson compile -C build
