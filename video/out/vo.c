mpv-player/mpv

commit d21b1f159c7700945b4e75843d68bee881c229a7

vo: move vo_gpu_next above vo_gpu in probe order

video/out/vo.c
@@ -70,8 +70,8 @@ extern const struct vo_driver video_out_kitty;
 static const struct vo_driver *const video_out_drivers[] =
 {
     // high-quality and well-supported VOs first:
-    &video_out_gpu,
     &video_out_gpu_next,
+    &video_out_gpu,
 
 #if HAVE_VDPAU
     &video_out_vdpau,

commit 136597c75b8a5e21508569a9d406e28eeff30454

vo: reject gpu-next from autoprobing, if blend-subtitles=video is used
Apparently it's critical for mpv to be anime media player and after
d21b1f159c7700945b4e75843d68bee881c229a7 there is a worry in community
that mpv is no longer anime enough, as it doesn't support VSFilter
style blending subtitles into video in default config.

As this is important use-case to ensure correct subtitle rendering and
blending, remove gpu-next from autoprobing if not explicity requested
by user and blend-subtitles=video is set.

Fixes: d21b1f159c7700945b4e75843d68bee881c229a7

	TOOLS/lua/mpv360.lua

video/out/vo.c
@@ -42,6 +42,7 @@
 #include "common/stats.h"
 #include "video/hwdec.h"
 #include "video/mp_image.h"
+#include "video/out/gpu/video.h"
 #include "sub/osd.h"
 #include "osdep/io.h"
 #include "osdep/threads.h"
@@ -334,6 +335,7 @@ error:
 struct vo *init_best_video_out(struct mpv_global *global, struct vo_extra *ex)
 {
     struct mp_vo_opts *opts = mp_get_config_group(NULL, global, &vo_sub_opts);
+    struct gl_video_opts *gl_opts = mp_get_config_group(NULL, global, &gl_video_conf);
     struct m_obj_settings *vo_list = opts->video_driver_list;
     struct vo *vo = NULL;
     // first try the preferred drivers, with their optional subdevice param:
@@ -355,11 +357,15 @@ autoprobe:
         const struct vo_driver *driver = video_out_drivers[i];
         if (driver == &video_out_null)
             break;
+        // reject gpu-next from autoprobing, if blend-subtitles=video is requested
+        if (driver == &video_out_gpu_next && gl_opts->blend_subs == BLEND_SUBS_VIDEO)
+            continue;
         vo = vo_create(true, global, ex, (char *)driver->name);
         if (vo)
             goto done;
     }
 done:
+    talloc_free(gl_opts);
     talloc_free(opts);
     return vo;
 }