mpv-player/mpv

commit 699054cfd96b10699ba5da9dcee0028e796fa3b1

vo_dmabuf_wayland: use pl_color_space_infer on HDR transfers
vo_gpu_next already does this. If we have HDR, try to fill out any
missing values before doing validation.

video/out/wayland_common.c
@@ -3456,6 +3456,7 @@ static void set_color_management(struct vo_wayland_state *wl)
     wp_image_description_creator_params_v1_set_primaries_named(image_creator_params, primaries);
     wp_image_description_creator_params_v1_set_tf_named(image_creator_params, transfer);
 
+    pl_color_space_infer(&wl->target_params.color);
     struct pl_hdr_metadata hdr = wl->target_params.color.hdr;
     bool use_metadata = hdr_metadata_valid(&hdr);
     if (!use_metadata)

commit a5fe0b7fb84d4e4123bc5b5f543571e582bc54b8

wayland: call pl_color_space_infer on local copy of pl_color_space
instead of directly using wl->target_params, which can modify
wl->target_params and cause vo_wayland_handle_color to not return early.

Also use the local copy when reading pl_hdr_metadata

Fixes: https://github.com/mpv-player/mpv/issues/16825
Fixes: 699054cfd96b ("vo_dmabuf_wayland: use pl_color_space_infer on HDR transfers")

video/out/wayland_common.c
@@ -3467,8 +3467,8 @@ static void set_color_management(struct vo_wayland_state *wl)
     wp_image_description_creator_params_v1_set_primaries_named(image_creator_params, primaries);
     wp_image_description_creator_params_v1_set_tf_named(image_creator_params, transfer);
 
-    pl_color_space_infer(&wl->target_params.color);
-    struct pl_hdr_metadata hdr = wl->target_params.color.hdr;
+    pl_color_space_infer(&color);
+    struct pl_hdr_metadata hdr = color.hdr;
     bool is_hdr = pl_color_transfer_is_hdr(color.transfer);
     bool use_metadata = hdr_metadata_valid(&hdr);
     if (!use_metadata)