mpv-player/mpv

commit 2fd88ffbaeea3d81ca48847a94d0c8246de7a7ac

various: print Image instead or Video for image tracks
Fixes #8561.

player/lua/stats.lua
@@ -952,9 +952,10 @@ local function add_video(s)
         return
     end
 
-    append(s, "", {prefix="Video:", nl=o.nl .. o.nl, indent=""})
     local track = mp.get_property_native("current-tracks/video")
-    if track and append(s, track["codec-desc"], {prefix_sep="", nl="", indent=""}) then
+    if track then
+        append(s, "", {prefix=track.image and "Image:" or "Video:", nl=o.nl .. o.nl, indent=""})
+        append(s, track["codec-desc"], {prefix_sep="", nl="", indent=""})
         append(s, track["codec-profile"], {prefix="[", nl="", indent=" ", prefix_sep="",
                no_prefix_markup=true, suffix="]"})
         if track["codec"] ~= track["decoder"] then
@@ -1217,7 +1218,7 @@ local function add_track(c, t, i)
         return
     end
 
-    local type = t["type"]:sub(1,1):upper() .. t["type"]:sub(2)
+    local type = t.image and "Image" or t["type"]:sub(1, 1):upper() .. t["type"]:sub(2)
     append(c, "", {prefix=type .. ":", nl=o.nl .. o.nl, indent=""})
     append(c, t["title"], {prefix_sep="", nl="", indent=""})
     append(c, t["id"], {prefix="ID:"})

player/command.c
@@ -2076,9 +2076,12 @@ static int get_track_entry(int item, int action, void *arg, void *ctx)
     return m_property_read_sub(props, action, arg);
 }
 
-static const char *track_type_name(enum stream_type t)
+static const char *track_type_name(struct track *t)
 {
-    switch (t) {
+    if (t->image)
+        return "Image";
+
+    switch (t->type) {
     case STREAM_VIDEO: return "Video";
     case STREAM_AUDIO: return "Audio";
     case STREAM_SUB: return "Sub";
@@ -2100,7 +2103,7 @@ static int property_list_tracks(void *ctx, struct m_property *prop,
                     continue;
 
                 res = talloc_asprintf_append(res, "%s: ",
-                                             track_type_name(track->type));
+                                             track_type_name(track));
                 res = talloc_strdup_append(res,
                                 track->selected ? list_current : list_normal);
                 res = talloc_asprintf_append(res, "(%d) ", track->user_tid);

player/loadfile.c
@@ -252,7 +252,7 @@ static void print_stream(struct MPContext *mpctx, struct track *t, bool indent)
     const char *langopt = "?";
     switch (t->type) {
     case STREAM_VIDEO:
-        tname = "Video"; selopt = "vid"; langopt = "vlang";
+        tname = t->image ? "Image" : "Video"; selopt = "vid"; langopt = "vlang";
         break;
     case STREAM_AUDIO:
         tname = "Audio"; selopt = "aid"; langopt = "alang";

player/lua/select.lua
@@ -87,7 +87,8 @@ mp.add_forced_key_binding(nil, "select-track", function ()
     local tracks = {}
 
     for i, track in ipairs(mp.get_property_native("track-list")) do
-        tracks[i] = track.type:sub(1, 1):upper() .. track.type:sub(2) .. ": " ..
+        tracks[i] = (track.image and "Image" or
+                     track.type:sub(1, 1):upper() .. track.type:sub(2)) .. ": " ..
                     format_track(track)
     end
 

commit 1f7b82aa394d413a7d0e9489337b13d4a15f3c8e

stats.lua: skip codec params if they are not available
Fixes: 2fd88ffbaeea3d81ca48847a94d0c8246de7a7ac

player/lua/stats.lua
@@ -1001,8 +1001,7 @@ local function add_video(s)
 
     local track = mp.get_property_native("current-tracks/video")
     append(s, "", {prefix=track and track.image and "Image:" or "Video:", nl=o.nl .. o.nl, indent=""})
-    if track then
-        append(s, track["codec-desc"], {prefix_sep="", nl="", indent=""})
+    if track and append(s, track["codec-desc"], {prefix_sep="", nl="", indent=""}) then
         append(s, track["codec-profile"], {prefix="[", nl="", indent=" ", prefix_sep="",
                no_prefix_markup=true, suffix="]"})
         if track["codec"] ~= track["decoder"] then