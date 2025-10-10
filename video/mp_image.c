mpv-player/mpv

commit add2bf50cd2a368f9bf011ecb387e7ba4aeef4a7

mp_image: use pl_color_space_nominal_luma_ex() to set nominal csp peak

video/mp_image.c
@@ -1016,15 +1016,15 @@ void mp_image_params_guess_csp(struct mp_image_params *params)
         params->color.transfer = PL_COLOR_TRC_UNKNOWN;
     }
 
-    if (!params->color.hdr.max_luma) {
-        if (params->color.transfer == PL_COLOR_TRC_HLG) {
-            params->color.hdr.max_luma = 1000; // reference display
-        } else {
-            // If the signal peak is unknown, we're forced to pick the TRC's
-            // nominal range as the signal peak to prevent clipping
-            params->color.hdr.max_luma = pl_color_transfer_nominal_peak(params->color.transfer) * MP_REF_WHITE;
-        }
-    }
+    // If the signal peak is unknown, we're forced to pick the TRC's
+    // nominal range as the signal peak to prevent clipping
+    pl_color_space_nominal_luma_ex(pl_nominal_luma_params(
+        .color      = &params->color,
+        .metadata   = PL_HDR_METADATA_HDR10,
+        .scaling    = PL_HDR_NORM,
+        .out_min    = NULL,
+        .out_max    = &params->color.hdr.max_luma,
+    ));
 
     if (!pl_color_space_is_hdr(&params->color)) {
         // Some clips have leftover HDR metadata after conversion to SDR, so to

commit 89d9f7865d3a3fb8a0a11f19858123e4505ffa80

mp_image: use correct scaling for nominal peak
Fixes copy-paste error

Fixes: add2bf50cd2a368f9bf011ecb387e7ba4aeef4a7

video/mp_image.c
@@ -1021,8 +1021,7 @@ void mp_image_params_guess_csp(struct mp_image_params *params)
     pl_color_space_nominal_luma_ex(pl_nominal_luma_params(
         .color      = &params->color,
         .metadata   = PL_HDR_METADATA_HDR10,
-        .scaling    = PL_HDR_NORM,
-        .out_min    = NULL,
+        .scaling    = PL_HDR_NITS,
         .out_max    = &params->color.hdr.max_luma,
     ));
 