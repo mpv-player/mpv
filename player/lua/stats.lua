mpv-player/mpv

commit 92aeb634790e6fc4ae989b67f09cce2cc86064ec

stats.lua: make video block even when no video track
fixes formatting with `mpv --idle --force-window` with no files loaded

player/lua/stats.lua
@@ -1000,8 +1000,8 @@ local function add_video(s)
     end
 
     local track = mp.get_property_native("current-tracks/video")
+    append(s, "", {prefix=track and track.image and "Image:" or "Video:", nl=o.nl .. o.nl, indent=""})
     if track then
-        append(s, "", {prefix=track.image and "Image:" or "Video:", nl=o.nl .. o.nl, indent=""})
         append(s, track["codec-desc"], {prefix_sep="", nl="", indent=""})
         append(s, track["codec-profile"], {prefix="[", nl="", indent=" ", prefix_sep="",
                no_prefix_markup=true, suffix="]"})

commit bdaabd678f4b1d8abb45b2cfaeb9464598367949

stats.lua: split lines to make them shorter
Fixes: 92aeb634790e6fc4ae989b67f09cce2cc86064ec

player/lua/stats.lua
@@ -1000,7 +1000,8 @@ local function add_video(s)
     end
 
     local track = mp.get_property_native("current-tracks/video")
-    append(s, "", {prefix=track and track.image and "Image:" or "Video:", nl=o.nl .. o.nl, indent=""})
+    local track_type = (track and track.image) and "Image:" or "Video:"
+    append(s, "", {prefix=track_type, nl=o.nl .. o.nl, indent=""})
     if track and append(s, track["codec-desc"], {prefix_sep="", nl="", indent=""}) then
         append(s, track["codec-profile"], {prefix="[", nl="", indent=" ", prefix_sep="",
                no_prefix_markup=true, suffix="]"})