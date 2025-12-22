41e1a18b628f074f73b13865d5e20373e4e775f5
Kacper Michajłow <kasper93@gmail.com>
2025-12-22 16:37:46 +0100

    ci/build-linux-old: remove custom libplacebo build
    Alpine 3.21 has libplacebo 6.338.2, we can use that.

--- ci/build-linux-old.sh
+++ ci/build-linux-old.sh
@@ -3,12 +3,6 @@ set -e
 
 . ./ci/build-common.sh
 
-# clone exactly the oldest libplacebo we want to support
-mkdir -p subprojects
-git clone https://code.videolan.org/videolan/libplacebo.git \
-    --recurse-submodules --shallow-submodules \
-    --depth=1 --branch v6.338 subprojects/libplacebo \
-
 meson setup build $common_args \
  -Dlibplacebo:vulkan=disabled \
  -Dlua=enabled \
