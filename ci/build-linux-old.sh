4e6594e06af31b144f5396ba55c5caeab7f9cf8f
Kacper Michajłow <kasper93@gmail.com>
2025-12-22 11:54:34 +0100

    ci/linux-ffmpeg-6-1: add wayland-protocols

--- ci/build-linux-old.sh
+++ ci/build-linux-old.sh
@@ -2,15 +2,19 @@
 set -e
 
 . ./ci/build-common.sh
+export CFLAGS="$CFLAGS -Wno-unused-function"
 
 # clone exactly the oldest libplacebo we want to support
 mkdir -p subprojects
 git clone https://code.videolan.org/videolan/libplacebo.git \
     --recurse-submodules --shallow-submodules \
-    --depth=1 --branch v6.338 subprojects/libplacebo \
+    --depth=1 --branch v6.338 subprojects/libplacebo
+git clone https://gitlab.freedesktop.org/wayland/wayland-protocols \
+    --depth=1 --branch 1.38 subprojects/wayland-protocols
 
 meson setup build $common_args \
  -Dlibplacebo:vulkan=disabled \
- -Dlua=enabled
+ -Dlua=enabled \
+ -Dwayland=enabled
 meson compile -C build
 ./build/mpv -v --no-config
