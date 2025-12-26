79a8c8507967834056a989089a48cc0591ebac40
der richter <der.richter@gmx.de>
2025-12-26 15:36:40 +0100

    ci/mac: add missing libraries for consistency with other platforms
    add libbluray, libarchive, libcaca, libcdio-paranoia, libdvdnav,
    rubberband and zimg to macOS builds for consistency with other platforms
    and detection tests.

--- ci/build-macos.sh
+++ ci/build-macos.sh
@@ -16,7 +16,8 @@ PKG_CONFIG_PATH="${FFMPEG_SYSROOT}/lib/pkgconfig/" CC="${CC}" CXX="${CXX}" \
 meson setup build $common_args \
   -Dprefix="${MPV_INSTALL_PREFIX}" \
   -Dobjc_args="-Wno-error=deprecated -Wno-error=deprecated-declarations" \
-  -D{gl,iconv,lcms2,lua,jpeg,plain-gl,zlib}=enabled \
+  -D{caca,cdda,dvdnav,gl,iconv,lcms2,libarchive,libbluray,lua,jpeg}=enabled \
+  -D{plain-gl,rubberband,zimg,zlib}=enabled \
   -D{cocoa,coreaudio,gl-cocoa,videotoolbox-gl,videotoolbox-pl}=enabled \
   -D{swift-build,macos-cocoa-cb,macos-media-player,macos-touchbar,vulkan}=enabled \
   -Dswift-flags="${SWIFT_FLAGS}"
