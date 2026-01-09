9229d52e756d880fe538680859315b170dc6f02b
llyyr <llyyr.public@gmail.com>
2026-01-09 22:41:52 +0530

    vo_gpu_next: undo dither-depth=auto meaning 8bpc for SDR when undetected
    This commit effectively reverts f914947dda421aa5903084cf1813412c8bacb003
    
    We don't need this hack anymore

--- video/out/vo_gpu_next.c
+++ video/out/vo_gpu_next.c
@@ -898,11 +898,7 @@ static void apply_target_options(struct priv *p, struct pl_frame *target,
     int dither_depth = opts->dither_depth;
     if (dither_depth == 0) {
         struct ra_swapchain *sw = p->ra_ctx->swapchain;
-        if (sw->fns->color_depth && sw->fns->color_depth(sw) != -1) {
-            dither_depth = sw->fns->color_depth(sw);
-        } else if (!pl_color_transfer_is_hdr(target->color.transfer)) {
-            dither_depth = 8;
-        }
+        dither_depth = sw->fns->color_depth ? sw->fns->color_depth(sw) : 0;
     }
     if (dither_depth > 0) {
         struct pl_bit_encoding *tbits = &target->repr.bits;
