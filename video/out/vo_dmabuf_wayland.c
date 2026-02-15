2f5561a77991361f2ce890ffda59299337d1a7db
Dudemanguy <random342@airmail.cc>
2026-02-15 01:34:36 -0600

    wayland: use set_color for vo_wayland_handle_color
    Move this out of swap_buffers. This makes it more consistent with what
    vo_dmabuf_wayland does.

--- video/out/vo_dmabuf_wayland.c
+++ video/out/vo_dmabuf_wayland.c
@@ -636,7 +636,7 @@ static bool draw_frame(struct vo *vo, struct vo_frame *frame)
                 vo_wait_on_vo(vo, !wl->image_description_processed);
                 p->vo_is_waiting = !wl->image_description_processed;
             }
-            vo_wayland_handle_color(wl);
+            vo_wayland_handle_color(wl, &frame->current->params);
             struct mp_image *image = buf->frame->current;
             wl_surface_attach(wl->video_surface, buf->buffer, 0, 0);
             wl_surface_damage_buffer(wl->video_surface, 0, 0, image->w,
