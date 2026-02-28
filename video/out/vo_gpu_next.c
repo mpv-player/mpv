mpv-player/mpv

commit 9d2da2567c03a14a0640998dbd44127259490d23

video/out/gpu: add set_color ra_swapchain_fns
Anything related color management gets complex and there is a lot of
platform-specific code that gets called in relation to this. Currently,
this stuff gets called in various places in the render loop which mostly
works but it's prone to "off by 1 frame" errors and also isn't clean.
Introduce this helper to hopefully unify all this kind of stuff under
one umbrella. For integration with vo_gpu_next, this needs to be called
before flip_page and we should stop abusing target_params for this
purpose. The next couple of commits will implement this in two places.

video/out/vo_gpu_next.c
@@ -944,6 +944,32 @@ static void apply_crop(struct pl_frame *frame, struct mp_rect crop,
     }
 }
 
+static void set_colorspace_hint(struct priv *p, struct pl_color_space *hint)
+{
+    struct ra_swapchain *sw = p->ra_ctx->swapchain;
+    int alpha = 0;
+#if PL_API_VER >= 344
+    alpha = PL_ALPHA_NONE;
+#endif
+
+    struct mp_image_params params = {
+        .color = hint ? *hint : pl_color_space_srgb,
+        .repr = {
+            .sys = PL_COLOR_SYSTEM_RGB,
+            .levels = p->output_levels,
+            .alpha = p->ra_ctx->opts.want_alpha ? PL_ALPHA_INDEPENDENT : alpha,
+        },
+    };
+
+    if (sw->fns->set_color) {
+        sw->fns->set_color(sw, &params);
+        if (hint)
+            *hint = params.color;
+    } else {
+        pl_swapchain_colorspace_hint(p->sw, hint);
+    }
+}
+
 static void update_tm_viz(struct pl_color_map_params *params,
                           const struct pl_frame *target)
 {
@@ -1223,11 +1249,11 @@ static bool draw_frame(struct vo *vo, struct vo_frame *frame)
         // Update again after possible max_luma change
         if (p->icc_profile)
             hint = p->icc_profile->csp;
-        pl_swapchain_colorspace_hint(p->sw, &hint);
+        set_colorspace_hint(p, &hint);
     } else if (!target_hint) {
         if (!hint.hdr.min_luma)
             hint.hdr.min_luma = target_csp.hdr.min_luma;
-        pl_swapchain_colorspace_hint(p->sw, NULL);
+        set_colorspace_hint(p, NULL);
     }
 
     struct pl_swapchain_frame swframe;
@@ -1254,6 +1280,8 @@ static bool draw_frame(struct vo *vo, struct vo_frame *frame)
     // Calculate target
     struct pl_frame target;
     pl_frame_from_swapchain(&target, &swframe);
+    if (target_hint && sw->fns->set_color)
+        target.color = hint;
     bool strict_sw_params = target_hint && p->next_opts->target_hint_strict;
     apply_target_options(p, &target, hint.hdr.min_luma, strict_sw_params);
     if (target.color.transfer == PL_COLOR_TRC_SRGB && frame->current &&

video/out/gpu/context.h
@@ -77,6 +77,9 @@ struct ra_ctx_params {
     // Preferred device color space. Optional.
     pl_color_space_t (*preferred_csp)(struct ra_ctx *ctx);
 
+    // See ra_swapchain_fns.set_color. Optional.
+    void (*set_color)(struct ra_ctx *ctx, struct mp_image_params *params);
+
     // See ra_swapchain_fns.get_vsync.
     void (*get_vsync)(struct ra_ctx *ctx, struct vo_vsync_info *info);
 
@@ -111,6 +114,10 @@ struct ra_swapchain_fns {
 
     // Target device color space. Optional.
     pl_color_space_t (*target_csp)(struct ra_swapchain *sw);
+
+    // Call into backends so they can use the appropriate platform-specific
+    // functions to configure color spaces.
+    void (*set_color)(struct ra_swapchain *sw, struct mp_image_params *params);
 
     // Called when rendering starts. Returns NULL on failure. This must be
     // followed by submit_frame, to submit the rendered frame. This function

video/out/opengl/context.c
@@ -302,6 +302,13 @@ static void ra_gl_ctx_get_vsync(struct ra_swapchain *sw,
         p->params.get_vsync(sw->ctx, info);
 }
 
+static void ra_gl_ctx_set_color(struct ra_swapchain *sw, struct mp_image_params *params)
+{
+    struct priv *p = sw->priv;
+    if (p->params.set_color)
+        p->params.set_color(sw->ctx, params);
+}
+
 static pl_color_space_t ra_gl_ctx_target_csp(struct ra_swapchain *sw)
 {
     struct priv *p = sw->priv;
@@ -312,6 +319,7 @@ static pl_color_space_t ra_gl_ctx_target_csp(struct ra_swapchain *sw)
 
 static const struct ra_swapchain_fns ra_gl_swapchain_fns = {
     .color_depth   = ra_gl_ctx_color_depth,
+    .set_color     = ra_gl_ctx_set_color,
     .target_csp    = ra_gl_ctx_target_csp,
     .start_frame   = ra_gl_ctx_start_frame,
     .submit_frame  = ra_gl_ctx_submit_frame,

video/out/vulkan/context.c
@@ -526,6 +526,13 @@ static void get_vsync(struct ra_swapchain *sw,
         p->params.get_vsync(sw->ctx, info);
 }
 
+static void set_color(struct ra_swapchain *sw, struct mp_image_params *params)
+{
+    struct priv *p = sw->priv;
+    if (p->params.set_color)
+        p->params.set_color(sw->ctx, params);
+}
+
 static pl_color_space_t target_csp(struct ra_swapchain *sw)
 {
     struct priv *p = sw->priv;
@@ -536,6 +543,7 @@ static pl_color_space_t target_csp(struct ra_swapchain *sw)
 
 static const struct ra_swapchain_fns vulkan_swapchain = {
     .color_depth   = color_depth,
+    .set_color     = set_color,
     .target_csp    = target_csp,
     .start_frame   = start_frame,
     .submit_frame  = submit_frame,

commit 4861f4ca75fc13ae8c26d78e1686955ccd390af3

vo_gpu_next: fix rgb range in Wayland hint
Fixes: 9d2da2567c03a14a0640998dbd44127259490d23
Fixes: #17479

video/out/vo_gpu_next.c
@@ -956,7 +956,7 @@ static void set_colorspace_hint(struct priv *p, struct pl_color_space *hint)
         .color = hint ? *hint : pl_color_space_srgb,
         .repr = {
             .sys = PL_COLOR_SYSTEM_RGB,
-            .levels = p->output_levels,
+            .levels = p->output_levels ? p->output_levels : PL_COLOR_LEVELS_FULL,
             .alpha = p->ra_ctx->opts.want_alpha ? PL_ALPHA_INDEPENDENT : alpha,
         },
     };