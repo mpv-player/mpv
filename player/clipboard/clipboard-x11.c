dd9f7949a2cdf91b4d6844e3df901683fd718256
nanahi <130121847+na-na-hi@users.noreply.github.com>
2026-03-15 08:06:51 -0400

    clipboard-x11: disable automatic content fetching when not monitoring

--- player/clipboard/clipboard-x11.c
+++ player/clipboard/clipboard-x11.c
@@ -44,6 +44,7 @@ struct clipboard_x11_priv {
     Window window;
     Display *display;
     int XFixesSelectionNotifyEvent;
+    bool monitor;
 };
 
 #define XA(x11, s) (XInternAtom((x11)->display, # s, False))
@@ -60,7 +61,7 @@ static void clipboard_x11_uninit(struct clipboard_x11_priv *x11)
         XCloseDisplay(x11->display);
 }
 
-static bool clipboard_x11_init(struct clipboard_x11_priv *x11, bool xwayland)
+static bool clipboard_x11_init(struct clipboard_x11_priv *x11, bool xwayland, bool monitor)
 {
     if (!xwayland && (getenv("WAYLAND_DISPLAY") || getenv("WAYLAND_SOCKET"))) {
         MP_VERBOSE(x11, "Stopping init due to suspected wayland environment\n");
@@ -77,6 +78,7 @@ static bool clipboard_x11_init(struct clipboard_x11_priv *x11, bool xwayland)
     if (!x11->window)
         goto err;
     x11->XFixesSelectionNotifyEvent = -1;
+    x11->monitor = monitor;
     int opcode, event, error;
     if (XQueryExtension(x11->display, "XFIXES", &opcode, &event, &error)) {
         x11->XFixesSelectionNotifyEvent = event + XFixesSelectionNotify;
@@ -91,11 +93,13 @@ static bool clipboard_x11_init(struct clipboard_x11_priv *x11, bool xwayland)
 
     // XFixesSelectionNotifyEvent does not give an initial notification
     // so request selection here
-    XConvertSelection(x11->display, XA(x11, CLIPBOARD), XA(x11, UTF8_STRING),
-                      XA(x11, MPV_CLIPBOARD), x11->window, CurrentTime);
-    XConvertSelection(x11->display, XA_PRIMARY, XA(x11, UTF8_STRING),
-                      XA(x11, MPV_PRIMARY), x11->window, CurrentTime);
-    XFlush(x11->display);
+    if (x11->monitor) {
+        XConvertSelection(x11->display, XA(x11, CLIPBOARD), XA(x11, UTF8_STRING),
+                          XA(x11, MPV_CLIPBOARD), x11->window, CurrentTime);
+        XConvertSelection(x11->display, XA_PRIMARY, XA(x11, UTF8_STRING),
+                          XA(x11, MPV_PRIMARY), x11->window, CurrentTime);
+        XFlush(x11->display);
+    }
 
     return true;
 
@@ -193,7 +197,7 @@ static bool clipboard_x11_dispatch_events(struct clipboard_x11_priv *x11, int64_
             XEvent event;
             XNextEvent(x11->display, &event);
             MP_TRACE(x11, "XEvent: %d\n", event.type);
-            if (event.type == x11->XFixesSelectionNotifyEvent) {
+            if (event.type == x11->XFixesSelectionNotifyEvent && x11->monitor) {
                 XFixesSelectionNotifyEvent *ev = (XFixesSelectionNotifyEvent *)&event;
                 if (ev->owner != x11->window && ev->selection == XA(x11, CLIPBOARD)) {
                     XConvertSelection(x11->display, ev->selection, XA(x11, UTF8_STRING),
@@ -248,7 +252,8 @@ static int init(struct clipboard_ctx *cl, struct clipboard_init_params *params)
 
     if (mp_make_wakeup_pipe(priv->message_pipe) < 0)
         goto pipe_err;
-    if (!clipboard_x11_init(priv, params->flags & CLIPBOARD_INIT_ENABLE_XWAYLAND))
+    if (!clipboard_x11_init(priv, params->flags & CLIPBOARD_INIT_ENABLE_XWAYLAND,
+                            params->flags & CLIPBOARD_INIT_ENABLE_MONITORING))
         goto init_err;
     if (mp_thread_create(&priv->thread, clipboard_thread, cl->priv))
         goto thread_err;
