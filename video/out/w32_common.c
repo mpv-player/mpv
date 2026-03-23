mpv-player/mpv

commit 7466a9f9f3c87a08b1f238a6e2c1d8b99526eca0

video/out/win_state: don't move window when force-window-position=no
When force-window-position=no place window on requested position only on
init (when VO requests `force_pos`), except that keep existing
position on auto resizes.

This applies to both user provided geometry and default center
position.

Fixes: #17588

video/out/w32_common.c
@@ -2348,6 +2348,10 @@ static int gui_thread_control(struct vo_w32_state *w32, int request, void *arg)
                 if (!w32->window_bounds_initialized)
                     return VO_TRUE;
 
+                // Force window repositioning if geometry xy is valid.
+                if (changed_option == &vo_opts->geometry)
+                    w32->window_bounds_initialized = !w32->opts->geometry.xy_valid;
+
                 if (w32->current_fs) {
                     w32->pending_resize = true;
                     w32->pending_maximize = false;

video/out/win_state.c
@@ -74,9 +74,9 @@ static void apply_autofit(int *w, int *h, int scr_w, int scr_h,
 //  monitor: position of the monitor on virtual desktop (used for pixelaspect).
 //  dpi_scale: the DPI multiplier to get from virtual to real coordinates
 //             (>1 for "hidpi")
-//  force_center: force centering x/y in the middle of the given screen even if
-//                opts->force_window_position is not set. ignored if the user
-//                supplies valid x/y coordinates on their own.
+//  force_pos: force window position even if opts->force_window_position is not
+//             set. If geometry x/y is not valid, this will center the window.
+//             Useful for initial window placement.
 //  size_constraint: if non-NULL, the resulting size will be limited by the size
 //                   specified in this geometry. This applies to both autofit
 //                   and geometry options.
@@ -88,7 +88,7 @@ static void apply_autofit(int *w, int *h, int scr_w, int scr_h,
 void vo_calc_window_geometry(struct vo *vo, struct mp_vo_opts *opts,
                              const struct mp_rect *screen,
                              const struct mp_rect *monitor, double dpi_scale,
-                             bool force_center, struct vo_win_geometry *out_geo,
+                             bool force_pos, struct vo_win_geometry *out_geo,
                              struct m_geometry *size_constraint)
 {
     *out_geo = (struct vo_win_geometry){0};
@@ -130,7 +130,7 @@ void vo_calc_window_geometry(struct vo *vo, struct mp_vo_opts *opts,
     out_geo->win.x0 = (int)(scr_w - d_w) / 2;
     out_geo->win.y0 = (int)(scr_h - d_h) / 2;
 
-    bool center = (opts->force_window_position || force_center) && !opts->geometry.xy_valid;
+    bool center = (opts->force_window_position || force_pos) && !opts->geometry.xy_valid;
     m_geometry_apply(&out_geo->win.x0, &out_geo->win.y0, &d_w, &d_h,
                      scr_w, scr_h, center, &opts->geometry);
 
@@ -148,7 +148,7 @@ void vo_calc_window_geometry(struct vo *vo, struct mp_vo_opts *opts,
     out_geo->win.x1 = out_geo->win.x0 + d_w;
     out_geo->win.y1 = out_geo->win.y0 + d_h;
 
-    if (opts->geometry.xy_valid || opts->force_window_position || force_center)
+    if (opts->force_window_position || force_pos)
         out_geo->flags |= VO_WIN_FORCE_POS;
 }
 

video/out/win_state.h
@@ -29,7 +29,7 @@ struct m_geometry;
 void vo_calc_window_geometry(struct vo *vo, struct mp_vo_opts *opts,
                              const struct mp_rect *screen,
                              const struct mp_rect *monitor, double dpi_scale,
-                             bool force_center, struct vo_win_geometry *out_geo,
+                             bool force_pos, struct vo_win_geometry *out_geo,
                              struct m_geometry *size_constraint);
 void vo_apply_window_geometry(struct vo *vo, const struct vo_win_geometry *geo);
 

video/out/x11_common.c
@@ -1810,7 +1810,7 @@ void vo_x11_config_vo_window(struct vo *vo)
     vo_apply_window_geometry(vo, &geo);
 
     struct mp_rect rc = !x11->pseudo_mapped || opts->auto_window_resize || opts->geometry.wh_valid ||
-                        opts->geometry.xy_valid ? geo.win : x11->nofsrc;
+                        (geo.flags & VO_WIN_FORCE_POS) ? geo.win : x11->nofsrc;
 
     if (x11->parent) {
         vo_x11_update_geometry(vo);
@@ -2132,6 +2132,9 @@ int vo_x11_control(struct vo *vo, int *events, int request, void *arg)
                             &x11->opts->window_maximized);
                     vo_x11_maximize(vo);
                 }
+                // Force window repositioning if geometry xy is valid.
+                if (opt == &opts->geometry)
+                    x11->pseudo_mapped = !x11->opts->geometry.xy_valid;
                 vo_x11_set_geometry(vo);
             }
         }

commit 43d678dd52ab3bbed101ad3ffe2aab382f39fb46

w32_common: fix Windows 10 workaround for initial window pos
Restores hack from 3d4170b81bae6f714952b24682618b25782cbcf1 to work
again.

Fixes: 7466a9f9f3c87a08b1f238a6e2c1d8b99526eca0

video/out/w32_common.c
@@ -1982,6 +1982,7 @@ set_pos_done:
         // adjustment to window position based on invisible borders, on
         // Windows 11 it should be a no-op.
         w32->pending_reset_size = true;
+        w32->window_bounds_initialized = false;
         window_resize(w32);
     }
 