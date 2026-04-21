mpv-player/mpv

commit 940ab990558a017e45cca3daa91f65ecff170c38

osc.lua: make shift+mbtn_left an alias of mbtn_mid
...instead of the opposite. This simplifies bind_mouse_buttons().

player/lua/osc.lua
@@ -1696,10 +1696,6 @@ local function bind_mouse_buttons(element_name)
         local command = user_opts[element_name .. "_" .. button .. "_command"]
 
         if command ~= "" then
-            if button == "mbtn_mid" then
-                button = "shift+mbtn_left"
-            end
-
             elements[element_name].eventresponder[button .. "_up"] = function ()
                 mp.command(command)
             end
@@ -1820,7 +1816,7 @@ local function osc_init()
     ne.content = "\238\128\132"
     ne.eventresponder["mbtn_left_down"] =
         function () mp.commandv("seek", -5) end
-    ne.eventresponder["shift+mbtn_left_down"] =
+    ne.eventresponder["mbtn_mid"] =
         function () mp.commandv("frame-back-step") end
     ne.eventresponder["mbtn_right_down"] =
         function () mp.commandv("seek", -30) end
@@ -1832,7 +1828,7 @@ local function osc_init()
     ne.content = "\238\128\133"
     ne.eventresponder["mbtn_left_down"] =
         function () mp.commandv("seek", 10) end
-    ne.eventresponder["shift+mbtn_left_down"] =
+    ne.eventresponder["mbtn_mid"] =
         function () mp.commandv("frame-step") end
     ne.eventresponder["mbtn_right_down"] =
         function () mp.commandv("seek", 60) end
@@ -2639,13 +2635,13 @@ do_enable_keybindings()
 mp.set_key_bindings({
     {"mbtn_left",           function() process_event("mbtn_left", "up") end,
                             function() process_event("mbtn_left", "down")  end},
-    {"shift+mbtn_left",     function() process_event("shift+mbtn_left", "up") end,
-                            function() process_event("shift+mbtn_left", "down")  end},
+    {"mbtn_mid",            function() process_event("mbtn_mid", "up") end,
+                            function() process_event("mbtn_mid", "down")  end},
     {"mbtn_right",          function() process_event("mbtn_right", "up") end,
                             function() process_event("mbtn_right", "down")  end},
-    -- alias to shift_mbtn_left for single-handed mouse use
-    {"mbtn_mid",            function() process_event("shift+mbtn_left", "up") end,
-                            function() process_event("shift+mbtn_left", "down")  end},
+    -- alias shift+mbtn_left to mbtn_mid for touchpads
+    {"shift+mbtn_left",     function() process_event("mbtn_mid", "up") end,
+                            function() process_event("mbtn_mid", "down")  end},
     {"wheel_up",            function() process_event("wheel_up", "press") end},
     {"wheel_down",          function() process_event("wheel_down", "press") end},
     {"mbtn_left_dbl",       "ignore"},

commit 19e9b0b5fa0c681f5564f872345f85a3dedaa961

osc.lua: fix frame stepping with skip_backward/forward buttons
The event responder must have up, down, or press suffix, but
940ab990558a017e45cca3daa91f65ecff170c38 removed it for frame stepping
function, making it unusable. Fix this by using the down suffix.

Fixes: 940ab990558a017e45cca3daa91f65ecff170c38

player/lua/osc.lua
@@ -2311,7 +2311,7 @@ local function osc_init()
     ne.content = icons.skip_backward
     ne.eventresponder["mbtn_left_down"] =
         function () mp.commandv("seek", -5) end
-    ne.eventresponder["mbtn_mid"] =
+    ne.eventresponder["mbtn_mid_down"] =
         function () mp.commandv("frame-back-step") end
     ne.eventresponder["mbtn_right_down"] =
         function () mp.commandv("seek", -30) end
@@ -2323,7 +2323,7 @@ local function osc_init()
     ne.content = icons.skip_forward
     ne.eventresponder["mbtn_left_down"] =
         function () mp.commandv("seek", 10) end
-    ne.eventresponder["mbtn_mid"] =
+    ne.eventresponder["mbtn_mid_down"] =
         function () mp.commandv("frame-step") end
     ne.eventresponder["mbtn_right_down"] =
         function () mp.commandv("seek", 60) end