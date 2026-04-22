5260fdc7d9832adc388ce6433cd87d067149f38e
Guido Cella <guido@guidocella.xyz>
2026-04-22 12:01:25 +0200

    osc.lua: use the cached chapter_list instead or retrieving chapters

--- player/lua/osc.lua
+++ player/lua/osc.lua
@@ -2339,7 +2339,6 @@ local function osc_init()
     elements = {}
 
     -- some often needed stuff
-    local have_ch = (mp.get_property_number("chapters", 0) > 0)
     local loop = mp.get_property("loop-playlist", "no")
 
     local ne
@@ -2425,14 +2424,14 @@ local function osc_init()
     --chapter_prev
     ne = new_element("chapter_prev", "button")
 
-    ne.enabled = have_ch
+    ne.enabled = #state.chapter_list > 0
     ne.content = icons.chapter_prev
     bind_mouse_buttons("chapter_prev")
 
     --chapter_next
     ne = new_element("chapter_next", "button")
 
-    ne.enabled = have_ch
+    ne.enabled = #state.chapter_list > 0
     ne.content = icons.chapter_next
     bind_mouse_buttons("chapter_next")
 
