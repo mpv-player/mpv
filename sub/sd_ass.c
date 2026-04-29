mpv-player/mpv

commit 1723f8a9d200bed5e225ba640411d06782cca8ca

sd_ass: fix fix_margins calculation
Saving old playresx method is incorrect after the first
configure after the subtitle is loaded.

Fixes: f862d3b6cd66abee98f2af21c36255ef7361cb80

sub/sd_ass.c
@@ -617,9 +617,10 @@ static void configure_ass(struct sd *sd, struct mp_osd_res *dim,
         if (override_playres) {
             int vidw = dim->w - (dim->ml + dim->mr);
             int vidh = dim->h - (dim->mt + dim->mb);
-            int old_playresx = track->PlayResX;
             track->PlayResX = track->PlayResY * (double)vidw / MPMAX(vidh, 1);
-            double fix_margins = track->PlayResX / (double)old_playresx;
+            // ffmpeg and mpv use a default PlayResX of 384 when it is not known,
+            // this comes from VSFilter.
+            double fix_margins = track->PlayResX / (double)MP_ASS_FONT_PLAYRESX;
             for (int n = 0; n < track->n_styles; n++) {
                 track->styles[n].MarginL = lrint(track->styles[n].MarginL * fix_margins);
                 track->styles[n].MarginR = lrint(track->styles[n].MarginR * fix_margins);

commit b5f1e517fe71c198e19cf2babc8ac935c1bf35e4

sub/sd_ass: reuse PlayResX instead of hardcoding to 384
Hardcoding 384 here isn't correct, we should be using the original
PlayResX after all.

The problem commit 1723f8a9d200bed5e225ba640411d06782cca8ca was trying
to fix is instead fixed by rounding PlayResX instead of implicitly
truncating it. Before 1723f8a9d200bed5e225ba640411d06782cca8ca, PlayResX
was calculated using doubles and implicitly truncated to int, causing
off by one errors (e.g. 511 vs 512) due to truncation errors.

Round to nearest integer instead to fix this.

Fixes: 1723f8a9d200bed5e225ba640411d06782cca8ca
Fixes: f862d3b6cd66abee98f2af21c36255ef7361cb80

sub/sd_ass.c
@@ -634,10 +634,9 @@ static void configure_ass(struct sd *sd, struct mp_osd_res *dim,
         if (override_playres) {
             int vidw = dim->w - (dim->ml + dim->mr);
             int vidh = dim->h - (dim->mt + dim->mb);
-            track->PlayResX = track->PlayResY * (double)vidw / MPMAX(vidh, 1);
-            // ffmpeg and mpv use a default PlayResX of 384 when it is not known,
-            // this comes from VSFilter.
-            double fix_margins = track->PlayResX / (double)MP_ASS_FONT_PLAYRESX;
+            int old_playresx = track->PlayResX;
+            track->PlayResX = lrint(track->PlayResY * (double)vidw / MPMAX(vidh, 1));
+            double fix_margins = track->PlayResX / (double)old_playresx;
             for (int n = 0; n < track->n_styles; n++) {
                 track->styles[n].MarginL = lrint(track->styles[n].MarginL * fix_margins);
                 track->styles[n].MarginR = lrint(track->styles[n].MarginR * fix_margins);