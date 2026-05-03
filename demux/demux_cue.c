6133fb49ca850fb0c8692cd40f23aeb79de3ee6a
Sai Viswanth Mullapudi <viswanth.mullapudi28@gmail.com>
2026-05-02 23:23:17 -0400

    remove mp_basename call from filename
    mp_basename on filename remove the dirname info from it

--- demux/demux_cue.c
+++ demux/demux_cue.c
@@ -90,9 +90,9 @@ static bool open_source(struct timeline *tl, char *filename)
     void *ctx = talloc_new(NULL);
     bool res = false;
 
-    struct bstr dirname = mp_dirname(filename);
+    struct bstr dirname = mp_dirname(tl->demuxer->filename);
 
-    struct bstr base_filename = bstr0(mp_basename(filename));
+    struct bstr base_filename = bstr0(filename);
     if (!base_filename.len) {
         MP_WARN(tl, "CUE: Invalid audio filename in .cue file!\n");
     } else {
