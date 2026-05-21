79713b466ce887764f196820b101d45e2cd9bb66
llyyr <llyyr.public@gmail.com>
2026-05-21 22:35:57 +0530

    ao_pipewire: avoid deadlock when starting thread loop
    loop_start starts a thread which immediately acquires the thread loop
    lock. Locking the loop before loop_start can therefore deadlock the
    newly created thread during initialization.
    
    Locking the thread loop before loop_start was always incorrect, but
    Pipewire commit fb49e0795c28* made the issue visible by switching
    pw_thread_loop_lock to the underlying SPA loop lock.
    
    To fix this, we just avoid starting the loop until after we're connected
    to the server.
    
    * https://github.com/PipeWire/pipewire/commit/fb49e0795c28c892116b03369785ba86c27c968d

--- audio/out/ao_pipewire.c
+++ audio/out/ao_pipewire.c
@@ -505,11 +505,6 @@ static int pipewire_init_boilerplate(struct ao *ao)
     if (p->loop == NULL)
         return -1;
 
-    pw_thread_loop_lock(p->loop);
-
-    if (pw_thread_loop_start(p->loop) < 0)
-        goto error;
-
     struct pw_properties *props = NULL;
 #if !PW_CHECK_VERSION(1, 3, 81)
     props = pw_properties_new(PW_KEY_CONFIG_NAME, "client-rt.conf", NULL);
@@ -533,7 +528,8 @@ static int pipewire_init_boilerplate(struct ao *ao)
     if (pw_core_add_listener(p->core, &p->core_listener, &core_events, ao) < 0)
         goto error;
 
-    pw_thread_loop_unlock(p->loop);
+    if (pw_thread_loop_start(p->loop) < 0)
+        goto error;
 
     if (!session_has_sinks(ao)) {
         MP_VERBOSE(ao, "PipeWire does not have any audio sinks, skipping\n");
@@ -543,7 +539,6 @@ static int pipewire_init_boilerplate(struct ao *ao)
     return 0;
 
 error:
-    pw_thread_loop_unlock(p->loop);
     return -1;
 }
 
