mpv-player/mpv

commit 9ce79bcaa0132660a2e45b6bfc1fb0c199665277

osc.lua: hide thumbnails when setting visibility to never
And also when using script-message osc-hide.

player/lua/osc.lua
@@ -962,6 +962,16 @@ local function get_chapter(possec)
     end
 end
 
+local function disable_thumbnail()
+    if state.hover_sec == -1 then
+        return
+    end
+
+    state.hover_sec = -1
+    mp.set_property_native("user-data/osc/hover-sec", nil)
+    mp.set_property_native("user-data/osc/draw-preview", nil)
+end
+
 local function render_elements(master_ass)
     -- when the slider is dragged or hovered and we have a target chapter name
     -- then we use it instead of the normal title. we calculate it before the
@@ -1263,10 +1273,8 @@ local function render_elements(master_ass)
 
                         mp.set_property_native("user-data/osc/draw-preview", thumb_req)
                     end
-                elseif state.hover_sec >= 0 then
-                    state.hover_sec = -1
-                    mp.set_property_native("user-data/osc/hover-sec", nil)
-                    mp.set_property_native("user-data/osc/draw-preview", nil)
+                else
+                    disable_thumbnail()
                 end
             end
 
@@ -2705,6 +2713,7 @@ end
 
 local function osc_visible(visible)
     set_bar_visible("osc_visible", visible)
+    disable_thumbnail()
 end
 
 local function set_wc_visible(visible)
@@ -3256,8 +3265,7 @@ mp.register_event("file-loaded", function()
 end)
 mp.add_hook("on_unload", 50, function()
     state.file_loaded = false
-    mp.set_property_native("user-data/osc/hover-sec", nil)
-    mp.set_property_native("user-data/osc/draw-preview", nil)
+    disable_thumbnail()
     request_tick()
 end)
 

commit 35e5545eced95a2cc6969521ac7c6abb2b005f64

osc: fix thumbnails clearing on visibility change
currently hide_bar() bypasses set_visible when visibility was
changed to `never`, this can leave the thumbnail hanging.

instead of scattering disable_thumbnail() calls around, just
call set_visible(false) instead.

Fixes: 9ce79bcaa

player/lua/osc.lua
@@ -2750,9 +2750,9 @@ end
 local function hide_bar(label, visible_key, anitype_key, set_visible)
     msg.trace("hide_" .. label)
     if not state.enabled then
+        set_visible(false)
         -- typically hide happens at render() from tick(), but now tick() is
         -- no-op and won't render again to remove the osc, so do that manually.
-        state[visible_key] = false
         render_wipe(state.osd)
     elseif user_opts.fadeduration > 0 then
         if state[visible_key] then