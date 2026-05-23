mpv-player/mpv

commit 62786c0c91304aab720cf52674868c52a5b76f72

stream_bluray: report chapter times
This was forgotten in commit 7294303.

stream/stream_bluray.c
@@ -132,6 +132,27 @@ static int bluray_stream_control(stream_t *s, int cmd, void *arg)
         return 1;
     }
 
+    case STREAM_CTRL_GET_CHAPTER_TIME: {
+        BLURAY_TITLE_INFO *ti;
+        int chapter = *(double *)arg;
+        double time = MP_NOPTS_VALUE;
+
+        ti = bd_get_title_info(b->bd, b->current_title, b->current_angle);
+        if (!ti)
+            return STREAM_UNSUPPORTED;
+
+        if (chapter >= 0 || chapter < ti->chapter_count) {
+            time = BD_TIME_TO_MP(ti->chapters[chapter].start);
+        }
+        bd_free_title_info(ti);
+
+        if (time != MP_NOPTS_VALUE) {
+            *(double *)arg = time;
+            return STREAM_OK;
+        }
+        return STREAM_ERROR;
+    }
+
     case STREAM_CTRL_GET_CURRENT_TITLE: {
         *((unsigned int *) arg) = b->current_title;
         return 1;

commit ca591a11d0fbfc67a2a75878069392806207329f

stream/stream_bluray: fix chapter range check
Fixes: 62786c0c91304aab720cf52674868c52a5b76f72

stream/stream_bluray.c
@@ -203,7 +203,7 @@ static int bluray_stream_control(stream_t *s, int cmd, void *arg)
             return STREAM_UNSUPPORTED;
         int chapter = *(double *)arg;
         double time = MP_NOPTS_VALUE;
-        if (chapter >= 0 || chapter < ti->chapter_count)
+        if (chapter >= 0 && chapter < ti->chapter_count)
             time = BD_TIME_TO_MP(ti->chapters[chapter].start);
         if (time == MP_NOPTS_VALUE)
             return STREAM_ERROR;