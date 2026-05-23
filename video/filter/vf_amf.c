c42393a280654f5929fe4f73c9a3752a97789d33
nanahi <130121847+na-na-hi@users.noreply.github.com>
2026-05-23 09:53:15 -0400

    video/filter/vf_amf: fix profile option
    Fixes: [Bug] vf_amf.c: Profile always forced to FRC_PROFILE_HIGH

--- video/filter/vf_amf.c
+++ video/filter/vf_amf.c
@@ -281,7 +281,6 @@ static int init_frc(struct mp_filter *f, struct mp_image *src, bool reinit)
         else
             profile = FRC_PROFILE_HIGH;
     }
-    profile = FRC_PROFILE_HIGH;
     AMF_ASSIGN_PROPERTY_INT64(res, a->frc, AMF_FRC_PROFILE, profile);
     if (res != AMF_OK) {
         MP_WARN(f, "Failed to set FRC profile: %d\n", res);
