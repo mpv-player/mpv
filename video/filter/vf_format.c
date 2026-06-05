0b7ed670f7c353dd3dd4f8ae0fc788a181a15aa6
Kacper Michajłow <kasper93@gmail.com>
2026-06-06 00:58:48 +0200

    vf_format: luminance data is also carried by HDR10+ now
    Since haasn/libplacebo@4d82c6898 the HDR10+ metadata also sets those
    values, so strip it when they are not from DV.

--- video/filter/vf_format.c
+++ video/filter/vf_format.c
@@ -203,6 +203,10 @@ static void vf_format_process(struct mp_filter *f)
                    sizeof(img->params.color.hdr.scene_max));
             img->params.color.hdr.scene_avg = 0;
             img->params.color.hdr.ootf = (struct pl_hdr_bezier){0};
+            if (img->params.repr.sys != PL_COLOR_SYSTEM_DOLBYVISION) {
+                img->params.color.hdr.max_pq_y = 0;
+                img->params.color.hdr.avg_pq_y = 0;
+            }
         }
 
         if (priv->opts->min_luma)
