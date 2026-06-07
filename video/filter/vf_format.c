mpv-player/mpv

commit 76276c92104c31ee936ba5c76a76072f09978c5f

video: rewrite filtering glue code
Get rid of the old vf.c code. Replace it with a generic filtering
framework, which can potentially handle more than just --vf. At least
reimplementing --af with this code is planned.

This changes some --vf semantics (including runtime behavior and the
"vf" command). The most important ones are listed in interface-changes.

vf_convert.c is renamed to f_swscale.c. It is now an internal filter
that can not be inserted by the user manually.

f_lavfi.c is a refactor of player/lavfi.c. The latter will be removed
once --lavfi-complex is reimplemented on top of f_lavfi.c. (which is
conceptually easy, but a big mess due to the data flow changes).

The existing filters are all changed heavily. The data flow of the new
filter framework is different. Especially EOF handling changes - EOF is
now a "frame" rather than a state, and must be passed through exactly
once.

Another major thing is that all filters must support dynamic format
changes. The filter reconfig() function goes away. (This sounds complex,
but since all filters need to handle EOF draining anyway, they can use
the same code, and it removes the mess with reconfig() having to predict
the output format, which completely breaks with libavfilter anyway.)

In addition, there is no automatic format negotiation or conversion.
libavfilter's primitive and insufficient API simply doesn't allow us to
do this in a reasonable way. Instead, filters can use f_autoconvert as
sub-filter, and tell it which formats they support. This filter will in
turn add actual conversion filters, such as f_swscale, to perform
necessary format changes.

vf_vapoursynth.c uses the same basic principle of operation as before,
but with worryingly different details in data flow. Still appears to
work.

The hardware deint filters (vf_vavpp.c, vf_d3d11vpp.c, vf_vdpaupp.c) are
heavily changed. Fortunately, they all used refqueue.c, which is for
sharing the data flow logic (especially for managing future/past

video/filter/vf_format.c
@@ -25,14 +25,21 @@
 
 #include "common/msg.h"
 #include "common/common.h"
-
+#include "filters/f_autoconvert.h"
+#include "filters/filter.h"
+#include "filters/filter_internal.h"
+#include "filters/user_filters.h"
 #include "video/img_format.h"
 #include "video/mp_image.h"
-#include "vf.h"
 
 #include "options/m_option.h"
 
-struct vf_priv_s {
+struct priv {
+    struct vf_format_opts *opts;
+    struct mp_pin *in_pin;
+};
+
+struct vf_format_opts {
     int fmt;
     int outfmt;
     int colormatrix;
@@ -51,43 +58,29 @@ struct vf_priv_s {
     float spherical_ref_angles[3];
 };
 
-static bool is_compatible(int fmt1, int fmt2)
+static void vf_format_process(struct mp_filter *f)
 {
-    struct mp_imgfmt_desc d1 = mp_imgfmt_get_desc(fmt1);
-    struct mp_imgfmt_desc d2 = mp_imgfmt_get_desc(fmt2);
-    if (d1.num_planes < d2.num_planes)
-        return false;
-    if (!(d1.flags & MP_IMGFLAG_BYTE_ALIGNED) ||
-        !(d2.flags & MP_IMGFLAG_BYTE_ALIGNED))
-        return false;
-    for (int n = 0; n < MPMIN(d1.num_planes, d2.num_planes); n++) {
-        if (d1.bytes[n] != d2.bytes[n])
-            return false;
-        if (d1.xs[n] != d2.xs[n] || d1.ys[n] != d2.ys[n])
-            return false;
+    struct priv *priv = f->priv;
+    struct vf_format_opts *p = priv->opts;
+
+    if (!mp_pin_can_transfer_data(f->ppins[1], priv->in_pin))
+        return;
+
+    struct mp_frame frame = mp_pin_out_read(priv->in_pin);
+
+    if (mp_frame_is_signaling(frame)) {
+        mp_pin_in_write(f->ppins[1], frame);
+        return;
     }
-    return true;
-}
-
-static int query_format(struct vf_instance *vf, unsigned int fmt)
-{
-    if (fmt == vf->priv->fmt || !vf->priv->fmt) {
-        if (vf->priv->outfmt) {
-            if (!is_compatible(fmt, vf->priv->outfmt))
-                return 0;
-            fmt = vf->priv->outfmt;
-        }
-        return vf_next_query_format(vf, fmt);
+    if (frame.type != MP_FRAME_VIDEO) {
+        MP_ERR(f, "unsupported frame type\n");
+        mp_frame_unref(&frame);
+        mp_filter_internal_mark_failed(f);
+        return;
     }
-    return 0;
-}
 
-static int reconfig(struct vf_instance *vf, struct mp_image_params *in,
-                    struct mp_image_params *out)
-{
-    struct vf_priv_s *p = vf->priv;
-
-    *out = *in;
+    struct mp_image *img = frame.data;
+    struct mp_image_params *out = &img->params;
 
     if (p->outfmt)
         out->imgfmt = p->outfmt;
@@ -98,8 +91,9 @@ static int reconfig(struct vf_instance *vf, struct mp_image_params *in,
     if (p->primaries)
         out->color.primaries = p->primaries;
     if (p->gamma) {
+        enum mp_csp_trc in_gamma = p->gamma;
         out->color.gamma = p->gamma;
-        if (in->color.gamma != out->color.gamma) {
+        if (in_gamma != out->color.gamma) {
             // When changing the gamma function explicitly, also reset stuff
             // related to the gamma function since that information will almost
             // surely be false now and have to be re-inferred
@@ -140,28 +134,47 @@ static int reconfig(struct vf_instance *vf, struct mp_image_params *in,
     // Make sure the user-overrides are consistent (no RGB csp for YUV, etc.).
     mp_image_params_guess_csp(out);
 
-    return 0;
+    mp_pin_in_write(f->ppins[1], frame);
 }
 
-static struct mp_image *filter(struct vf_instance *vf, struct mp_image *mpi)
+static const struct mp_filter_info vf_format_filter = {
+    .name = "format",
+    .process = vf_format_process,
+    .priv_size = sizeof(struct priv),
+};
+
+static struct mp_filter *vf_format_create(struct mp_filter *parent, void *options)
 {
-    if (vf->priv->outfmt)
-        mp_image_setfmt(mpi, vf->priv->outfmt);
-    return mpi;
+    struct mp_filter *f = mp_filter_create(parent, &vf_format_filter);
+    if (!f) {
+        talloc_free(options);
+        return NULL;
+    }
+
+    struct priv *priv = f->priv;
+    priv->opts = talloc_steal(priv, options);
+
+    mp_filter_add_pin(f, MP_PIN_IN, "in");
+    mp_filter_add_pin(f, MP_PIN_OUT, "out");
+
+    struct mp_autoconvert *conv = mp_autoconvert_create(f);
+    if (!conv) {
+        talloc_free(f);
+        return NULL;
+    }
+
+    if (priv->opts->fmt)
+        mp_autoconvert_add_imgfmt(conv, priv->opts->fmt, 0);
+
+    priv->in_pin = conv->f->pins[1];
+    mp_pin_connect(conv->f->pins[0], f->ppins[0]);
+
+    return f;
 }
 
-static int vf_open(vf_instance_t *vf)
-{
-    vf->query_format = query_format;
-    vf->reconfig = reconfig;
-    vf->filter = filter;
-    return 1;
-}
-
-#define OPT_BASE_STRUCT struct vf_priv_s
+#define OPT_BASE_STRUCT struct vf_format_opts
 static const m_option_t vf_opts_fields[] = {
     OPT_IMAGEFORMAT("fmt", fmt, 0),
-    OPT_IMAGEFORMAT("outfmt", outfmt, 0),
     OPT_CHOICE_C("colormatrix", colormatrix, 0, mp_csp_names),
     OPT_CHOICE_C("colorlevels", colorlevels, 0, mp_csp_levels_names),
     OPT_CHOICE_C("primaries", primaries, 0, mp_csp_prim_names),
@@ -184,14 +197,16 @@ static const m_option_t vf_opts_fields[] = {
     {0}
 };
 
-const vf_info_t vf_info_format = {
-    .description = "force output format",
-    .name = "format",
-    .open = vf_open,
-    .priv_size = sizeof(struct vf_priv_s),
-    .options = vf_opts_fields,
-    .priv_defaults = &(const struct vf_priv_s){
-        .rotate = -1,
-        .spherical_ref_angles = {NAN, NAN, NAN},
+const struct mp_user_filter_entry vf_format = {
+    .desc = {
+        .description = "force output format",
+        .name = "format",
+        .priv_size = sizeof(OPT_BASE_STRUCT),
+        .priv_defaults = &(const OPT_BASE_STRUCT){
+            .rotate = -1,
+            .spherical_ref_angles = {NAN, NAN, NAN},
+        },
+        .options = vf_opts_fields,
     },
+    .create = vf_format_create,
 };

common/common.c
@@ -302,3 +302,14 @@ char *mp_tprintf_buf(char *buf, size_t buf_size, const char *format, ...)
     va_end(ap);
     return buf;
 }
+
+char **mp_dup_str_array(void *tctx, char **s)
+{
+    char **r = NULL;
+    int num_r = 0;
+    for (int n = 0; s && s[n]; n++)
+        MP_TARRAY_APPEND(tctx, r, num_r, talloc_strdup(tctx, s[n]));
+    if (r)
+        MP_TARRAY_APPEND(tctx, r, num_r, NULL);
+    return r;
+}

common/common.h
@@ -112,4 +112,6 @@ char *mp_tag_str_buf(char *buf, size_t buf_size, uint32_t tag);
 char *mp_tprintf_buf(char *buf, size_t buf_size, const char *format, ...)
     PRINTF_ATTRIBUTE(3, 4);
 
+char **mp_dup_str_array(void *tctx, char **s);
+
 #endif /* MPLAYER_MPCOMMON_H */

filters/f_auto_filters.c
@@ -1,0 +1,244 @@
+#include <math.h>
+
+#include "common/common.h"
+#include "common/msg.h"
+#include "options/m_config.h"
+#include "options/options.h"
+#include "video/mp_image.h"
+
+#include "f_auto_filters.h"
+#include "f_swscale.h"
+#include "f_utils.h"
+#include "filter.h"
+#include "filter_internal.h"
+#include "user_filters.h"
+
+struct deint_priv {
+    struct mp_subfilter sub;
+    int prev_imgfmt;
+    int prev_setting;
+    struct m_config_cache *opts;
+};
+
+static void deint_process(struct mp_filter *f)
+{
+    struct deint_priv *p = f->priv;
+
+    if (!mp_subfilter_read(&p->sub))
+        return;
+
+    struct mp_frame frame = p->sub.frame;
+
+    if (mp_frame_is_signaling(frame)) {
+        mp_subfilter_continue(&p->sub);
+        return;
+    }
+
+    if (frame.type != MP_FRAME_VIDEO) {
+        MP_ERR(f, "video input required!\n");
+        mp_filter_internal_mark_failed(f);
+        return;
+    }
+
+    m_config_cache_update(p->opts);
+    struct filter_opts *opts = p->opts->opts;
+
+    if (!opts->deinterlace)
+        mp_subfilter_destroy(&p->sub);
+
+    struct mp_image *img = frame.data;
+
+    if (img->imgfmt == p->prev_imgfmt && p->prev_setting == opts->deinterlace) {
+        mp_subfilter_continue(&p->sub);
+        return;
+    }
+
+    if (!mp_subfilter_drain_destroy(&p->sub))
+        return;
+
+    assert(!p->sub.filter);
+
+    p->prev_imgfmt = img->imgfmt;
+    p->prev_setting = opts->deinterlace;
+    if (!p->prev_setting) {
+        mp_subfilter_continue(&p->sub);
+        return;
+    }
+
+    if (img->imgfmt == IMGFMT_VDPAU) {
+        char *args[] = {"deint", "yes", NULL};
+        p->sub.filter =
+            mp_create_user_filter(f, MP_OUTPUT_CHAIN_VIDEO, "vdpaupp", args);
+    } else if (img->imgfmt == IMGFMT_VAAPI) {
+        p->sub.filter =
+            mp_create_user_filter(f, MP_OUTPUT_CHAIN_VIDEO, "vavpp", NULL);
+    } else if (img->imgfmt == IMGFMT_D3D11) {
+        p->sub.filter =
+            mp_create_user_filter(f, MP_OUTPUT_CHAIN_VIDEO, "d3d11vpp", NULL);
+    } else if (mp_sws_supports_input(img->imgfmt)) {
+        char *args[] = {"mode", "send_field", "deint", "interlaced", NULL};
+        p->sub.filter =
+            mp_create_user_filter(f, MP_OUTPUT_CHAIN_VIDEO, "yadif", args);
+    } else {
+        MP_ERR(f, "no deinterlace filter available for this format\n");
+        mp_subfilter_continue(&p->sub);
+        return;
+    }
+
+    if (!p->sub.filter)
+        MP_ERR(f, "creating deinterlacer failed\n");
+
+    mp_subfilter_continue(&p->sub);
+}
+
+static void deint_reset(struct mp_filter *f)
+{
+    struct deint_priv *p = f->priv;
+
+    mp_subfilter_reset(&p->sub);
+}
+
+static void deint_destroy(struct mp_filter *f)
+{
+    struct deint_priv *p = f->priv;
+
+    mp_subfilter_reset(&p->sub);
+    TA_FREEP(&p->sub.filter);
+}
+
+static const struct mp_filter_info deint_filter = {
+    .name = "deint",
+    .priv_size = sizeof(struct deint_priv),
+    .process = deint_process,
+    .reset = deint_reset,
+    .destroy = deint_destroy,
+};
+
+struct mp_filter *mp_deint_create(struct mp_filter *parent)
+{
+    struct mp_filter *f = mp_filter_create(parent, &deint_filter);
+    if (!f)
+        return NULL;
+
+    struct deint_priv *p = f->priv;
+
+    p->sub.in = mp_filter_add_pin(f, MP_PIN_IN, "in");
+    p->sub.out = mp_filter_add_pin(f, MP_PIN_OUT, "out");
+
+    p->opts = m_config_cache_alloc(f, f->global, &filter_conf);
+
+    return f;
+}
+
+struct rotate_priv {
+    struct mp_subfilter sub;
+    int prev_rotate;
+    int prev_imgfmt;
+    int target_rotate;
+};
+
+static void rotate_process(struct mp_filter *f)
+{
+    struct rotate_priv *p = f->priv;
+
+    if (!mp_subfilter_read(&p->sub))
+        return;
+
+    struct mp_frame frame = p->sub.frame;
+
+    if (mp_frame_is_signaling(frame)) {
+        mp_subfilter_continue(&p->sub);
+        return;
+    }
+
+    if (frame.type != MP_FRAME_VIDEO) {
+        MP_ERR(f, "video input required!\n");
+        return;
+    }
+
+    struct mp_image *img = frame.data;
+
+    if (img->params.rotate == p->prev_rotate &&
+        img->imgfmt == p->prev_imgfmt)
+    {
+        img->params.rotate = p->target_rotate;
+        mp_subfilter_continue(&p->sub);
+        return;
+    }
+
+    if (!mp_subfilter_drain_destroy(&p->sub))
+        return;
+
+    assert(!p->sub.filter);
+
+    int rotate = p->prev_rotate = img->params.rotate;
+    p->target_rotate = rotate;
+    p->prev_imgfmt = img->imgfmt;
+
+    struct mp_stream_info *info = mp_filter_find_stream_info(f);
+    if (rotate == 0 || (info && info->rotate90 && !(rotate % 90))) {
+        mp_subfilter_continue(&p->sub);
+        return;
+    }
+
+    if (mp_sws_supports_input(img->imgfmt)) {
+        MP_ERR(f, "Video rotation with this format not supported\n");
+        mp_subfilter_continue(&p->sub);
+        return;
+    }
+
+    double angle = rotate / 360.0 * M_PI * 2;
+    char *args[] = {"angle", mp_tprintf(30, "%f", angle),
+                    "ow", mp_tprintf(30, "rotw(%f)", angle),
+                    "oh", mp_tprintf(30, "roth(%f)", angle),
+                    NULL};
+    p->sub.filter =
+        mp_create_user_filter(f, MP_OUTPUT_CHAIN_VIDEO, "rotate", args);
+
+    if (p->sub.filter) {
+        MP_INFO(f, "Inserting rotation filter.\n");
+        p->target_rotate = 0;
+    } else {
+        MP_ERR(f, "could not create rotation filter\n");
+    }
+
+    mp_subfilter_continue(&p->sub);
+}
+
+static void rotate_reset(struct mp_filter *f)
+{
+    struct rotate_priv *p = f->priv;
+
+    mp_subfilter_reset(&p->sub);
+}
+
+static void rotate_destroy(struct mp_filter *f)
+{
+    struct rotate_priv *p = f->priv;
+
+    mp_subfilter_reset(&p->sub);
+    TA_FREEP(&p->sub.filter);
+}
+
+static const struct mp_filter_info rotate_filter = {
+    .name = "autorotate",
+    .priv_size = sizeof(struct rotate_priv),
+    .process = rotate_process,
+    .reset = rotate_reset,
+    .destroy = rotate_destroy,
+};
+
+struct mp_filter *mp_autorotate_create(struct mp_filter *parent)
+{
+    struct mp_filter *f = mp_filter_create(parent, &rotate_filter);
+    if (!f)
+        return NULL;
+
+    struct rotate_priv *p = f->priv;
+    p->prev_rotate = -1;
+
+    p->sub.in = mp_filter_add_pin(f, MP_PIN_IN, "in");
+    p->sub.out = mp_filter_add_pin(f, MP_PIN_OUT, "out");
+
+    return f;
+}

filters/f_auto_filters.h
@@ -1,0 +1,10 @@
+#pragma once
+
+#include "filter.h"
+
+// A filter which inserts the required deinterlacing filter based on the
+// hardware decode mode and the deinterlace user option.
+struct mp_filter *mp_deint_create(struct mp_filter *parent);
+
+// Rotate according to mp_image.rotate and VO capabilities.
+struct mp_filter *mp_autorotate_create(struct mp_filter *parent);

filters/f_autoconvert.c
@@ -1,0 +1,288 @@
+#include "config.h"
+
+#include "common/common.h"
+#include "common/msg.h"
+#include "video/hwdec.h"
+#include "video/mp_image.h"
+
+#include "f_autoconvert.h"
+#include "f_hwtransfer.h"
+#include "f_swscale.h"
+#include "f_utils.h"
+#include "filter.h"
+#include "filter_internal.h"
+
+struct priv {
+    struct mp_log *log;
+
+    struct mp_subfilter sub;
+
+    bool force_update;
+
+    int *imgfmts;
+    int *subfmts;
+    int num_imgfmts;
+
+    // Enable special conversion for the final stage before the VO.
+    bool vo_convert;
+
+    // sws state
+    int in_imgfmt, in_subfmt;
+
+    struct mp_autoconvert public;
+};
+
+// Dummy filter for bundling sub-conversion filters.
+static const struct mp_filter_info convert_filter = {
+    .name = "convert",
+};
+
+// For hw decoding: thing which can convert between underlying surface formats.
+// The filter detects the needed target format from struct mp_hwdec_ctx.
+struct subfmt_conv {
+    int hw_imgfmt;
+    struct mp_filter *(*create)(struct mp_filter *parent);
+};
+
+static const struct subfmt_conv subfmt_converters[] = {
+#if HAVE_D3D_HWACCEL
+    {IMGFMT_D3D11, vf_d3d11_create_outconv},
+#endif
+    {0}
+};
+
+void mp_autoconvert_clear(struct mp_autoconvert *c)
+{
+    struct priv *p = c->f->priv;
+
+    p->num_imgfmts = 0;
+}
+
+void mp_autoconvert_add_imgfmt(struct mp_autoconvert *c, int imgfmt, int subfmt)
+{
+    struct priv *p = c->f->priv;
+
+    MP_TARRAY_GROW(p, p->imgfmts, p->num_imgfmts);
+    MP_TARRAY_GROW(p, p->subfmts, p->num_imgfmts);
+
+    p->imgfmts[p->num_imgfmts] = imgfmt;
+    p->subfmts[p->num_imgfmts] = subfmt;
+
+    p->num_imgfmts += 1;
+    p->force_update = true;
+}
+
+void mp_autoconvert_add_vo_hwdec_subfmts(struct mp_autoconvert *c,
+                                         struct mp_hwdec_devices *devs)
+{
+    struct priv *p = c->f->priv;
+    assert(devs);
+
+    int prev_format = 0;
+
+    for (int n = 0; ; n++) {
+        struct mp_hwdec_ctx *ctx = hwdec_devices_get_n(devs, n);
+        if (!ctx)
+            break;
+        if (!ctx->hw_imgfmt || !ctx->supported_formats)
+            continue;
+        // Very hacky: don't let d3d11-egl-rgb overwrite d3d11-egl
+        if (ctx->hw_imgfmt == prev_format)
+            continue;
+        prev_format = ctx->hw_imgfmt;
+        // Stupidity: VOs export imgfmt only, so subfmt is always 0. Remove it
+        // to fix it up.
+        for (int i = 0; i < p->num_imgfmts; i++) {
+            if (p->imgfmts[i] != ctx->hw_imgfmt)
+                continue;
+
+            int count = p->num_imgfmts;
+            MP_TARRAY_REMOVE_AT(p->imgfmts, count, i);
+            count = p->num_imgfmts;
+            MP_TARRAY_REMOVE_AT(p->subfmts, count, i);
+            p->num_imgfmts -= 1;
+            break;
+        }
+        for (int i = 0; ctx->supported_formats[i]; i++)
+            mp_autoconvert_add_imgfmt(c, ctx->hw_imgfmt, ctx->supported_formats[i]);
+    }
+
+    p->vo_convert = true;
+}
+
+static void handle_video_frame(struct mp_filter *f)
+{
+    struct priv *p = f->priv;
+
+    struct mp_frame frame = p->sub.frame;
+    if (frame.type != MP_FRAME_VIDEO) {
+        MP_ERR(p, "video input required!\n");
+        mp_filter_internal_mark_failed(f);
+        return;
+    }
+
+    struct mp_image *img = frame.data;
+
+    if (p->force_update)
+        p->in_imgfmt = p->in_subfmt = 0;
+
+    if (img->imgfmt == p->in_imgfmt && img->params.hw_subfmt == p->in_subfmt) {
+        mp_subfilter_continue(&p->sub);
+        return;
+    }
+
+    if (!mp_subfilter_drain_destroy(&p->sub)) {
+        p->in_imgfmt = p->in_subfmt = 0;
+        return;
+    }
+
+    p->in_imgfmt = img->params.imgfmt;
+    p->in_subfmt = img->params.hw_subfmt;
+    p->force_update = false;
+
+    bool different_subfmt = false;
+
+    for (int n = 0; n < p->num_imgfmts; n++) {
+        bool samefmt = img->params.imgfmt == p->imgfmts[n];
+        bool samesubffmt = img->params.hw_subfmt == p->subfmts[n];
+        if (samefmt && !samesubffmt)
+            different_subfmt = true;
+        if (samefmt && (samesubffmt || !p->subfmts[n])) {
+            mp_subfilter_continue(&p->sub);
+            return;
+        }
+    }
+
+    struct mp_stream_info *info = mp_filter_find_stream_info(f);
+
+    struct mp_filter *conv = mp_filter_create(f, &convert_filter);
+    mp_filter_add_pin(conv, MP_PIN_IN, "in");
+    mp_filter_add_pin(conv, MP_PIN_OUT, "out");
+
+    struct mp_filter *filters[2] = {0};
+    bool need_sws = true;
+
+    int *fmts = p->imgfmts;
+    int num_fmts = p->num_imgfmts;
+
+    // Source is sw, all targets are hw -> try to upload.
+    bool sw_to_hw = !IMGFMT_IS_HWACCEL(img->imgfmt);
+    for (int n = 0; n < num_fmts; n++)
+        sw_to_hw &= IMGFMT_IS_HWACCEL(fmts[n]);
+
+    if (sw_to_hw && num_fmts > 0) {
+        // We can probably use this! Very lazy and very approximate.
+        struct mp_hwupload *upload = mp_hwupload_create(conv, fmts[0]);
+        if (upload) {
+            MP_INFO(p, "HW-uploading to %s\n", mp_imgfmt_to_name(fmts[0]));
+            filters[1] = upload->f;
+            fmts = upload->upload_fmts;
+            num_fmts = upload->num_upload_fmts;
+        }
+    } else if (p->vo_convert && different_subfmt && info && info->hwdec_devs) {
+        for (int n = 0; subfmt_converters[n].hw_imgfmt; n++) {
+            if (subfmt_converters[n].hw_imgfmt == img->imgfmt) {
+                MP_INFO(p, "Using HW sub-conversion.\n");
+                filters[1] = subfmt_converters[n].create(conv);
+                if (filters[1]) {
+                    need_sws = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (need_sws) {
+        // Create a new conversion filter.
+        struct mp_sws_filter *sws = mp_sws_filter_create(conv);
+        if (!sws) {
+            MP_ERR(p, "error creating conversion filter\n");
+            return;
+        }
+
+        int out = mp_sws_find_best_out_format(img->imgfmt,  fmts, num_fmts);
+        if (!out) {
+            MP_ERR(p, "can't find video conversion for %s/%s\n",
+                   mp_imgfmt_to_name(img->imgfmt),
+                   mp_imgfmt_to_name(img->params.hw_subfmt));
+            talloc_free(conv);
+            mp_filter_internal_mark_failed(f);
+            return;
+        }
+
+        if (out == img->imgfmt) {
+            // Can happen if hwupload goes to same format.
+            talloc_free(sws->f);
+        } else {
+            sws->out_format = out;
+            MP_INFO(p, "Converting %s -> %s\n", mp_imgfmt_to_name(img->imgfmt),
+                    mp_imgfmt_to_name(sws->out_format));
+            filters[0] = sws->f;
+        }
+    }
+
+    mp_chain_filters(conv->ppins[0], conv->ppins[1], filters, 2);
+
+    p->sub.filter = conv;
+    mp_subfilter_continue(&p->sub);
+}
+
+static void process(struct mp_filter *f)
+{
+    struct priv *p = f->priv;
+
+    if (!mp_subfilter_read(&p->sub))
+        return;
+
+    struct mp_frame frame = p->sub.frame;
+
+    if (!mp_frame_is_signaling(frame)) {
+        if (p->num_imgfmts) {
+            handle_video_frame(f);
+            return;
+        }
+    }
+
+    mp_subfilter_continue(&p->sub);
+}
+
+static void reset(struct mp_filter *f)
+{
+    struct priv *p = f->priv;
+
+    mp_subfilter_reset(&p->sub);
+}
+
+static void destroy(struct mp_filter *f)
+{
+    struct priv *p = f->priv;
+
+    mp_subfilter_reset(&p->sub);
+    TA_FREEP(&p->sub.filter);
+}
+
+static const struct mp_filter_info autoconvert_filter = {
+    .name = "autoconvert",
+    .priv_size = sizeof(struct priv),
+    .process = process,
+    .reset = reset,
+    .destroy = destroy,
+};
+
+struct mp_autoconvert *mp_autoconvert_create(struct mp_filter *parent)
+{
+    struct mp_filter *f = mp_filter_create(parent, &autoconvert_filter);
+    if (!f)
+        return NULL;
+
+    mp_filter_add_pin(f, MP_PIN_IN, "in");
+    mp_filter_add_pin(f, MP_PIN_OUT, "out");
+
+    struct priv *p = f->priv;
+    p->public.f = f;
+    p->log = f->log;
+    p->sub.in = f->ppins[0];
+    p->sub.out = f->ppins[1];
+
+    return &p->public;
+}

filters/f_autoconvert.h
@@ -1,0 +1,39 @@
+#pragma once
+
+#include "filter.h"
+
+// A filter which automatically creates and uses a conversion filter based on
+// the filter settings, or passes through data unchanged if no conversion is
+// required.
+struct mp_autoconvert {
+    // f->pins[0] is input, f->pins[1] is output
+    struct mp_filter *f;
+};
+
+// (to free this, free the filter itself, mp_autoconvert.f)
+struct mp_autoconvert *mp_autoconvert_create(struct mp_filter *parent);
+
+// Add the imgfmt as allowed video image format, and error on non-video frames.
+// Each call adds to the list of allowed formats. Before the first call, all
+// formats are allowed (even non-video).
+// subfmt can be used to specify underlying surface formats for hardware formats,
+// otherwise must be 0.
+void mp_autoconvert_add_imgfmt(struct mp_autoconvert *c, int imgfmt, int subfmt);
+
+// Add the formats supported by the hwdec interops (or essentially refine them),
+// and trigger conversion if hw_subfmts mismatch. This is mostly a hack for
+// D3D11/ANGLE (which supports NV12 only).
+// Must be called mp_autoconvert_add_imgfmt(), and overrides them where formats
+// collide.
+struct mp_hwdec_devices;
+void mp_autoconvert_add_vo_hwdec_subfmts(struct mp_autoconvert *c,
+                                         struct mp_hwdec_devices *devs);
+
+// Reset set of allowed formats back to initial state. (This does not flush
+// any frames or remove currently active filters, although to get reasonable
+// behavior, you need to readd all previously allowed formats, or reset the
+// filter.)
+void mp_autoconvert_clear(struct mp_autoconvert *c);
+
+// vf_d3d11vpp.c
+struct mp_filter *vf_d3d11_create_outconv(struct mp_filter *parent);

commit 5f24c8090876c0b81a516f4cf754e4ea68f084ce

video/filter/vf_format: fix changing gamma check
Fixes: 76276c92104c31ee936ba5c76a76072f09978c5f

video/filter/vf_format.c
@@ -79,7 +79,6 @@ static void set_params(struct vf_format_opts *p, struct mp_image_params *out,
         out->color.primaries = p->primaries;
     if (p->gamma) {
         enum pl_color_transfer in_gamma = p->gamma;
-        out->color.transfer = p->gamma;
         if (in_gamma != out->color.transfer) {
             // When changing the gamma function explicitly, also reset stuff
             // related to the gamma function since that information will almost
@@ -87,6 +86,7 @@ static void set_params(struct vf_format_opts *p, struct mp_image_params *out,
             out->color.hdr = (struct pl_hdr_metadata){0};
             out->light = MP_CSP_LIGHT_AUTO;
         }
+        out->color.transfer = p->gamma;
     }
     if (out->repr.sys != PL_COLOR_SYSTEM_DOLBYVISION) {
         out->primaries_orig = out->color.primaries;