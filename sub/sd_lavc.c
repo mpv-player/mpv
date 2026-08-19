7e540a8d606153d52b7b0239cf1d66069238df94
posi <posiibrahim@gmail.com>
2026-08-19 20:40:17 +0100

    vo_gpu_next: use video colorspace for overlays only when requested
    Bitmap subtitles muxed in media file are generally the same colorspace
    as video. Generic overlays may not be.

--- sub/sd_lavc.c
+++ sub/sd_lavc.c
@@ -465,6 +465,7 @@ static struct sub_bitmaps *get_bitmaps(struct sd *sd, struct mp_osd_res d,
     res->packed_w = current->bound_w;
     res->packed_h = current->bound_h;
     res->format = SUBBITMAP_BGRA;
+    res->video_color_space = true;
 
     double video_par = 0;
     if (priv->avctx->codec_id == AV_CODEC_ID_DVD_SUBTITLE &&
