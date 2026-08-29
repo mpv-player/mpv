e7a1cf9ece06ea7f56ba102d4470e59d1e82ecb6
der richter <der.richter@gmx.de>
2026-08-29 14:50:08 +0200

    ci/mac: remove old unneeded travis build remnants

--- ci/build-macos.sh
+++ ci/build-macos.sh
@@ -4,16 +4,8 @@ set -e
 
 . ./ci/build-common.sh
 
-MPV_INSTALL_PREFIX="${HOME}/out/mpv"
-MPV_VARIANT="${TRAVIS_OS_NAME}"
-
-if [[ -d "./build/${MPV_VARIANT}" ]] ; then
-    rm -rf "./build/${MPV_VARIANT}"
-fi
-
 PKG_CONFIG_PATH="$(brew --prefix libarchive)/lib/pkgconfig/" CC="${CC}" CXX="${CXX}" \
 meson setup build $common_args \
-  -Dprefix="${MPV_INSTALL_PREFIX}" \
   -Dobjc_args="-Wno-error=deprecated -Wno-error=deprecated-declarations" \
   -D{caca,cdda,dvdnav,gl,iconv,lcms2,libarchive,libbluray,lua,jpeg}=enabled \
   -D{plain-gl,rubberband,zimg,zlib}=enabled \
