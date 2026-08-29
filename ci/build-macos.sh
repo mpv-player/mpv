33333c0019bb19dd36658c1d569ee959657677f7
der richter <der.richter@gmx.de>
2026-08-29 15:38:54 +0200

    ci/mac: add back -werror for compiling only for test build
    meson added -fatal-warnings to the -werror build flag for linking in the
    newest release. this leads to errors on linking on the test builds that
    are expected to output warnings on linking.
    see 764da9919c9dcdd951d9441dac2b56b8a03770ad
    
    we removed -werror for the test builds on the previous commit, here we
    add it back but for compiling only and not for linking.

--- ci/build-macos.sh
+++ ci/build-macos.sh
@@ -4,14 +4,20 @@ set -e
 
 . ./ci/build-common.sh
 
+objc_args="-Wno-error=deprecated -Wno-error=deprecated-declarations"
+c_args=""
 if [[ "${MACOS_ARCH}" == "test" ]]; then
     # remove -werror + tests and keep libmpv for test builds only
     common_args="-Dlibmpv=true"
+    # add back -werror for compiling only, not for linking
+    objc_args="-Werror ${objc_args}"
+    c_args="-Werror"
 fi
 
 PKG_CONFIG_PATH="$(brew --prefix libarchive)/lib/pkgconfig/" CC="${CC}" CXX="${CXX}" \
 meson setup build $common_args \
-  -Dobjc_args="-Wno-error=deprecated -Wno-error=deprecated-declarations" \
+  -Dobjc_args="${objc_args}" \
+  -Dc_args="${c_args}" \
   -D{caca,cdda,dvdnav,gl,iconv,lcms2,libarchive,libbluray,lua,jpeg}=enabled \
   -D{plain-gl,rubberband,zimg,zlib}=enabled \
   -D{cocoa,coreaudio,gl-cocoa,videotoolbox-gl,videotoolbox-pl}=enabled \
