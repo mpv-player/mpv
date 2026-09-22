6fd80b2003873ef2bed09e78374549687a143236
Kacper Michajłow <kasper93@gmail.com>
2026-09-22 12:06:45 +0200

    ci/build-win32.ps1: bump ffmpeg to 9.0 branch

--- ci/build-win32.ps1
+++ ci/build-win32.ps1
@@ -235,7 +235,7 @@ $projects = @(
     @{
         Path = "$subprojects/ffmpeg.wrap"
         URL = "https://gitlab.freedesktop.org/gstreamer/meson-ports/ffmpeg.git"
-        Revision = "meson-8.1"
+        Revision = "meson-9.0"
         Provides = @(
             "dependency_names = libavcodec, libavdevice, libavfilter, libavformat, libavutil, libswresample, libswscale"
             "program_names = ffmpeg"
